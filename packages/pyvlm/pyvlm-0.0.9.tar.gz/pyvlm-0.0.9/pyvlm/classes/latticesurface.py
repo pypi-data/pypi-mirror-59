c81e01e8b054786b2b921d03c351712c4cd68e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:13 +0000

    Add package pyvlm/0.0.9/pyvlm-0.0.9.tar.gz

---
+++ packages/pyvlm/pyvlm-0.0.9.tar.gz/pyvlm-0.0.9/pyvlm/classes/latticesurface.py
@@ -0,0 +1,313 @@
+from math import pi, cos, sqrt
+from .latticestrip import LatticeStrip
+from .latticesheet import LatticeSheet
+from .latticepanel import LatticePanel
+
+class LatticeSurface(object):
+    name = None
+    sects = None
+    cspace = None
+    xspace = None
+    strps = None
+    pnts = None
+    pnls = None
+    msect = None
+    def __init__(self, name: str, sects: list, mirror: bool, funcs:list):
+        self.name = name
+        self.sects = sects
+        self.mirror = mirror
+        self.funcs = funcs
+        self.update()
+    def update(self):
+        if self.mirror and self.sects[0].pnt.y == 0.0:
+            numsect = len(self.sects)
+            newsects = []
+            for i in range(numsect-1):
+                sect = self.sects[numsect-1-i]
+                msect = sect.return_mirror()
+                newsects.append(msect)
+            for sect in self.sects:
+                newsects.append(sect)
+            self.sects = newsects
+        elif self.mirror and self.sects[0].pnt.y != 0.0:
+            print(f'Warning: Cannot mirror {self.name}.')
+            self.mirror = False
+    def set_chord_distribution(self, cspace: list):
+        from pyvlm.tools import normalise_spacing
+        self.cspace = normalise_spacing(cspace)
+    def set_chord_equal_distribution(self, numc: int):
+        from pyvlm.tools import equal_spacing
+        csp = equal_spacing(4*numc)
+        self.cspace = [tuple(csp[i*4:i*4+5]) for i in range(numc)]
+    def set_chord_cosine_distribution(self, numc: int):
+        from pyvlm.tools import full_cosine_spacing
+        if numc > 1:
+            csp = full_cosine_spacing(4*numc+2)
+            csp = [0.0]+csp[2:-2]+[1.0]
+            self.cspace = [tuple(csp[i*4:i*4+5]) for i in range(numc)]
+        else:
+            self.set_chord_equal_distribution(numc)
+    def mesh(self, lsid: int, lpid: int):
+        from pygeom.geom3d import Point
+        from numpy.matlib import empty
+        nums = len(self.sects)
+        self.shts = []
+        for i in range(nums-1):
+            a, b = i, i+1
+            secta = self.sects[a]
+            sectb = self.sects[b]
+            self.shts.append(LatticeSheet(secta, sectb))
+        self.strps = []
+        for sht in self.shts:
+            lsid = sht.mesh_strips(lsid)
+            self.strps += sht.strps
+        pnts = [strp.pnt1 for strp in self.strps]
+        pnts.append(self.strps[-1].pnt2)
+        crds = [strp.crd1 for strp in self.strps]
+        crds.append(self.strps[-1].crd2)
+        lenb = len(pnts)
+        lenc = len(self.cspace)
+        self.pnts = empty((lenb, lenc+1), dtype=Point)
+        for i in range(lenb):
+            minx = pnts[i].x
+            y = pnts[i].y
+            z = pnts[i].z
+            c = crds[i]
+            cd = self.cspace[0][0]
+            x = minx+cd*c
+            self.pnts[i, 0] = Point(x, y, z)
+            for j in range(1, lenc+1):
+                cd = self.cspace[j-1][-1]
+                x = minx+cd*c
+                self.pnts[i, j] = Point(x, y, z)
+        self.pnls = empty((lenb-1, lenc), dtype=LatticePanel)
+        for i, strp in enumerate(self.strps):
+            for j in range(lenc):
+                pnts = [
+                    self.pnts[i, j],
+                    self.pnts[i+1, j],
+                    self.pnts[i, j+1],
+                    self.pnts[i+1, j+1]
+                ]
+                cspc = self.cspace[j]
+                pnl = LatticePanel(lpid, pnts, cspc, strp)
+                self.pnls[i, j] = pnl
+                lpid += 1
+        if self.mirror:
+            numstrp = len(self.strps)
+            hlfstrp = int(numstrp/2)
+            for i in range(hlfstrp):
+                strp = self.strps[numstrp-1-i]
+                mstrp = self.strps[i]
+                strp.set_mirror(mstrp)
+        bpos = [0.0]
+        for sht in self.shts:
+            sht.inherit_panels()
+            sht.set_control_panels()
+            bpos.append(bpos[-1]+sht.width)
+        if self.mirror:
+            numsht = len(self.shts)
+            wmir = bpos[int(numsht/2)]
+            for i in range(len(bpos)):
+                bpos[i] = bpos[i]-wmir
+        for i, sect in enumerate(self.sects):
+            sect.bpos = bpos[i]
+        for sht in self.shts:
+            sht.set_strip_bpos()
+        bmax = max(bpos)
+        for func in self.funcs:
+            func.set_spline(bmax)
+            var = func.var
+            if var == 'angle':
+                var = '_ang'
+            if self.mirror:
+                for i in range(hlfstrp):
+                    strp = self.strps[numstrp-1-i]
+                    mstrp = self.strps[i]
+                    bpos = strp.bpos
+                    val = func.interpolate(bpos)
+                    strp.__dict__[var] = val
+                    mstrp.__dict__[var] = val
+            else:
+                for strp in self.strps:
+                    bpos = strp.bpos
+                    val = func.interpolate(bpos)
+                    strp.__dict__[var] = val
+        return lsid, lpid
+    def point_xyz(self):
+        from numpy.matlib import zeros
+        x = zeros(self.pnts.shape)
+        y = zeros(self.pnts.shape)
+        z = zeros(self.pnts.shape)
+        for i in range(self.pnts.shape[0]):
+            for j in range(self.pnts.shape[1]):
+                x[i, j] = self.pnts[i, j].x
+                y[i, j] = self.pnts[i, j].y
+                z[i, j] = self.pnts[i, j].z
+        return x, y, z
+    def return_panels(self):
+        pnls = []
+        shp = self.pnls.shape
+        for i in range(shp[0]):
+            for j in range(shp[1]):
+                pnls.append(self.pnls[i, j])
+        return pnls
+    # def print_geom(self):
+    #     print(f'Printing {self.name:s} geometry.')
+    #     print('Sections')
+    #     for i, sect in enumerate(self.sects):
+    #         print(f'{i:d}\t{sect.pnt:.5f}\t{sect.chord:.5f}')
+    #     print('Sheets')
+    #     for i, sht in enumerate(self.shts):
+    #         print(f'{i:d}')
+    #         print(f'{sht.sect1.pnt:.5f}\t{sht.sect1.chord:.5f}')
+    #         print(f'{sht.sect2.pnt:.5f}\t{sht.sect2.chord:.5f}')
+    @property
+    def strpb(self):
+        return [strp.bpos for strp in self.strps]
+    @property
+    def strpy(self):
+        return [strp.pnti.y for strp in self.strps]
+    @property
+    def strpz(self):
+        return [strp.pnti.z for strp in self.strps]
+    @property
+    def strpi(self):
+        return [strp.lsid for strp in self.strps]
+    @property
+    def pnli(self):
+        lpids = []
+        for i in range(self.pnls.shape[0]):
+            for j in range(self.pnls.shape[1]):
+                lpids.append(self.pnls[i, j].lpid)
+        return lpids
+    def __repr__(self):
+        return '<LatticeSurface {:s}>'.format(self.name)
+
+def latticesurface_from_json(surfdata: dict, display: bool=False):
+    from .latticesection import latticesecttion_from_json
+    name = surfdata['name']
+    if 'mirror' in surfdata:
+        mirror = surfdata['mirror']
+    else:
+        mirror = False
+    if display: print(f'Loading Surface: {name:s}')
+    # Read Section Variables
+    sects = []
+    for sectdata in surfdata['sections']:
+        sect = latticesecttion_from_json(sectdata)
+        sects.append(sect)
+    # Linear Interpolate Missing Variables
+    x, y, z, c, a = [], [], [], [], []
+    for sect in sects:
+        x.append(sect.pnt.x)
+        y.append(sect.pnt.y)
+        z.append(sect.pnt.z)
+        c.append(sect.chord)
+        a.append(sect.angle)
+    if None in y:
+        if None is z:
+            return ValueError
+        else:
+            y = linear_interpolate_none(z, y)
+    else:
+        z = linear_interpolate_none(y, z)
+    lensects = len(sects)
+    b = [0.0]
+    for i in range(lensects-1):
+        bi = b[i]+sqrt((y[i+1]-y[i])**2+(z[i+1]-z[i])**2)
+        b.append(bi)
+    x = linear_interpolate_none(b, x)
+    c = linear_interpolate_none(b, c)
+    a = linear_interpolate_none(b, a)
+    for i, sect in enumerate(sects):
+        sect.pnt.x = x[i]
+        sect.pnt.y = y[i]
+        sect.pnt.z = z[i]
+        sect.chord = c[i]
+        sect.angle = a[i]
+    # Read in Function Data
+    funcs = []
+    if 'functions' in surfdata:
+        for funcdata in surfdata['functions']:
+            func = surffunc_from_json(funcdata)
+            funcs.append(func)
+    # Entire Surface Position
+    xpos, ypos, zpos = 0.0, 0.0, 0.0
+    if 'xpos' in surfdata:
+        xpos = surfdata['xpos']
+    if 'ypos' in surfdata:
+        ypos = surfdata['ypos']
+    if 'zpos' in surfdata:
+        zpos = surfdata['zpos']
+    angle = 0.0
+    if 'angle' in surfdata:
+        angle = surfdata['angle']
+    for sect in sects:
+        sect.offset_position(xpos, ypos, zpos)
+        sect.offset_angle(angle)
+    surf = LatticeSurface(name, sects, mirror, funcs)
+    if 'numc' in surfdata and 'cspace' in surfdata:
+        numc = surfdata['numc']
+        cspace = surfdata['cspace']
+        if cspace == 'equal':
+            surf.set_chord_equal_distribution(numc)
+        elif cspace == 'cosine':
+            surf.set_chord_cosine_distribution(numc)
+    return surf
+
+def linear_interpolate_none(x: list, y: list):
+    for i, yi in enumerate(y):
+        if yi is None:
+            for j in range(i, -1, -1):
+                if y[j] is not None:
+                    a = j
+                    break
+            for j in range(i, len(y)):
+                if y[j] is not None:
+                    b = j
+                    break
+            xa, xb = x[a], x[b]
+            ya, yb = y[a], y[b]
+            y[i] = (yb-ya)/(xb-xa)*(x[i]-xa)+ya
+    return y
+
+class SurfaceFunction(object):
+    var = None
+    dist = None
+    interp = None
+    values = None
+    spline = None
+    def __init__(self, var: str, spacing: str, interp: str, values: list):
+        self.var = var
+        self.spacing = spacing
+        self.interp = interp
+        self.values = values
+    def set_spline(self, bmax: float):
+        if self.spacing == 'equal':
+            num = len(self.values)
+            from pyvlm.tools import equal_spacing
+            nspc = equal_spacing(num-1)
+            spc = [bmax*nspci for nspci in nspc]
+        if self.interp == 'linear':
+            from pygeom.geom1d import LinearSpline
+            self.spline = LinearSpline(spc, self.values)
+        elif self.interp == 'cubic':
+            from pygeom.geom1d import CubicSpline
+            self.spline = CubicSpline(spc, self.values)
+    def interpolate(self, b: float):
+        return self.spline.single_interpolate_spline(b)
+    
+
+def surffunc_from_json(funcdata: dict):
+    var = funcdata["variable"]
+    if "spacing" in funcdata:
+        spacing = funcdata["spacing"]
+    else:
+        spacing = "equal"
+    if "interp" in funcdata:
+        interp = funcdata["interp"]
+    else:
+        interp = "linear"
+    values = funcdata["values"]
+    return SurfaceFunction(var, spacing, interp, values)
---
+++ packages/pyvlm/pyvlm-0.0.9.tar.gz/pyvlm-0.0.9/pyvlm/classes/latticesystem.py
@@ -0,0 +1,487 @@
+from time import perf_counter
+from math import pi, atan2
+from numpy.matlib import zeros, matrix, multiply, divide, fill_diagonal, seterr
+from pygeom.geom3d import Point, Vector, ihat, jhat, zero_vector
+from pygeom.matrix3d import zero_matrix_vector, MatrixVector
+from pygeom.matrix3d import elementwise_dot_product, elementwise_cross_product
+from pygeom.matrix3d import elementwise_multiply, elementwise_divide
+
+seterr(divide='ignore', invalid='ignore')
+
+class LatticeSystem(object):
+    source = None
+    name = None
+    srfcs = None
+    strps = None
+    mstrp = None
+    pnls = None
+    mpnl = None
+    bref = None
+    cref = None
+    sref = None
+    rref = None
+    ctrls = None
+    _ra = None
+    _rb = None
+    _rc = None
+    _rg = None
+    _gam = None
+    _avg = None
+    _afg = None
+    _avc = None
+    _aic = None
+    _afs = None
+    _adc = None
+    _ada = None
+    _bvg = None
+    _bdg = None
+    _blg = None
+    _byg = None
+    _bmg = None
+    _bda = None
+    _ar = None
+    _cdo = None
+    _cdo_ff = None
+    results = None
+    def __init__(self, name: str, srfcs: list, bref: float, cref: float, sref: float, rref: Point):
+        self.name = name
+        self.srfcs = srfcs
+        self.bref = bref
+        self.cref = cref
+        self.sref = sref
+        self.rref = rref
+        self.results = {}
+        self.mesh()
+        self.inherit()
+        # self.build()
+    def mesh(self):
+        lsid = 0
+        lpid = 0
+        for srfc in self.srfcs:
+            lsid, lpid = srfc.mesh(lsid, lpid)
+    def inherit(self):
+        strpdct = {}
+        pnldct = {}
+        istrp = 0
+        ipnl = 0
+        self.strps = []
+        self.mstrp = []
+        self.pnls = []
+        self.mpnl = []
+        self.ctrls = {}
+        for srfc in self.srfcs:
+            for strp in srfc.strps:
+                self.strps.append(strp)
+                if strp.msid is None:
+                    strpind = istrp
+                    strpdct[strp.lsid] = istrp
+                else:
+                    strpind = strpdct[strp.msid]
+                self.mstrp.append(strpind)
+                istrp += 1
+            pnls = srfc.return_panels()
+            for pnl in pnls:
+                self.pnls.append(pnl)
+                if pnl.mpid is None:
+                    pnlind = ipnl
+                    pnldct[pnl.lpid] = ipnl
+                else:
+                    pnlind = pnldct[pnl.mpid]
+                self.mpnl.append(pnlind)
+                ipnl += 1
+        ind = 2
+        for srfc in self.srfcs:
+            for sht in srfc.shts:
+                for control in sht.ctrls:
+                    if control not in self.ctrls:
+                        self.ctrls[control] = (ind, ind+1, ind+2, ind+3)
+                        ind += 4
+    def build(self):
+        self.ra
+        self.rb
+        self.rc
+        self.rg
+        self.avc
+        self.aic
+        self.afs
+        self.gam
+        self.avg
+        self.afg
+        self.bvg
+        self.bdg
+        self.blg
+        self.byg
+        self.bmg
+    def reset(self):
+        for attr in self.__dict__:
+            if attr[0] == '_':
+                self.__dict__[attr] = None
+    @property
+    def ra(self):
+        if self._ra is None:
+            num = len(self.pnls)
+            self._ra = zero_matrix_vector((num, num))
+            for pnlj in self.pnls:
+                j = pnlj.lpid
+                self._ra[:, j] = pnlj.pnta
+        return self._ra
+    @property
+    def rb(self):
+        if self._rb is None:
+            num = len(self.pnls)
+            self._rb = zero_matrix_vector((num, num))
+            for pnlj in self.pnls:
+                j = pnlj.lpid
+                self._rb[:, j] = pnlj.pntb
+        return self._rb
+    @property
+    def rc(self):
+        if self._rc is None:
+            num = len(self.pnls)
+            self._rc = zero_matrix_vector((num, num))
+            for pnli in self.pnls:
+                i = pnli.lpid
+                self._rc[i, :] = pnli.pntc
+        return self._rc
+    @property
+    def rg(self):
+        if self._rg is None:
+            num = len(self.pnls)
+            self._rg = zero_matrix_vector((num, num))
+            for pnli in self.pnls:
+                i = pnli.lpid
+                self._rg[i, :] = pnli.pnti
+        return self._rg
+    @property
+    def avc(self):
+        if self._avc is None:
+            veli, vela, velb = velocity_matrix(self.ra, self.rb, self.rc)
+            self._avc = (veli+vela-velb)/(4*pi)
+        return self._avc
+    @property
+    def aic(self):
+        if self._aic is None:
+            num = len(self.pnls)
+            self._aic = zeros((num, num))
+            for pnl in self.pnls:
+                i = pnl.lpid
+                self._aic[i, :] = self.avc[i, :]*pnl.nrml
+        return self._aic
+    @property
+    def afs(self):
+        if self._afs is None:
+            num = len(self.pnls)
+            numc = len(self.ctrls)
+            self._afs = zero_matrix_vector((num, 2+4*numc))
+            for pnl in self.pnls:
+                lpid = pnl.lpid
+                rrel = pnl.pntc-self.rref
+                self._afs[lpid, 0] = pnl.nrml
+                self._afs[lpid, 1] = -rrel**pnl.nrml
+            for srfc in self.srfcs:
+                for sht in srfc.shts:
+                    for control in sht.ctrls:
+                        ctrl = sht.ctrls[control]
+                        ctup = self.ctrls[control]
+                        for pnl in ctrl.pnls:
+                            lpid = pnl.lpid
+                            rrel = pnl.pntc-self.rref
+                            dndlp = pnl.dndl(ctrl.posgain, ctrl.uhvec)
+                            self._afs[lpid, ctup[0]] = dndlp
+                            self._afs[lpid, ctup[1]] = rrel**dndlp
+                            dndln = pnl.dndl(ctrl.neggain, ctrl.uhvec)
+                            self._afs[lpid, ctup[2]] = dndln
+                            self._afs[lpid, ctup[3]] = rrel**dndln
+        return self._afs
+    @property
+    def gam(self):
+        if self._gam is None:
+            from pygeom.matrix3d import solve_matrix_vector
+            self._gam = -solve_matrix_vector(self.aic, self.afs)
+        return self._gam
+    @property
+    def avg(self):
+        if self._avg is None:
+            veli, vela, velb = velocity_matrix(self.ra, self.rb, self.rg)
+            fill_diagonal(veli.x, 0.0)
+            fill_diagonal(veli.y, 0.0)
+            fill_diagonal(veli.z, 0.0)
+            self._avg = (veli+vela-velb)/(4*pi)
+        return self._avg
+    @property
+    def afg(self):
+        if self._afg is None:
+            num = len(self.pnls)
+            self._afg = zero_matrix_vector((num, num))
+            for pnl in self.pnls:
+                i = pnl.lpid
+                if not pnl.noload:
+                    self._afg[i, :] = self.avg[i, :]**pnl.leni
+        return self._afg
+    @property
+    def adc(self):
+        if self._adc is None:
+            num = len(self.pnls)
+            self._adc = zeros((num, num))
+            for pnl in self.pnls:
+                i = pnl.lpid
+                self._adc[i, :] = self.avc[i, :]*pnl.tang
+        return self._adc
+    @property
+    def ada(self):
+        if self._ada is None:
+            num = len(self.pnls)
+            self._ada = zeros((num, 1))
+            for pnl in self.pnls:
+                i = pnl.lpid
+                self._ada[i, 0] = pnl.cdoarea
+        return self._ada
+    @property
+    def cdo(self):
+        if self._cdo is None:
+            dragarea = 0.0
+            for pnl in self.pnls:
+                i = pnl.lpid
+                dragarea += self.ada[i, 0]
+            self._cdo = dragarea/self.sref
+        return self._cdo
+    @property
+    def bvg(self):
+        if self._bvg is None:
+            num = len(self.strps)
+            self._bvg = zeros((num, num))
+            for strpi in self.strps:
+                i = strpi.lsid
+                for strpj in self.strps:
+                    j =  strpj.lsid
+                    self._bvg[i, j] = strpj.trefftz_velocity(strpi.pnti)*strpi.nrmt
+        return self._bvg
+    @property
+    def bdg(self):
+        if self._bdg is None:
+            num = len(self.strps)
+            self._bdg = zeros((num, num))
+            for strp in self.strps:
+                i = strp.lsid
+                for j in range(num):
+                    self._bdg[i, j] = strp.trefftz_drag(self.bvg[i, j])
+        return self._bdg
+    @property
+    def blg(self):
+        if self._blg is None:
+            num = len(self.strps)
+            self._blg = zeros((num, 1))
+            for strp in self.strps:
+                i = strp.lsid
+                self._blg[i, 0] = strp.trefftz_lift()
+        return self._blg
+    @property
+    def byg(self):
+        if self._byg is None:
+            num = len(self.strps)
+            self._byg = zeros((num, 1))
+            for strp in self.strps:
+                i = strp.lsid
+                self._byg[i, 0] = strp.trefftz_yfrc()
+        return self._byg
+    @property
+    def bmg(self):
+        if self._bmg is None:
+            num = len(self.strps)
+            self._bmg = zeros((num, 1))
+            for strp in self.strps:
+                i = strp.lsid
+                self._bmg[i, 0] = strp.pnti.y*self.blg[i, 0]-strp.pnti.z*self.byg[i, 0]
+        return self._bmg
+    @property
+    def bda(self):
+        if self._bda is None:
+            num = len(self.strps)
+            self._bda = zeros((num, 1))
+            for strp in self.strps:
+                i = strp.lsid
+                self._bda[i, 0] = strp.cdoarea
+        return self._bda
+    @property
+    def cdo_ff(self):
+        if self._cdo_ff is None:
+            dragarea = 0.0
+            for strp in self.strps:
+                i = strp.lsid
+                dragarea += self.bda[i, 0]
+            self._cdo_ff = dragarea/self.sref
+        return self._cdo_ff
+    @property
+    def ar(self):
+        if self._ar is None:
+            self._ar = self.bref**2/self.sref
+        return self._ar
+    def set_strip_alpha(self, alpha: list):
+        for i, strp in enumerate(self.strps):
+            strp._ang = alpha[i]
+        self._aic = None
+        self._afs = None
+        self._gam = None
+    @property
+    def strip_geometry(self):
+        from py2md.classes import MDTable
+        table = MDTable()
+        table.add_column('#', 'd')
+        table.add_column('Xle', '.5f')
+        table.add_column('Yle', '.5f')
+        table.add_column('Zle', '.5f')
+        table.add_column('Chord', '.4f')
+        table.add_column('Width', '.5f')
+        table.add_column('Area', '.6f')
+        table.add_column('Dihed', '.4f')
+        table.add_column('Incid', '.4f')
+        for strp in self.strps:
+            j = strp.lsid
+            xle = strp.pnti.x
+            yle = strp.pnti.y
+            zle = strp.pnti.z
+            chord = strp.chord
+            width = strp.dst
+            area = strp.area
+            dihed = strp.dihedral
+            angle = strp.angle
+            table.add_row([j, xle, yle, zle, chord, width, area, dihed, angle])
+        return table
+    @property
+    def panel_geometry(self):
+        from py2md.classes import MDTable
+        table = MDTable()
+        table.add_column('#', 'd')
+        table.add_column('X', '.5f')
+        table.add_column('Y', '.5f')
+        table.add_column('Z', '.5f')
+        table.add_column('DX', '.5f')
+        for pnl in self.pnls:
+            j = pnl.lpid
+            x = pnl.pntg.x
+            y = pnl.pntg.y
+            z = pnl.pntg.z
+            dx = pnl.crd
+            table.add_row([j, x, y, z, dx])
+        return table
+    def copy_from_source(self):
+        lsys = latticesystem_from_json(self.source, build=False)
+        for attr in self.__dict__:
+            if attr[0] == '_':
+                lsys.__dict__[attr] = self.__dict__[attr].copy()
+        lsys.build()
+        return lsys
+    def __repr__(self):
+        return '<LatticeSystem: {:s}>'.format(self.name)
+    def __str__(self):
+        from py2md.classes import MDTable
+        outstr = '# Lattice System '+self.name+'\n'
+        table = MDTable()
+        table.add_column('Name', 's', data=[self.name])
+        table.add_column('Sref', 'g', data=[self.sref])
+        table.add_column('cref', 'g', data=[self.cref])
+        table.add_column('bref', 'g', data=[self.bref])
+        table.add_column('xref', '.3f', data=[self.rref.x])
+        table.add_column('yref', '.3f', data=[self.rref.y])
+        table.add_column('zref', '.3f', data=[self.rref.z])
+        outstr += table._repr_markdown_()
+        table = MDTable()
+        table.add_column('# Strips', 'd', data=[len(self.strps)])
+        table.add_column('# Panels', 'd', data=[len(self.pnls)])
+        table.add_column('# Controls', 'd', data=[len(self.ctrls)])
+        outstr += table._repr_markdown_()
+        return outstr
+    def _repr_markdown_(self):
+        return self.__str__()
+
+def latticesystem_from_json(jsonfilepath: str, build: bool=True):
+    from .latticesurface import latticesurface_from_json
+    from .latticeresult import latticeresult_from_json
+    from .latticetrim import latticetrim_from_json
+    from json import load
+
+    with open(jsonfilepath, 'rt') as jsonfile:
+        data = load(jsonfile)
+    
+    from os.path import dirname, join, exists
+
+    path = dirname(jsonfilepath)
+
+    for surfdata in data['surfaces']:
+        for sectdata in surfdata['sections']:
+            if 'airfoil' in sectdata:
+                airfoil = sectdata['airfoil']
+                if airfoil[-4:] == '.dat':
+                    airfoil = join(path, airfoil)
+                    if not exists(airfoil):
+                        print(f'Airfoil {airfoil} does not exist.')
+                        del sectdata['airfoil']
+                    else:
+                        sectdata['airfoil'] = airfoil
+    
+    name = data['name']
+    sfcs = []
+    for surfdata in data['surfaces']:
+        sfc = latticesurface_from_json(surfdata)
+        sfcs.append(sfc)
+    bref = data['bref']
+    cref = data['cref']
+    sref = data['sref']
+    xref = data['xref']
+    yref = data['yref']
+    zref = data['zref']
+    rref = Point(xref, yref, zref)
+    lsys = LatticeSystem(name, sfcs, bref, cref, sref, rref)
+
+    if 'cases' in data and build:
+        lsys.build()
+        for i in range(len(data['cases'])):
+            resdata = data['cases'][i]
+            if 'trim' in resdata:
+                latticetrim_from_json(lsys, resdata)
+            else:
+                latticeresult_from_json(lsys, resdata)
+    
+    lsys.source = jsonfilepath
+
+    return lsys
+
+def velocity_matrix(ra: matrix, rb: matrix, rc: matrix):
+    
+    a = rc-ra
+    b = rc-rb
+
+    am = a.return_magnitude()
+    bm = b.return_magnitude()
+
+    # Velocity from Bound Vortex
+    adb = elementwise_dot_product(a, b)
+    abm = multiply(am, bm)
+    dm = multiply(abm, abm+adb)
+    axb = elementwise_cross_product(a, b)
+    axbm = axb.return_magnitude()
+    chki = (axbm == 0.0)
+    veli = elementwise_multiply(axb, divide(am+bm, dm))
+    veli.x[chki] = 0.0
+    veli.y[chki] = 0.0
+    veli.z[chki] = 0.0
+
+    # Velocity from Trailing Vortex A
+    axx = MatrixVector(zeros(a.shape), a.z, -a.y)
+    axxm = axx.return_magnitude()
+    chka = (axxm == 0.0)
+    vela = elementwise_divide(axx, multiply(am, am-a.x))
+    vela.x[chka] = 0.0
+    vela.y[chka] = 0.0
+    vela.z[chka] = 0.0
+
+    # Velocity from Trailing Vortex B
+    bxx = MatrixVector(zeros(b.shape), b.z, -b.y)
+    bxxm = bxx.return_magnitude()
+    chkb = (bxxm == 0.0)
+    velb = elementwise_divide(bxx, multiply(bm, bm-b.x))
+    velb.x[chkb] = 0.0
+    velb.y[chkb] = 0.0
+    velb.z[chkb] = 0.0
+
+    return veli, vela, velb
