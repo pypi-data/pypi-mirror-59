c8bb687a434ff3e17e21b30532838e9688fa5399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:54 +0000

    Add package scrapyu/0.1.11/scrapyu-0.1.11-py3-none-any.whl

---
+++ packages/scrapyu/scrapyu-0.1.11-py3-none-any.whl/scrapyu/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.11'
+
+
+from ._useragent import UserAgentMiddleware
+from ._markdown import MarkdownPipeline
+from ._cookies import FirefoxCookiesMiddleware
+from ._mongodb import MongoDBPipeline
+from ._redis import RedisDupeFilter
---
+++ packages/scrapyu/scrapyu-0.1.11-py3-none-any.whl/scrapyu/_cookies.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+from .utils import get_firefox_cookies
+
+
+class FirefoxCookiesMiddleware(object):
+    """使用Selenium驱动打开Firefox浏览器获取网页Cookies
+    Firefox浏览器版本 69.0.1, geckodriver版本要和浏览器版本对应
+    geckodriver下载: https://github.com/mozilla/geckodriver/releases
+    Chrome浏览器的headless模式好像出了点问题, 就用Firefox浏览器好了
+    """
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        cls.executable_path = crawler.settings.get('GECKODRIVER_PATH', 'geckodriver')
+        return cls()
+
+    def process_request(self, request, spider):
+        
+        request.cookies = get_firefox_cookies(request.url, executable_path=self.executable_path)
