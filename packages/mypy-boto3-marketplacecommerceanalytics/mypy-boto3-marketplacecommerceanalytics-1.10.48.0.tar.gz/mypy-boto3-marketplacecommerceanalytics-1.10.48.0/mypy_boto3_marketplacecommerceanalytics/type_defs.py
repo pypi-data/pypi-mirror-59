46ba256c01c1acbae20d863529bd73ae485b63e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package mypy-boto3-marketplacecommerceanalytics/1.10.48.0/mypy-boto3-marketplacecommerceanalytics-1.10.48.0.tar.gz

---
+++ packages/mypy-boto3-marketplacecommerceanalytics/mypy-boto3-marketplacecommerceanalytics-1.10.48.0.tar.gz/mypy-boto3-marketplacecommerceanalytics-1.10.48.0/mypy_boto3_marketplacecommerceanalytics/type_defs.py
@@ -0,0 +1,28 @@
+"""
+Main interface for marketplacecommerceanalytics service type definitions.
+
+Usage::
+
+    from mypy_boto3.marketplacecommerceanalytics.type_defs import GenerateDataSetResultTypeDef
+
+    data: GenerateDataSetResultTypeDef = {...}
+"""
+from __future__ import annotations
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("GenerateDataSetResultTypeDef", "StartSupportDataExportResultTypeDef")
+
+GenerateDataSetResultTypeDef = TypedDict(
+    "GenerateDataSetResultTypeDef", {"dataSetRequestId": str}, total=False
+)
+
+StartSupportDataExportResultTypeDef = TypedDict(
+    "StartSupportDataExportResultTypeDef", {"dataSetRequestId": str}, total=False
+)
