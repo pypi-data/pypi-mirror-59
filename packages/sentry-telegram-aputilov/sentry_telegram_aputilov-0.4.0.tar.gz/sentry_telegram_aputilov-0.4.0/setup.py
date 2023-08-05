a821ff4b56131caf99145764e4ac06470de8abc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:30 +0000

    Add package sentry-telegram-aputilov/0.4.0/sentry_telegram_aputilov-0.4.0.tar.gz

---
+++ packages/sentry-telegram-aputilov/sentry_telegram_aputilov-0.4.0.tar.gz/sentry_telegram_aputilov-0.4.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+from sentry_telegram import __version__
+
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='sentry_telegram_aputilov',
+    version=__version__,
+    packages=['sentry_telegram'],
+    url='https://github.com/butorov/sentry-telegram',
+    author='Viacheslav Butorov',
+    author_email='butorovv@gmail.com',
+    description='Plugin for Sentry which allows sending notification via Telegram messenger.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    entry_points={
+        'sentry.plugins': [
+            'sentry_telegram = sentry_telegram.plugin:TelegramNotificationsPlugin',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Bug Tracking',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: System :: Monitoring',
+    ],
+    include_package_data=True,
+)
