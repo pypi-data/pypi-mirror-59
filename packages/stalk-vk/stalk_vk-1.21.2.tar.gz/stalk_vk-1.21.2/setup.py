11a811f6bd5fbde991c857d94de77d52ff933ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package stalk-vk/1.21.2/stalk_vk-1.21.2.tar.gz

---
+++ packages/stalk-vk/stalk_vk-1.21.2.tar.gz/stalk_vk-1.21.2/setup.py
@@ -0,0 +1,20 @@
+import stalk
+
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='stalk_vk',
+    url='https://github.com/Adonai-ai',
+    author='Adonai-ai',
+    license='MIT',
+    version=stalk.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    entry_points={
+        'console_scripts':['stalk_vk = stalk.core:main']
+    },
+    install_requires=[
+        'Flask','vk_api','bs4'
+    ],
+)
