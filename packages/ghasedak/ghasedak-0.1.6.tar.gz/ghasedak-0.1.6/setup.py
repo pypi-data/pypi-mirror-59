f922331b58ca0d1d32ce913b59c0f77941d7e192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:54 +0000

    Add package ghasedak/0.1.6/ghasedak-0.1.6.tar.gz

---
+++ packages/ghasedak/ghasedak-0.1.6.tar.gz/ghasedak-0.1.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ghasedak",
+    version="0.1.6",
+    author="Dariush Abbasi",
+    author_email="poshtehani@gmail.com",
+    description="Ghasedak sms webservice api wrapper for python.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="http://github.com/ghasedakapi/ghasedak-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
