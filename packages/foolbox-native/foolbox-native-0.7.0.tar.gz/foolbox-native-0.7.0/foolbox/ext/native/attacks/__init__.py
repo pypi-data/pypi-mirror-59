f34ccca2668309cd323897f1f960b528c5778297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:19 +0000

    Add package foolbox-native/0.7.0/foolbox-native-0.7.0.tar.gz

---
+++ packages/foolbox-native/foolbox-native-0.7.0.tar.gz/foolbox-native-0.7.0/foolbox/ext/native/attacks/__init__.py
@@ -0,0 +1,21 @@
+from .basic_iterative_method import L2BasicIterativeAttack  # noqa: F401
+from .basic_iterative_method import LinfinityBasicIterativeAttack  # noqa: F401
+from .fast_gradient_method import L2FastGradientAttack  # noqa: F401
+from .fast_gradient_method import LinfinityFastGradientAttack  # noqa: F401
+from .carlini_wagner import L2CarliniWagnerAttack  # noqa: F401
+from .ead import EADAttack  # noqa: F401
+from .projected_gradient_descent import ProjectedGradientDescentAttack  # noqa: F401
+from .contrast import L2ContrastReductionAttack  # noqa: F401
+from .contrast import BinarySearchContrastReductionAttack  # noqa: F401
+from .contrast import LinearSearchContrastReductionAttack  # noqa: F401
+from .inversion import InversionAttack  # noqa: F401
+from .blended_noise import LinearSearchBlendedUniformNoiseAttack  # noqa: F401
+from .additive_noise import L2AdditiveGaussianNoiseAttack  # noqa: F401
+from .additive_noise import L2RepeatedAdditiveGaussianNoiseAttack  # noqa: F401
+from .saltandpepper import SaltAndPepperNoiseAttack  # noqa: F401
+from .binarization import BinarizationRefinementAttack  # noqa: F401
+from .boundary_attack import BoundaryAttack  # noqa: F401
+
+FGM = L2FastGradientAttack
+FGSM = LinfinityFastGradientAttack
+PGD = ProjectedGradientDescentAttack
