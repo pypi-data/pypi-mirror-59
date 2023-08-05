fd1a8e9bff1987499cdd76b4a29fff2bf9b1547f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package cgdat/2.4.1/cgdat-2.4.1-py3-none-any.whl

---
+++ packages/cgdat/cgdat-2.4.1-py3-none-any.whl/cgdat/qt_custom/__init__.py
@@ -0,0 +1,15 @@
+"""This package contains all the additional classes and functions that are used in the CGDAT data anaylsis tool. It contains
+the following components:
+
+**Modules:**
+
+.. autosummary::
+   :toctree: _autosummary
+
+    cgdat.qt_custom.qt_dialogs
+    cgdat.qt_custom.qt_extra
+    cgdat.qt_custom.qt_thread
+"""
+
+# Set all
+__all__ = ["qt_extra", "qt_thread", "qt_dialogs"]
