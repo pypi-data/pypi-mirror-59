ab1f81856c13dc4fe7e02aa22b33e5d3006cafbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:52 +0000

    Add package automation_rest_server/1.0.9/automation_rest_server-1.0.9.tar.gz

---
+++ packages/automation_rest_server/automation_rest_server-1.0.9.tar.gz/automation_rest_server-1.0.9/automation_rest_server/tool/device/windows.py
@@ -0,0 +1,42 @@
+
+import os
+from ctypes import windll
+from utils.buf import Malloc
+from .library.nvme_cmd import SmartHealth
+
+
+class NVME(object):
+
+    def __init__(self, dev_index=None):
+        self.dev_index = dev_index
+        path = os.path.realpath(os.path.join(os.path.dirname(__file__), "library", "nvme_ioctl.dll"))
+        self.__dll = windll.LoadLibrary(path)
+
+    def getlog(self, lid=0x01, numd=100, numdu=0, lpol=0, lpou=0, nsid=0, type_=SmartHealth):
+        ret, prp = None, None
+        if lid == 0x02:
+            ret, prp = self.get_smart_log()
+        return ret, prp
+
+    def get_smart_log(self):
+        prp = Malloc(types=SmartHealth)
+        ret = self.__dll.get_smartlog(self.dev_index, prp.buffer())
+        if ret != 0:
+            print("get smart log failed")
+        return ret, prp
+
+    def identify(self, cns, cntid, nsid, types):
+        prp = Malloc(types=types)
+        ret = -1
+        if cns == 0x1:
+            ret = self.__dll.identify_controller(self.dev_index, prp.buffer())
+        elif cns == 0x11:
+            ret = self.__dll.identify_namespace(self.dev_index, prp.buffer())
+        return ret, prp
+
+    def upgrade_fw(self, fw_path, device_index, slot):
+        ret = self.__dll.upgrade_fw(device_index, slot, fw_path)
+        return ret
+
+    def list_ssd(self):
+        pass
---
+++ packages/automation_rest_server/automation_rest_server-1.0.9.tar.gz/automation_rest_server-1.0.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+packages = ["automation_rest_server"]
+
+setup(
+    name = 'automation_rest_server',
+    version = '1.0.9',
+    keywords = ['runner', 'server'],
+    description = 'NVMe production server',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+    license = 'MIT License',
+	url = 'https://pypi.org/project/automation_rest_server',
+    install_requires = ['flask',
+						'flask-restful',
+						'requests',
+						'PyMySQL',
+						'pyftpdlib'],
+    include_package_data=True, 
+    author = 'yuwen123441',
+    author_email = 'yuwen123441@qq.com',
+    packages = find_packages(),
+    platforms = 'any',
+    entry_points = {
+        'console_scripts': [
+        'prunner = automation_rest_server.run:run'
+        ]}
+)
