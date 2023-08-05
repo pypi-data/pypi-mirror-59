81f6eb5aeb74285d3cb4b62da19a9701a78b3997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:14 +0000

    Add package enamel/0.0.2/enamel-0.0.2.tar.gz

---
+++ packages/enamel/enamel-0.0.2.tar.gz/enamel-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='enamel',
+  version='0.0.2',
+  author='Unknown',
+  author_email='unknown@example.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="This is a helper library",
+  packages=['enamel'],
+  scripts=[]
+)
+
