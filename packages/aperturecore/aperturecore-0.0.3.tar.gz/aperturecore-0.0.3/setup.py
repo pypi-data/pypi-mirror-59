0ca434410fda124d524b30d1d084642da3265140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package aperturecore/0.0.3/aperturecore-0.0.3.tar.gz

---
+++ packages/aperturecore/aperturecore-0.0.3.tar.gz/aperturecore-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.ctv", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aperturecore",
+    version="0.0.3",
+    author="LazyNeko",
+    author_email="lazynekoo@gmail.com",
+    description="A (unofficial) package for our Aperture Science Cores (tm)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LazyNeko1/ApertureCore",  # IS NOT PUBLIC YET
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.ctv', 'aperture/*.py', 'examples/*']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
