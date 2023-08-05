4211bb73fe4c47b964e065e51735bee05e16e020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package coach-ml/0.19/coach-ml-0.19.tar.gz

---
+++ packages/coach-ml/coach-ml-0.19.tar.gz/coach-ml-0.19/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coach-ml",
+    version="0.19",
+    author="Loren Kuich",
+    author_email="loren@lkuich.com",
+    description="Python client for coach",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coach-ml/coach-python",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'numpy==1.16.4', 'tensorflow==1.14'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
