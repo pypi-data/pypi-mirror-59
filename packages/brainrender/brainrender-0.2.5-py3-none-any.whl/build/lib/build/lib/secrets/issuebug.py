17cd3d4e0f3f79cfa5063f5aec812bfa5019ac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package brainrender/0.2.5/brainrender-0.2.5-py3-none-any.whl

---
+++ packages/brainrender/brainrender-0.2.5-py3-none-any.whl/build/lib/build/lib/secrets/issuebug.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('./')
+from brainrender.scene import Scene
+
+regions = ['PRT', 'SCm', 'SCs']
+for region in regions:
+    scene = Scene()
+    scene.add_brain_regions([region], colors='white', alpha=0.5)
+    scene.render()
