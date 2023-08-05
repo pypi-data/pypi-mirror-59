3354b60cbf8513cf880e1578f684d3f0fa37ce5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package sgx.py/0.2.1.dev2/sgx.py-0.2.1.dev2.tar.gz

---
+++ packages/sgx.py/sgx.py-0.2.1.dev2.tar.gz/sgx.py-0.2.1.dev2/sgx/sgx_rpc_handler.py
@@ -0,0 +1,149 @@
+#    -*- coding: utf-8 -*-
+#
+#     This file is part of sgx.py
+#
+#     Copyright (C) 2019 SKALE Labs
+#
+#     sgx.py is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as published
+#     by the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     sgx.py is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with sgx.py.  If not, see <https://www.gnu.org/licenses/>.
+
+from urllib.parse import urlparse
+from sgx.ssl_utils import send_request
+
+
+class SgxRPCHandler:
+    def __init__(self, sgx_endpoint, path_to_cert):
+        self.sgx_endpoint = check_provider(sgx_endpoint)
+        self.path_to_cert = path_to_cert
+
+    def ecdsa_sign(self, keyName, transactionHash):
+        params = dict()
+        params['base'] = 10
+        params['keyName'] = keyName
+        params['messageHash'] = transactionHash
+        response = self.__send_request('ecdsaSignMessageHash', params)
+        signature = response['result']
+        vrs = (signature['signature_v'], signature['signature_r'], signature['signature_s'])
+        return vrs
+
+    def generate_key(self):
+        params = dict()
+        response = self.__send_request("generateECDSAKey", params)
+        key_name = response['result']['KeyName']
+        public_key = response['result']['PublicKey']
+        return (key_name, public_key)
+
+    def get_public_key(self, keyName):
+        params = dict()
+        params['keyName'] = keyName
+        response = self.__send_request("getPublicECDSAKey", params)
+        publicKey = response['result']['PublicKey']
+        return publicKey
+
+    def generate_dkg_poly(self, poly_name, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['t'] = t
+        response = self.__send_request("generateDKGPoly", params)
+        return response['result']['status'] == 0
+
+    def get_verification_vector(self, poly_name, n, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['n'] = n
+        params['t'] = t
+        response = self.__send_request("getVerificationVector", params)
+        verification_vector = response['result']['Verification Vector']
+        return verification_vector
+
+    def get_secret_key_contribution(self, poly_name, public_keys, n, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['n'] = n
+        params['t'] = t
+        params['publicKeys'] = public_keys
+        response = self.__send_request("getSecretShare", params)
+        secret_key_contribution = response['result']['SecretShare']
+        return secret_key_contribution
+
+    def verify_secret_share(self, public_shares, eth_key_name, secret_share, n, t, index):
+        params = dict()
+        params['publicShares'] = public_shares
+        params['EthKeyName'] = eth_key_name
+        params['SecretShare'] = secret_share
+        params['n'] = n
+        params['t'] = t
+        params['index'] = index
+        response = self.__send_request("DKGVerification", params)
+        result = response['result']
+        return result['result']
+
+    def create_bls_private_key(self, poly_name, bls_key_name, eth_key_name, secret_shares, n, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['BLSKeyName'] = bls_key_name
+        params['EthKeyName'] = eth_key_name
+        params['SecretShare'] = secret_shares
+        params['n'] = n
+        params['t'] = t
+        response = self.__send_request("CreateBLSPrivateKey", params)
+        return response['result']['status'] == 0
+
+    def get_bls_public_key(self, bls_key_name):
+        params = dict()
+        params["BLSKeyName"] = bls_key_name
+        response = self.__send_request("GetBLSPublicKeyShare", params)
+        return response['result']['BLSPublicKeyShare']
+
+    def complaint_response(self, poly_name, n, t, idx):
+        params = dict()
+        params['polyName'] = poly_name
+        params['n'] = n
+        params['t'] = t
+        params['ind'] = idx
+        response = self.__send_request("ComplaintResponse", params)
+        return (response['result']['share*G2'], response['result']['DHKey'])
+
+    def mult_g2(self, to_mult):
+        params = dict()
+        params['x'] = to_mult
+        response = self.__send_request("MultG2", params)
+        return response['result']['x*G2']
+
+    def import_bls_private_key(self, key_share_name, n, t, index, key_share):
+        params = dict()
+        params['keyShareName'] = key_share_name
+        params['n'] = n
+        params['t'] = t
+        params['signerIndex'] = index
+        params['keyShareName'] = key_share
+        response = self.__send_request("ImportBLSKeyShare", params)
+        encrypted_key = response['encryptedKeyShare']
+        return encrypted_key
+
+    def __send_request(self, method, params):
+        response = send_request(self.sgx_endpoint, method, params, self.path_to_cert)
+        if response.get('error') is not None:
+            raise Exception(response['error']['message'])
+        if response['result']['status']:
+            raise Exception(response['result']['errorMessage'])
+        return response
+
+
+def check_provider(endpoint):
+    scheme = urlparse(endpoint).scheme
+    if scheme == 'https':
+        return endpoint
+    raise Exception(
+        'Wrong sgx endpoint. Supported schemes: https'
+    )
