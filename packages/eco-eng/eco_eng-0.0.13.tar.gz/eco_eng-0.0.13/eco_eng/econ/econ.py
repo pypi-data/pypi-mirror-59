dca628cc1517bdba7b606e7a2a1d096cb0dfc89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:53 +0000

    Add package eco-eng/0.0.13/eco_eng-0.0.13.tar.gz

---
+++ packages/eco-eng/eco_eng-0.0.13.tar.gz/eco_eng-0.0.13/eco_eng/econ/econ.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def GetNPVTable(installCost,maintCost,disposalCost,annualRevenue,discountRate,lifetime):
+    # determine annual net cash flow
+    df = pd.DataFrame( index=np.arange(0, lifetime),
+                  columns=["Year","Annual Cost","Annual Revenue","Net Cash Flow","Cumulative Cash Flow","Period NPV", "Cumulative NPV"])
+    cumCashFlow = 0
+    cumNPV = 0
+    for year in range(0,lifetime):
+        if year == 0:   # first year?
+            annualCost = installCost
+        elif year == lifetime-1:  # last year?
+            annualCost = maintCost + disposalCost
+        else:
+            annualCost = maintCost
+            
+        netCashFlow = annualRevenue-annualCost
+        cumCashFlow += netCashFlow
+        npv = netCashFlow/((1+discountRate)**year)        
+        cumNPV += npv
+        df.iloc[year] = (year+1,annualCost,annualRevenue,netCashFlow,cumCashFlow,npv,cumNPV)
+    return df
+
+
+def GetNPV(i,R):
+    npv = 0
+    for p in range(0,len(R)):
+        npv += R[p]/((1+i)**p)
+    return npv
