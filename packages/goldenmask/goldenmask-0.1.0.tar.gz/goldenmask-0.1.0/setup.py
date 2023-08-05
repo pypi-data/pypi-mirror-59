9fc4b84cf71869660557a795810a08566fcf3f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package goldenmask/0.1.0/goldenmask-0.1.0.tar.gz

---
+++ packages/goldenmask/goldenmask-0.1.0.tar.gz/goldenmask-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['goldenmask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0']
+
+entry_points = \
+{'console_scripts': ['goldenmask = goldenmask.cli:goldenmask']}
+
+setup_kwargs = {
+    'name': 'goldenmask',
+    'version': '0.1.0',
+    'description': 'Protect your python source code with one command.',
+    'long_description': None,
+    'author': 'youngquan',
+    'author_email': 'youngquan@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
