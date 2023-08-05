7a8f86a04095c9de87c30f12ee225bca9254c852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:51 +0000

    Add package django-jalali/3.2.0/django-jalali-3.2.0.tar.gz

---
+++ packages/django-jalali/django-jalali-3.2.0.tar.gz/django-jalali-3.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-jalali',
+    version='3.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    description=("Jalali Date support for Django model and admin interface"),
+    url='http://github.com/slashmili/django-jalali',
+    download_url='http://github.com/slashmili/django-jalali/tarball/master',
+    author='Milad Rastian',
+    author_email='eslashmili@gmail.com',
+    keywords="django jalali",
+    license='Python Software Foundation License',
+    platforms='any',
+    install_requires=["jdatetime>=2.0", "django>=2.0"],
+    long_description=open('README.rst', encoding="utf-8").read()
+)
