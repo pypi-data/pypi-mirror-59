a95b7a50ad6d9d0497b9d1b35836e9ef355c5c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:48:10 +0000

    Add package connect-sdk-haxe-port/18.0.1/connect_sdk_haxe_port-18.0.1-py3-none-any.whl

---
+++ packages/connect-sdk-haxe-port/connect_sdk_haxe_port-18.0.1-py3-none-any.whl/connect/__init__.py
@@ -0,0 +1,35 @@
+from connect.autogen import connect_Base as Base
+from connect.autogen import connect_Blob as Blob
+from connect.autogen import connect_Collection as Collection
+from connect.autogen import connect_Config as Config
+from connect.autogen import connect_DateTime as DateTime
+from connect.autogen import connect_Dictionary as Dictionary
+from connect.autogen import connect_Diff as Diff
+from connect.autogen import connect_Env as Env
+from connect.autogen import connect_Flow as Flow
+from connect.autogen import connect_Inflection as Inflection
+from connect.autogen import connect_Processor as Processor
+from connect.autogen import connect_StepData as StepData
+from connect.autogen import connect_StepStorage as StepStorage
+from connect.autogen import connect_Util as Util
+
+
+SYNCREQUEST_PATH = 'connect.autogen.connect_api_impl_ApiClientImpl.syncRequest'
+
+__all__ = [
+    'SYNCREQUEST_PATH',
+    'Base',
+    'Blob',
+    'Collection',
+    'Config',
+    'DateTime',
+    'Dictionary',
+    'Diff',
+    'Env',
+    'Flow',
+    'Inflection',
+    'Processor',
+    'StepData',
+    'StepStorage',
+    'Util',
+]
