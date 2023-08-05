22e65fbb6c7148693ad10de3e7ad830103cd3d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:30 +0000

    Add package mygrid/0.2/mygrid-0.2.tar.gz

---
+++ packages/mygrid/mygrid-0.2.tar.gz/mygrid-0.2/mygrid/__init__.py
@@ -0,0 +1,4 @@
+from mygrid import grid
+from mygrid import rnp
+from mygrid import util
+from mygrid import power_flow, short_circuit
---
+++ packages/mygrid/mygrid-0.2.tar.gz/mygrid-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mygrid',
+    version='0.2',
+    author='Lucas Melo',
+    author_email='lucassmelo@dee.ufc.br',
+    packages=find_packages(),
+    data_files=[('mygrid/data', ['mygrid/data/conductors.json'])],
+    url='https://github.com/grei-ufc/mygrid',
+    description='A package to represent a electric grid \
+    topology with extensions to make power flow and short circuit \
+    analysis',
+    install_requires=['numpy',
+                      'numba',
+                      'pandas',
+                      'terminaltables',
+                      'graphviz'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Scientific/Engineering',
+        ],
+)
