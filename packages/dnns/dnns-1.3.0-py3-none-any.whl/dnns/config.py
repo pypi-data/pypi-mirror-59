7cdf38a0fddef9e9d6daeb3cb8afbd5c86671c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:50:38 +0000

    Add package dnns/1.3.0/dnns-1.3.0-py3-none-any.whl

---
+++ packages/dnns/dnns-1.3.0-py3-none-any.whl/dnns/config.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import yaml
+import argparse
+import pprint
+import time
+import os
+
+class Config:
+    def __init__(self):
+        self.parseArgs()
+        # set the default config
+        self.defaultConfig()
+        
+        self.parseConfig()
+
+    def defaultConfig(self):
+        self.config = {}
+        self.config['cpus_per_task'] = 1
+        self.config['epochs'] = 10
+        self.config['batch_size'] = 128
+        self.config['learning_rate'] = 1e-3
+        self.config['input_label'] = 'X'
+        self.config['output_label'] = 'Y'
+        self.config['model'] = 'dnn.py'
+        self.config['mixed_precision'] = True
+
+    def parseArgs(self):
+        parser = argparse.ArgumentParser(description='Machine learning with Pytorch. Change dnn.py and your YAML input file to modify training.')
+        parser.add_argument('-lr', '--local_rank', default=0, type=int,
+                            help='ranking within the nodes')
+        parser.add_argument('-i', '--input', default='input.yaml', type=str)
+        self.args = parser.parse_args()
+
+    def parseConfig(self):
+        yaml_config = yaml.safe_load(open(self.args.input, 'r'))
+        for key in yaml_config:
+            self.config[key] = yaml_config[key]
+
+    def printConfig(self):
+        pp = pprint.PrettyPrinter(indent=4)
+        print()
+        print('Here is your configuration:')
+        pp.pprint(self.config)
+        print()
+
+    def getConfig(self):
+        return self.config
+
+    def getArgs(self):
+        return self.args
+
+    def set(self, key, val):
+        self.config[key] = val
+
+        
