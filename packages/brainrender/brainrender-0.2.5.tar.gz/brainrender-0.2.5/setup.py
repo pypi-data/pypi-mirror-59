627113accdb10f54d411355c863fedb467fac20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package brainrender/0.2.5/brainrender-0.2.5.tar.gz

---
+++ packages/brainrender/brainrender-0.2.5.tar.gz/brainrender-0.2.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = [
+    "numpy",
+    "pandas",
+    "vtkplotter>=2020.0.1",
+    "vtk",
+    "allensdk",
+    "tqdm",
+    "pyyaml",
+    "scikit-image",
+    "brainio>=0.0.9",
+]
+
+setup(
+    name="brainrender",
+    version="0.2.5",
+    description="Python scripts to use Allen Brain Map data for analysis "
+                "and rendering",
+    install_requires=requirements,
+    extras_require={"nb": ["jupyter", "k3d"]},
+    python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=(
+        "Installation", "Meshes", "Metadata", "Screenshots")),
+    include_package_data=True,
+    url="https://github.com/BrancoLab/brainrender",
+    author="Federico Claudi",
+    zip_safe=False,
+)
