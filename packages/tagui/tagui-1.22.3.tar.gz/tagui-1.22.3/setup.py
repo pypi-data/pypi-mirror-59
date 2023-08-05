66fe5221ceb288b8b14c407da20144f0fcc4bd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:47 +0000

    Add package tagui/1.22.3/tagui-1.22.3.tar.gz

---
+++ packages/tagui/tagui-1.22.3.tar.gz/tagui-1.22.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='tagui',
+    version='1.22.3',
+    py_modules=['tagui'],
+    author='Ken Soh',
+    author_email='opensource@tebel.org',
+    license='Apache License 2.0',
+    url='https://github.com/tebelorg/TagUI-Python',
+    description='TagUI for Python is a Python package for RPA (robotic process automation)',
+    long_description='TagUI for Python homepage - https://github.com/tebelorg/TagUI-Python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
