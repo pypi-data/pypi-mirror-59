10f228399bafadcf758ced2827831d01ab2de0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:51 +0000

    Add package selene/2.0.0a13/selene-2.0.0a13-py3-none-any.whl

---
+++ packages/selene/selene-2.0.0a13-py3-none-any.whl/selene/support/shared/__init__.py
@@ -0,0 +1,69 @@
+# MIT License
+#
+# Copyright (c) 2015-2019 Iakiv Kramarenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from __future__ import annotations
+
+import functools
+import os
+import warnings
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from selene.core.configuration import Hooks, WaitHooks
+from selene.support.shared.browser import SharedBrowser
+from selene.support.shared.config import SharedConfig
+
+config = SharedConfig()
+
+browser = SharedBrowser(config)
+
+
+def _save_and_log_screenshot(error: TimeoutException) -> Exception:
+    path = browser.save_screenshot()
+    return TimeoutException(
+        error.msg + f'''
+Screenshot: file://{path}''',
+        error.screen,
+        error.stacktrace)
+
+
+def _save_and_log_page_source(error: TimeoutException) -> Exception:
+    filename = browser.latest_screenshot.replace('.png', '.html') if browser.latest_screenshot else None
+    path = browser.save_page_source(filename)
+    return TimeoutException(
+        error.msg + f'''
+PageSource: file://{path}''',
+        error.screen,
+        error.stacktrace)
+
+
+def _pipe(*functions):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions[::-1], lambda x: x)
+
+
+# todo: consider more flat style: Hooks(wait_failure=_save_and_log_screenshot)
+# todo: consider making screenshots configurable (turn on/off)
+config.hooks = Hooks(wait=WaitHooks(failure=_pipe(
+    _save_and_log_screenshot,
+    _save_and_log_page_source)))
