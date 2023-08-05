f85751b255f71ab1d6057e03bb03f85d6e7da70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:08 +0000

    Add package iredis/0.8.4/iredis-0.8.4-py3-none-any.whl

---
+++ packages/iredis/iredis-0.8.4-py3-none-any.whl/iredis/__init__.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from pathlib import Path
+
+__version__ = "0.8.4"
+
+
+logging.basicConfig(
+    filename=os.path.join(os.getenv("HOME"), ".iredis.log"),
+    filemode="a",
+    format="%(levelname)5s %(message)s",
+    level="DEBUG",
+)
+
+logger = logging.getLogger(__name__)
+logger.info("------ iRedis ------")
+project_path = Path(os.path.dirname(os.path.abspath(__file__))) / "data"
