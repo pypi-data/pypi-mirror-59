ce05101cead8bb187c23b5c6b2b1ff3af21db5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:59 +0000

    Add package david-distributions/0.1/david-distributions-0.1.tar.gz

---
+++ packages/david-distributions/david-distributions-0.1.tar.gz/david-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='david-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author='David Tan',
+      packages=['distributions'],
+      zip_safe=False)
