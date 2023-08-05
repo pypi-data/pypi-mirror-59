1746ccb8c772229188242e4e1474c11fe80d1c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package mypy-boto3-apigatewaymanagementapi/1.10.49.0/mypy-boto3-apigatewaymanagementapi-1.10.49.0.tar.gz

---
+++ packages/mypy-boto3-apigatewaymanagementapi/mypy-boto3-apigatewaymanagementapi-1.10.49.0.tar.gz/mypy-boto3-apigatewaymanagementapi-1.10.49.0/mypy_boto3_apigatewaymanagementapi/type_defs.py
@@ -0,0 +1,29 @@
+"""
+Main interface for apigatewaymanagementapi service type definitions.
+
+Usage::
+
+    from mypy_boto3.apigatewaymanagementapi.type_defs import IdentityTypeDef
+
+    data: IdentityTypeDef = {...}
+"""
+from __future__ import annotations
+
+from datetime import datetime
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("IdentityTypeDef", "GetConnectionResponseTypeDef")
+
+IdentityTypeDef = TypedDict("IdentityTypeDef", {"SourceIp": str, "UserAgent": str})
+
+GetConnectionResponseTypeDef = TypedDict(
+    "GetConnectionResponseTypeDef",
+    {"ConnectedAt": datetime, "Identity": IdentityTypeDef, "LastActiveAt": datetime},
+    total=False,
+)
