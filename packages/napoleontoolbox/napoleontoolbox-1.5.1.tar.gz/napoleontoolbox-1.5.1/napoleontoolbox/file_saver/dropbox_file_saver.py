8ffd213c8b7e328c85f07c1f8ed1b729c3818863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:08 +0000

    Add package napoleontoolbox/1.5.1/napoleontoolbox-1.5.1.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-1.5.1.tar.gz/napoleontoolbox-1.5.1/napoleontoolbox/file_saver/dropbox_file_saver.py
@@ -0,0 +1,92 @@
+"""Upload the contents of your Downloads folder to Dropbox.
+This is an example app for API v2.
+"""
+
+from __future__ import print_function
+
+import contextlib
+import datetime
+import os
+import time
+
+import dropbox
+
+@contextlib.contextmanager
+def stopwatch(message):
+    """Context manager to print how long a block of code took."""
+    t0 = time.time()
+    try:
+        yield
+    finally:
+        t1 = time.time()
+        print('Total elapsed time for %s: %.3f' % (message, t1 - t0))
+
+class NaPoleonDropboxConnector(object):
+    def __init__(self, drop_token):
+        self.drop_token = drop_token
+        self.dbx = dropbox.Dropbox(self.drop_token)
+
+    def list_folder(self, folder, subfolder):
+        """List a folder.
+        Return a dict mapping unicode filenames to
+        FileMetadata|FolderMetadata entries.
+        """
+        path = '/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'))
+        while '//' in path:
+            path = path.replace('//', '/')
+        path = path.rstrip('/')
+        try:
+            with stopwatch('list_folder'):
+                res = self.dbx.files_list_folder(path)
+        except dropbox.exceptions.ApiError as err:
+            print('Folder listing failed for', path, '-- assumed empty:', err)
+            return {}
+        else:
+            rv = {}
+            for entry in res.entries:
+                rv[entry.name] = entry
+            return rv
+
+    def upload(self, fullname, folder, subfolder, name, overwrite=False):
+        """Upload a file.
+        Return the request response, or None in case of error.
+        """
+        path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+        while '//' in path:
+            path = path.replace('//', '/')
+        mode = (dropbox.files.WriteMode.overwrite
+                if overwrite
+                else dropbox.files.WriteMode.add)
+        mtime = os.path.getmtime(fullname)
+        with open(fullname, 'rb') as f:
+            data = f.read()
+        with stopwatch('upload %d bytes' % len(data)):
+            try:
+                res = self.dbx.files_upload(
+                    data, path, mode,
+                    client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
+                    mute=True)
+            except dropbox.exceptions.ApiError as err:
+                print('*** API error', err)
+                return None
+        print('uploaded as', res.name.encode('utf8'))
+        return res
+
+    def download(self, folder, subfolder, name):
+        """Download a file.
+        Return the bytes of the file, or None if it doesn't exist.
+        """
+        path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+        while '//' in path:
+            path = path.replace('//', '/')
+        with stopwatch('download'):
+            try:
+                md, res = self.dbx.files_download(path)
+            except dropbox.exceptions.HttpError as err:
+                print('*** HTTP error', err)
+                return None
+        data = res.content
+        print(len(data), 'bytes; md:', md)
+        return data
+
+
