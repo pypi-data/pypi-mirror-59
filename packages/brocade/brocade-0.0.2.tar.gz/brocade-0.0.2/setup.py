5777bd32d50299bc894aee891299c19525aff88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:10 +0000

    Add package brocade/0.0.2/brocade-0.0.2.tar.gz

---
+++ packages/brocade/brocade-0.0.2.tar.gz/brocade-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='brocade',
+  version='0.0.2',
+  author='Unknown',
+  author_email='unknown@example.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="nylon is a helper library",
+  packages=['brocade'],
+  scripts=[]
+)
+
