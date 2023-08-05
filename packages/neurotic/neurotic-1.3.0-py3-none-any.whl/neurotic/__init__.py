28c04b251464956413c4de7aaffb699ddff60bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package neurotic/1.3.0/neurotic-1.3.0-py3-none-any.whl

---
+++ packages/neurotic/neurotic-1.3.0-py3-none-any.whl/neurotic/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Curate, visualize, annotate, and share your behavioral ephys data using Python
+"""
+
+from .version import version as __version__
+from .version import git_revision as __git_revision__
+
+from .datasets import *
+from .gui import *
+from .scripts import *
