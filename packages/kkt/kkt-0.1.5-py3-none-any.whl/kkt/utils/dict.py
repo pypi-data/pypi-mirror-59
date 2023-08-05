2ed37ce13c96041818018476ae7e4c38c63d23b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:16 +0000

    Add package kkt/0.1.5/kkt-0.1.5-py3-none-any.whl

---
+++ packages/kkt/kkt-0.1.5-py3-none-any.whl/kkt/utils/dict.py
@@ -0,0 +1,14 @@
+def merge(source, destination):
+    """
+    Reference: https://stackoverflow.com/questions/20656135/python-deep-merge-dictionary-data
+    """
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            if key not in destination:
+                destination[key] = value
+
+        return destination
