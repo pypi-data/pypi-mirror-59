3f4bb748579a08ff9071f54294e8b4990cf732ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package SQSnobFit/0.4.0/SQSnobFit-0.4.0.tar.gz

---
+++ packages/SQSnobFit/SQSnobFit-0.4.0.tar.gz/SQSnobFit-0.4.0/python/SQSnobFit/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import logging
+
+log = logging.getLogger('SKQ.SnobFit')
+
+log.info("""
+------------------------------------------------------------------------
+W. Huyer and A. Neumaier, "Snobfit - Stable Noisy Optimization by Branch and Fit",
+ ACM Trans. Math. Software 35 (2008), Article 9.
+Software available at www.mat.univie.ac.at/~neum/software/snobfit
+------------------------------------------------------------------------""")
+
+__all__ = ['minimize', 'optset', 'log', 'snobfit']
+
+from ._snobfit import minimize, snobfit
+from ._optset import optset
