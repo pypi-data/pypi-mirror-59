0dfca310d8179c1e74c3f7a768cd4639b64680a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:40 +0000

    Add package altoshift/0.1.66/altoshift-0.1.66.tar.gz

---
+++ packages/altoshift/altoshift-0.1.66.tar.gz/altoshift-0.1.66/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+# Used in pypi.org as the README description of your package
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='altoshift', 
+        version='0.1.66',
+        description='learning today make you better tomorrow',
+        author='eko',
+        author_email='eko@altoshift.com',
+        license="MIT",
+        url="https://altoshift.com",
+        packages=['altoshift'],
+        #scripts=['scripts/some_script.py'],
+        #python_requires='>=3',
+        requires=['requests(>=1.2.3)','pandas','xlrd','tqdm'],
+        install_requires=[
+            'pandas',
+            'xlrd',
+            'requests',
+	    'tqdm'
+        ],
+        long_description=long_description
+)
