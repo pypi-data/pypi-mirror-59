6271eb0ead2a6a94e2b1bc0d6631293f9f406a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:43 +0000

    Add package yourmum/0.1/yourmum-0.1.tar.gz

---
+++ packages/yourmum/yourmum-0.1.tar.gz/yourmum-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='yourmum',
+      version='0.1',
+      description='Probably the funniest your mum jokes! Customize jokes to use \'mum\', \'mom\', \'momma\' or any other qualifier.',
+      url='https://github.com/bottines/yourmum.git',
+      author='Stéphane Bottine',
+      author_email='stephane.bottine@gmail.com',
+      license=None,
+      packages=['yourmum'],
+      zip_safe=False)
