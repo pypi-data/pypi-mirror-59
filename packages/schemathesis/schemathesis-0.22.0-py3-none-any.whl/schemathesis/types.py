2be56627a1a1f6d0b9a517cb01256f18b69af1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package schemathesis/0.22.0/schemathesis-0.22.0-py3-none-any.whl

---
+++ packages/schemathesis/schemathesis-0.22.0-py3-none-any.whl/schemathesis/types.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Any, Dict, List, NewType, Set, Tuple, Union
+
+Schema = NewType("Schema", Dict[str, Any])  # pragma: no mutate
+PathLike = Union[Path, str]  # pragma: no mutate
+
+Query = Dict[str, Any]  # pragma: no mutate
+Body = Union[Dict[str, Any], bytes]  # pragma: no mutate
+PathParameters = Dict[str, Any]  # pragma: no mutate
+Headers = Dict[str, Any]  # pragma: no mutate
+Cookies = Dict[str, Any]  # pragma: no mutate
+FormData = Dict[str, Any]  # pragma: no mutate
+
+# A filter for endpoint / method
+Filter = Union[str, List[str], Tuple[str], Set[str], object]  # pragma: no mutate
