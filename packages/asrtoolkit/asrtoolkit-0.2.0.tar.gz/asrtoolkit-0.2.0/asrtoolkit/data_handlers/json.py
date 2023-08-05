3af49738cfdef46b31c654c5f4b9f1643da86700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package asrtoolkit/0.2.0/asrtoolkit-0.2.0.tar.gz

---
+++ packages/asrtoolkit/asrtoolkit-0.2.0.tar.gz/asrtoolkit-0.2.0/asrtoolkit/data_handlers/json.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+"""
+Module for reading/writing JSON files
+
+This expects a segment from class derived in convert_text
+"""
+
+import json
+import logging
+
+from asrtoolkit.data_structures.segment import segment
+
+LOGGER = logging.getLogger(__name__)
+separator = ",\n"
+
+
+def header():
+    " Returns empty header "
+    return '{\n"segments":['
+
+
+def footer():
+    " Returns empty footer "
+    return "]}\n"
+
+
+def format_segment(seg):
+    """
+    Formats a segment assuming it's an instance of class segment with elements
+    filename, channel, speaker, start and stop times, label, and text
+
+    :param: seg: segment object
+    :return: dict: key/val pairs contain 'segment'-level information
+      values of output segment-dict are values of corresponding input segment attributes
+
+        output_dict['startTimeSec'] = segment.start          (converse of parse_segment)
+
+    the segment dict structure generated is the output of all microservices and input to most
+      each segment-dict contains a fragment of text (+ additional information)
+    """
+    output_dict = {}
+    output_dict["speakerInfo"] = seg.speaker
+    output_dict["startTimeSec"] = float(seg.start)
+    output_dict["endTimeSec"] = float(seg.stop)
+    output_dict["genderInfo"] = {
+        "gender": seg.label.split(",")[-1].replace(">", "")
+    }
+    output_dict["transcript"] = seg.text
+    output_dict["confidence"] = seg.confidence
+
+    if len(seg.formatted_text) > 0:
+        output_dict["formatted_transcript"] = seg.formatted_text
+
+    return json.dumps(output_dict, ensure_ascii=True)
+
+
+def parse_segment(input_seg):
+    """
+    Creates a segment object from an input GreenKey segment
+    :param: input_seg: dict  (segment-level dict: input_data['segments'][i]['segment'])
+        -> dict with keys 'channel', 'startTimeSec' etc mapping to segment object attributes
+
+    :return: segment object; attribute values are set to those of corresponding segment-dict keys
+
+        segment.start = segment_dict['startTimeSec']         (reverse mapping from format_segment)
+    """
+    extracted_dict = {}
+
+    def assign_if_present(value,
+                          dict_key=None,
+                          interior_key=None,
+                          proc_val=lambda val: val):
+        """
+        :param value: type?
+        :param dict_key:
+        :param interior_key:
+        :param proc_val:
+        :return: type?
+
+        Assigns value to extracted_dict object if present in input_seg
+        """
+        dict_key = value if dict_key is None else dict_key
+
+        if value in input_seg and interior_key and interior_key in input_seg[
+                value]:
+            extracted_dict[dict_key] = proc_val(input_seg[value][interior_key])
+        elif value in input_seg and not interior_key:
+            extracted_dict[dict_key] = proc_val(input_seg[value])
+
+    seg = None
+    try:
+        assign_if_present("channel")
+        assign_if_present("startTimeSec", "start")
+        assign_if_present("stopTimeSec", "stop")
+        assign_if_present("endTimeSec", "stop")
+        assign_if_present("transcript", "text")
+        assign_if_present("corrected_transcript", "text")
+        assign_if_present("formatted_transcript", "formatted_text")
+        assign_if_present("punctuated_transcript", "formatted_text")
+        assign_if_present("speakerInfo", "speaker", "ID")
+        assign_if_present("genderInfo", "label", "gender",
+                          lambda gender: "<o,f0,{:}>".format(gender))
+        assign_if_present("confidence", "confidence")
+
+        seg = segment(extracted_dict)
+
+    except Exception as exc:
+        LOGGER.exception(exc)
+
+    return seg if seg and seg.validate() else None
+
+
+def read_in_memory(input_data):
+    """
+    Reads input json objects
+
+    :param: input_data: dict with key 'segments'
+      input_data['segments']: List[Dict]; each dict has key 'segment' with a dict as the value (segment_dict)
+      - segment_dicts contain key/val pairs that map to `segment` object attributes
+      - labels of mapped key-attribute pairs may differ: segment['startTimeSec'] -> segment.start
+
+    :return: list of segment objects
+      applies parse_segment function to each dict in input_data['segments']
+       - func creates a `segment` object for each segment_dict, mapping corresponding attributes
+
+    input_data['segments'][i]['segment'] --> mapped to ith segment object (with 'start', 'stop' etc'
+    """
+    segments = [
+        _ for _ in map(parse_segment, input_data["segments"]) if _ is not None
+    ]
+    return segments
+
+
+def read_file(file_name):
+    """
+    Reads a JSON file, skipping any bad segments
+    """
+    with open(file_name, encoding="utf-8") as f:
+        input_json = json.load(f)
+        segments = read_in_memory(input_json)
+    return segments
---
+++ packages/asrtoolkit/asrtoolkit-0.2.0.tar.gz/asrtoolkit-0.2.0/asrtoolkit/wer.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+"""
+Python function for computing word error rates metric for Automatic Speech Recognition files
+"""
+
+import argparse
+import re
+
+import editdistance
+from asrtoolkit.clean_formatting import clean_up
+from asrtoolkit.data_structures.time_aligned_text import time_aligned_text
+from asrtoolkit.file_utils.script_input_validation import assign_if_valid
+
+# defines global regex for tagged noises and silence
+re_tagged_nonspeech = re.compile(r"[\[<][A-Za-z #]*[\]>]")
+
+# defines global regex to remove these nsns
+nonsilence_noises = [
+    "noise",
+    "um",
+    "ah",
+    "er",
+    "umm",
+    "uh",
+    "mm",
+    "mn",
+    "mhm",
+    "mnh",
+    "huh",
+    "hmm",
+]
+re_nonsilence_noises = re.compile(r"\b({})\b".format(
+    "|".join(nonsilence_noises)))
+
+
+def remove_nonsilence_noises(input_text):
+    """
+    Removes nonsilence noises from a transcript
+    """
+    return re.sub(re_nonsilence_noises, "", input_text)
+
+
+def wer(ref, hyp, remove_nsns=False):
+    """
+    Calculate word error rate between two string or time_aligned_text objects
+    >>> wer("this is a cat", "this is a dog")
+    25.0
+    """
+
+    # accept time_aligned_text objects too
+    if type(ref) == time_aligned_text:
+        ref = ref.text()
+
+    if type(hyp) == time_aligned_text:
+        hyp = hyp.text()
+
+    # remove tagged noises and other nonspeech events
+    ref = re.sub(re_tagged_nonspeech, " ", ref)
+    hyp = re.sub(re_tagged_nonspeech, " ", hyp)
+
+    # optionally, remove non silence noises
+    if remove_nsns:
+        ref = remove_nonsilence_noises(ref)
+        hyp = remove_nonsilence_noises(hyp)
+
+    # clean punctuation, etc.
+    ref = clean_up(ref)
+    hyp = clean_up(hyp)
+
+    # calculate WER
+    return (100 * editdistance.eval(ref.split(" "), hyp.split(" ")) /
+            max(1, len(ref.split(" "))))
+
+
+def cer(ref, hyp, remove_nsns=False):
+    """
+    Calculate character error rate between two strings or time_aligned_text objects
+    >>> cer("this cat", "this bad")
+    25.0
+    """
+
+    # accept time_aligned_text objects too
+    if type(ref) == time_aligned_text:
+        ref = ref.text()
+
+    if type(hyp) == time_aligned_text:
+        hyp = hyp.text()
+
+    if remove_nsns:
+        ref = remove_nonsilence_noises(ref)
+        hyp = remove_nonsilence_noises(hyp)
+
+    ref = clean_up(ref)
+    hyp = clean_up(hyp)
+
+    # calculate per line CER
+    return 100 * editdistance.eval(ref, hyp) / max(1, len(ref))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=
+        "Compares a reference and transcript file and calculates word error rate (WER) between these two files"
+    )
+    parser.add_argument(
+        "reference_file",
+        metavar="reference_file",
+        type=str,
+        help='reference "truth" file',
+    )
+    parser.add_argument(
+        "transcript_file",
+        metavar="transcript_file",
+        type=str,
+        help="transcript possibly containing errors",
+    )
+    parser.add_argument(
+        "--char-level",
+        help="calculate character error rate instead of word error rate",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--ignore-nsns",
+        help="ignore non silence noises like um, uh, etc.",
+        action="store_true",
+    )
+
+    # parse arguments
+    args = parser.parse_args()
+
+    # read files from arguments
+    ref = assign_if_valid(args.reference_file)
+    hyp = assign_if_valid(args.transcript_file)
+
+    if ref is None or hyp is None:
+        print(
+            "Error with an input file. Please check all files exist and are accepted by ASRToolkit"
+        )
+    elif args.char_level:
+        print("CER: {:5.3f}%".format(cer(ref, hyp, args.ignore_nsns)))
+    else:
+        print("WER: {:5.3f}%".format(wer(ref, hyp, args.ignore_nsns)))
+
+
+if __name__ == "__main__":
+    main()
