ce2b9d98256a1885a5d62f622097df945c70d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:23 +0000

    Add package lil-pwny/1.0.0/lil-pwny-1.0.0.tar.gz

---
+++ packages/lil-pwny/lil-pwny-1.0.0.tar.gz/lil-pwny-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='lil-pwny',
+    version='1.0.0',
+    packages=['src'],
+    url='https://github.com/PaperMtn/little-pwny',
+    license='GPL-3.0',
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Topic :: Security',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    author='PaperMtn',
+    author_email='ab10adg@gmail.com',
+    long_description='A multiprocessing approach to auditing Active Directory passwords against HIBP using Python.',
+    description='Auditing Active Directory Passwords ',
+    entry_points={
+        'console_scripts': ['lil-pwny=src:main']
+    }
+)
