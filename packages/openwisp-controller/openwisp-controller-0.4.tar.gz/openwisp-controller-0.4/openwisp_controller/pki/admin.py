297439511c7c6555e8b6ab5c7f307aa3e30d436a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package openwisp-controller/0.4/openwisp-controller-0.4.tar.gz

---
+++ packages/openwisp-controller/openwisp-controller-0.4.tar.gz/openwisp-controller-0.4/openwisp_controller/pki/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django_x509.base.admin import AbstractCaAdmin, AbstractCertAdmin
+from reversion.admin import VersionAdmin
+
+from openwisp_users.multitenancy import MultitenantOrgFilter
+
+from ..admin import MultitenantAdminMixin
+from .models import Ca, Cert
+
+
+class CaAdmin(MultitenantAdminMixin, VersionAdmin, AbstractCaAdmin):
+    pass
+
+
+CaAdmin.fields.insert(2, 'organization')
+CaAdmin.list_filter.insert(0, ('organization', MultitenantOrgFilter))
+CaAdmin.list_display.insert(1, 'organization')
+
+
+class CertAdmin(MultitenantAdminMixin, VersionAdmin, AbstractCertAdmin):
+    multitenant_shared_relations = ('ca',)
+
+
+CertAdmin.fields.insert(2, 'organization')
+CertAdmin.list_filter.insert(0, ('organization', MultitenantOrgFilter))
+CertAdmin.list_filter.remove('ca')
+CertAdmin.list_display.insert(1, 'organization')
+
+
+admin.site.register(Ca, CaAdmin)
+admin.site.register(Cert, CertAdmin)
