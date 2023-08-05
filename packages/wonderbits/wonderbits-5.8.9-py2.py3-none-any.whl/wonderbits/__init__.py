bd924a4322e9a53a6253b7585452291c30886453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package wonderbits/5.8.9/wonderbits-5.8.9-py2.py3-none-any.whl

---
+++ packages/wonderbits/wonderbits-5.8.9-py2.py3-none-any.whl/wonderbits/__init__.py
@@ -0,0 +1,13 @@
+from .core import *
+from .Tool import wb_tool
+from .event import Event
+# from .cli import cli
+
+# print(__name__)
+# if __name__ == "__main__":
+#     cli()
+# elif __name__ == "wonderbits":
+#     # print('-----------------WonderBits---------------------')
+#     pass
+
+# del MyCore
