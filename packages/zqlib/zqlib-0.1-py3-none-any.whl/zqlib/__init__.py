badb4fba135752a993d1f9a6eec17caa34ec489e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:53:02 +0000

    Add package zqlib/0.1/zqlib-0.1-py3-none-any.whl

---
+++ packages/zqlib/zqlib-0.1-py3-none-any.whl/zqlib/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 qiang.zhou <qiang.zhou@Macbook>
+#
+# Distributed under terms of the MIT license.
+
+
+from zqlib.functions import imgs2vid
