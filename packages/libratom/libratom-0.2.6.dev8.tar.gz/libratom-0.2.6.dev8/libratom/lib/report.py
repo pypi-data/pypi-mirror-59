0edf03847d4892fa77d386ce79f884e725c1eb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:51 +0000

    Add package libratom/0.2.6.dev8/libratom-0.2.6.dev8.tar.gz

---
+++ packages/libratom/libratom-0.2.6.dev8.tar.gz/libratom-0.2.6.dev8/libratom/lib/report.py
@@ -0,0 +1,125 @@
+# pylint: disable=broad-except,protected-access
+"""
+Set of utility functions to generate job related reports
+"""
+
+import hashlib
+import logging
+import multiprocessing
+import os
+from functools import partial
+from pathlib import Path
+from typing import Callable, Dict, Iterable, Optional, Tuple
+
+from sqlalchemy.orm.session import Session
+
+import libratom
+from libratom.lib.concurrency import imap_job, worker_init
+from libratom.lib.core import open_mail_archive
+from libratom.models import Configuration, FileReport
+
+logger = logging.getLogger(__name__)
+
+
+@imap_job
+def get_file_info(path: Path) -> Tuple[Dict, Optional[str]]:
+    """
+    For a given file path, returns the size, md5 and sha256 checksums
+    """
+
+    path_str, name = str(path), path.name
+    res = {"path": path_str, "name": name}
+
+    try:
+        size = os.stat(path_str).st_size
+
+        md5 = hashlib.md5()
+        sha256 = hashlib.sha256()
+
+        # First we read the file one block at a time and update digests
+        with open(path_str, "rb") as f:
+            for block in iter(partial(f.read, 128), b""):
+                md5.update(block)
+                sha256.update(block)
+
+        md5, sha256 = md5.hexdigest(), sha256.hexdigest()
+
+        res.update({"size": size, "md5": md5, "sha256": sha256})
+
+        # Then we try to get a message count
+        try:
+            with open_mail_archive(path) as archive:
+                res["msg_count"] = archive.message_count
+
+        except Exception as exc:
+            res["error"] = str(exc)
+
+    except Exception as exc:
+        return res, str(exc)
+
+    return res, None
+
+
+def scan_files(
+    files: Iterable[Path],
+    session: Session,
+    jobs=None,
+    progress_callback: Callable = None,
+) -> int:
+    """
+    Extracts information from a list of email files and stores it in a database via an ORM session object
+    """
+
+    # Default progress callback to no-op
+    update_progress = progress_callback or (lambda *_, **__: None)
+
+    with multiprocessing.Pool(processes=jobs, initializer=worker_init) as pool:
+
+        logger.debug(f"Starting pool with {pool._processes} processes")
+
+        try:
+            for values, exc in pool.imap(
+                get_file_info, ({"path": file} for file in files), chunksize=1
+            ):
+                if not exc:
+                    # Make a new FileReport object with the results
+                    session.add(FileReport(**values))
+                else:
+                    logger.info(
+                        f"Unable to retrieve file information for {values['path']}, error: {exc}"
+                    )
+
+                update_progress()
+
+        except KeyboardInterrupt:
+            logger.warning("Aborting")
+
+            # Politely terminate workers
+            pool.terminate()
+            pool.join()
+
+            return 1
+
+    return 0
+
+
+def store_configuration_in_db(
+    session: Session, spacy_model: str, spacy_model_version: int
+) -> None:
+    """
+    Store configuration / environment information in the database output during a ratom command execution
+    """
+
+    configuration = {
+        "cpu_count": multiprocessing.cpu_count(),
+        "libratom_version": libratom.__version__,
+        "spacy_model_name": spacy_model,
+        "spacy_model_version": spacy_model_version,
+    }
+
+    session.add_all(
+        [
+            Configuration(name=name, value=str(value))
+            for name, value in configuration.items()
+        ]
+    )
