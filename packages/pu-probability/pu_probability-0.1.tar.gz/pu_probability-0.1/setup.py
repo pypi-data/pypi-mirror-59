1f205604c08dff3f7edc9b9ac0d1e7df9585b0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:48 +0000

    Add package pu-probability/0.1/pu_probability-0.1.tar.gz

---
+++ packages/pu-probability/pu_probability-0.1.tar.gz/pu_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pu_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pu_probability'],
+      author='Priit Ulmas',
+      author_email='myemail@something.com',
+      zip_safe=False)
