9671704c4c5ed6fcdd7640bec8391e3d37439e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:56 +0000

    Add package ScratchABit/0.0/ScratchABit-0.0.tar.gz

---
+++ packages/ScratchABit/ScratchABit-0.0.tar.gz/ScratchABit-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="ScratchABit",
+      version="0.0",
+      description="""Pure-Python interactive disassembler""",
+      url='https://github.com/pfalcon/ScratchABit',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='GPL3',
+      py_modules=['ScratchABit'])
