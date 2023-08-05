9fecd32399b8ca65abe0034161db814a3244872e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:23 +0000

    Add package libratom/0.2.6.dev7/libratom-0.2.6.dev7.tar.gz

---
+++ packages/libratom/libratom-0.2.6.dev7.tar.gz/libratom-0.2.6.dev7/libratom/lib/database.py
@@ -0,0 +1,50 @@
+# pylint: disable=broad-except
+"""
+Database related utilities
+"""
+import logging
+from contextlib import contextmanager
+from typing import ContextManager
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+logger = logging.getLogger(__name__)
+
+Base = declarative_base()
+
+
+def db_init(db_file: str) -> sessionmaker:
+    """
+    Initializes the database and returns a session factory
+    """
+
+    logger.info(f"Creating database file: {db_file}")
+    engine = create_engine(f"sqlite:///{db_file}")
+    Base.metadata.create_all(engine)
+
+    return sessionmaker(bind=engine)
+
+
+@contextmanager
+def db_session(session_factory: sessionmaker) -> ContextManager[Session]:
+    """
+    Database session context manager
+    """
+
+    session = session_factory()
+
+    try:
+        yield session
+        if session.new or session.dirty or session.deleted:
+            session.commit()
+
+    except SQLAlchemyError as exc:
+        logger.exception(exc)
+        session.rollback()
+
+    finally:
+        session.close()
