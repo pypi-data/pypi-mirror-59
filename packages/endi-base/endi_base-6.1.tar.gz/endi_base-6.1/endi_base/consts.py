92d2984c3f8fd293b23efc452e63bdb03bffdc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:50:09 +0000

    Add package endi-base/6.1/endi_base-6.1.tar.gz

---
+++ packages/endi-base/endi_base-6.1.tar.gz/endi_base-6.1/endi_base/consts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+SEX_OPTIONS = (
+    ('', '', ),
+    ('M', 'Homme', ),
+    ('F', 'Femme', ),
+)
+
+
+CIVILITE_OPTIONS = (
+    ('', u'Non renseigné', ),
+    ('Monsieur', u'Monsieur',),
+    ('Madame', u'Madame',),
+)
