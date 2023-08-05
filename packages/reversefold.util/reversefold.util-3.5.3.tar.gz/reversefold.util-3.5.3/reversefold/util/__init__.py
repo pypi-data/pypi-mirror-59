aaf3acc3e9b3c3d1a7eb1dbe7be9de4c51408cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package reversefold.util/3.5.3/reversefold.util-3.5.3.tar.gz

---
+++ packages/reversefold.util/reversefold.util-3.5.3.tar.gz/reversefold.util-3.5.3/reversefold/util/__init__.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+import logging
+import os
+import sys
+
+
+LOG = logging.getLogger(__name__)
+
+
+RATE_LIMIT_SENTINEL = object()
+
+
+def rate_limit_gen(gen, period, limit):
+    start = datetime.now()
+    # start at -1 since we pre-increment below and we actually want to allow up to `limit` values to be generated
+    # within `period` before we yield RATE_LIMIT_SENTINEL.
+    count = -1
+    for value in gen:
+        count += 1
+        if datetime.now() - start < period:
+            if count == limit:
+                yield RATE_LIMIT_SENTINEL
+                continue
+            elif count > limit:
+                continue
+        else:
+            start = datetime.now()
+            count = 0
+        yield value
+
+
+def chunked(seq, chunk_size):
+    """Take an input sequence (any iterable, including generators) and yield it in chunks of the given size.
+        [i for i in chunked(xrange(1, 10), 2)]
+        [[1, 2], [3, 4], [5, 6], [7, 8], [9]]
+    """
+    chunk = []
+    i = 0
+    for val in seq:
+        chunk.append(val)
+        i += 1
+        if i == chunk_size:
+            yield chunk
+            chunk = []
+            i = 0
+    if i > 0:
+        yield chunk
+
+
+def force_stdout_line_buffer():
+    if hasattr(sys.stdout, "fileno"):
+        # Force stdout to be line-buffered
+        sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 1)
+    else:
+        LOG.warning("sys.stdout has no fileno attribute, it cannot be line-buffered")
---
+++ packages/reversefold.util/reversefold.util-3.5.3.tar.gz/reversefold.util-3.5.3/reversefold/util/stream.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""Streams
+
+Usage:
+    stream.py -h | --help
+    stream.py [--tail] [--byte [--bufsize=<bufsize>]] <filename>
+
+Options:
+    -h --help           Help.
+    --tail              Only stream lines from the end of the file.
+    --byte              Don't buffer, output new bytes as soon as they are
+                        read.
+    --bufsize=<bufsize> The number of bytes that will be attempted to be read
+                        at a time. [Default: 1024]
+"""
+from __future__ import print_function
+import os
+import sys
+
+from docopt import docopt
+
+from reversefold.util import follow
+
+
+def main():
+    args = docopt(__doc__)
+    if args["--byte"]:
+        if hasattr(sys.stdout, "fileno"):
+            # Force stdout to be un-buffered
+            sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 0)
+
+        with follow.Follower(
+            args["<filename>"], args["--tail"], int(args["--bufsize"])
+        ) as follower:
+            try:
+                for data in follower:
+                    sys.stdout.write(data)
+                    sys.stdout.flush()
+            except KeyboardInterrupt:
+                follower.finish = True
+                for data in follower:
+                    sys.stdout.write(data)
+                sys.stdout.flush()
+                raise
+
+    else:
+        with follow.FilenameLineFollower(
+            args["<filename>"], args["--tail"]
+        ) as follower:
+            try:
+                for data in follower:
+                    print(data)
+            except KeyboardInterrupt:
+                follower.finish = True
+                for data in follower:
+                    print(data)
+                raise
+
+
+if __name__ == "__main__":
+    main()
