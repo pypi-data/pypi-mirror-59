bf99d6554be5b665bc2228c35c0badfb7dd2590f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package SentenceSentiment/0.3/SentenceSentiment-0.3.tar.gz

---
+++ packages/SentenceSentiment/SentenceSentiment-0.3.tar.gz/SentenceSentiment-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="SentenceSentiment",
+version="0.3",
+description="This is code  with Sentence Sentiment package",
+long_description = "This is a very very long description",
+author="Salma",
+packages=['SentenceSentiment'],
+install_requires=[])
