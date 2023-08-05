e98c1a02cc6197c284e49a2ad42b69ff38b7e291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:34 +0000

    Add package pyatv/0.4.0a11/pyatv-0.4.0a11-py3-none-any.whl

---
+++ packages/pyatv/pyatv-0.4.0a11-py3-none-any.whl/pyatv/exceptions.py
@@ -0,0 +1,61 @@
+"""Local exceptions used by library."""
+
+
+class NoServiceError(Exception):
+    """Thrown when connecting to a device with no usable service."""
+
+
+class UnsupportedProtocolError(Exception):
+    """Thrown when an unsupported protocol was requested."""
+
+
+class AuthenticationError(Exception):
+    """Thrown when login fails."""
+
+
+class NotSupportedError(NotImplementedError):
+    """Thrown when trying to perform an action that is not supported."""
+
+
+class InvalidDmapDataError(Exception):
+    """Thrown when invalid DMAP data is parsed."""
+
+
+class UnknownServerResponseError(Exception):
+    """Thrown when somethins unknown is send back from the Apple TV."""
+
+
+class UnknownMediaKind(Exception):
+    """Thrown when an unknown media kind is found."""
+
+
+class UnknownPlayState(Exception):
+    """Thrown when an unknown play state is found."""
+
+
+class NoAsyncListenerError(Exception):
+    """Thrown when starting AsyncUpdater with no listener."""
+
+
+class AsyncUpdaterRunningError(Exception):
+    """Thrown when performing an invalid action in AsyncUpdater.."""
+
+
+class NoCredentialsError(Exception):
+    """Thrown if performing an action before initialize is called."""
+
+
+class DeviceAuthenticationError(Exception):
+    """Thrown when device authentication fails."""
+
+
+class DeviceIdMissingError(Exception):
+    """Thrown when device id is missing."""
+
+
+class BackOffError(Exception):
+    """Thrown when device mandates a backoff period."""
+
+
+class PlaybackError(Exception):
+    """Thrown when media playback failed."""
