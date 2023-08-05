1355897fd32a8e873d4351ce133f99fe7b4244a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:50:09 +0000

    Add package endi-base/6.1/endi_base-6.1-py3-none-any.whl

---
+++ packages/endi-base/endi_base-6.1-py3-none-any.whl/endi_base/exception.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+class UndeliveredMail(Exception):
+    """
+    A custom class for undelivered emails
+    """
+    pass
+
+
+class MailAlreadySent(Exception):
+    """
+    A custom class for mail that were already sent
+    """
+    pass
---
+++ packages/endi-base/endi_base-6.1-py3-none-any.whl/endi_base/utils/export.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def format_boolean(value):
+    """
+    Format a boolean value
+    """
+    return value and "Y" or "N"
