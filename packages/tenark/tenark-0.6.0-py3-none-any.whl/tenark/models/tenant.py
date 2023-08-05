48c1b1a1a752ab0ce3a00860930d9f01c7528f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package tenark/0.6.0/tenark-0.6.0-py3-none-any.whl

---
+++ packages/tenark/tenark-0.6.0-py3-none-any.whl/tenark/models/tenant.py
@@ -0,0 +1,36 @@
+import time
+import unicodedata
+from typing import Mapping
+from ..common import TenantLocationError, TenantCreationError
+
+
+class Tenant:
+    def __init__(self, **attributes):
+        self.id = attributes['id']
+        self.created_at = attributes.get('created_at', int(time.time()))
+        self.updated_at = attributes.get('updated_at', self.created_at)
+        self.name = attributes['name']
+        self.email = attributes.get('email', '')
+        self.active = attributes.get('active', True)
+        self.slug = self._normalize_slug(attributes.get('slug', self.name))
+        self.attributes = attributes.get('attributes', {})
+        self.zone = attributes.get('zone', '')
+        self.data: Mapping[str, str] = attributes.get('data', {
+            'memory': ""
+        })
+
+    def location(self, type: str = 'memory') -> str:
+        if type not in self.data:
+            raise TenantLocationError(
+                f"No location found for '{type}' type "
+                f"in tenant '{self.name}'.")
+        return self.data[type]
+
+    @staticmethod
+    def _normalize_slug(slug: str) -> str:
+        stripped_slug = slug.strip().replace(" ", "_").lower()
+        normalized_slug = unicodedata.normalize(
+            'NFKD', stripped_slug).encode('ascii', 'ignore').decode('utf-8')
+        if not normalized_slug:
+            raise TenantCreationError("Invalid tenant 'slug' name.")
+        return normalized_slug
