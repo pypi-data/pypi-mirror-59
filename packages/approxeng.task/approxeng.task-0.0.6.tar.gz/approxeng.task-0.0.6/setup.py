4d43b7ef903d4b09e1a0e23f403498b34adaffe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:36 +0000

    Add package approxeng.task/0.0.6/approxeng.task-0.0.6.tar.gz

---
+++ packages/approxeng.task/approxeng.task-0.0.6.tar.gz/approxeng.task-0.0.6/setup.py
@@ -0,0 +1,18 @@
+__author__ = 'tom'
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='approxeng.task',
+    version='0.0.6',
+    description='Simple Python task framework for robots',
+    classifiers=['Programming Language :: Python :: 3.5'],
+    url='https://github.com/ApproxEng/approxeng.task/',
+    author='Tom Oinn',
+    author_email='tomoinn@gmail.com',
+    license='ASL2.0',
+    packages=find_namespace_packages(),
+    install_requires=['pyyaml==5.3'],
+    include_package_data=True,
+    dependency_links=[],
+    zip_safe=False)
