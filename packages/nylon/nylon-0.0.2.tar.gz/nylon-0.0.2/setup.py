a52af90feb2a62b04ed443520ffd4692d1e01811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:50 +0000

    Add package nylon/0.0.2/nylon-0.0.2.tar.gz

---
+++ packages/nylon/nylon-0.0.2.tar.gz/nylon-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='nylon',
+  version='0.0.2',
+  author='Unknown',
+  author_email='unknown@example.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="nylon is a helper library",
+  packages=['nylon'],
+  scripts=[]
+)
+
