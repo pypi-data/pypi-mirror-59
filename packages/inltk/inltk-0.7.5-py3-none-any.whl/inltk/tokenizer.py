b7beb4e7eac862a5d2c6601826ef3c04b135192d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package inltk/0.7.5/inltk-0.7.5-py3-none-any.whl

---
+++ packages/inltk/inltk-0.7.5-py3-none-any.whl/inltk/tokenizer.py
@@ -0,0 +1,93 @@
+from fastai.text import *
+import sentencepiece as spm
+from pathlib import Path
+
+path = Path(__file__).parent
+
+
+class LanguageTokenizer(BaseTokenizer):
+    def __init__(self, lang: str):
+        self.lang = lang
+        self.sp = spm.SentencePieceProcessor()
+        model_path = path/f'models/{lang}/tokenizer.model'
+        self.sp.Load(str(model_path))
+
+    def tokenizer(self, t: str) -> List[str]:
+        return self.sp.EncodeAsPieces(t)
+
+    def numericalize(self, t: str) -> List[int]:
+        return self.sp.EncodeAsIds(t)
+
+    def textify(self, ids: List[int]) -> str:
+        return (''.join([self.sp.IdToPiece(id).replace('▁', ' ') for id in ids])).strip()
+
+    def remove_foreign_tokens(self, t: str):
+        local_pieces = []
+        for i in self.sp.EncodeAsIds(t):
+            local_pieces.append(self.sp.IdToPiece(i))
+        return local_pieces
+
+
+class AllLanguageTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class SanskritTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class BengaliTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class GujaratiTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class HindiTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class KannadaTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class MalyalamTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class MarathiTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class NepaliTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class OriyaTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class PanjabiTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class TamilTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
+
+
+class UrduTokenizer(LanguageTokenizer):
+    def __init__(self, lang: str):
+        LanguageTokenizer.__init__(self, lang)
