95545aec89782c45b9f8ffa7dbe654dcf11ec9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:57 +0000

    Add package featuresfinder/1.2/featuresfinder-1.2.tar.gz

---
+++ packages/featuresfinder/featuresfinder-1.2.tar.gz/featuresfinder-1.2/setup.py
@@ -0,0 +1,32 @@
+
+"""Setup for the chocobo package."""
+
+import setuptools
+
+
+
+
+setuptools.setup(
+    author="Sachin Limbachiya",
+    author_email="sachinlimbachiya10@gmail.com",
+    name='featuresfinder',
+    license="MIT",
+    description='featuresfinder is a python package for feature extration using nearly 6 different algorithsm.',
+    version='v1.2',
+   
+    url='https://github.com/sachinL/featuresfinder',
+    packages=setuptools.find_packages(),
+    python_requires=">=3",
+    install_requires=['sklearn','lightgbm','numpy'],
+    classifiers=[
+       
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
