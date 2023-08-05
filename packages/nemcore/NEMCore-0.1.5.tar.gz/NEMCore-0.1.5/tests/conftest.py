e9d63c7f4098f1dd4e421dbb380a40b30e51c79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:05 +0000

    Add package nemcore/0.1.5/NEMCore-0.1.5.tar.gz

---
+++ packages/nemcore/NEMCore-0.1.5.tar.gz/NEMCore-0.1.5/tests/conftest.py
@@ -0,0 +1,24 @@
+from pytest import fixture
+from os import environ
+from nemcore.api import NetEaseApi
+
+
+@fixture()
+def username():
+    assert 'username' in environ, 'Set environment variable "username" before start testing.'
+    return environ['username']
+
+
+@fixture()
+def password():
+    assert 'password' in environ, 'Set environment variable "password" before start testing.'
+    return environ['password']
+
+
+@fixture(scope='session')
+def api():
+    yield NetEaseApi(
+        cookie_path='.session-cookies',
+        cache_path='.session-cache',
+        cache_ttl=600,
+    )
