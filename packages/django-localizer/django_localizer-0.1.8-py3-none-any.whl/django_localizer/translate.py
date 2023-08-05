5e4ec0ff2508a8b53d29e56e908235c418cb0746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:53:07 +0000

    Add package django-localizer/0.1.8/django_localizer-0.1.8-py3-none-any.whl

---
+++ packages/django-localizer/django_localizer-0.1.8-py3-none-any.whl/django_localizer/translate.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext, ngettext
+
+
+def _get_n(*args, **kwargs):
+    if args:
+        return args[0]
+    elif kwargs and len(kwargs) == 1:
+        return list(kwargs.items())[0][1]
+    elif kwargs and 'n' in kwargs:
+        return kwargs['n']
+    return None
+
+
+def _get_str(key, n):
+    if n is None:
+        return gettext(key)
+    return ngettext(key, key, n)
+
+
+def translate(key, *args, **kwargs):
+    n = _get_n(*args, **kwargs)
+    s = _get_str(key, n)
+    return s.format(*args, **kwargs)
