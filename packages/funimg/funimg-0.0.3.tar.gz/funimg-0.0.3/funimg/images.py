712f63bafe3d57a8df87c0b35acdca38aad37b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package funimg/0.0.3/funimg-0.0.3.tar.gz

---
+++ packages/funimg/funimg-0.0.3.tar.gz/funimg-0.0.3/funimg/images.py
@@ -0,0 +1,23 @@
+def achievement(text: str):
+	url = f"https://api.alexflipnote.dev/achievement?text={text}"
+	return url
+
+def calling(text: str):
+	url = f"https://api.alexflipnote.dev/calling?text={text}"
+	return url
+	
+def captcha(text: str):
+	url = f"https://api.alexflipnote.dev/captcha?text={text}"
+	return url
+	
+def scroll(text: str):
+	url = f"https://api.alexflipnote.dev/scroll?text={text}"
+	return url
+	
+def facts(text: str):
+	url = f"https://api.alexflipnote.dev/facts?text={text}"
+	return url
+	
+def challenge(text: str):
+	url = f"https://api.alexflipnote.dev/challenge?text={text}"
+	return url
