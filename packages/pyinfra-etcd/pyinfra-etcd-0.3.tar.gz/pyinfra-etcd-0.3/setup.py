c27a6d45706c308e067e9c20da52a0ff6a363d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:44 +0000

    Add package pyinfra-etcd/0.3/pyinfra-etcd-0.3.tar.gz

---
+++ packages/pyinfra-etcd/pyinfra-etcd-0.3.tar.gz/pyinfra-etcd-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+if __name__ == '__main__':
+    setup(
+        version='0.3',
+        name='pyinfra-etcd',
+        description='Install & bootstrap etcd clusters with pyinfra.',
+        packages=find_packages(),
+        install_requires=('pyinfra>=0.5'),
+        include_package_data=True,
+    )
