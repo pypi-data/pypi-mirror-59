4851c4860a3ba9599059efaf9807b56e263f13e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package quantlw-sdk/0.0.1/quantlw_sdk-0.0.1.tar.gz

---
+++ packages/quantlw-sdk/quantlw_sdk-0.0.1.tar.gz/quantlw_sdk-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='quantlw_sdk',
+    version='0.0.1',
+    author='lw',
+    author_email='415381550@qq.com',
+    url='https://github.com/linshilogin/quant_sdk',
+    description=u'量化辅助sdk',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+)
