4113a0fc8581ef540a3fef5108e70cc36232110e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:31 +0000

    Add package mroylib-min/2.2.5/mroylib_min-2.2.5.tar.gz

---
+++ packages/mroylib-min/mroylib_min-2.2.5.tar.gz/mroylib_min-2.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(name='mroylib_min',
+    version='2.2.5',
+    description='some libs',
+    url='https://github.com/Qingluan/.git',
+    author='Qing luan',
+    author_email='darkhackdevil@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=['tornado','motor','tabulate', 'redis', 'pymysql','pymongo','tqdm', 'xlrd','xlwt','bs4','requests','termcolor','simplejson','pysocks','telethon', 'aiohttp'],
+    entry_points={
+        'console_scripts' : [
+            'Mr=mroylib.cmd:main',
+            'repo-upload=mroylib.api:repo_upload_client',
+            'm-server=mroylib.servers.tornado:main',
+        ]
+    }
+)
+
+
