7bcaa3e58726a9f3aba92a1d9bc032cc7565d357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:20 +0000

    Add package Jigmepdf/1.0/Jigmepdf-1.0.tar.gz

---
+++ packages/Jigmepdf/Jigmepdf-1.0.tar.gz/Jigmepdf-1.0/setup.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import setuptools
+
+setuptools.setup(
+    name="Jigmepdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["datas", "tests"])
+)
