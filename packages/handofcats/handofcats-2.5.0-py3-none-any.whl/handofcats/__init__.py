978314f746611fe76c11bdc976c098e4a7e2cc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:12 +0000

    Add package handofcats/2.5.0/handofcats-2.5.0-py3-none-any.whl

---
+++ packages/handofcats/handofcats-2.5.0-py3-none-any.whl/handofcats/__init__.py
@@ -0,0 +1,34 @@
+import sys
+from .driver import Driver
+from .util import import_symbol
+
+
+def import_symbol_maybe(ob_or_path, sep=":"):
+    if not isinstance(ob_or_path, str):
+        return ob_or_path
+    return import_symbol(ob_or_path, sep=sep)
+
+
+def as_command(fn=None, *, argv=None, driver=Driver, level=2, _force=False, ignore_logging=False):
+    create_driver = import_symbol_maybe(driver)
+    if argv is None:
+        argv = sys.argv[1:]
+
+    def call(fn, level=1, argv=argv):
+        if not _force:
+            # caller module extraction, if it is __main__, calling as command
+            frame = sys._getframe(level)
+            name = frame.f_globals["__name__"]
+            if name != "__main__":
+                return fn
+        driver = create_driver()
+        return driver.run(fn, argv, ignore_logging=ignore_logging)
+
+    if fn is None:
+        return call
+    else:
+        return call(fn, level=level, argv=argv)
+
+
+# alias
+handofcats = as_command
---
+++ packages/handofcats/handofcats-2.5.0-py3-none-any.whl/handofcats/driver.py
@@ -0,0 +1,51 @@
+from .injector import Injector
+from . import injectlogging
+
+
+class Driver:
+    def create_injector(self, fn):
+        return Injector(fn)
+
+    def create_parser(self, fn, *, argv=None, description=None):
+        if "--expose" in (argv or []):
+            from .parsers import expose
+
+            inplace = "--inplace" in (argv or [])
+            typed = "--typed" in (argv or [])
+            description = description or fn.__doc__
+            parser = expose.create_parser(
+                fn, description=description, inplace=inplace, typed=typed
+            )
+        else:
+            from .parsers import commandline
+
+            parser = commandline.create_parser(
+                fn, description=description or fn.__doc__
+            )
+            parser.add_argument(
+                "--expose", action="store_true"
+            )  # xxx (for ./expose.py)
+            parser.add_argument(
+                "--inplace", action="store_true"
+            )  # xxx (for ./expose.py)
+            parser.add_argument("--typed", action="store_true")  # xxx (for ./expose.py)
+        return parser
+
+    def run(self, fn, argv=None, *, ignore_logging=False):
+        parser = self.create_parser(fn, argv=argv, description=fn.__doc__)
+
+        injector = self.create_injector(fn)
+        injector.inject(parser)
+        if not ignore_logging:
+            injectlogging.setup(parser)
+
+        args = parser.parse_args(argv)
+        params = vars(args).copy()
+
+        if not ignore_logging:
+            injectlogging.activate(params)
+
+        params.pop("expose", None)  # xxx: for ./parsers/expose.py
+        params.pop("inplace", None)  # xxx: for ./parsers/expose.py
+        params.pop("typed", None)  # xxx: for ./parsers/expose.py
+        return fn(**params)
