b00dd99030f33a26c9ee76b0855499171457dc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:02 +0000

    Add package udacityMLE-distributions/0.1/udacityMLE_distributions-0.1.tar.gz

---
+++ packages/udacityMLE-distributions/udacityMLE_distributions-0.1.tar.gz/udacityMLE_distributions-0.1/README.md
@@ -0,0 +1,5 @@
+# udacityMLE-distributions package
+
+This is a package to make computations on Gaussian and 
+Binomial distributions. Created for the Udacity MLE 
+nanodegree program.
---
+++ packages/udacityMLE-distributions/udacityMLE_distributions-0.1.tar.gz/udacityMLE_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacityMLE_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacityMLE_distributions'],
+      zip_safe=False)
