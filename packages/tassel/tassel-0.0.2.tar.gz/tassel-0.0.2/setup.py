92752f73f51d017a5265b3d7e3891666fdabf425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:14 +0000

    Add package tassel/0.0.2/tassel-0.0.2.tar.gz

---
+++ packages/tassel/tassel-0.0.2.tar.gz/tassel-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='tassel',
+  version='0.0.2',
+  author='Unknown',
+  author_email='unknown@example.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="This is a helper library",
+  packages=['tassel'],
+  scripts=[]
+)
+
