97d21fa50cecb68edf35250a55247bc142803684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package falsecolor/1.1.1/FalseColor-1.1.1.tar.gz

---
+++ packages/falsecolor/FalseColor-1.1.1.tar.gz/FalseColor-1.1.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(name='FalseColor',
+    packages = ['FalseColor'],
+    version='1.1.1',
+    license = 'GNU Affero General Public License v3.0',
+    description='Methods for H&E pseudo coloring of grayscale fluorescent images',
+    author='Robert Serafin',
+    author_email='serrob23@uw.edu',
+    url = 'https://github.com/serrob23/FalseColor',
+    download_url = 'https://github.com/serrob23/FalseColor/archive/v1.1.tar.gz',
+    keywords = ['Image Analysis', 
+                            'Fluorescence Microscopy', 
+                            'Color Translation', 
+                            'Virtual Staining',
+                            'Histology'],
+    install_requires =[
+                        'opencv-python',
+                        'numpy',
+                        'scikit-image',
+                        'scipy',
+                        'pathos',
+                        'numba',
+                        'tifffile',
+                        'h5py',
+                        'matplotlib',
+                        'json',
+                        'os'
+                        ],
+
+)
