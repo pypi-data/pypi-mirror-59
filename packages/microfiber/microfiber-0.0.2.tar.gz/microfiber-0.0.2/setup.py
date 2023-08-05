177b741f34fc4a4f6de9e0f46ee0fbf9d9e3804d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:23 +0000

    Add package microfiber/0.0.2/microfiber-0.0.2.tar.gz

---
+++ packages/microfiber/microfiber-0.0.2.tar.gz/microfiber-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='microfiber',
+  version='0.0.2',
+  author='Unknown',
+  author_email='unknown@example.com',
+  url='https://github.com',
+  license="MIT",
+  long_description="This is a helper library",
+  packages=['microfiber'],
+  scripts=[]
+)
+
