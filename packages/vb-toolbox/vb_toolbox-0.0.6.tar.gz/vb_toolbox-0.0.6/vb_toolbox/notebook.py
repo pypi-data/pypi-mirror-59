07fa95cab324e0368e8d3b93ad440bcb207205ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package vb-toolbox/0.0.6/vb_toolbox-0.0.6.tar.gz

---
+++ packages/vb-toolbox/vb_toolbox-0.0.6.tar.gz/vb_toolbox-0.0.6/vb_toolbox/notebook.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+#tkNotebook
+#Created By: Patrick T. Cossette <cold_soul79078@yahoo.com>
+from tkinter import *
+from tkinter import filedialog
+
+#tkNotebook allows users to make notebook widgets in Tk
+
+#This software may be modified and redistributed, as long as any and all changes made
+#From here on are stated, and ("Created By: Patrick T. Cossette <cold_soul79078@yahoo.com>") still remains somewhere
+#In the code.
+
+#This software is opensource, and free in hopes that it may be useful, and comes AS IS
+#With no warrenty.
