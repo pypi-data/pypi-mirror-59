283846ba8b3834c3d8b4297f520ddb59f05a78f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:40 +0000

    Add package kac/0.2.0/kac-0.2.0.tar.gz

---
+++ packages/kac/kac-0.2.0.tar.gz/kac-0.2.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='kac',
+    version='0.2.0',
+    author="Adam Walsh",
+    author_email="adam@grid.sh",
+    description="Automatic version bumping for Keep-a-Changelog style Changelog files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atwalsh/kac",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'click==7.0',
+        'pyperclip==1.7.0',
+        'questionary==1.4.0'
+    ],
+    entry_points='''
+        [console_scripts]
+        kac=kac.kac:cli
+    ''',
+    python_requires='>=3.6',
+)
