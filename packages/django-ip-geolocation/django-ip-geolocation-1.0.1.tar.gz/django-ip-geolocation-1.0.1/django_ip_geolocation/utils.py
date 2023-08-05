bdd74925deb910d99bdf53b9fc9490e2c1f10e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package django-ip-geolocation/1.0.1/django-ip-geolocation-1.0.1.tar.gz

---
+++ packages/django-ip-geolocation/django-ip-geolocation-1.0.1.tar.gz/django-ip-geolocation-1.0.1/django_ip_geolocation/utils.py
@@ -0,0 +1,17 @@
+from django_ip_geolocation import settings
+from django.utils.module_loading import import_string
+import logging
+
+def get_remote_ip_from_request(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def get_geolocation_backend_cls():
+    geolocation_backend_cls = import_string(settings.IP_GEOLOCATION_SETTINGS.get('BACKEND'))
+    return geolocation_backend_cls
+
