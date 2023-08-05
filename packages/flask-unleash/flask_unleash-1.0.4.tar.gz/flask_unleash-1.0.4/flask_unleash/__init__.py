a5635528b4c0be1d3859446a10456be6ca7538cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package flask-unleash/1.0.4/flask_unleash-1.0.4.tar.gz

---
+++ packages/flask-unleash/flask_unleash-1.0.4.tar.gz/flask_unleash-1.0.4/flask_unleash/__init__.py
@@ -0,0 +1,48 @@
+import os
+from UnleashClient import UnleashClient
+from flask import current_app, _app_ctx_stack  # type: ignore
+
+
+CONFIG_MAPPING = {
+    'UNLEASH_INSTANCE_ID': 'instance_id',
+    'UNLEASH_REFRESH_INTERVAL': 'refresh_interval',
+    'UNLEASH_METRIC_INTERVAL': 'metrics_interval',
+    'UNLEASH_DISABLE_METRICS': 'disable_metrics',
+    'UNLEASH_DISABLE_REGISTRATION': 'disable_registration',
+    'UNLEASH_CUSTOM_HEADERS': 'custom_headers',
+    'UNLEASH_CUSTOM_OPTIONS': 'custom_options',
+    'UNLEASH_CUSTOM_STRATEGIES': 'custom_strategies',
+    'UNLEASH_CACHE_DIRECTORY': 'cache_directory'
+}
+
+
+class Unleash():
+    def __init__(self, app=None):
+        # Constants
+        self.name = 'unleash_client'
+        self.client = None
+
+        # Setup
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        # Populate required arguments.
+        unleash_args = {
+            'url': app.config['UNLEASH_URL'],
+            'app_name': app.config['UNLEASH_APP_NAME'],
+            'environment': app.config['UNLEASH_ENVIRONMENT']
+        }
+
+        # Populate optional arguments.
+        populated_optional_args = filter(lambda x: x in app.config, CONFIG_MAPPING.keys())
+        for option in populated_optional_args:
+            unleash_args[CONFIG_MAPPING[option]] = app.config[option]
+
+        # Set up client.
+        self.client = UnleashClient(**unleash_args)
+
+        self.client.initialize_client()
+        app.extensions = getattr(app, 'extensions', {})
+        app.extensions['Unleash'] = self
---
+++ packages/flask-unleash/flask_unleash-1.0.4.tar.gz/flask_unleash-1.0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as file:
+        return file.read()
+
+setup(
+    name='flask_unleash',
+    version='1.0.4',
+    url='https://github.com/Unleash/Flask-Unleash',
+    license='MIT',
+    author='Ivan Lee',
+    author_email='ivanklee86@gmail.com',
+    description='Flask extension for unleash-client-python.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'UnleashClient==3.4.0',
+        'Flask'
+    ],
+    python_requres=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ]
+)
