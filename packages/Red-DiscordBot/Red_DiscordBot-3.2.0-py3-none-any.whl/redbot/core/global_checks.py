68f6c3e2404c252623c15fb3ae267d674320b42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:33 +0000

    Add package Red-DiscordBot/3.2.0/Red_DiscordBot-3.2.0-py3-none-any.whl

---
+++ packages/Red-DiscordBot/Red_DiscordBot-3.2.0-py3-none-any.whl/redbot/core/global_checks.py
@@ -0,0 +1,21 @@
+"""The checks in this module run on every command."""
+from . import commands
+
+
+def init_global_checks(bot):
+    @bot.check_once
+    def actually_up(ctx):
+        """ 
+        Uptime is set during the initial startup process.
+        If this hasn't been set, we should assume the bot isn't ready yet. 
+        """
+        return ctx.bot.uptime is not None
+
+    @bot.check_once
+    async def whiteblacklist_checks(ctx):
+        return await ctx.bot.allowed_by_whitelist_blacklist(ctx.author)
+
+    @bot.check_once
+    def bots(ctx):
+        """Check the user is not another bot."""
+        return not ctx.author.bot
