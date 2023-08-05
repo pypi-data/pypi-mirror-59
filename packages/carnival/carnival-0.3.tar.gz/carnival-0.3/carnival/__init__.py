6080baebf0b4b3a4434cbd8b20b24fd2b0c9acc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package carnival/0.3/carnival-0.3.tar.gz

---
+++ packages/carnival/carnival-0.3.tar.gz/carnival-0.3/carnival/__init__.py
@@ -0,0 +1,18 @@
+from carnival.role import Role
+from carnival.host import Host
+from carnival import secrets
+from carnival import cmd
+from carnival.utils import log
+
+
+__all__ = [
+    'Role',
+    'Host',
+    'secrets',
+    'cmd',
+    'log',
+
+    '__version__',
+]
+
+__version__ = '0.3'
