a06dc2b2b99e90f0f9604e0c4be68b5371eb87e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package tryton/5.2.11/tryton-5.2.11-py3-none-any.whl

---
+++ packages/tryton/tryton-5.2.11-py3-none-any.whl/tryton/__init__.py
@@ -0,0 +1,18 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+__version__ = "5.2.11"
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+gi.require_foreign('cairo')
+try:
+    gi.require_version('GtkSpell', '3.0')
+except ValueError:
+    pass
+
+try:
+    # Import earlier otherwise there is a segmentation fault on MSYS2
+    import goocalendar
+except ImportError:
+    pass
