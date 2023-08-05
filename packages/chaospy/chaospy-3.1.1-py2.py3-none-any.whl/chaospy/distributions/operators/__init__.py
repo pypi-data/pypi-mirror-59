6bbc5fe1bbdd973b420f0b106caeb023f9af7090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package chaospy/3.1.1/chaospy-3.1.1-py2.py3-none-any.whl

---
+++ packages/chaospy/chaospy-3.1.1-py2.py3-none-any.whl/chaospy/distributions/operators/__init__.py
@@ -0,0 +1,27 @@
+"""Collection of mathematical operators."""
+from .addition import Add, add
+from .multiply import Mul, mul
+from .negative import Neg, neg
+from .power import Pow
+
+from .joint import J
+from .iid import Iid
+
+from .trunkation import Trunc, Trunk, trunk
+
+from .logarithm import Log
+from .logarithm10 import Log10
+from .logarithmn import Logn
+
+from .sin import Sin
+from .arcsin import Arcsin
+from .cos import Cos
+from .arccos import Arccos
+from .tan import Tan
+from .arctan import Arctan
+from .sinh import Sinh
+from .arcsinh import Arcsinh
+from .cosh import Cosh
+from .arccosh import Arccosh
+from .tanh import Tanh
+from .arctanh import Arctanh
