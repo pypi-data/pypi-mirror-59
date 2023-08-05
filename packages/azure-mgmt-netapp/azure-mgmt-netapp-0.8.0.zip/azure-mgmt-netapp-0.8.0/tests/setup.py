355941b6ee911c7d1820d25b390feb003b3695f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:41 +0000

    Add package azure-mgmt-netapp/0.8.0/azure-mgmt-netapp-0.8.0.zip

---
+++ packages/azure-mgmt-netapp/azure-mgmt-netapp-0.8.0.zip/azure-mgmt-netapp-0.8.0/tests/setup.py
@@ -0,0 +1,22 @@
+# resource constants
+# some minor setup required to run in record mode
+#  - actual subsid to be used
+#  - rg must exist
+#  - vnet must exist
+
+TEST_RG='sdk-py-tests-cys' #'sdk-wus2-rg-qat2'
+TEST_ACC_1='sdk-py-tests-acc-1'
+TEST_ACC_2='sdk-py-tests-acc-2'
+TEST_POOL_1='sdk-py-tests-pool-1'
+TEST_POOL_2='sdk-py-tests-pool-2'
+TEST_VOL_1='sdk-py-tests-vol-1'
+TEST_VOL_2='sdk-py-tests-vol-2'
+TEST_SNAPSHOT_1='sdk-py-tests-snapshot-1'
+TEST_SNAPSHOT_2='sdk-py-tests-snapshot-2'
+DEFAULT_SIZE=4398046511104
+GIGABYTE=1024 * 1024 * 1024
+SUBSID='8f38cfec-0ecd-413a-892e-2494f77a3b56' #'69a75bda-882e-44d5-8431-63421204132a'
+SERVICE_LEVEL='Premium'
+LOCATION='eastus'
+VNET='sdk-py-tests-cys-vnet' #'api-test-vnet-westus2stage'
+SERVICE_LEVEL='Premium'
