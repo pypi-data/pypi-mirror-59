34eb907cd91d502424cada99546f1f1811c94005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:30 +0000

    Add package cxsystem2/0.1.2.0/cxsystem2-0.1.2.0.tar.gz

---
+++ packages/cxsystem2/cxsystem2-0.1.2.0.tar.gz/cxsystem2-0.1.2.0/cxsystem2/bui/cx_bui/editor/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path("simulate", views.simulate),
+    path("load_example", views.load_example),
+    path("download_workspace", views.download_workspace),
+    path("download_files", views.download_files),
+    path("ls_workspace", views.ls_workspace),
+    path("simstatus", views.sim_status),
+    path("delete", views.delete_all),
+    path("visualize", views.visualize),
+]
