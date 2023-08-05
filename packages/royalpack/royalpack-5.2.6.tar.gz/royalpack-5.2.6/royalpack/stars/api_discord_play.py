0d6c391b829353c8f93f6394dcd34e3dd387ca48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:32 +0000

    Add package royalpack/5.2.6/royalpack-5.2.6.tar.gz

---
+++ packages/royalpack/royalpack-5.2.6.tar.gz/royalpack-5.2.6/royalpack/stars/api_discord_play.py
@@ -0,0 +1,18 @@
+from typing import *
+from starlette.requests import Request
+from starlette.responses import *
+from royalnet.constellation import *
+from royalnet.utils import *
+
+
+class ApiDiscordPlayStar(PageStar):
+    path = "/api/discord/play"
+
+    async def page(self, request: Request) -> JSONResponse:
+        url = request.query_params.get("url", "")
+        try:
+            guild_id: Optional[int] = int(request.path_params.get("guild_id", None))
+        except (ValueError, TypeError):
+            guild_id = None
+        response = await self.interface.call_herald_event("discord", "discord_play", url=url, guild_id=guild_id)
+        return JSONResponse(response)
