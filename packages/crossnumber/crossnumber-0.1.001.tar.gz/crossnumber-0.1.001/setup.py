3da12c1ef22b2dd9def2117ecdcf9058b39ce9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:29 +0000

    Add package crossnumber/0.1.001/crossnumber-0.1.001.tar.gz

---
+++ packages/crossnumber/crossnumber-0.1.001.tar.gz/crossnumber-0.1.001/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'crossnumber',
+  packages = ['crossnumber'], # this must be the same as the name above
+  install_requires=['numpy', 'termcolor'],
+  version = '0.1.001',
+  description = 'A collection of Python functions which may be of use when solving crossnumber puzzles.',
+  author = 'Adam Vellender',
+  author_email = 'dev@vellender.com',
+  url = 'https://github.com/vellender/crossnumber',
+  download_url = 'https://github.com/vellender/crossnumber/tarball/0.1.001',
+  keywords = ['crossnumber','solver','solve','tools','tool','puzzle','listener','magpie','numerical','numericals','crossword','mathematical','mathematicals'], 
+  classifiers = [],
+)
