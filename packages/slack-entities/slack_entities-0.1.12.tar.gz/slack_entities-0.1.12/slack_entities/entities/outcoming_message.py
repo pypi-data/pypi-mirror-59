1f3c65f2cdf9ff000fde05f163b30ad3deaf38bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:12 +0000

    Add package slack-entities/0.1.12/slack_entities-0.1.12.tar.gz

---
+++ packages/slack-entities/slack_entities-0.1.12.tar.gz/slack_entities-0.1.12/slack_entities/entities/outcoming_message.py
@@ -0,0 +1,74 @@
+from slack_entities.client.client import get_client
+from slack_entities.entities.user import User
+from .channel import Channel
+
+
+class OutcomingMessage:
+    """
+    Describes message sent to Slack
+    """
+    def __init__(
+        self,
+        channel: Channel,
+        attachments: list = None,
+        text: str = '',
+        token: str = None,
+        blocks: list = None,
+        *args,
+        **kwargs,
+    ):
+        self.channel = channel
+        self.token = token
+        self.text = text
+        self.attachments = attachments if attachments else []
+        self.blocks = blocks if blocks else []
+
+    def send(self):
+        return get_client(token=self.token).api_call(
+            'chat.postMessage',
+            body_encoding='json',
+            channel=self.channel.id,
+            text=self.text,
+            attachments=self.attachments,
+            blocks=self.blocks,
+        )
+
+    def send_ephemeral(self, user: User):
+        return get_client(token=self.token).api_call(
+            'chat.postEphemeral',
+            body_encoding='json',
+            channel=self.channel.id,
+            text=self.text,
+            attachments=self.attachments,
+            blocks=self.blocks,
+            user=user.id
+        )
+
+    def update(self, ts):
+        """
+        Updates the message with the timestamp
+        :param ts: Timestamp of the message to update
+        """
+        return get_client(token=self.token).api_call(
+            'chat.update',
+            body_encoding='json',
+            channel=self.channel.id,
+            ts=ts,
+            text=self.text,
+            attachments=self.attachments,
+            blocks=self.blocks,
+        )
+
+    def send_in_thread(self, thread_ts: str):
+        """ Sends threaded message.
+        :param ts: Timestamp of the original parent message which thread belongs to.
+        """
+        return get_client(token=self.token).api_call(
+            'chat.postMessage',
+            body_encoding='json',
+            channel=self.channel.id,
+            text=self.text,
+            attachments=self.attachments,
+            blocks=self.blocks,
+            thread_ts=thread_ts,
+        )
