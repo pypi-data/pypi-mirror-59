6b6a08c65416a7079e048a3276260bf1bd1c2597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:49:50 +0000

    Add package ewlibs/0.0.16/ewlibs-0.0.16.tar.gz

---
+++ packages/ewlibs/ewlibs-0.0.16.tar.gz/ewlibs-0.0.16/ewlibs/char_handler.py
@@ -0,0 +1,36 @@
+"""汉字处理的工具:
+判断unicode是否是汉字，数字，英文，或者其他字符。
+全角符号转半角符号。"""
+
+
+def is_chinese(uchar):
+    """判断一个unicode是否是汉字"""
+    if uchar >= u'\u4e00' and uchar <= u'\u9fa5':
+        return True
+    else:
+        return False
+
+
+def is_number(uchar):
+    """判断一个unicode是否是数字"""
+    if uchar >= u'\u0030' and uchar <= u'\u0039':
+        return True
+    else:
+        return False
+
+
+def is_alphabet(uchar):
+    """判断一个unicode是否是英文字母"""
+    if (uchar >= u'\u0041' and uchar <= u'\u005a') or (uchar >= u'\u0061'
+                                                       and uchar <= u'\u007a'):
+        return True
+    else:
+        return False
+
+
+def is_other(uchar):
+    """判断是否非汉字，数字和英文字符"""
+    if not (is_chinese(uchar) or is_number(uchar) or is_alphabet(uchar)):
+        return True
+    else:
+        return False
