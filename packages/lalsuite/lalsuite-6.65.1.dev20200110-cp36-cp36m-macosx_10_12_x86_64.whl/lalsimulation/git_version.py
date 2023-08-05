b8462bd6ab568a212b3089dba3d046c592a84c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:09 +0000

    Add package lalsuite/6.65.1.dev20200110/lalsuite-6.65.1.dev20200110-cp36-cp36m-macosx_10_12_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.65.1.dev20200110-cp36-cp36m-macosx_10_12_x86_64.whl/lalsimulation/git_version.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# git_version.py - vcs information module
+#
+# Copyright (C) 2010 Nickolas Fotopoulos
+# Copyright (C) 2012-2013 Adam Mercer
+# Copyright (C) 2016 Leo Singer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with with program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+
+id = "381f2ce760270a97afd4c91f7766b3304ff68cd1"
+date = "2020-01-9 18:51:51 +0000"
+branch = "None"
+tag = "None"
+if tag == "None":
+    tag = None
+author = "Adam Mercer <adam.mercer@ligo.org>"
+builder = "Unknown User <>"
+committer = "Adam Mercer <adam.mercer@ligo.org>"
+status = "CLEAN: All modifications committed"
+version = id
+verbose_msg = """Branch: None
+Tag: None
+Id: 381f2ce760270a97afd4c91f7766b3304ff68cd1
+
+Builder: Unknown User <>
+Repository status: CLEAN: All modifications committed"""
+
+import warnings
+
+class VersionMismatchError(ValueError):
+    pass
+
+def check_match(foreign_id, onmismatch="raise"):
+    """
+    If foreign_id != id, perform an action specified by the onmismatch
+    kwarg. This can be useful for validating input files.
+
+    onmismatch actions:
+      "raise": raise a VersionMismatchError, stating both versions involved
+      "warn": emit a warning, stating both versions involved
+    """
+    if onmismatch not in ("raise", "warn"):
+        raise ValueError(onmismatch + " is an unrecognized value of onmismatch")
+    if foreign_id == "381f2ce760270a97afd4c91f7766b3304ff68cd1":
+        return
+    msg = "Program id (381f2ce760270a97afd4c91f7766b3304ff68cd1) does not match given id (%s)." % foreign_id
+    if onmismatch == "raise":
+        raise VersionMismatchError(msg)
+
+    # in the backtrace, show calling code
+    warnings.warn(msg, UserWarning)
