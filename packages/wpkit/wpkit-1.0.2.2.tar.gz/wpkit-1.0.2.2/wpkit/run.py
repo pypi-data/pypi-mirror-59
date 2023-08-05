59380e8faa5828e1a979fd1937a3c3d44d1eff34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:34 +0000

    Add package wpkit/1.0.2.2/wpkit-1.0.2.2.tar.gz

---
+++ packages/wpkit/wpkit-1.0.2.2.tar.gz/wpkit-1.0.2.2/wpkit/run.py
@@ -0,0 +1,8 @@
+
+from wpkit.web.applications.demo import demo as demoapp
+from wpkit.projects.setup import setup_default
+from wpkit.install import install_requirements as install
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire()
