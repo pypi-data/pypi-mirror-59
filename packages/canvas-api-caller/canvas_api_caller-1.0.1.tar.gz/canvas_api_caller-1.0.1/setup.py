4d225e23c7d9c5fd0c08ffac5179aee6bc68a269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package canvas-api-caller/1.0.1/canvas_api_caller-1.0.1.tar.gz

---
+++ packages/canvas-api-caller/canvas_api_caller-1.0.1.tar.gz/canvas_api_caller-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='canvas_api_caller',
+    version='1.0.1',
+    url="https://github.com/Nudge-Crew/canvas-api-caller",
+    author="Noah Scharrenberg",
+    author_email="nscharrenberg@hotmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    license="MIT",
+    install_requires=[
+        'requests',
+        'flask',
+        'werkzeug'
+    ]
+)
