649631eda6ab90296a4abe0cfc2fd6e919ba7b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package demyst-analytics/0.8.13/demyst-analytics-0.8.13.tar.gz

---
+++ packages/demyst-analytics/demyst-analytics-0.8.13.tar.gz/demyst-analytics-0.8.13/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(
+    name='demyst-analytics',
+
+    version='0.8.13',
+
+    description='',
+    long_description='',
+
+    author='Demyst Data',
+    author_email='info@demystdata.com',
+
+    license='',
+
+    packages=['demyst.analytics'],
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+        'demyst-common>=0.8.13',
+        'yattag',
+        'IPython',
+        'tqdm',
+        'ipywidgets',
+        'pandas',
+        'pandas_schema',
+        'keras',
+        'tensorflow==2.0.0',
+        'usaddress'
+    ]
+)
