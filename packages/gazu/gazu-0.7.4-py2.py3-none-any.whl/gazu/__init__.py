88db4a6dcfabf401661d5b2adc1d6419a5a3ee5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:28 +0000

    Add package gazu/0.7.4/gazu-0.7.4-py2.py3-none-any.whl

---
+++ packages/gazu/gazu-0.7.4-py2.py3-none-any.whl/gazu/__init__.py
@@ -0,0 +1,44 @@
+from . import client
+from . import cache
+from . import helpers
+from . import events
+
+from . import asset
+from . import casting
+from . import context
+from . import entity
+from . import files
+from . import project
+from . import person
+from . import shot
+from . import task
+from . import user
+from . import playlist
+
+from .exception import AuthFailedException
+from .__version__ import __version__
+
+
+def get_host():
+    return client.get_host()
+
+
+def set_host(url):
+    client.set_host(url)
+
+
+def log_in(email, password):
+    tokens = client.post("auth/login", {"email": email, "password": password})
+    if "login" in tokens and tokens["login"] == False:
+        raise AuthFailedException
+    else:
+        client.set_tokens(tokens)
+    return tokens
+
+
+def get_event_host():
+    return client.get_event_host()
+
+
+def set_event_host(url):
+    client.set_event_host(url)
