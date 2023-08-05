eb5c822902f48aaba4efe96126a1ad36f14b1e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:11 +0000

    Add package royalpack/5.2.8/royalpack-5.2.8.tar.gz

---
+++ packages/royalpack/royalpack-5.2.8.tar.gz/royalpack-5.2.8/royalpack/stars/api_discord_play.py
@@ -0,0 +1,25 @@
+from typing import *
+from starlette.requests import Request
+from starlette.responses import *
+from royalnet.constellation import *
+from royalnet.utils import *
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class ApiDiscordPlayStar(PageStar):
+    path = "/api/discord/play"
+
+    async def page(self, request: Request) -> JSONResponse:
+        url = request.query_params.get("url", "")
+        try:
+            guild_id: Optional[int] = int(request.query_params.get("guild_id", None))
+        except (ValueError, TypeError):
+            guild_id = None
+        log.info(f"Received request to play {url} on guild_id {guild_id} via web")
+        response = await self.interface.call_herald_event("discord", "discord_play", url=url, guild_id=guild_id)
+        return JSONResponse(response, headers={
+            "Access-Control-Allow-Origin": self.interface.config['Funkwhale']['instance_url'],
+        })
