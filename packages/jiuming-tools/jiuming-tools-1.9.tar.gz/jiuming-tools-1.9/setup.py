9a3c35335260af5c82dad59f84edd859edf0e45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package jiuming-tools/1.9/jiuming-tools-1.9.tar.gz

---
+++ packages/jiuming-tools/jiuming-tools-1.9.tar.gz/jiuming-tools-1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jiuming-tools',
+    # 版本 如果需要发布更新 需要调整版本号
+    version='1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    # 需要的依赖
+    install_requires=[
+        'Click', 'psutil', 'PyYAML', 'paramiko'
+    ],
+    # 命令行入口
+    entry_points='''
+        [console_scripts]
+        ming=ming:cli
+    ''',
+    author='ming',
+    license='MIT',
+)
