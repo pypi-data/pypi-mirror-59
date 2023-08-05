d3caa941315bb72bd3288682bd572902761b9cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:35 +0000

    Add package shippo/2.0.0/shippo-2.0.0.tar.gz

---
+++ packages/shippo/shippo-2.0.0.tar.gz/shippo-2.0.0/shippo/__init__.py
@@ -0,0 +1,14 @@
+from shippo.resource import (
+    Address,
+    Batch,
+    CarrierAccount,
+    CustomsDeclaration,
+    CustomsItem,
+    Manifest,
+    Parcel,
+    Rate,
+    Refund,
+    Shipment,
+    Track,
+    Transaction,
+)
