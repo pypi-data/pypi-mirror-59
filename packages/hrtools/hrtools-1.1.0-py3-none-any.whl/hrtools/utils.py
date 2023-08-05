3b03a45547393ad980dac545d6256cc2bbbe9016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package hrtools/1.1.0/hrtools-1.1.0-py3-none-any.whl

---
+++ packages/hrtools/hrtools-1.1.0-py3-none-any.whl/hrtools/utils.py
@@ -0,0 +1,33 @@
+       #Barra de Progresso
+
+
+class utils:
+
+    def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+        # Print New Line on Complete
+        if iteration == total: 
+            print()
+
+
+    def kFun(x):
+        if x.find("k") > 0:
+            val = x.replace("k","").replace(",",".").strip()
+            return float(val) * 1000
+        else:
+            val = x.strip()
+            return float(val)
