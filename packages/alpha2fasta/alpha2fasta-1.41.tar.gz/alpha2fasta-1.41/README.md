4d91b5e5039f387346638e89ee528b8b71b67bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:49:59 +0000

    Add package alpha2fasta/1.41/alpha2fasta-1.41.tar.gz

---
+++ packages/alpha2fasta/alpha2fasta-1.41.tar.gz/alpha2fasta-1.41/README.md
@@ -0,0 +1,7 @@
+# alpha2fasta
+The alpha2fasta library can be used to encode texts written in natural language to a FASTA-based format. The purpose of coding is to enable the application of various bioinformatics tools in text mining.
+
+Functions
+--------------------------------------------------------------------------------------------------------------------------------
+- aminocode
+- dnabits
---
+++ packages/alpha2fasta/alpha2fasta-1.41.tar.gz/alpha2fasta-1.41/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+ 
+setup(name='alpha2fasta',
+      version='1.41',
+#      url='https://github.com/diogomachado-bioinfo/alpha2fasta',
+      author='Diogo de Jesus Soares Machado',
+      author_email='diogomachado.bioinfo@gmail.com',
+      description='Encode text written in natural language in a FASTA-based format',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.rst').read(),
+      zip_safe=False)
