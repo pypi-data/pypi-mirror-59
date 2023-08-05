509407da5695b6851ebc580078537398468b3703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package doctalk/0.0.1/doctalk-0.0.1-py3-none-any.whl

---
+++ packages/doctalk/doctalk-0.0.1-py3-none-any.whl/doctalk/params.py
@@ -0,0 +1,16 @@
+annotators=['tokenize','ssplit','pos','lemma','depparse','ner']+['natlog','openie']
+openie='openie' in annotators
+quiet=True
+max_answers=3
+answers_by_rank=True
+trace=1
+force=False
+from nltk.stem import PorterStemmer
+#stemmer=PorterStemmer()
+stemmer=None
+lower=True
+pers=True
+show=True
+def ppp(*args) :
+  print('DEBUG:',end='')
+  print(*args)
