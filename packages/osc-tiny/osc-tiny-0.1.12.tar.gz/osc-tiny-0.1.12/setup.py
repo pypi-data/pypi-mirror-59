8591afcae0b7df7415f2aee81f3b6848a2635c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package osc-tiny/0.1.12/osc-tiny-0.1.12.tar.gz

---
+++ packages/osc-tiny/osc-tiny-0.1.12.tar.gz/osc-tiny-0.1.12/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='osc-tiny',
+    version='0.1.12',
+    description='Client API for openSUSE BuildService',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Andreas Hasenkopf',
+    author_email='ahasenkopf@suse.com',
+    url='http://github.com/crazyscientist/osc-tiny',
+    download_url='http://github.com/crazyscientist/osc-tiny/tarball/master',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        "lxml",
+        "requests",
+        "responses",
+        "python-dateutil",
+        "pytz"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ]
+)
