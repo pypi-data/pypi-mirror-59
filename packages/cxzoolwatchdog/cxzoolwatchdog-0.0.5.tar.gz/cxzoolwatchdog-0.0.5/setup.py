8a755796112e08d00f95a70b833876c0202ead6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:06 +0000

    Add package cxzoolwatchdog/0.0.5/cxzoolwatchdog-0.0.5.tar.gz

---
+++ packages/cxzoolwatchdog/cxzoolwatchdog-0.0.5.tar.gz/cxzoolwatchdog-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cxzoolwatchdog",
+    version="0.0.5",
+    author="cxzool",
+    author_email="954542623@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruanzq/watchdog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # exapmle
+        'argparse',
+    ],
+)
