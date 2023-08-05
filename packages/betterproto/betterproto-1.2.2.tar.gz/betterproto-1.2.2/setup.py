19fca0b08432d6979f91f3826c4aa99a1939231a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package betterproto/1.2.2/betterproto-1.2.2.tar.gz

---
+++ packages/betterproto/betterproto-1.2.2.tar.gz/betterproto-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="betterproto",
+    version="1.2.2",
+    description="A better Protobuf / gRPC generator & library",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/danielgtaylor/python-betterproto",
+    author="Daniel G. Taylor",
+    author_email="danielgtaylor@gmail.com",
+    license="MIT",
+    entry_points={
+        "console_scripts": ["protoc-gen-python_betterproto=betterproto.plugin:main"]
+    },
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "output", "output.*"]
+    ),
+    package_data={"betterproto": ["py.typed", "templates/template.py"]},
+    python_requires=">=3.7",
+    install_requires=["grpclib", "stringcase"],
+    extras_require={"compiler": ["black", "jinja2", "protobuf"]},
+    zip_safe=False,
+)
