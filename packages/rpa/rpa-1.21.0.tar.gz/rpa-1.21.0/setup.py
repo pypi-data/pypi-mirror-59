959ad1ec47286d99e2f5208e5a464132298a00ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:10 +0000

    Add package rpa/1.21.0/rpa-1.21.0.tar.gz

---
+++ packages/rpa/rpa-1.21.0.tar.gz/rpa-1.21.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='rpa',
+    version='1.21.0',
+    install_requires=['tagui==1.21.0'],
+    author='Ken Soh',
+    author_email='opensource@tebel.org',
+    license='Apache License 2.0',
+    url='https://github.com/tebelorg/TagUI-Python',
+    description='TagUI for Python is a Python package for digital process automation (RPA)',
+    long_description='TagUI for Python homepage - https://github.com/tebelorg/TagUI-Python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
