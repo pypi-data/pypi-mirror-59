a1fef03d3a4fec8f51cac6e0878a94c295c48ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package py-News-key/0.0.8/py_News_key-0.0.8-py3-none-any.whl

---
+++ packages/py-News-key/py_News_key-0.0.8-py3-none-any.whl/py_News_key/__init__.py
@@ -0,0 +1,13 @@
+#-*-coding:utf 8-*-
+
+version = "0.0.8"
+data = {
+    "WWK": [
+        "WWK",
+        "\u738b\u6587\u5eb7"
+    ],
+    "WZC": [
+        "WZC",
+        "\u738b\u5fd7\u8d85"
+    ]
+}
