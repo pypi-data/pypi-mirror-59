9f591f892a6b418554a78848278b7fa60f85ce8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:20 +0000

    Add package vk-botting/0.5.1/vk_botting-0.5.1-py3-none-any.whl

---
+++ packages/vk-botting/vk_botting-0.5.1-py3-none-any.whl/vk_botting/context.py
@@ -0,0 +1,136 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2019 MrDandycorn
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from vk_botting.abstract import Messageable
+from vk_botting.user import get_pages
+
+
+class Context(Messageable):
+
+    def __init__(self, **attrs):
+        self.message = attrs.pop('message', None)
+        self.bot = attrs.pop('bot', None)
+        self.args = attrs.pop('args', [])
+        self.kwargs = attrs.pop('kwargs', {})
+        self.prefix = attrs.pop('prefix')
+        self.command = attrs.pop('command', None)
+        self.view = attrs.pop('view', None)
+        self.invoked_with = attrs.pop('invoked_with', None)
+        self.invoked_subcommand = attrs.pop('invoked_subcommand', None)
+        self.subcommand_passed = attrs.pop('subcommand_passed', None)
+        self.command_failed = attrs.pop('command_failed', False)
+
+    async def invoke(self, *args, **kwargs):
+
+        try:
+            command = args[0]
+        except IndexError:
+            raise TypeError('Missing command to invoke.') from None
+
+        arguments = []
+        if command.cog is not None:
+            arguments.append(command.cog)
+
+        arguments.append(self)
+        arguments.extend(args[1:])
+
+        ret = await command.callback(*arguments, **kwargs)
+        return ret
+
+    async def reinvoke(self, *, call_hooks=False, restart=True):
+        cmd = self.command
+        view = self.view
+        if cmd is None:
+            raise ValueError('This context is not valid.')
+
+        index, previous = view.index, view.previous
+        invoked_with = self.invoked_with
+        invoked_subcommand = self.invoked_subcommand
+        subcommand_passed = self.subcommand_passed
+
+        if restart:
+            to_call = cmd.root_parent or cmd
+            view.index = len(self.prefix)
+            view.previous = 0
+            view.get_word()
+        else:
+            to_call = cmd
+
+        try:
+            await to_call.reinvoke(self, call_hooks=call_hooks)
+        finally:
+            self.command = cmd
+            view.index = index
+            view.previous = previous
+            self.invoked_with = invoked_with
+            self.invoked_subcommand = invoked_subcommand
+            self.subcommand_passed = subcommand_passed
+
+    async def reply(self, message=None, *, attachment=None, sticker_id=None, keyboard=None):
+        return await self.message.reply(message, attachment=attachment, sticker_id=sticker_id, keyboard=keyboard)
+
+    async def get_user(self):
+        user = await get_pages(self.bot.token, self.message.from_id)
+        return user[0]
+
+    async def get_author(self):
+        return await self.get_user()
+
+    async def fetch_user(self):
+        return await self.get_user()
+
+    async def fetch_author(self):
+        return await self.get_user()
+
+    @property
+    def cog(self):
+        if self.command is None:
+            return None
+        return self.command.cog
+
+    @property
+    def valid(self):
+        return self.prefix is not None and self.command is not None
+
+    @property
+    def author(self):
+        return self.message.from_id
+
+    @property
+    def from_id(self):
+        return self.message.from_id
+
+    @property
+    def peer_id(self):
+        return self.message.peer_id
+
+    @property
+    def text(self):
+        return self.message.text
+
+    async def _get_conversation(self):
+        return self.message.peer_id
+
+    def me(self):
+        return self.bot.group or self.bot.user
