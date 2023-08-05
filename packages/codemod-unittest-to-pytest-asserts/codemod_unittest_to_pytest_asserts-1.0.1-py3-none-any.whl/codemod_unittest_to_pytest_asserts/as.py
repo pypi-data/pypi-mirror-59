5ad7003fb768ae9f8ca966b3ccfb47f8fbe986c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package codemod-unittest-to-pytest-asserts/1.0.1/codemod_unittest_to_pytest_asserts-1.0.1-py3-none-any.whl

---
+++ packages/codemod-unittest-to-pytest-asserts/codemod_unittest_to_pytest_asserts-1.0.1-py3-none-any.whl/codemod_unittest_to_pytest_asserts/as.py
@@ -0,0 +1,12 @@
+class A:
+    def one():
+        def two():
+            assert 1 != 2
+        
+    def three():
+        assert 3 == 3
+        def four():
+            self.assertEqual(
+                4,
+                4
+            )
