4d3ba1346442f7e297495971a1f52517a52416df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:40 +0000

    Add package rips/2019.12.0.1/rips-2019.12.0.1.tar.gz

---
+++ packages/rips/rips-2019.12.0.1.tar.gz/rips-2019.12.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+	license = f.read()
+
+RIPS_DIST_VERSION = '1'
+	
+setup(
+    name='rips',
+    version='2019.12.0.' + RIPS_DIST_VERSION,
+    description='Python Interface for ResInsight',
+    long_description=readme,
+    author='Ceetron Solutions',
+    author_email='info@ceetronsolutions.com',
+    url='http://www.resinsight.org',
+    license=license,
+    packages=['rips'],
+    package_data={'rips': ['*.py', 'generated/*.py', 'PythonExamples/*.py', 'tests/*.py']}
+)
