ab59841a3d64bf181fe8c6f78bf74f30d669678e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package jellyfin-mpv-shim/0.0.1/jellyfin-mpv-shim-0.0.1.tar.gz

---
+++ packages/jellyfin-mpv-shim/jellyfin-mpv-shim-0.0.1.tar.gz/jellyfin-mpv-shim-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='jellyfin-mpv-shim',
+    version='0.0.1',
+    author="Ian Walton",
+    author_email="iwalton3@gmail.com",
+    description="Cast media from Jellyfin Mobile and Web apps to MPV. (Unofficial)",
+    license='GPLv3',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwalton3/jellyfin-mpv-shim",
+    classifiers=['Development Status :: 1 - Planning'],
+)
