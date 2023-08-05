9f6f90316598737de5dfcff49666618a084c1a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package django-fieldsignals/0.5.0/django-fieldsignals-0.5.0.tar.gz

---
+++ packages/django-fieldsignals/django-fieldsignals-0.5.0.tar.gz/django-fieldsignals-0.5.0/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-fieldsignals',
+    version='0.5.0',
+    packages=['fieldsignals', 'fieldsignals.tests'],
+    include_package_data=True,
+    test_suite='fieldsignals.tests.test_signals',
+    description='Django fieldsignals simply makes it easy to tell when the fields on your model have changed.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/craigds/django-fieldsignals',
+    author='Craig de Stigter',
+    author_email='craig.ds@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
