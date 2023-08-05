352ee7e9d01163937a8ca2b5d1a3cfa480ebf015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:49:01 +0000

    Add package ocr4all-pixel-classifier/0.1.5/ocr4all_pixel_classifier-0.1.5.tar.gz

---
+++ packages/ocr4all-pixel-classifier/ocr4all_pixel_classifier-0.1.5.tar.gz/ocr4all_pixel_classifier-0.1.5/ocr4all_pixel_classifier/scripts/create_dataset_file.py
@@ -0,0 +1,88 @@
+import argparse
+from ocr4all_pixel_classifier.lib.dataset import list_dataset
+from random import shuffle, seed
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=67452)
+    parser.add_argument("--dataset_path", type=str, required=True)
+    parser.add_argument("--output_file", type=str, required=True)
+    parser.add_argument("--char_height_of_n", type=int, default=None)
+    parser.add_argument("--n_eval", type=float, default=0, help="For final model evaluation")
+    parser.add_argument("--n_train", type=float, default=-1, help="For training")
+    parser.add_argument("--n_test", type=float, default=20, help="For picking the best model")
+    parser.add_argument("--binary_dir", type=str, default="binary",
+                        help="directory name of the binary images")
+    parser.add_argument("--images_dir", type=str, default="images",
+                        help="directory name of the images on which to train")
+    parser.add_argument("--masks_dir", type=str, default="masks",
+                        help="directory name of the masks")
+    parser.add_argument("--masks_postfix", type=str, default="",
+                        help="Postfix to distinguish masks and images")
+    parser.add_argument("--normalizations_dir", type=str, default="norm")
+    parser.add_argument("--verify_filenames", action="store_true",
+                        help="File names must match")
+
+    args = parser.parse_args()
+
+    seed(args.seed)
+
+    data_files = list_dataset(args.dataset_path, args.char_height_of_n,
+                              binary_dir_=args.binary_dir,
+                              images_dir_=args.images_dir,
+                              masks_dir_=args.masks_dir,
+                              masks_postfix=args.masks_postfix,
+                              normalizations_dir=args.normalizations_dir,
+                              verify_filenames=args.verify_filenames)
+
+    def fraction_or_absolute(part, collection):
+        if 0 < part < 1:
+            return int(part * len(collection))
+        else:
+            return int(part)
+
+    args.n_eval = fraction_or_absolute(args.n_eval, data_files)
+    args.n_test = fraction_or_absolute(args.n_test, data_files)
+    args.n_train = fraction_or_absolute(args.n_train, data_files)
+
+    if sum([args.n_eval < 0, args.n_train < 0, args.n_test < 0]) > 1:
+        raise Exception("Only one dataset may get all remaining files")
+
+    if args.n_eval < 0:
+        args.n_eval = len(data_files) - args.n_train - args.n_test
+    elif args.n_train < 0:
+        args.n_train = len(data_files) - args.n_eval - args.n_test
+    elif args.n_test < 0:
+        args.n_test = len(data_files) - args.n_eval - args.n_train
+
+    if len(data_files) < args.n_eval + args.n_train + args.n_test:
+        raise Exception("The dataset consists of {} files, but eval + train + test = {} + {} + {} = {}".format(
+            len(data_files), args.n_eval, args.n_train, args.n_test,
+            args.n_eval + args.n_train + args.n_test)
+        )
+
+    indices = list(range(len(data_files)))
+    shuffle(indices)
+
+    eval = [data_files[d] for d in indices[:args.n_eval]]
+    train = [data_files[d] for d in indices[args.n_eval:args.n_eval + args.n_train]]
+    test = [data_files[d] for d in indices[args.n_eval + args.n_train:args.n_eval + args.n_train + args.n_test]]
+
+    content = json.dumps({
+        "seed": args.seed,
+        "dataset_path": args.dataset_path,
+        "eval": eval,
+        "train": train,
+        "test": test,
+    }, indent=4)
+
+    with open(args.output_file, "w") as f:
+        f.write(content)
+
+        print("File written to {}".format(args.output_file))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ocr4all-pixel-classifier/ocr4all_pixel_classifier-0.1.5.tar.gz/ocr4all_pixel_classifier-0.1.5/ocr4all_pixel_classifier/scripts/evaluate.py
@@ -0,0 +1,115 @@
+import argparse
+import glob
+import json
+import os
+import numpy as np
+from typing import Generator, List, Callable, Optional, Union
+
+import tqdm
+
+from ocr4all_pixel_classifier.lib.dataset import DatasetLoader, SingleData
+from ocr4all_pixel_classifier.lib.postprocess import vote_connected_component_class
+from ocr4all_pixel_classifier.lib.predictor import Predictor, PredictSettings, Prediction
+
+
+def glob_all(filenames):
+    return [glob.glob(f) for f in filenames]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load", type=str, required=True,
+                        help="Model to load")
+    parser.add_argument("--char_height", type=int, required=False,
+                        help="Average height of character m or n, ...")
+    parser.add_argument("--target_line_height", type=int, default=6,
+                        help="Scale the data images so that the line height matches this value (must be the same as in training)")
+    parser.add_argument("--output", required=True,
+                        help="Output dir")
+    parser.add_argument("--binary", type=str, required=True, nargs="+",
+                        help="directory name of the binary images")
+    parser.add_argument("--images", type=str, required=True, nargs="+",
+                        help="directory name of the images on which to train")
+    parser.add_argument("--norm", type=str, required=False, nargs="+",
+                        help="directory name of the norms on which to train")
+    parser.add_argument("--keep_low_res", action="store_true",
+                        help="keep low resolution prediction instead of rescaling output to orignal image size")
+    parser.add_argument("--cc_majority", action="store_true",
+                        help="classify all pixels of each connected component as most frequent class")
+    args = parser.parse_args()
+
+    os.makedirs(args.output, exist_ok=True)
+
+    image_file_paths = sorted(glob_all(args.images))
+    binary_file_paths = sorted(glob_all(args.binary))
+
+    norm_file_paths = sorted(glob_all(args.norm)) if args.norm else []
+
+    if len(image_file_paths) != len(binary_file_paths):
+        raise Exception("Got {} images but {} binary images".format(len(image_file_paths), len(binary_file_paths)))
+
+    print("Loading {} files with character height {}".format(len(image_file_paths), args.char_height))
+
+    if not args.char_height and len(norm_file_paths) == 0:
+        raise Exception("Either char height or norm files must be provided")
+
+    if args.char_height:
+        line_heights = [args.char_height] * len(image_file_paths)
+    elif len(norm_file_paths) == 1:
+        line_heights = [json.load(open(norm_file_paths[0]))["char_height"]] * len(image_file_paths)
+    else:
+        if len(norm_file_paths) != len(image_file_paths):
+            raise Exception("Number of norm files must be one or equals the number of image files")
+        line_heights = [json.load(open(n))["char_height"] for n in norm_file_paths]
+
+    post_processors = []
+    if args.cc_majority:
+        post_processors += [vote_connected_component_class]
+
+    predictions = predict(args.output,
+                          binary_file_paths,
+                          image_file_paths,
+                          line_heights,
+                          target_line_height=args.target_line_height,
+                          model=args.load,
+                          high_res_output=not args.keep_low_res,
+                          post_processors=post_processors
+                          )
+
+    for _, _ in tqdm.tqdm(enumerate(predictions)):
+        pass
+
+
+def predict(output,
+            binary_file_paths: List[str],
+            image_file_paths: List[str],
+            line_heights: Union[List[int], int],
+            target_line_height: int,
+            model: str,
+            high_res_output: bool = True,
+            post_processors: Optional[List[Callable[[np.ndarray, SingleData], np.ndarray]]] = None
+            ) -> Generator[Prediction, None, None]:
+    dataset_loader = DatasetLoader(target_line_height, prediction=True)
+
+    if type(line_heights) is int:
+        line_heights = [line_heights] * len(image_file_paths)
+
+    data = dataset_loader.load_data(
+        [SingleData(binary_path=b, image_path=i, line_height_px=n)
+         for b, i, n in zip(binary_file_paths, image_file_paths, line_heights)]
+    )
+
+    settings = PredictSettings(
+        mode='meta',
+        network=os.path.abspath(model),
+        output=output,
+        high_res_output=high_res_output,
+        post_process=post_processors,
+    )
+    predictor = Predictor(settings)
+
+    return predictor.predict(data)
+
+
+if __name__ == "__main__":
+    main()
