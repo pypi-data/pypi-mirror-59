983eeacc21e5ff1d95c19393608ffb841995c38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package dist-dist/0.0/dist_dist-0.0.tar.gz

---
+++ packages/dist-dist/dist_dist-0.0.tar.gz/dist_dist-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_dist',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_dist'],
+      author_email='mihajlovic.aleksa@gmail.com',
+      zip_safe=False)
