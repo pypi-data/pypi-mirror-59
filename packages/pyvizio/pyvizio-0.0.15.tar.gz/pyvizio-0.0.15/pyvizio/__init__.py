1af9a190777ed373efdbdec1b3cc5440029ea215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package pyvizio/0.0.15/pyvizio-0.0.15.tar.gz

---
+++ packages/pyvizio/pyvizio-0.0.15.tar.gz/pyvizio-0.0.15/pyvizio/__init__.py
@@ -0,0 +1,1 @@
+from .vizio import async_guess_device_type, guess_device_type, Vizio, VizioAsync
---
+++ packages/pyvizio/pyvizio-0.0.15.tar.gz/pyvizio-0.0.15/pyvizio/cmd_remote.py
@@ -0,0 +1,22 @@
+from .protocol import CommandBase, KeyCodes, Endpoints
+
+
+class KeyPressEvent(object):
+    def __init__(self, key_code, action=KeyCodes.KeyPressActions.KEY_PRESS):
+        self.CODESET = key_code[0]
+        self.CODE = key_code[1]
+        self.ACTION = action
+
+
+class EmulateRemoteCommand(CommandBase):
+    def __init__(self, key_codes, device_type):
+        super(EmulateRemoteCommand, self).__init__()
+        CommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["KEY_PRESS"])
+        # noinspection SpellCheckingInspection
+        self.KEYLIST = []
+        for key_code in key_codes:
+            event = KeyPressEvent(key_code)
+            self.KEYLIST.append(event)
+
+    def process_response(self, json_obj):
+        return True
