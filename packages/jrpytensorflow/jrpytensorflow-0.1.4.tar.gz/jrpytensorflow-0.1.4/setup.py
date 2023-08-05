c87c9252b1936e0116e345c9f3ee8674190e9f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package jrpytensorflow/0.1.4/jrpytensorflow-0.1.4.tar.gz

---
+++ packages/jrpytensorflow/jrpytensorflow-0.1.4.tar.gz/jrpytensorflow-0.1.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jrpytensorflow', 'jrpytensorflow.datasets']
+
+package_data = \
+{'': ['*'],
+ 'jrpytensorflow': ['vignettes/*'],
+ 'jrpytensorflow.datasets': ['data/*']}
+
+install_requires = \
+['pip>=19,<20',
+ 'scikit-learn>=0.22,<0.23',
+ 'tensorflow-datasets>=1.3,<2.0',
+ 'tensorflow==2.1.0rc1']
+
+setup_kwargs = {
+    'name': 'jrpytensorflow',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
