eca43b9d9bff100a2b7700aa340823f156aa55c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:11 +0000

    Add package pytitan/1.0.13/pytitan-1.0.13.tar.gz

---
+++ packages/pytitan/pytitan-1.0.13.tar.gz/pytitan-1.0.13/titan/utils.py
@@ -0,0 +1,14 @@
+import logging
+import yaml
+import logging.config
+import os
+
+
+def setup_logging(default_path='log.yaml', default_level=logging.INFO):
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            config = yaml.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
