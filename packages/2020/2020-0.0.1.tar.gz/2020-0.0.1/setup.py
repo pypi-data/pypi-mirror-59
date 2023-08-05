692df8733f230014fdd182a1d200f65c92311314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:29 +0000

    Add package 2020/0.0.1/2020-0.0.1.tar.gz

---
+++ packages/2020/2020-0.0.1.tar.gz/2020-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='2020',
+    version='0.0.1',
+    author='asimov',
+    author_email='1019022410@qq.com',
+    packages=setuptools.find_packages(),
+
+)
