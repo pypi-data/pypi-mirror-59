87b59a361891a55ccd601adfaeb855e4194bc40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package sklearn-hierarchical-classification/1.3.2/sklearn-hierarchical-classification-1.3.2.tar.gz

---
+++ packages/sklearn-hierarchical-classification/sklearn-hierarchical-classification-1.3.2.tar.gz/sklearn-hierarchical-classification-1.3.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+project = "sklearn-hierarchical-classification"
+version = "1.3.2"
+
+
+setup(
+    name=project,
+    version=version,
+    description="Hierarchical classification interface extensions for scikit-learn",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Globality Engineering",
+    author_email="engineering@globality.com",
+    url="https://github.com/globality-corp/sklearn-hierarchical-classification",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "networkx>=2.4",
+        "numpy>=1.13.1",
+        "scikit-learn>=0.19.0",
+        "scipy>=0.19.1",
+    ],
+    setup_requires=[
+        "nose>=1.3.7",
+    ],
+    extras_require={
+        "test": [
+            "PyHamcrest>=1.9.0",
+            "coverage>=3.7.1",
+            "inflect>=4.0.0",
+            "parameterized>=0.7.1",
+        ],
+    },
+)
