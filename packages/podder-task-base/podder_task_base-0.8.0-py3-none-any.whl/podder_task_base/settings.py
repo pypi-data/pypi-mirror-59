ecff68afc5828a210b5aabb7d67633742ae87d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:29 +0000

    Add package podder-task-base/0.8.0/podder_task_base-0.8.0-py3-none-any.whl

---
+++ packages/podder-task-base/podder_task_base-0.8.0-py3-none-any.whl/podder_task_base/settings.py
@@ -0,0 +1,18 @@
+import os
+from typing import Optional
+
+from dotenv import load_dotenv
+
+
+def init() -> None:
+    load_dotenv()
+
+
+def get(key: str) -> Optional[str]:
+    return os.getenv(key)
+
+
+# Shortcut variables for Framework.
+PIPELINE_DATABASE_URL = get('PIPELINE_DATABASE_URL')
+PIPELINE_READ_ONLY_DATABASE_URL = get('PIPELINE_READ_ONLY_DATABASE_URL')
+GOOGLE_STORAGE_BUCKET = get('GOOGLE_STORAGE_BUCKET')
