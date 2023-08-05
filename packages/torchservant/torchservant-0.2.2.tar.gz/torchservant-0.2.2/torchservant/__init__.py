b4a5b5337062ba38dc0f37954cc8806acf24ed54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:23 +0000

    Add package torchservant/0.2.2/torchservant-0.2.2.tar.gz

---
+++ packages/torchservant/torchservant-0.2.2.tar.gz/torchservant-0.2.2/torchservant/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/09/15 19:34
+# @Author  : LQX
+# @Email   : qxsoftware@163.com
+# @File    : __init__.py
+# @Software: PyCharm
+
+# from torchservant import modelservant
+# from torchservant import cfgenator
+# from torchservant import procmonitor
+# from torchservant import stats
+# from torchservant import classicmodels
+# from torchservant import visualfeature
+# from torchservant import weightransfer
---
+++ packages/torchservant/torchservant-0.2.2.tar.gz/torchservant-0.2.2/torchservant/cfgenator/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/01/09 22:04
+# @Author  : LQX
+# @Email   : qxsoftware@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from torchservant.cfgenator.config import BasicConfig
