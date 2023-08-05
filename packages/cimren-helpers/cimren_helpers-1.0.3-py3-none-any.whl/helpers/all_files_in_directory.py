ba60beb079a1770370403246decd5e75fa965e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:25 +0000

    Add package cimren-helpers/1.0.3/cimren_helpers-1.0.3-py3-none-any.whl

---
+++ packages/cimren-helpers/cimren_helpers-1.0.3-py3-none-any.whl/helpers/all_files_in_directory.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def searching_all_files_in_directory(directory):
+    '''
+    Function to pull files in a directory
+    :param directory: path to the directory
+    :return: list of files with whole path
+    '''
+    dirpath = Path(directory)
+    assert(dirpath.is_dir())
+    file_list = []
+    for x in dirpath.iterdir():
+        print(x)
+        if x.is_file():
+            file_list.append(x)
+        elif x.is_dir():
+            file_list.extend(searching_all_files_in_directory(x))
+
+    return file_list
---
+++ packages/cimren-helpers/cimren_helpers-1.0.3-py3-none-any.whl/helpers/haversine_distance.py
@@ -0,0 +1,61 @@
+import math
+import numpy as np
+
+
+def _get_rads(latitude, longitude):
+    '''
+    Calculate radius
+    :param latitude:
+    :param longitude:
+    :return:
+    '''
+
+    latitude_rads = (math.pi / 180) * latitude
+    longitude_rads = (math.pi / 180) * longitude
+
+    latitude_cos = np.cos(latitude_rads)
+
+    cord_x = 0.5 * latitude_cos * np.sin(longitude_rads)
+    cord_y = 0.5 * latitude_cos * np.cos(longitude_rads)
+    cord_z = 0.5 * np.sin(latitude_rads)
+
+    return cord_x, cord_y, cord_z
+
+
+def calculate_haversine_distance(from_latitude,
+                                 from_longitude,
+                                 to_latitude,
+                                 to_longitude,
+                                 circuity=1.17,
+                                 earth_radius=3959):
+
+    '''
+    :param from_latitude: List of origin latitudes
+    :param from_longitude: List of origin longitudes
+    :param to_latitude: List of destination latitudes
+    :param to_longitude: List of destination longitudes
+    :param circuity:
+    :param earth_radius:
+    :return: List of distances in Miles
+    '''
+
+    from_latitude = np.array(from_latitude)
+    from_longitude = np.array(from_longitude)
+
+    to_latitude = np.array(to_latitude)
+    to_longitude = np.array(to_longitude)
+
+    from_x, from_y, from_z = _get_rads(from_latitude, from_longitude)
+    to_x, to_y, to_z = _get_rads(to_latitude, to_longitude)
+
+    distance_x = from_x - to_x
+    distance_y = from_y - to_y
+    distance_z = from_z - to_z
+
+    distance = np.arcsin(np.sqrt((distance_x * distance_x +
+                                  distance_y * distance_y +
+                                  distance_z * distance_z)))
+
+    distance = distance * 2 * circuity * earth_radius
+
+    return distance
