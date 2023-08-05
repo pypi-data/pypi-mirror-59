136ac69ffa93cc45af16c5b26ad36e4b18bd1300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:36 +0000

    Add package arcade/2.2.3b1/arcade-2.2.3b1-py2.py3-none-any.whl

---
+++ packages/arcade/arcade-2.2.3b1-py2.py3-none-any.whl/arcade/test.py
@@ -0,0 +1,10 @@
+
+import arcade
+
+
+player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png")
+print(player_sprite.points)
+player_sprite.angle = 45
+print(player_sprite.points)
+player_sprite.scale = 0.5
+print(player_sprite.points)
