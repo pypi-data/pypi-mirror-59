14be2d67a20a2addd913a56e453ddb82c6b56ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:52 +0000

    Add package eyes-images/4.1.5/eyes_images-4.1.5.tar.gz

---
+++ packages/eyes-images/eyes_images-4.1.5.tar.gz/eyes_images-4.1.5/applitools/images/__init__.py
@@ -0,0 +1,21 @@
+from applitools.common import BatchInfo, MatchLevel, Region, logger
+from applitools.core.cut import (
+    FixedCutProvider,
+    NullCutProvider,
+    UnscaledFixedCutProvider,
+)
+from applitools.images.fluent import Target
+
+from .eyes import Eyes
+
+__all__ = (
+    "Eyes",
+    "BatchInfo",
+    "Region",
+    "MatchLevel",
+    "logger",
+    "Target",
+    "FixedCutProvider",
+    "UnscaledFixedCutProvider",
+    "NullCutProvider",
+)
