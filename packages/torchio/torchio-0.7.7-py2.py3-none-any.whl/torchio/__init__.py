e945d87041ce44dbb91942bc645c26bd16461b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package torchio/0.7.7/torchio-0.7.7-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.7.7-py2.py3-none-any.whl/torchio/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for torchio."""
+
+__author__ = """Fernando Perez-Garcia"""
+__email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
+__version__ = '0.7.7'
+
+from . import io
+from . import utils
+from . import sampler
+from . import inference
+from .torchio import *
+from .queue import Queue
+from .transforms import *
+from .dataset import ImagesDataset
