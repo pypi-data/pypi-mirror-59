12b10f6ede2c96863e263b1cd4336a02addce91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package mypy-boto3-sagemaker-runtime/1.10.48.0/mypy-boto3-sagemaker-runtime-1.10.48.0.tar.gz

---
+++ packages/mypy-boto3-sagemaker-runtime/mypy-boto3-sagemaker-runtime-1.10.48.0.tar.gz/mypy-boto3-sagemaker-runtime-1.10.48.0/mypy_boto3_sagemaker_runtime/type_defs.py
@@ -0,0 +1,36 @@
+"""
+Main interface for sagemaker-runtime service type definitions.
+
+Usage::
+
+    from mypy_boto3.sagemaker_runtime.type_defs import InvokeEndpointOutputTypeDef
+
+    data: InvokeEndpointOutputTypeDef = {...}
+"""
+from __future__ import annotations
+
+import sys
+from typing import IO, Union
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("InvokeEndpointOutputTypeDef",)
+
+_RequiredInvokeEndpointOutputTypeDef = TypedDict(
+    "_RequiredInvokeEndpointOutputTypeDef", {"Body": Union[bytes, IO]}
+)
+_OptionalInvokeEndpointOutputTypeDef = TypedDict(
+    "_OptionalInvokeEndpointOutputTypeDef",
+    {"ContentType": str, "InvokedProductionVariant": str, "CustomAttributes": str},
+    total=False,
+)
+
+
+class InvokeEndpointOutputTypeDef(
+    _RequiredInvokeEndpointOutputTypeDef, _OptionalInvokeEndpointOutputTypeDef
+):
+    pass
