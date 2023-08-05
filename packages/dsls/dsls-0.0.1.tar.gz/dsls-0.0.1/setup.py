9c9307f125b32868e4e03e8c6ca32eabe1d7b57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package dsls/0.0.1/dsls-0.0.1.tar.gz

---
+++ packages/dsls/dsls-0.0.1.tar.gz/dsls-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dsls',
+    version='0.0.1',
+    description='DSLs parent package',
+    long_description='',
+    url='https://github.com/python-memes-for-dsl-teens',
+    author='python-memes-for-dsl-teens',
+    author_email='david@daviddworken.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
