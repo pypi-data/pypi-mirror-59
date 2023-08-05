b7a564de35df9e3642169fff60478b8295bb90ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package aiolos-commissioning-tools/0.1.0/aiolos-commissioning-tools-0.1.0.tar.gz

---
+++ packages/aiolos-commissioning-tools/aiolos-commissioning-tools-0.1.0.tar.gz/aiolos-commissioning-tools-0.1.0/code_instrumentation/timer/timer.py
@@ -0,0 +1,50 @@
+
+import inspect
+import os
+import time
+import sys
+import logging
+
+from code_instrumentation.add_logging_level import addLoggingLevel, TIMER
+addLoggingLevel("TIMER", TIMER, methodName=None)
+
+
+log = logging.getLogger(__name__)
+
+class Timer:
+
+    m_name: str
+    m_mod: str
+    m_start: float
+    m_pid: int
+
+    def __init__(self):
+        self.m_name = func_name()
+        self.m_mod  = mod_name()
+        self.m_start = time.time()
+        self.m_pid = os.getppid()
+
+    def __del__(self):
+        log.timer("<%s.%s> completed in %.2f ms" % (self.m_mod, self.m_name, (time.time() - self.m_start)*1000))
+
+
+def func_name():
+    return inspect.stack()[2][3]
+
+def mod_name():
+    # return inspect.stack()[3][3]
+    return inspect.getmodule(inspect.stack()[2][0]).__name__
+
+def test():
+
+    timer = Timer()
+    for i in range(10000):
+        x = i**2
+        print(x)
+
+
+if __name__ == '__main__':
+
+
+    test()
+    input('Press <Enter> to exit')
