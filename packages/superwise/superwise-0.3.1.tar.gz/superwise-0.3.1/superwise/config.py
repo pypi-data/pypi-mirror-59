004aa204009fd89db3490fee45825b1e258c65ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package superwise/0.3.1/superwise-0.3.1.tar.gz

---
+++ packages/superwise/superwise-0.3.1.tar.gz/superwise-0.3.1/superwise/config.py
@@ -0,0 +1,15 @@
+import os
+
+import yaml
+
+from superwise.project_root import PROJECT_ROOT
+
+
+class Config:
+
+    config_file_path = os.path.join(PROJECT_ROOT, "resources", "config.yml")
+
+    with open(config_file_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+    SW_HOST = os.getenv('SW_HOST', config['SW_HOST'])
