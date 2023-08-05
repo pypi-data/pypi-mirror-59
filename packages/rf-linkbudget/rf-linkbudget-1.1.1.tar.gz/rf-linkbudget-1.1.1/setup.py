9d0f18041cead1c28e9478c6ffda3607973bde91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package rf-linkbudget/1.1.1/rf-linkbudget-1.1.1.tar.gz

---
+++ packages/rf-linkbudget/rf-linkbudget-1.1.1.tar.gz/rf-linkbudget-1.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='rf-linkbudget',
+                 version='1.1.1',
+                 description='A simple rf-linkbudget calculation tool',
+                 url='https://github.com/hwengineer/rf_linkbudget',
+                 author='Alexander Ott',
+                 author_email='alexander.ott.ottale@gmail.com',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 classifiers=["Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent", ],
+                 python_requires='>=3.5',
+                 zip_safe=False)
