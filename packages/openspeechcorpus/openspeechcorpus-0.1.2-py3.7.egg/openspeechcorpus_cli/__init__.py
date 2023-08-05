22f2dd0b7da28e67a3992b3ea978448d5515bba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:08 +0000

    Add package openspeechcorpus/0.1.2/openspeechcorpus-0.1.2-py3.7.egg

---
+++ packages/openspeechcorpus/openspeechcorpus-0.1.2-py3.7.egg/openspeechcorpus_cli/__init__.py
@@ -0,0 +1,3 @@
+from .ops import execute_from_command_line as ops_execute_from_command_line
+from .recursive_convert import execute_from_command_line as recursive_convert_execute_from_command_line
+from .cmu_sphinx import execute_from_command_line as configure_sphinx_execute_from_command_line
