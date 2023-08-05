62bddb4cf42f180b2709c06f8d49d74c00c60600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package iptvtools/0.1.6/iptvtools-0.1.6-py3-none-any.whl

---
+++ packages/iptvtools/iptvtools-0.1.6-py3-none-any.whl/iptvtools/constants/defaults.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: constants.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: Defaults for iptvtools.
+"""
+CONFIG = 'config.json'
+INPUT = 'https://iptv-org.github.io/iptv/index.m3u'
+INTERVAL = 1
+MIN_HEIGHT = 0
+OUTPUT = 'iptvtools.m3u'
+TIMEOUT = 10
+UDPXY = None
---
+++ packages/iptvtools/iptvtools-0.1.6-py3-none-any.whl/iptvtools/iptv_filter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: main.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: Filter IPTV m3u playlists according to customized criteria.
+"""
+import argparse
+
+from . import __version__
+from .config import Config
+from .constants import defaults
+from .constants import helps
+from .models import Playlist
+
+
+def parse_args():
+    """Arguments Parsers."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--min-height', default=defaults.MIN_HEIGHT, type=int,
+                        help=helps.MIN_HEIGHT)
+    parser.add_argument('-c', '--config', default=defaults.CONFIG,
+                        help=helps.CONFIG)
+    parser.add_argument('-i', '--input', action='append', default=[],
+                        help=helps.INPUT)
+    parser.add_argument('-I', '--interval', default=defaults.INTERVAL,
+                        type=int, help=helps.INTERVAL)
+    parser.add_argument('-o', '--output', default=defaults.OUTPUT,
+                        help=helps.OUTPUT)
+    parser.add_argument('-r', '--replace-group-by-source', action='store_true',
+                        help=helps.REPLACE_GROUP_BY_SOURCE)
+    parser.add_argument('-t', '--template', action='append', default=[],
+                        help=helps.TEMPLATE)
+    parser.add_argument('-T', '--timeout', default=defaults.TIMEOUT, type=int,
+                        help=helps.TIMEOUT)
+    parser.add_argument('-u', '--udpxy', default=defaults.UDPXY,
+                        help=helps.UDPXY)
+    parser.add_argument('-v', '--version', action='version',
+                        version=__version__)
+    return parser.parse_args()
+
+
+def main():
+    """Main process."""
+    args = parse_args()
+    if not args.input:
+        args.input = [defaults.INPUT]
+    Config.init(args.config)
+    playlist = Playlist()
+    playlist.parse(args)
+    playlist.filter(args)
+    open(args.output, 'w', encoding='utf-8').write(playlist.export(args))
+    print('Invalid Urls:')
+    print('\n'.join(sorted(playlist.invalid_urls)))
+
+
+if __name__ == '__main__':
+    main()
