928cc7552f121c12c3fd6fd166650129e50006d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:20 +0000

    Add package sequelize-to-excel/1.0.0/sequelize_to_excel-1.0.0.tar.gz

---
+++ packages/sequelize-to-excel/sequelize_to_excel-1.0.0.tar.gz/sequelize_to_excel-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sequelize_to_excel',
+    version='1.0.0',
+    py_modules=['sequelize_to_excel'],
+    author='Darshan Bharat',
+    author_email='mailshahdarshan@gmail.com',
+    url='https://github.com/darshan99/sequelize_to_excel',
+    keywords=['Node','excel','Sequelize'],
+    description="map your sequelize columns to excel"
+)
