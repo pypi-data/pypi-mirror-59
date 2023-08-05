d6740a84a568532793359242a6ddffb63dfaa08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:19 +0000

    Add package linearmodels/4.15.4/linearmodels-4.15.4-cp36-cp36m-win_amd64.whl

---
+++ packages/linearmodels/linearmodels-4.15.4-cp36-cp36m-win_amd64.whl/linearmodels/asset_pricing/__init__.py
@@ -0,0 +1,5 @@
+from linearmodels.asset_pricing.model import (LinearFactorModel,
+                                              LinearFactorModelGMM,
+                                              TradedFactorModel)
+
+__all__ = ["TradedFactorModel", "LinearFactorModelGMM", "LinearFactorModel"]
