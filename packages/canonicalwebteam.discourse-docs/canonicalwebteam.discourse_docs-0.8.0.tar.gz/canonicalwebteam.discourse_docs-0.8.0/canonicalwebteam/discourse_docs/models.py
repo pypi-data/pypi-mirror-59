504e37e0f6c145bccac2cc0b1cc37329955f0640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:29 +0000

    Add package canonicalwebteam.discourse-docs/0.8.0/canonicalwebteam.discourse_docs-0.8.0.tar.gz

---
+++ packages/canonicalwebteam.discourse-docs/canonicalwebteam.discourse_docs-0.8.0.tar.gz/canonicalwebteam.discourse_docs-0.8.0/canonicalwebteam/discourse_docs/models.py
@@ -0,0 +1,32 @@
+# Packages
+from canonicalwebteam.http import CachedSession
+
+
+class DiscourseAPI:
+    """
+    Retrieve information from a Discourse installation
+    through its API
+    """
+
+    def __init__(
+        self, base_url, session=CachedSession(fallback_cache_duration=300)
+    ):
+        """
+        @param base_url: The Discourse URL (e.g. https://discourse.example.com)
+        """
+
+        self.base_url = base_url.rstrip("/")
+        self.session = session
+
+    def __del__(self):
+        self.session.close()
+
+    def get_topic(self, topic_id):
+        """
+        Retrieve topic object by path
+        """
+
+        response = self.session.get(f"{self.base_url}/t/{topic_id}.json")
+        response.raise_for_status()
+
+        return response.json()
