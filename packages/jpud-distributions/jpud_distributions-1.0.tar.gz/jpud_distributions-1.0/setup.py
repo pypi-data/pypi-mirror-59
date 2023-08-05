dd8d214b93f02b29b7828b4fec5d393a4a39dc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:07 +0000

    Add package jpud-distributions/1.0/jpud_distributions-1.0.tar.gz

---
+++ packages/jpud-distributions/jpud_distributions-1.0.tar.gz/jpud_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jpud_distributions',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['jpud_distributions'],
+      author = 'Joao Paulo Ramos',
+      author_emai = 'joao.oramos11@gmail.com',
+      zip_safe=False)
