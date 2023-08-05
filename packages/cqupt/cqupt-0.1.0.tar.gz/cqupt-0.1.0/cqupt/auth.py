1576ccf8a362fd3517eb78fca5779cfbb5e4f00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:08 +0000

    Add package cqupt/0.1.0/cqupt-0.1.0.tar.gz

---
+++ packages/cqupt/cqupt-0.1.0.tar.gz/cqupt-0.1.0/cqupt/auth.py
@@ -0,0 +1,84 @@
+from cqupt import PIPER_HOME
+from cqupt.log import Log
+from cqupt.fs import b64_fwrite, b64_fread
+from time import time
+from getpass import getpass
+from os import mkdir, path
+from shutil import rmtree
+
+PATH_PIPER_COOKIE = f'{PIPER_HOME}/cookie'
+PATH_PIPER_USER = f'{PIPER_HOME}/user'
+
+MAX_COOKIE_LIVE = 60 * 15  # 15mins
+
+class Auth:
+    @classmethod
+    def init(cls):
+        if not path.isdir(PIPER_HOME):
+            mkdir(PIPER_HOME)
+        if not path.isfile(PATH_PIPER_COOKIE):
+            b64_fwrite(PATH_PIPER_COOKIE, None)
+        if not path.isfile(PATH_PIPER_USER):
+            b64_fwrite(PATH_PIPER_USER, None)
+
+    @classmethod
+    def drop_config(cls):
+        rmtree(PIPER_HOME)
+
+    @classmethod
+    def clear_cookie(cls):
+        cls.save_cookie(None)
+
+    @classmethod
+    def load_config(cls) -> dict:
+        return {
+            'user': cls.load_user(),
+            'cookie': cls.read_cookie()
+        }
+
+    @classmethod
+    def read_cookie(cls) -> dict or None:
+        cookie: dict = eval(b64_fread(PATH_PIPER_COOKIE))
+        # The math here can't be wrong!
+        if cookie and int(time()) - cookie.get('ts') < MAX_COOKIE_LIVE:
+            return cookie.get('cookie')
+        return None
+
+    @classmethod
+    def save_cookie(cls, cookie: dict):
+        b64_fwrite(PATH_PIPER_COOKIE, {
+            'cookie': cookie,
+            'ts': int(time())
+        })
+
+    @classmethod
+    def load_user(cls) -> dict or None:
+        user: dict = eval(b64_fread(PATH_PIPER_USER))
+        return user
+
+    @classmethod
+    def save_user(cls, user: dict):
+        b64_fwrite(PATH_PIPER_USER, user)
+
+    @classmethod
+    def add_user(cls):
+        print('为确保本人操作')
+        print('第一次登录需输入学号密码')
+        print('输入密码时不可见, 直接输就行')
+        return cls.enter_user()
+
+    @classmethod
+    def update_user(cls, message: str = '要更改绑定吗?'):
+        if input(f'{message} [y/N]: ') != 'N':
+            cls.enter_user()
+            Log.fatal('更改成功')
+        else:
+            Log.fatal('运行命令 cqupt --auth 可以更改密码哦~')
+
+    @classmethod
+    def enter_user(cls):
+        user: dict = {}
+        user['userid'] = input('学号: ')
+        user['password'] = getpass(prompt='密码: ')
+        cls.save_user(user)
+        return user
---
+++ packages/cqupt/cqupt-0.1.0.tar.gz/cqupt-0.1.0/cqupt/piper.py
@@ -0,0 +1,103 @@
+from cqupt import __version__, __description__
+from cqupt.log import Log
+from cqupt.auth import Auth
+from cqupt.urls import URL_LOGIN
+from cqupt.error import Catch
+from cqupt.request import Request
+from cqupt.crawlers import Crawler
+from argparse import ArgumentParser, SUPPRESS
+from requests import Session, ConnectionError
+from sys import argv
+
+
+class Piper:
+    def __init__(self):
+        Auth.init()
+        self.session: Session = Session()
+        self.user: dict = Auth.load_user()
+        self.cookie: dict = Auth.read_cookie()
+
+    def authorize(self):
+        if not self.user:
+            self.user = Auth.add_user()
+
+        if self.cookie is None:
+            try:
+                self.session.get(URL_LOGIN)
+            except ConnectionError:
+                Log.fatal('网络走丢啦~')
+            
+            self.cookie = self.session.cookies.get_dict()
+            # print(self.cookie)
+            Auth.save_cookie(self.cookie)
+            Request.handle_login(Request.login(self.user, self.cookie))
+        else:
+            self.session.cookies.update(self.cookie)
+
+    def handle_args(self, args):
+        if args.drop:
+            self.drop()
+        if args.clear:
+            self.clear()
+        if args.whoami:
+            self.show_config()
+        if args.auth:
+            Auth.update_user('确定要更改绑定吗?')
+
+    def clear(self):
+        Auth.clear_cookie()
+        Log.fatal('已清除缓存')
+
+    def drop(self):
+        Auth.drop_config()
+        Log.fatal('已删除配置')
+
+    def show_config(self):
+        config: dict = Auth.load_config()
+        if config.get('user'):
+            print('学号:', config.get('user').get('userid'))
+            print('密码:', config.get('user').get('password'))
+        exit(0)
+
+
+def construct_args():
+    parser = ArgumentParser(prog='cqupt', description=__description__, )
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    
+    group_internal = parser.add_argument_group('设置')
+    group_internal.add_argument('-a', '--auth', action='store_true', help='绑定学号')
+    group_internal.add_argument('-d', '--drop', action='store_true', help='删除配置')
+    group_internal.add_argument('-c', '--clear', action='store_true', help='清除缓存')
+    group_internal.add_argument('--whoami', action='store_true', help='显示配置')
+
+    group_crawler = parser.add_argument_group('教务在线查询')
+    group_crawler.add_argument('--fee', metavar='学年', type=int, const=-1, nargs='?', help='获取学年学费')
+    group_crawler.add_argument('--gpa', metavar='学年', type=int, const=-1, nargs='?', help='获取学年绩点')
+    group_crawler.add_argument('--credit', metavar='学年', type=int, const=-1, nargs='?', help='获取学年学分')
+    group_crawler.add_argument('--photo', metavar='学号', type=str, const='self', nargs='?', help='获取学生照片')
+    group_crawler.add_argument('--task', action='store_true', help='获取考试安排')
+    group_crawler.set_defaults(handle=Crawler().handle)
+
+    return parser.parse_args()
+
+
+@Catch.all
+@Catch.keyboard_interrupt
+def cli():
+    if len(argv) <= 1:
+        exit(0)
+
+    piper = Piper()
+
+    args = construct_args()
+    # print(args)
+
+    piper.handle_args(args)
+   
+    piper.authorize()
+
+    if hasattr(args, 'handle'):
+        args.handle(piper, args)
+    else:
+        print('help')
+
