f4abb7b55d33df8071a2b13b2dad246cb35ea913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:47:31 +0000

    Add package tsrc/1.0.0/tsrc-1.0.0-py3-none-any.whl

---
+++ packages/tsrc/tsrc-1.0.0-py3-none-any.whl/tsrc/repo.py
@@ -0,0 +1,26 @@
+""" Repo value object """
+
+import attr
+from typing import Optional, List  # noqa
+
+
+@attr.s(frozen=True)
+class Remote:
+    name = attr.ib()  # type: str
+    url = attr.ib()  # type: str
+
+
+@attr.s(frozen=True)
+class Repo:
+    src = attr.ib()  # type: str
+    branch = attr.ib(default="master")  # type: str
+    sha1 = attr.ib(default=None)  # type: Optional[str]
+    tag = attr.ib(default=None)  # type: Optional[str]
+    shallow = attr.ib(default=None)  # type: Optional[bool]
+
+    remotes = attr.ib(default=[])  # type: List[Remote]
+
+    @property
+    def clone_url(self) -> str:
+        assert self.remotes
+        return self.remotes[0].url
