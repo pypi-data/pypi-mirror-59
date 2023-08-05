b556d13ac97d460ef4199ccc6a252199ca477079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:10 +0000

    Add package ai2thor/2.2.0/ai2thor-2.2.0.tar.gz

---
+++ packages/ai2thor/ai2thor-2.2.0.tar.gz/ai2thor-2.2.0/ai2thor/_builds.py
@@ -0,0 +1,11 @@
+# GENERATED FILE - DO NOT EDIT
+VERSION = '202001071627'
+BUILDS = {'Darwin': {'build_exception': '',
+            'log': 'thor-202001071627-OSXIntel64.log',
+            'sha256': '284c618c18fa66f6595ec2f3af50e7ee0e739d86c3154e098dec4e2d9a5d48cc',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-202001071627-OSXIntel64.zip'},
+ 'Docker': {'tag': '202001071627'},
+ 'Linux': {'build_exception': '',
+           'log': 'thor-202001071627-Linux64.log',
+           'sha256': '7a75c58289627736b5905ac1687a783f0d9c519fdba000331ed6448c64d8a1ab',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-202001071627-Linux64.zip'}}
