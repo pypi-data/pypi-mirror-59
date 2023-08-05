06de36af5ce741de333f50694a06869e8d714674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:50:19 +0000

    Add package ewlibs/0.0.18/ewlibs-0.0.18-py3-none-any.whl

---
+++ packages/ewlibs/ewlibs-0.0.18-py3-none-any.whl/ewlibs/file/paging.py
@@ -0,0 +1,39 @@
+#mongodb数据分页
+def file_paging(
+        file_name,
+        const_page_count="1000",
+        const_skip_count="0",
+        skip_head=False,
+):
+    def decorator(fn):
+        def objectMethod(data_list, *args, **kwargs):
+            with open(file_name, "r") as f:
+                line = f.readline()
+                if skip_head:
+                    n = 0
+                    dl = []
+                else:
+                    n = 1
+                    dl = [
+                        line,
+                    ]
+                while line:
+                    line = f.readline().strip()
+                    if not line:
+                        break
+                    n += 1
+                    if n <= int(const_skip_count):
+                        dl = []
+                        continue
+                    dl.append(line)
+                    if len(dl) == int(const_page_count):
+                        fn(dl, *args, **kwargs)
+                        print(f"finished:{n}")
+                        dl = []
+                if dl:
+                    fn(dl, *args, **kwargs)
+                    print(f"finished:{n}")
+
+        return objectMethod
+
+    return decorator
