9759bd7da67fcc68e030558401b864742d6bd9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package kili/1.0.13/kili-1.0.13.tar.gz

---
+++ packages/kili/kili-1.0.13.tar.gz/kili-1.0.13/kili/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__version__ = "1.0.13"
+
+from kili import mutations, quality, queries
---
+++ packages/kili/kili-1.0.13.tar.gz/kili-1.0.13/kili/queries/project.py
@@ -0,0 +1,69 @@
+from ..helper import format_result
+
+
+def get_projects(client, user_id: str):
+    result = client.execute('''
+    query {
+      getProjects(userID: "%s") {
+        id
+        title
+        consensusTotCoverage
+        maxWorkerCount
+        minAgreement
+        minConsensusSize
+        numberOfAssets
+        completionPercentage
+        numberOfRemainingAssets
+        numberOfAssetsWithSkippedLabels
+        numberOfReviewedAssets
+        numberOfLatestLabels
+        roles {
+          id
+          user { id, name, email }
+          role
+          lastLabelingAt
+          numberOfAnnotations
+          numberOfLabeledAssets
+          totalDuration
+          durationPerLabel
+          honeypotMark
+        }
+        dataset {
+          id
+          honeypotMark
+        }
+      }
+    }
+    ''' % (user_id))
+    return format_result('getProjects', result)
+
+
+def get_project(client, project_id: str):
+    result = client.execute('''
+    query {
+      getProject(projectID: "%s") {
+        id
+        title
+        numberOfAssets
+        interfaceCategory
+        calculatedConsensusMark
+        calculatedHoneypotMark
+        consensusMark
+        roles {
+          id
+          user { id, name, email }
+          role
+          lastLabelingAt
+          numberOfAnnotations
+          numberOfLabeledAssets
+          totalDuration
+          durationPerLabel
+          honeypotMark
+          calculatedConsensusMark
+          calculatedHoneypotMark
+          consensusMark
+        }
+      }
+    }
+    ''' % (project_id))
+    return format_result('getProject', result)
