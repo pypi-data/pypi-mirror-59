23834a14e2a6b0c54ff06da52bebd9100df0f5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:59 +0000

    Add package cwl-utils/0.3/cwl-utils-0.3.tar.gz

---
+++ packages/cwl-utils/cwl-utils-0.3.tar.gz/cwl-utils-0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cwl-utils',
+    version='0.3',
+    author='Common workflow language working group',
+    author_email='common-workflow-language@googlegroups.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'ruamel.yaml',
+        'six',
+        'requests',
+        'schema_salad',
+        'typing_extensions',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    scripts=['cwl_utils/docker_extract.py'],
+)
