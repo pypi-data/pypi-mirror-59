cb6e5c35bfade4465c538048b5e86e252038dfae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:20 +0000

    Add package xl-tool/0.0.2/xl_tool-0.0.2-py3.7.egg

---
+++ packages/xl-tool/xl_tool-0.0.2-py3.7.egg/xl_tool/xl_image_preprocessing.py
@@ -0,0 +1,23 @@
+"""图片预处理"""
+
+def grey_world(img_array):
+    """白平衡处理函数
+    Args:
+        img_array:输入三通道图像数组
+    """
+    R = img_array[:,:,0].mean()
+    G = img_array[:,:,1].mean()
+    B = img_array[:,:,2].mean()
+    avg = (B + G + R) / 3  
+    img_array[:,:,0] = np.minimum(img_array[:,:,0] * (avg / R), 255)  
+    img_array[:,:,1] = np.minimum(img_array[:,:,1] * (avg / G), 255)  
+    img_array[:,:,2] = np.minimum(img_array[:,:,2] * (avg / B), 255)  
+    return  img_array.astype(np.uint8)
+
+def read_image(file, target_size=(224,224)):
+    """以指定的尺寸读取数据，并返回图像数组"""
+    img = Image.open(file)
+    img = img.resize(target_size)
+    if img.mode in ["P","L"]:
+        img = img.convert("RGB")
+    return np.array(img)
