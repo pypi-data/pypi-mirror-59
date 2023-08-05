feee854c2af3c40dd4a27ad6600bbf1dcf0f802a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:16 +0000

    Add package nestercam/1.0.0/nestercam-1.0.0.tar.gz

---
+++ packages/nestercam/nestercam-1.0.0.tar.gz/nestercam-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nestercam',
+	version = '1.0.0',
+	py_modules = ['nestercam'],
+	author = 'cameronprattedwards',
+	author_email = 'cameronprattedwards@gmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'a simple printer of nested lists',
+)
