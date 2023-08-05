87cc14540d7cb6ba756cd715010f169fa854cf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package epcpm/2020.1/epcpm-2020.1.tar.gz

---
+++ packages/epcpm/epcpm-2020.1.tar.gz/epcpm-2020.1/src/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '30034881'
+build_number = '568'
+build_version = '1.0.568'
+job_id = 'lk3vbfbpxsluv8if'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/pm/build/job/lk3vbfbpxsluv8if'
