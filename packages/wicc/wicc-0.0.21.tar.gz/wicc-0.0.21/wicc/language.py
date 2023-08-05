c5b9123ecf098003d9b398d966920479b5c6da6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:30 +0000

    Add package wicc/0.0.21/wicc-0.0.21.tar.gz

---
+++ packages/wicc/wicc-0.0.21.tar.gz/wicc-0.0.21/wicc/language.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+
+
+class Language(Enum):
+    """
+    语言类型
+    language
+    """
+    English = 0
+    French = 1
+    Spanish = 2
+    Italian = 3
+    Korean = 4
+    Japanese = 5
+    Simplified_Chinese = 6
