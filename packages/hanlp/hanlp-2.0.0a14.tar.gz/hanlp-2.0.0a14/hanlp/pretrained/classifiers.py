4fee5c3e61e08fdad403d7e4dd1983db6235fe63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:04 +0000

    Add package hanlp/2.0.0a14/hanlp-2.0.0a14.tar.gz

---
+++ packages/hanlp/hanlp-2.0.0a14.tar.gz/hanlp-2.0.0a14/hanlp/pretrained/classifiers.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2020-01-01 03:51
+from hanlp.common.constant import HANLP_URL
+
+CHNSENTICORP_BERT_BASE_ZH = HANLP_URL + 'classification/chnsenticorp_bert_base_20200104_164655.zip'
+SST2_BERT_BASE_EN = HANLP_URL + 'classification/sst2_bert_base_uncased_en_20200104_175422.zip'
+
+ALL = {}
