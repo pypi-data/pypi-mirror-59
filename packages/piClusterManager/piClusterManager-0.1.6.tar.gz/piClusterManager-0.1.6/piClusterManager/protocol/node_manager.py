4d7defd6a9a73d8e547fc7d8568babfc90885cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package piClusterManager/0.1.6/piClusterManager-0.1.6.tar.gz

---
+++ packages/piClusterManager/piClusterManager-0.1.6.tar.gz/piClusterManager-0.1.6/piClusterManager/protocol/node_manager.py
@@ -0,0 +1,127 @@
+import paramiko
+
+class node_manager:
+    '''
+    exec command on ssh client
+    @param self
+    @param command - command to execute
+    @return stdout form ssh
+    '''
+    def exec(self, command):
+        ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(command)
+        #wait to complete
+        return ssh_stdout.read()
+    
+    '''
+    close ssh client
+    @param self
+    @param command - command to execute
+    @return None
+    '''
+    def close(self):
+        self.ssh.close()
+
+    '''
+    init ssh client
+    @param self
+    @param ip
+    @param username
+    @param password
+    @return None
+    '''
+    def __init__(self, ip, username, password):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.load_system_host_keys()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(ip, username=username, password=password)
+
+    '''
+    disable send hello
+    @param self
+    @return None
+    '''
+    def no_hello(self):
+        self.exec("sudo touch /no-hello")
+
+    '''
+    set new hostname
+    @param self
+    @param name - new hostname
+    @return None
+    '''
+    def set_hostname(self, name):
+        self.exec("sudo sethostname {}".format(name))
+
+    '''
+    reboot ssh server
+    @param self
+    @return None
+    '''
+    def reboot(self):
+        self.exec("sudo reboot")
+
+    '''
+    enable send hello
+    @param self
+    @return None
+    '''
+    def yes_hello(self):
+        self.exec("sudo rm /no-hello")
+
+    '''
+    set new password for pi and root
+    @param self
+    @param password
+    @return None
+    '''
+    def set_password(self, password):
+        self.exec("echo 'pi:{}' | sudo chpasswd".format(password))
+        self.exec("echo 'root:{}' | sudo chpasswd".format(password))
+
+    '''
+    update node (update && upgrade)
+    @param self
+    @return None
+    '''
+    def update(self):
+        self.exec("sudo apt update && sudo apt upgrade -y")
+
+    '''
+    install docker on node
+    @param self
+    @return None
+    '''
+    def docker_install(self):
+        self.exec("curl -sSL https://get.docker.com | sudo sh")
+
+    '''
+    setup node as docker swarm master
+    @param self
+    @param out_ip - IP of this node
+    @return None
+    '''
+    def docker_setup_master(self, out_ip):
+        self.exec("sudo usermod -aG docker pi")
+        self.exec("sudo docker swarm init --advertise-addr {}".format(out_ip))
+        return self.exec("sudo docker swarm join-token worker -q")
+
+    '''
+    setup node as docker swarm worker
+    @param self
+    @param token - join token
+    @param master-ip - IP of docker swarm master
+    @return None
+    '''
+    def docker_setup_worker(self, token, master_ip):
+        return self.exec("sudo docker swarm join --token {} {}:2377".format(token, master_ip))
+
+    '''
+    install docker compose on node
+    @param self
+    @return None
+    '''
+    def docker_compose_install(self):
+        self.exec("sudo apt-get install libffi-dev libssl-dev -y")
+        self.exec("sudo apt-get install -y python python-pip -y")
+        self.exec("sudo apt-get remove python-configparser -y")
+        self.exec("sudo pip install docker-compose")
---
+++ packages/piClusterManager/piClusterManager-0.1.6.tar.gz/piClusterManager-0.1.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="piClusterManager",
+    version="0.1.6",
+    author="Lukáš Plevač",
+    author_email="lukasplevac@gmail.com",
+    description="Open Source Pi Clusters manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lukas0025/piClusterManager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        'bin/picluster',
+        'bin/piclusterdeamon',
+        'bin/sethostname'
+    ],
+    python_requires='>=3.0',
+    install_requires=[
+        'paramiko'
+    ],
+)
