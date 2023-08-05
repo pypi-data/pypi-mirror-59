e5258c4fdc86f71202f474d667556855d033d4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:36 +0000

    Add package aiosysbus/0.1.4/aiosysbus-0.1.4-py3-none-any.whl

---
+++ packages/aiosysbus/aiosysbus-0.1.4-py3-none-any.whl/aiosysbus/__init__.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+'''
+Provides authentification and row access to Livebox.
+'''
+name='aiosysbus'
+__version__ = '0.1.4'
+__all__ = ['aiosysbus']
+
+from .aiosysbus import Sysbus
