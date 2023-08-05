f6ad8bc18a0b2f0a9a8c8e79bfcc84a572fb8fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package bitcart-async/0.8.5/bitcart-async-0.8.5.tar.gz

---
+++ packages/bitcart-async/bitcart-async-0.8.5.tar.gz/bitcart-async-0.8.5/bitcart/providers/jsonrpcrequests.py
@@ -0,0 +1,84 @@
+from typing import Any, Callable, Optional, Union
+
+ASYNC = True
+
+
+try:
+    import jsonrpcclient
+
+    if ASYNC:
+        import aiohttp
+        from jsonrpcclient.clients.aiohttp_client import AiohttpClient as RPC
+        import asyncio
+    else:
+        import requests
+        from jsonrpcclient.clients.http_client import HTTPClient as RPC
+except (ModuleNotFoundError, ImportError):
+    pass  # probably during CI build
+
+
+class RPCProxy:
+    def __init__(
+        self: "RPCProxy",
+        url: str,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        xpub: Optional[str] = None,
+        session: Optional[Union["aiohttp.ClientSession", "requests.Session"]] = None,
+        verify: Optional[bool] = True,
+    ):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.xpub = xpub
+        self.verify = verify
+        self.session: Union["aiohttp.ClientSession", "requests.Session"]
+        if ASYNC:
+            self._loop = asyncio.get_event_loop()
+            self._loop.set_exception_handler(lambda loop, context: None)
+        if session:
+            self.sesson = session
+        else:
+            self.create_session()
+        if ASYNC:
+            self.rpc = RPC(endpoint=self.url, session=self.session)
+        else:
+            self.rpc = RPC(endpoint=self.url)
+            self.rpc.session = self.session
+
+    def create_session(self: "RPCProxy") -> None:
+        if ASYNC:
+            self.session = aiohttp.ClientSession(
+                connector=aiohttp.TCPConnector(ssl=self.verify),
+                loop=self._loop,
+                auth=aiohttp.BasicAuth(self.username, self.password),  # type: ignore
+            )
+        else:
+            self.session = requests.Session()
+            self.session.auth = (self.username, self.password)  # type: ignore
+
+    def __getattr__(
+        self: "RPCProxy", method: str, *args: Any, **kwargs: Any
+    ) -> Callable:
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                return (
+                    await self.rpc.request(
+                        method,
+                        validate_against_schema=False,
+                        xpub=self.xpub,
+                        *args,
+                        **kwargs,
+                    )
+                ).data.result
+            except jsonrpcclient.exceptions.ReceivedErrorResponseError as e:
+                raise ValueError("Error from server: {}".format(e.response.message))
+
+        return wrapper
+
+    async def _close(self: "RPCProxy") -> None:
+        await self.session.close()  # type: ignore
+
+    def __del__(self: "RPCProxy") -> None:
+        if ASYNC and self._loop.is_running():
+            self._loop.create_task(self._close())
