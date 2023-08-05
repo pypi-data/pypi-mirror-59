1aba6a97b070dc83aebbf6b6156237ec41bdee1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:59 +0000

    Add package kobebatman/1.0/kobebatman-1.0.tar.gz

---
+++ packages/kobebatman/kobebatman-1.0.tar.gz/kobebatman-1.0/setup.py
@@ -0,0 +1,9 @@
+
+import setuptools
+
+setuptools.setup(
+    name='kobebatman',
+    version=1.0,
+    long_description='Энэ package нь ийм ийм юм хийх зориулалттай',
+    packages=setuptools.find_packages(exclude=['test', 'data'])
+)
