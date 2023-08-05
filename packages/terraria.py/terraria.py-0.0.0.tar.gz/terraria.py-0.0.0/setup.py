374c1b6d474aa4f20f9c6c58feef54d66ae165bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:28 +0000

    Add package terraria.py/0.0.0/terraria.py-0.0.0.tar.gz

---
+++ packages/terraria.py/terraria.py-0.0.0.tar.gz/terraria.py-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="terraria.py",
+    version="0.0.0",
+    description="Reserved name",
+    long_description="This name is reserved for my future terraria package :)",
+    url="https://github.com/aiden2480",
+    author="chocolatejade42",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
