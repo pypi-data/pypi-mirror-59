9980caf179ee668959faf75808641da62704904b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package circum-hc-sr04/0.0.1/circum-hc-sr04-0.0.1.tar.gz

---
+++ packages/circum-hc-sr04/circum-hc-sr04-0.0.1.tar.gz/circum-hc-sr04-0.0.1/circum_hc_sr04/hc_sr04.py
@@ -0,0 +1,67 @@
+import click
+import circum.endpoint
+import copy
+import logging
+
+from threading import Semaphore, Thread
+
+
+logger = logging.getLogger(__name__)
+tracking_semaphore = None
+tracking_info = {"objects": []}
+vector_info = []
+updated = False
+
+
+def _get_targets():
+    raise NotImplementedError()
+    return []
+
+
+def _update_thread():
+    global tracking_info
+    global vector_info
+    global updated
+
+    while True:
+        targets = _get_targets()
+
+        tracking_semaphore.acquire()
+
+        if targets and targets is not None:
+            tracking_info["objects"] = \
+                [{"x": target.xPosCm / 100, "y": target.yPosCm / 100, "z": target.zPosCm / 100} for target in targets]
+            for i, target in enumerate(targets):
+                logger.debug('Target #{}:\nx: {}\ny: {}\nz: {}\namplitude: {}\n'.format(
+                    i + 1, target.xPosCm, target.yPosCm, target.zPosCm,
+                    target.amplitude))
+        else:
+            tracking_info["objects"] = []
+
+        updated = True
+
+        tracking_semaphore.release()
+        # time.sleep(update_interval)
+
+
+def run_hc_sr04(hc_sr04_args: {}) -> {}:
+    global updated
+    ret = None
+    tracking_semaphore.acquire()
+    if updated:
+        ret = copy.deepcopy(tracking_info)
+        updated = False
+    tracking_semaphore.release()
+    return ret
+
+
+@click.command()
+@click.pass_context
+def hc_sr04(ctx):
+    global tracking_semaphore
+    tracking_semaphore = Semaphore()
+
+    tracker_thread = Thread(target=_update_thread)
+    tracker_thread.daemon = True
+    tracker_thread.start()
+    circum.endpoint.start_endpoint(ctx, "cam", run_hc_sr04)
