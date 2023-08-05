2a7f33fce84f3e1a02121f0c7fcf9e77c38c847d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package PLTable/1.0.1/PLTable-1.0.1.tar.gz

---
+++ packages/PLTable/PLTable-1.0.1.tar.gz/PLTable-1.0.1/prettytable/__init__.py
@@ -0,0 +1,4 @@
+from .prettytable import PrettyTable
+from .prettytable import FRAME, ALL, NONE, HEADER
+from .prettytable import DEFAULT, MSWORD_FRIENDLY, PLAIN_COLUMNS, UNICODE_LINES, RANDOM
+from .factory import from_csv, from_db_cursor, from_html, from_html_one, from_md
