fad65c9fb659c1e374f35aa6b21d35b93e4f2532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:53 +0000

    Add package py-trade-signal/1.0.2/py_trade_signal-1.0.2-py3-none-any.whl

---
+++ packages/py-trade-signal/py_trade_signal-1.0.2-py3-none-any.whl/py_trade_signal/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .macd import MacdSignal
+from .mfi import MfiSignal
+from .vzo import VzoSignal
+
+
+class SignalException(Exception):
+    pass
+
+
+class TradeSignal:
+
+    def __init__(self):
+        """Initialize the TradeSignal class
+
+        This class uses trading instrument agnostic technical indicators from peerchemist's finta Python module: https://github.com/peerchemist/finta
+        """
+        self.macd_signal = MacdSignal()
+        self.mfi_signal = MfiSignal()
+        self.vzo_signal = VzoSignal()
