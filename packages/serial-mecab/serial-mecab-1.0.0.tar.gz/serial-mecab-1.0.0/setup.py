4d6b857a7691f0987d017805f9cfa6f1eadf7b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:17 +0000

    Add package serial-mecab/1.0.0/serial-mecab-1.0.0.tar.gz

---
+++ packages/serial-mecab/serial-mecab-1.0.0.tar.gz/serial-mecab-1.0.0/setup.py
@@ -0,0 +1,25 @@
+"""Metadata of the package."""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='serial-mecab',
+    version="1.0.0",
+    description=('mecab-python3 wrapper for serialization'),
+    packages=find_packages(),
+    install_requires=[
+        'mecab-python3',
+        'joblib'
+    ],
+    extras_require={
+        'test': [
+            'pytest'
+        ],
+        'dev': [
+            'ipython',
+            'python-language-server[all]'
+        ],
+        'doc': [
+            'sphinx',
+            'sphinx_rtd_theme'
+        ]})
