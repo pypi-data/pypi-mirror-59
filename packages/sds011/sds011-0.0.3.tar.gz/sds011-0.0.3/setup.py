53455a03bf73cb7733121f7f9f4a9c5c663b49c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:08 +0000

    Add package sds011/0.0.3/sds011-0.0.3.tar.gz

---
+++ packages/sds011/sds011-0.0.3.tar.gz/sds011-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sds011",
+    version="0.0.3",
+    author="Patrick Menschel",
+    author_email="menschel.p@posteo.de",
+    description="A python 3 interface to a sds011 fine dust sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/menschel/sds011",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable", #used for years without any problems
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: Utilities',
+    ],
+    python_requires='>=3.6',
+    install_requires=["pyserial"],
+    keywords='fine dust sensor nova sds011',
+
+)
