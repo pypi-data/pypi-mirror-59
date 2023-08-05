ee8c6c2aeecc4e83f34c7977cdce92e95de03a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package kevin-siry-lib-session/0.1/kevin_siry_lib_session-0.1.tar.gz

---
+++ packages/kevin-siry-lib-session/kevin_siry_lib_session-0.1.tar.gz/kevin_siry_lib_session-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='kevin_siry_lib_session',
+    packages=['kevin_siry_lib_session'],
+    version='0.1',
+    license='MIT',
+    description='Manage the creation and the deletion of kevin_siry_lib_session',
+    author='ALTEN',
+    author_email='kevin.siry@alten.com',
+    url='http://gitlab-alten.francecentral.cloudapp.azure.com/mane/lib-session',
+    download_url='http://gitlab-alten.francecentral.cloudapp.azure.com/mane/lib-session/mane/lib-session/repository/master/archive.tar.gz',
+    keywords=['SESSION'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
