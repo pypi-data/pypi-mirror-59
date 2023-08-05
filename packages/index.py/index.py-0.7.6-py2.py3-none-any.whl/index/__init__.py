f6a33c1719f29973c997b818361e8c4be484f911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:17 +0000

    Add package index.py/0.7.6/index.py-0.7.6-py2.py3-none-any.whl

---
+++ packages/index.py/index.py-0.7.6-py2.py3-none-any.whl/index/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import asyncio
+import importlib
+
+from .utils import _import_module
+from .config import Config
+from .core import app
+
+_import_module("mounts")
+_import_module("commands")
+_import_module("events")
+_import_module("responses")
