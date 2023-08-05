456c8899aa3bfb6005139faa8e6976af7a1e9ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package DockerBuildSystem/1.1.42/DockerBuildSystem-1.1.42-py2.py3-none-any.whl

---
+++ packages/DockerBuildSystem/DockerBuildSystem-1.1.42-py2.py3-none-any.whl/DockerBuildSystem/DockerImageTools.py
@@ -0,0 +1,100 @@
+from DockerBuildSystem import TerminalTools
+import re
+import json
+
+def BuildImage(imageName, dockerfile = 'Dockerfile', context = '.'):
+    dockerCommand = "docker build -f " + dockerfile + " -t " + imageName + " " + context
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def RunImage(imageName, properties = ""):
+    dockerCommand = "docker run " + properties + " " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def PullImage(imageName):
+    dockerCommand = "docker pull " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def PushImage(imageName):
+    dockerCommand = "docker push " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def TagImage(sourceImage, targetImage):
+    dockerCommand = "docker tag " + sourceImage + " " + targetImage
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def SaveImage(imageName, outputPath):
+    dockerCommand = "docker save -o " + outputPath + " " + imageName
+    TerminalTools.ExecuteTerminalCommands([dockerCommand], True)
+
+
+def GetContainerExitCode(containerName):
+    terminalCommand = "docker inspect " + containerName + " --format='{{.State.ExitCode}}'"
+    output = TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand)
+    exitCode = TerminalTools.GetNumbersFromString(output)[0]
+    return int(exitCode)
+
+
+def GetContainerRunningCode(containerName):
+    terminalCommand = "docker inspect " + \
+        containerName + " --format='{{.State.Running}}'"
+    output = TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand)
+    running = bool(re.search('true', str(output).lower()))
+    return running
+
+
+def CopyFromContainerToHost(containerName, containerSrc, hostDest):
+    terminalCommand = "docker cp " + \
+        containerName + ":" + containerSrc + " " + hostDest
+    TerminalTools.ExecuteTerminalCommands([terminalCommand])
+
+
+def GetImageRepoDigest(imageName):
+    terminalCommand = "docker inspect --format=\"{{index .RepoDigests 0}}\" " + imageName
+    repoDigest = str(TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand).decode("utf-8")).replace('\n', '')
+    return repoDigest
+
+
+def GetImageLabels(imageName):
+    jsonInfo = GetImageInfo(imageName)
+    labels = jsonInfo['ContainerConfig']['Labels']
+    return labels
+
+
+def GetImageLabel(imageName, labelKey):
+    labels = GetImageLabels(imageName)
+    if labelKey in labels:
+        return labels[labelKey]
+    return '<no value>'
+
+
+def CheckImageLabelExists(imageName, labelKey):
+    labelValue = GetImageLabel(imageName, labelKey)
+    return not(labelValue == '<no value>')
+
+
+def GetImageId(imageName):
+    terminalCommand = "docker inspect --format=\"{{.Id}}\" " + imageName
+    imageId = str(TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand).decode("utf-8")).replace('\n', '')
+    return imageId
+
+
+def GetImageInfo(imageName):
+    terminalCommand = "docker inspect " + imageName
+    info = str(TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand).decode("utf-8"))
+    jsonInfo = json.loads(info)[0]
+    return jsonInfo
+
+
+def GetContainerInfo(containerName):
+    return GetImageInfo(containerName)
+
+
+def GetLogsFromContainer(containerName):
+    terminalCommand = 'docker logs {0}'.format(containerName)
+    logs = str(TerminalTools.ExecuteTerminalCommandAndGetOutput(terminalCommand).decode("utf-8"))
+    return logs
---
+++ packages/DockerBuildSystem/DockerBuildSystem-1.1.42-py2.py3-none-any.whl/DockerBuildSystem/TerminalTools.py
@@ -0,0 +1,56 @@
+import subprocess
+import os
+import re
+from dotenv import load_dotenv
+
+
+def LoadEnvironmentVariables(environmentVariablesFile):
+    load_dotenv(environmentVariablesFile)
+
+
+def LoadDefaultEnvironmentVariablesFile(defaultEnvFile = '.env'):
+    if os.path.isfile(defaultEnvFile):
+        LoadEnvironmentVariables(defaultEnvFile)
+
+
+def PrintAvailableCommands(availableCommands):
+    for availableCommand in availableCommands:
+        print(availableCommand)
+
+
+def ExecuteTerminalCommands(terminalCommands, raiseExceptionWithErrorCode=False):
+    for terminalCommand in terminalCommands:
+        print("Executing: " + terminalCommand)
+        keyboardInterrupt = False
+        returnCode = 0
+        try:
+            returnCode = subprocess.Popen(terminalCommand, shell=True).wait()
+        except KeyboardInterrupt:
+            keyboardInterrupt = True
+        if keyboardInterrupt and raiseExceptionWithErrorCode:
+            raise Exception("Exception thrown due to keyboardinterrupt")
+        if returnCode > 0:
+            errorMsg = "Terminal command: '" + terminalCommand + \
+                "' executed with error return code: " + str(returnCode)
+            if raiseExceptionWithErrorCode:
+                raise Exception(errorMsg)
+            print(errorMsg)
+
+
+def ExecuteTerminalCommandAndGetOutput(terminalCommand):
+    print("Executing: " + terminalCommand)
+    outputList = subprocess.Popen(terminalCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+    output = b""
+    for outputLine in outputList:
+        output += outputLine
+    return output
+
+
+def GetNumbersFromString(string):
+    strNumbers = re.findall(r'\d+', str(string))
+    numbers = [int(i) for i in strNumbers]
+    return numbers
+
+
+def ExportVariableToEnvironment(variable, variableName):
+    os.environ[variableName] = str(variable)
