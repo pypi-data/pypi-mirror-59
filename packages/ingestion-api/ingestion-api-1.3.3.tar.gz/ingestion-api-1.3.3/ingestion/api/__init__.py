4774a1ff885a889af5938e8b8214b987fd0197c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package ingestion-api/1.3.3/ingestion-api-1.3.3.tar.gz

---
+++ packages/ingestion-api/ingestion-api-1.3.3.tar.gz/ingestion-api-1.3.3/ingestion/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ingestion.api.default_api import DefaultApi
+from ingestion.api.result_api import ResultApi
+from ingestion.api.summary_api import SummaryApi
