c3440e66c15c4e3613fa99725cf3f84d5200080f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package kaldi-active-grammar/1.2.1.dev1/kaldi_active_grammar-1.2.1.dev1-py2.py3-none-manylinux2010_x86_64.whl

---
+++ packages/kaldi-active-grammar/kaldi_active_grammar-1.2.1.dev1-py2.py3-none-manylinux2010_x86_64.whl/kaldi_active_grammar/__init__.py
@@ -0,0 +1,21 @@
+#
+# This file is part of kaldi-active-grammar.
+# (c) Copyright 2019 by David Zurow
+# Licensed under the AGPL-3.0, with exceptions; see LICENSE.txt file.
+#
+
+_name = 'kaldi_active_grammar'
+__version__ = '1.2.1.dev1'
+REQUIRED_MODEL_VERSION = '0.5.0'
+
+import logging
+_log = logging.getLogger('kaldi')
+
+class KaldiError(Exception):
+    pass
+
+from .compiler import Compiler, KaldiRule
+from .model import Model
+from .wrapper import KaldiAgfNNet3Decoder, KaldiPlainNNet3Decoder
+from .wfst import WFST
+from .plain_dictation import PlainDictationRecognizer
