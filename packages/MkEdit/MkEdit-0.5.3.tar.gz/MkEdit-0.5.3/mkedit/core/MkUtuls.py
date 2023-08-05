a1ea654c9a19fa20586b66146812d290de335bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package MkEdit/0.5.3/MkEdit-0.5.3.tar.gz

---
+++ packages/MkEdit/MkEdit-0.5.3.tar.gz/MkEdit-0.5.3/mkedit/core/MkUtuls.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from mistune import Markdown, Renderer, escape
+
+
+class MkRenderer(Renderer):
+    def __init__(self, **kwargs):
+        Renderer.__init__(self, **kwargs)
+        self.hasMermaid = False
+
+    def block_code(self, code, lang=None):
+        """Rendering block level code. ``pre > code``.
+
+        :param code: text content of the code block.
+        :param lang: language of the given code.
+        """
+        code = code.rstrip('\n')
+        mermaid = False
+        if lang and lang == "mermaid":
+            mermaid = True
+        else:
+            code1 = str(code).lstrip()
+            if code1.startswith('graph TD') or code1.startswith('sequenceDiagram') or code1.startswith(
+                    'gantt') or code1.startswith('gitGraph') or code1.startswith('classDiagram') or code1.startswith('gantt') or code1.startswith('stateDiagram') or code1.startswith('pie'):
+                mermaid = True
+
+        if mermaid:
+            code = escape(code, smart_amp=False)
+            self.hasMermaid = True
+            return '<div class="mermaid">%s\n</div>\n' % code
+
+        return Renderer.block_code(self, code, lang)
+
+
+class MkUtuls:
+    def __init__(self):
+        self.renderer = MkRenderer(escape=True, hard_wrap=True)
+        self.markdown = Markdown(renderer=self.renderer)
+
+    def parse(self, txt):
+        return self.markdown.parse(txt)
+
+    def hasMermaidCode(self, txt):
+        self.markdown.parse(txt)
+        return self.renderer.hasMermaid
