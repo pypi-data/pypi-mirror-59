58310a1b40a7fce4d86e81e9c4b32851f337d6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:52:55 +0000

    Add package ScratchABlock/0.0/ScratchABlock-0.0.tar.gz

---
+++ packages/ScratchABlock/ScratchABlock-0.0.tar.gz/ScratchABlock-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name="ScratchABlock",
+      version="0.0",
+      description="""Pure-Python decompiler""",
+      url='https://github.com/pfalcon/ScratchABlock',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='GPL3',
+      py_modules=['ScratchABlock'])
