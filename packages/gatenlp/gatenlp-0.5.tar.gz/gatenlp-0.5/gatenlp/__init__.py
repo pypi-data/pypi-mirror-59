03e47122587437f5a3d17cf2c381cacf830d834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:20 +0000

    Add package gatenlp/0.5/gatenlp-0.5.tar.gz

---
+++ packages/gatenlp/gatenlp-0.5.tar.gz/gatenlp-0.5/gatenlp/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "0.5"
+import logging
+try:
+    import sortedcontainers
+except Exception as ex:
+    raise Exception("Required package sortedcontainers cannot be imported!")
+# TODO: check version of sortedcontainers (we have 2.1.0)
+logging.basicConfig()
+logger = logging.getLogger("gatenlp")
+logger.setLevel(logging.INFO)
+
+# this attribute globally holds the processing resource last defined
+# so it can be used for interacting with the GATE python plugin
+from gatenlp.gate_interaction import _pr_decorator as GateNlpPr
+from gatenlp.gate_interaction import interact
+from gatenlp.annotation import Annotation
+from gatenlp.document import Document
+from gatenlp.annotation_set import AnnotationSet
+from gatenlp.exceptions import InvalidOffsetException
+from gatenlp.changelog import ChangeLog
+
+__all__ = ["GateNlpPr", "Annotation", "Document", "AnnotationSet",
+           "InvalidOffsetException", "ChangeLog", "logger"]
+
+gate_python_plugin_pr = None
+
---
+++ packages/gatenlp/gatenlp-0.5.tar.gz/gatenlp-0.5/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Packaging script for the gatenlp library."""
+
+import os
+from setuptools import setup, find_packages
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    readme = f.read()
+
+def versionfromfile(*filepath):
+    here = os.path.abspath(os.path.dirname(__file__))
+    infile = os.path.join(here, *filepath)
+    with open(infile) as fp:
+        version_match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]",
+                                  fp.read(), re.M)
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string in {}.".format(infile))
+
+setup(
+    name="gatenlp",
+    version=versionfromfile("gatenlp/__init__.py"),
+    author="Johann Petrak",
+    author_email="johann.petrak@gmail.com",
+    description='GATE NLP implementation in Python.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    setup_requires=["pytest-runner"],
+    install_requires=["sortedcontainers", "typing"],
+    python_requires=">=3.4",
+    tests_require=['pytest'],
+    platforms='any',
+    license="MIT",
+    keywords="",
+    url="https://github.com/GateNLP/python-gatenlp",
+    packages=find_packages(),
+    test_suite='tests',
+    classifiers=[
+        # "Development Status :: 6 - Mature",
+        # "Development Status :: 5 - Production/Stable",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+      ],
+)
+
