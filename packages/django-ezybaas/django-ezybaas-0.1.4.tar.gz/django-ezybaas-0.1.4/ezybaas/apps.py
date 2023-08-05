6fa28193129f05125d6eb9d3fcd04c017e5f0079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:46 +0000

    Add package django-ezybaas/0.1.4/django-ezybaas-0.1.4.tar.gz

---
+++ packages/django-ezybaas/django-ezybaas-0.1.4.tar.gz/django-ezybaas-0.1.4/ezybaas/apps.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2019 Ezybaas by Bhavik Shah.
+# Susthitsoft Technologies Private Limited.
+# All rights reserved.
+# Please see the LICENSE.txt included as part of this package.
+
+from django.apps import AppConfig
+
+
+class EzybaasConfig(AppConfig):
+    name = 'ezybaas'
---
+++ packages/django-ezybaas/django-ezybaas-0.1.4.tar.gz/django-ezybaas-0.1.4/ezybaas/config.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2019 Ezybaas by Bhavik Shah.
+# Susthitsoft Technologies Private Limited.
+# All rights reserved.
+# Please see the LICENSE.txt included as part of this package.
+
+# EZYBAAS RELEASE CONFIG
+EZYBAAS_RELEASE_NAME = 'EzyBaaS'
+EZYBAAS_RELEASE_AUTHOR = 'SusthitSoft Technologies'
+EZYBAAS_RELEASE_VERSION = '0.1.4'
+EZYBAAS_RELEASE_DATE = '2019-07-20'
+EZYBAAS_RELEASE_NOTES = 'http://github.com/susthitsoft'
+EZYBAAS_RELEASE_STANDALONE = True
+EZYBAAS_RELEASE_LICENSE = 'http://github.com/susthitsoft'
+
+EZYBAAS_SWAGGER_ENABLED = True
+
+# EZYBAAS OPERATIONAL CONFIG
+
+BAAS_NAME = 'ezybaas'
+SERIALIZERS_FILE_NAME = 'api'
+VIEWS_FILE_NAME = 'api'
+URLS_FILE_NAME = 'urls'
+MODELS_FILE_NAME = 'models'
+TESTS_FILE_NAME = 'tests'
