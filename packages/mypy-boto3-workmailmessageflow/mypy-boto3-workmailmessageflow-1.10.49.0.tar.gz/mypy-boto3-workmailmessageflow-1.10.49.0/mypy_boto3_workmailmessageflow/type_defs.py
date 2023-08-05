9857fd54bde97a438b23ea82e5ca99f0f238b31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:16 +0000

    Add package mypy-boto3-workmailmessageflow/1.10.49.0/mypy-boto3-workmailmessageflow-1.10.49.0.tar.gz

---
+++ packages/mypy-boto3-workmailmessageflow/mypy-boto3-workmailmessageflow-1.10.49.0.tar.gz/mypy-boto3-workmailmessageflow-1.10.49.0/mypy_boto3_workmailmessageflow/type_defs.py
@@ -0,0 +1,25 @@
+"""
+Main interface for workmailmessageflow service type definitions.
+
+Usage::
+
+    from mypy_boto3.workmailmessageflow.type_defs import GetRawMessageContentResponseTypeDef
+
+    data: GetRawMessageContentResponseTypeDef = {...}
+"""
+from __future__ import annotations
+
+import sys
+from typing import IO, Union
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("GetRawMessageContentResponseTypeDef",)
+
+GetRawMessageContentResponseTypeDef = TypedDict(
+    "GetRawMessageContentResponseTypeDef", {"messageContent": Union[bytes, IO]}
+)
