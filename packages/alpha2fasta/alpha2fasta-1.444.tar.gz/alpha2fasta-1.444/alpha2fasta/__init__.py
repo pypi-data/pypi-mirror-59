6194e89c7cd90cf418848c6e4d8c1c8c84ad3e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package alpha2fasta/1.444/alpha2fasta-1.444.tar.gz

---
+++ packages/alpha2fasta/alpha2fasta-1.444.tar.gz/alpha2fasta-1.444/alpha2fasta/__init__.py
@@ -0,0 +1,11 @@
+from alpha2fasta.AMINOcode import *
+from alpha2fasta.DNAbits import *
+
+from re import sub,findall,search
+import unidecode
+from Bio import SeqIO
+import sys
+from alpha2fasta import *
+import codecs
+
+from alpha2fasta.fileEncode import *
