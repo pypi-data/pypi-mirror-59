89d8c8b7067790b267ac47efe14c9777ad6e88d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:51:54 +0000

    Add package wonderbits/5.9.2/wonderbits-5.9.2-py2.py3-none-any.whl

---
+++ packages/wonderbits/wonderbits-5.9.2-py2.py3-none-any.whl/wonderbits/core.py
@@ -0,0 +1,35 @@
+# Insert your code here.
+
+from .WBControl import Control
+from .WBDisplay import Display
+from .WBLed import Led
+from .WBRfCommunication import RfCommunication
+from .WBRfTelecontroller import RfTelecontroller
+from .WBDriver import Driver
+from .WBHall import Hall
+from .WBLightBelt import LightBelt
+from .WBObserver import Observer
+from .WBPulse import Pulse
+from .WBSignal import Signal
+from .WBUltrasonic import Ultrasonic
+from .WBAcceleration import Acceleration
+from .WBBuggy import Buggy
+from .WBDistance import Distance
+from .WBMakeyMakey import MakeyMakey
+from .WBAudioPlayer import AudioPlayer
+from .WBClimate import Climate
+from .WBDisplaymini import Displaymini
+from .WBIoExtension import IoExtension
+from .WBFingerprint import Fingerprint
+from .WBLedMatrix import LedMatrix
+from .WBLight import Light
+from .WBNebulier import Nebulier
+from .WBNfc import Nfc
+from .WBPressure import Pressure
+from .WBRtc import Rtc
+from .WBSlider import Slider
+from .WBTransmitter import Transmitter
+from .WBVoice import Voice
+from .WBMultimeter import Multimeter
+from .WBPressurePad import PressurePad
+from .WBPi import pi as Pi  #这个模块会导致串口连接，因为这个模块会定义wonder
