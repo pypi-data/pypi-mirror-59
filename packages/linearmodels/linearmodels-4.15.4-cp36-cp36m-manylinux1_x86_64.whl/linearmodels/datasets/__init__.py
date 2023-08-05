0d042371ec56f0fc3a8b2f7d9fe00d4d31ff63a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package linearmodels/4.15.4/linearmodels-4.15.4-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/linearmodels/linearmodels-4.15.4-cp36-cp36m-manylinux1_x86_64.whl/linearmodels/datasets/__init__.py
@@ -0,0 +1,11 @@
+from os.path import abspath, join, split
+
+import pandas as pd
+
+
+def get_path(f):
+    return split(abspath(f))[0]
+
+
+def load(module, file_name):
+    return pd.read_csv(join(get_path(module), file_name), compression="bz2")
