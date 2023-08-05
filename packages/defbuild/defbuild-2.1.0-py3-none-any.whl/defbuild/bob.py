2148c55b955a4512a0cd2b592111a9951c7d7f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:32 +0000

    Add package defbuild/2.1.0/defbuild-2.1.0-py3-none-any.whl

---
+++ packages/defbuild/defbuild-2.1.0-py3-none-any.whl/defbuild/bob.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import requests
+import logging
+import defbuild.versions as versions
+
+
+class Project:
+    def __init__(self, config):
+        self.config = config
+        self.cache_dir = os.path.join(os.path.expanduser("~"), ".builder", "cache")
+        self.bob = config.get("config", "bob", fallback="")
+
+    def final(self):
+        self.config.set("config", "bob", self.bob)
+
+        with open(os.path.join(self.cache_dir, "session"), 'w') as f:
+            self.config.write(f)
+
+
+def exists(project, sha):
+    target = os.path.join(project.cache_dir, "bob", "bob_{}.jar".format(sha))
+    if os.path.exists(target):
+        return target
+    return
+
+
+def update(project, sha, force):
+    target = os.path.join(project.cache_dir, "bob", "bob_{}.jar".format(sha))
+
+    bob_exists = exists(project, sha)
+    if bob_exists and not force:
+        project.bob = bob_exists
+        logging.info("Using cached version {}".format(get_version_from_sha(sha)))
+    else:
+        download(project.cache_dir, sha)
+        project.bob = target
+        logging.info("Bob set to {}".format(get_version_from_sha(sha)))
+
+
+def download(cache, sha):
+    if requests.head("http://d.defold.com/archive/{}/bob/bob.jar".format(sha)).status_code > 400:
+        logging.error("Can't find bob version {}".format(sha))
+        sys.exit(1)
+
+    logging.info("Downloading new bob {}".format(get_version_from_sha(sha)))
+
+    bob_directory = os.path.join(cache, "bob")
+    bob_url = "http://d.defold.com/archive/{}/bob/bob.jar".format(sha)
+    if not os.path.exists(bob_directory):
+        os.makedirs(bob_directory, exist_ok=True)
+
+    target = os.path.join(bob_directory, "bob_{}.jar".format(sha))
+    r = requests.get(bob_url, stream=True)
+    with open(target, "wb") as f:
+        total_size = int(r.headers.get('content-length', 0))
+        if total_size:
+            dl = 0
+            for data in r.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+
+                # Progressbar
+                done = int(50 * dl / total_size)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                sys.stdout.flush()
+            sys.stdout.write("\n")
+        else:
+            f.write(r.content)
+
+
+def beta():
+    return versions.beta()
+
+
+def get_version_from_sha(sha):
+    json_data = versions.get(True)
+    for x in json_data["versions"]:
+        if x["sha1"] == sha:
+            return x["version"]
+    beta_sha, beta_version = beta()
+    if beta_sha == sha:
+        logging.info("Using beta version")
+        return beta_version
+    return "unknown"
+
+
+def get_sha_from_version(version):
+    json_data = versions.get(True)
+    for x in json_data["versions"]:
+        if x["version"] == version:
+            return x["sha1"]
+
+
+def get_version_from_file_name(file_name):
+    return get_version_from_sha(file_name.replace(".jar", "").split("bob_")[-1])
---
+++ packages/defbuild/defbuild-2.1.0-py3-none-any.whl/defbuild/commands.py
@@ -0,0 +1,183 @@
+import os
+import sys
+import defbuild.bob as _bob
+import time
+import shutil
+import logging
+import requests
+from subprocess import call
+from distutils.version import StrictVersion
+
+
+def build(project):
+    if not project.bob:
+        logging.error("Can't find a bob version, download with 'builder bob --update'")
+        sys.exit(1)
+
+    os.chdir(project.source_directory)
+    command = ["java", "-jar", project.bob,
+               "--archive",
+               "--platform", project.platform,
+               "--texture-compression", "true",
+               "--bundle-output", project.output]
+
+    bob_version = _bob.get_version_from_file_name(project.bob)
+    if len(bob_version.split(".")) == 3 and StrictVersion(bob_version) >= StrictVersion("1.2.137"):
+        if project.variant == "debug":
+            command.extend(["--variant", "debug"])
+        else:
+            command.extend(["--strip-executable"])
+    else:
+        if project.variant == "debug":
+            command.extend(["--debug"])
+
+    if project.report:
+        command.extend(["--build-report-html", os.path.join(project.cache_dir, "report.html")])
+
+    if project.platform == "armv7-android":
+        project.android_build = os.path.join(project.output, project.name, "{}.apk".format(project.name))
+    else:
+        project.ios_build = os.path.join(project.output, "{}.ipa".format(project.name))
+        if not project.identity:
+            logging.error("""Please set a identity with 'set config identity "iPhone Developer: My Identity"'""")
+            cert = project.config.get(project.name, "certificate", None)
+            pk = project.config.get(project.name, "private-key", None)
+            if cert and pk:
+                command.extend(["--certificate", cert, "--private-key", pk])
+        if not project.provision:
+            logging.error(
+                "Please set a mobile provision with 'set config provision /path/to/mobile_provision.mobileprovision'")
+        if not project.provision or not project.identity:
+            sys.exit(1)
+        command.extend(["--identity", project.identity, "-mp", project.provision])
+
+    if project.resolve:
+        logging.info("Resolving please supply your credentials")
+        user, pw = _get_user()
+        command.extend(["--email", user, "--auth", pw, "resolve"])
+
+    if not project.quick:
+        command.extend(["distclean"])
+
+    platform = "android" if project.platform == "armv7-android" else "ios" if project.platform == "armv7-darwin" else ""
+    logging.info("Building project {} as {} for {}".format(project.name, project.variant, platform))
+    logging.info("Using bob version {}".format(bob_version, project.variant))
+
+    command.extend(["build", "bundle"])
+    start_time = time.time()
+    call(command)
+    m, s = divmod(time.time() - start_time, 60)
+    logging.info("Building done in {:.0f}:{:.0f}".format(m, s))
+
+    if project.report:
+        import webbrowser
+        webbrowser.open("file://" + os.path.join(project.cache_dir, "report.html"))
+
+
+def install(project):
+    if project.force:
+        uninstall(project)
+
+    if project.platform == "armv7-android":
+        bundle = project.android_build
+        command = ["adb", "install", bundle]
+    else:
+        bundle = project.ios_build
+        command = ["ideviceinstaller", "-i", bundle]
+
+    logging.info("Installing {}".format(os.path.basename(bundle)))
+    if not shutil.which(command[0]):
+        logging.error("Can not find dependency {}".format(command[0]))
+        sys.exit(-1)
+    call(command)
+
+
+def uninstall(project):
+    if project.platform == "armv7-android":
+        logging.info("Uninstalling {}".format(project.android_id))
+        command = ["adb", "uninstall", project.android_id]
+    else:
+        logging.info("Uninstalling {}".format(project.ios_id))
+        command = ["ideviceinstaller", "-U", project.ios_id]
+
+    if not shutil.which(command[0]):
+        logging.error("Can not find dependency {}".format(command[0]))
+        sys.exit(-1)
+
+    call(command)
+
+
+def start(project):
+    if project.platform == "armv7-android":
+        call(["adb", "shell", "am", "start", "-n", "{}/com.dynamo.android.DefoldActivity".format(project.android_id)])
+    else:
+        logging.error("Starting app not supported for iOS")
+        sys.exit(-1)
+
+
+def bob(config, options):
+    project = _bob.Project(config)
+    if options.update:
+        latest = requests.get("http://d.defold.com/stable/info.json").json()["sha1"]
+        _bob.update(project, latest, options.force)
+
+    elif options.set:
+        sha = options.set
+        if len(sha.split(".")) == 3:
+            sha = _bob.get_sha_from_version(sha)
+            _bob.update(project, sha, options.force)
+        elif sha == "beta":
+            sha, _ = _bob.beta()
+            _bob.update(project, sha, options.force)
+        else:
+            _bob.update(project, sha, options.force)
+    else:
+        if not project.bob:
+            logging.error("Can't find a bob version, download with 'builder bob --update'")
+            sys.exit(1)
+        sha = project.bob.split("bob_")[-1].split(".jar")[0]
+        version = _bob.get_version_from_sha(sha)
+        logging.info("Using version '{}', sha1: {}\n".format(version, sha))
+    return project
+
+
+def listen(project):
+    if project.platform == "armv7-android":
+        call(["adb", "logcat", "-s", "defold"])
+    else:
+        logging.error("Listening on log not supported for iOS")
+        sys.exit(-1)
+
+
+def _get_user():
+    import getpass
+    user = input("User: ")
+    pw = getpass.getpass()
+    return user, pw
+
+
+def resolve(project):
+    user, pw = _get_user()
+    command = ["java", "-jar", project.bob, "--email", user, "--auth", pw, "resolve"]
+    os.chdir(project.source_directory)
+    call(command)
+
+
+def config_set(project, command):
+    if hasattr(project, command.key):
+        setattr(project, command.key, command.value)
+    else:
+        logging.error("Attribute doesn't exists")
+
+
+def print_help():
+    print("Usage: builder <command> [<args>]\n")
+    print("Some useful commands are:")
+    print("    build      Use bob to build a Defold project")
+    print("    install    Install a project to a connected device")
+    print("    uninstall  Uninstall the Defold project on a connected device")
+    print("    bob        Update or set the version of bob that is used")
+    print("    config     Update config values, used for setting up iOS Provisional Profiles\n"
+          "               and others")
+    print("    resolve    Resolve all external library dependencies\n")
+    print("See `builder <command> --help' for information on a specific command.")
