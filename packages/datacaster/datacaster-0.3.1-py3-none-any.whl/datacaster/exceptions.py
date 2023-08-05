46d3ffef663924165445428b5e4a0872a7641911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package datacaster/0.3.1/datacaster-0.3.1-py3-none-any.whl

---
+++ packages/datacaster/datacaster-0.3.1-py3-none-any.whl/datacaster/exceptions.py
@@ -0,0 +1,18 @@
+class InvalidDefaultValue(TypeError):
+    pass
+
+
+class UnexpectedArgument(ValueError):
+    pass
+
+
+class MissingArgument(ValueError):
+    pass
+
+
+class UnsupportedType(ValueError):
+    pass
+
+
+class CastFailed(ValueError):
+    pass
