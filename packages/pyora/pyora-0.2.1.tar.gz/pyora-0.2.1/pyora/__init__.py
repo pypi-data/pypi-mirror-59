ac4e4bd2bac93c2f26d5b784c3702cee81f25b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:56 +0000

    Add package pyora/0.2.1/pyora-0.2.1.tar.gz

---
+++ packages/pyora/pyora-0.2.1.tar.gz/pyora-0.2.1/pyora/__init__.py
@@ -0,0 +1,8 @@
+TYPE_LAYER = 0
+TYPE_GROUP = 1
+ORA_VERSION = "0.0.1"
+
+from .OpenRasterProject import Project
+from .OpenRasterProject import Layer
+from .OpenRasterProject import Group
+from .Render import Renderer
