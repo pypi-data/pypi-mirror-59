8ac2d22866ee48d47571d7ca68f140a8e324045e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:23 +0000

    Add package ft-api/1.0.0/ft_api-1.0.0-py3-none-any.whl

---
+++ packages/ft-api/ft_api-1.0.0-py3-none-any.whl/ft/__init__.py
@@ -0,0 +1,12 @@
+from .api import FtAPI
+
+__copyright__ = 'Copyright (C) 2020 Yuta Ura'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__author__ = 'Yuta Ura'
+__author_email__ = 'yuuta3594@outlook.jp'
+__url__ = 'http://github.com/YutaUra/42-api'
+
+__all__ = [
+    'FtAPI',
+]
