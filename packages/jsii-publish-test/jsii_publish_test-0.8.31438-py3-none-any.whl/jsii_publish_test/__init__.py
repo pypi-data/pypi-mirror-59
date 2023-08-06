e1b8f02632a0de09ec616dc8cf7ca75a1c21ed8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package jsii-publish-test/0.8.31438/jsii_publish_test-0.8.31438-py3-none-any.whl

---
+++ packages/jsii-publish-test/jsii_publish_test-0.8.31438-py3-none-any.whl/jsii_publish_test/__init__.py
@@ -0,0 +1,40 @@
+"""
+# JSII Publish Test
+
+This is a test package, created by [JSII Publish](https://github.com/udondan/jsii-publish) to test the publishing functionality of the Docker image.
+"""
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_s3
+import aws_cdk.core
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("jsii-publish-test", "0.8.31438", __name__, "jsii-publish-test@0.8.31438.jsii.tgz")
+
+
+class Test(aws_cdk.core.Construct, metaclass=jsii.JSIIMeta, jsii_type="jsii-publish-test.Test"):
+    """
+    stability
+    :stability: experimental
+    """
+    def __init__(self, scope: aws_cdk.core.Construct, id: str) -> None:
+        """
+        :param scope: -
+        :param id: -
+
+        stability
+        :stability: experimental
+        """
+        jsii.create(Test, self, [scope, id])
+
+
+__all__ = ["Test", "__jsii_assembly__"]
+
+publication.publish()
