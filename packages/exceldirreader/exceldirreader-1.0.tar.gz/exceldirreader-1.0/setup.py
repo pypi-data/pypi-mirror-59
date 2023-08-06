98005c55f69c097f111560086aa93bd34c34dab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:27 +0000

    Add package exceldirreader/1.0/exceldirreader-1.0.tar.gz

---
+++ packages/exceldirreader/exceldirreader-1.0.tar.gz/exceldirreader-1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name="exceldirreader",
+    version=1.0,
+    description="Reads multiple excel files from a directory",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
