bf7d46e07c4e1a3c7ff2139a892cd77795387ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:25 +0000

    Add package dist-nd-lesson/0.1/dist_nd_lesson-0.1.tar.gz

---
+++ packages/dist-nd-lesson/dist_nd_lesson-0.1.tar.gz/dist_nd_lesson-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_nd_lesson',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['dist_nd_lesson'],
+      author = 'Nathan Dennison',
+      author_email = 'nathan.dennison@gmail.com',
+      zip_safe=False)
