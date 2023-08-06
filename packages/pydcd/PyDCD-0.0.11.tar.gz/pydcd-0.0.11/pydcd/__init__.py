8ce69e177d57fb4830f87fc1bf2ab147d95ba224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:06 +0000

    Add package pydcd/0.0.11/PyDCD-0.0.11.tar.gz

---
+++ packages/pydcd/PyDCD-0.0.11.tar.gz/pydcd-0.0.11/pydcd/__init__.py
@@ -0,0 +1,5 @@
+from .dcd import DCD
+from .kmeans import KM
+from .modularity_maximization import MM  
+from .generate_random_networks import RandNet
+from .load_package_data import LoadPackageData
