0f94db3d285a46d0cea46779215365bc2ae4e94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:46 +0000

    Add package hypercluster/0.1.5/hypercluster-0.1.5-py3-none-any.whl

---
+++ packages/hypercluster/hypercluster-0.1.5-py3-none-any.whl/hypercluster/__init__.py
@@ -0,0 +1,9 @@
+import hypercluster
+from hypercluster import *
+from hypercluster.classes import AutoClusterer, MultiAutoClusterer
+__version__ = '0.1.5'
+__all__ = [
+    "AutoClusterer",
+    "MultiAutoClusterer"
+]
+
