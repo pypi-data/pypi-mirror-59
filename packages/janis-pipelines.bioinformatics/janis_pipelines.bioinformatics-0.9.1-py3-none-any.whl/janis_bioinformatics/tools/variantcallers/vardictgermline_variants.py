06a9bca6f84dfcab1b42065ae3765fcb1f219615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:37 +0000

    Add package janis-pipelines.bioinformatics/0.9.1/janis_pipelines.bioinformatics-0.9.1-py3-none-any.whl

---
+++ packages/janis-pipelines.bioinformatics/janis_pipelines.bioinformatics-0.9.1-py3-none-any.whl/janis_bioinformatics/tools/variantcallers/vardictgermline_variants.py
@@ -0,0 +1,68 @@
+from janis_core import File, String, Float
+
+from janis_bioinformatics.data_types import FastaWithDict, BamBai, Bed
+from janis_bioinformatics.tools import BioinformaticsWorkflow
+from janis_bioinformatics.tools.bcftools import BcfToolsAnnotate_1_5
+from janis_bioinformatics.tools.common import SplitMultiAllele
+from janis_bioinformatics.tools.vardict import VarDictGermline_1_6_0
+from janis_bioinformatics.tools.pmac.trimiupac.versions import TrimIUPAC_0_0_5
+
+
+class VardictGermlineVariantCaller(BioinformaticsWorkflow):
+    def id(self):
+        return "vardictGermlineVariantCaller"
+
+    def friendly_name(self):
+        return "Vardict Germline Variant Caller"
+
+    def tool_provider(self):
+        return "Variant Callers"
+
+    def version(self):
+        return "v0.1.0"
+
+    def constructor(self):
+
+        self.input("bam", BamBai)
+        self.input("intervals", Bed)
+
+        self.input("sampleName", String)
+        self.input("alleleFreqThreshold", Float, default=0.5)
+        self.input("headerLines", File)
+
+        self.input("reference", FastaWithDict)
+
+        self.step(
+            "vardict",
+            VarDictGermline_1_6_0(
+                intervals=self.intervals,
+                bam=self.bam,
+                reference=self.reference,
+                sampleName=self.sampleName,
+                var2vcfSampleName=self.sampleName,
+                alleleFreqThreshold=self.alleleFreqThreshold,
+                var2vcfAlleleFreqThreshold=self.alleleFreqThreshold,
+                chromNamesAreNumbers=True,
+                vcfFormat=True,
+                chromColumn=1,
+                regStartCol=2,
+                geneEndCol=3,
+            ),
+        )
+        self.step(
+            "annotate",
+            BcfToolsAnnotate_1_5(file=self.vardict.out, headerLines=self.headerLines),
+        )
+        self.step(
+            "split", SplitMultiAllele(vcf=self.annotate.out, reference=self.reference)
+        )
+        self.step("trim", TrimIUPAC_0_0_5(vcf=self.split.out))
+
+        self.output("vardictVariants", source=self.vardict.out)
+        self.output("out", source=self.trim.out)
+
+
+if __name__ == "__main__":
+    v = VardictGermlineVariantCaller()
+    v.translate("wdl", with_resource_overrides=False)
+    # print(v.generate_resources_file("wdl", { "CaptureType": "targeted" }))
---
+++ packages/janis-pipelines.bioinformatics/janis_pipelines.bioinformatics-0.9.1-py3-none-any.whl/janis_bioinformatics/tools/variantcallers/vardictsomatic_variants.py
@@ -0,0 +1,72 @@
+from janis_core import File, String, Float, Int, Boolean
+
+from janis_bioinformatics.data_types import FastaWithDict, BamBai, Bed
+from janis_bioinformatics.tools import BioinformaticsWorkflow
+from janis_bioinformatics.tools.bcftools import BcfToolsAnnotate_1_5
+from janis_bioinformatics.tools.common import SplitMultiAllele
+from janis_bioinformatics.tools.vardict import VarDictSomatic_1_6_0
+from janis_bioinformatics.tools.pmac import TrimIUPAC_0_0_5
+
+
+class VardictSomaticVariantCaller(BioinformaticsWorkflow):
+    def id(self):
+        return "vardictSomaticVariantCaller"
+
+    def friendly_name(self):
+        return "Vardict Somatic Variant Caller"
+
+    def tool_provider(self):
+        return "Variant Callers"
+
+    def version(self):
+        return "v0.1.0"
+
+    def constructor(self):
+
+        self.input("normalBam", BamBai)
+        self.input("tumorBam", BamBai)
+
+        self.input("normalName", String)
+        self.input("tumorName", String)
+
+        self.input("intervals", Bed)
+
+        self.input("alleleFreqThreshold", Float(), 0.05)
+        self.input("headerLines", File)
+
+        self.input("reference", FastaWithDict)
+
+        self.step(
+            "vardict",
+            VarDictSomatic_1_6_0(
+                normalBam=self.normalBam,
+                tumorBam=self.tumorBam,
+                intervals=self.intervals,
+                reference=self.reference,
+                normalName=self.normalName,
+                tumorName=self.tumorName,
+                alleleFreqThreshold=self.alleleFreqThreshold,
+                chromNamesAreNumbers=True,
+                vcfFormat=True,
+                chromColumn=1,
+                regStartCol=2,
+                geneEndCol=3,
+            ),
+        )
+        self.step(
+            "annotate",
+            BcfToolsAnnotate_1_5(file=self.vardict.out, headerLines=self.headerLines),
+        )
+        self.step(
+            "split", SplitMultiAllele(reference=self.reference, vcf=self.annotate.out)
+        )
+        self.step("trim", TrimIUPAC_0_0_5(vcf=self.split.out))
+
+        self.output("vardictVariants", source=self.vardict.out)
+        self.output("out", source=self.trim.out)
+
+
+if __name__ == "__main__":
+    v = VardictSomaticVariantCaller()
+    v.translate("wdl", with_resource_overrides=False)
+    # print(v.generate_resources_file("wdl", { "CaptureType": "targeted" }))
