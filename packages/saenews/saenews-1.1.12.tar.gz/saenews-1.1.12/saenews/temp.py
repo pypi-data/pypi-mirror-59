dd7d9b33f33205a4b242ec02c6d5f10f6f0db27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:10 +0000

    Add package saenews/1.1.12/saenews-1.1.12.tar.gz

---
+++ packages/saenews/saenews-1.1.12.tar.gz/saenews-1.1.12/saenews/temp.py
@@ -0,0 +1,4 @@
+title = "Arise, Awake, stop not till the goal is reached."
+tag_line = "(Kathopanishad)"
+input_file = "SV.jpg"
+quote(title,tag_line,input_file, cord = (0.03,0.666), border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord=(0.03,0.766))
