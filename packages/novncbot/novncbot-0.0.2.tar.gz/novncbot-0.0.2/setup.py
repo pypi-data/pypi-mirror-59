16007b41f9305673e742a768e0c025c614d389c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:43 +0000

    Add package novncbot/0.0.2/novncbot-0.0.2.tar.gz

---
+++ packages/novncbot/novncbot-0.0.2.tar.gz/novncbot-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="novncbot",
+    version="0.0.2",
+    author="Shawn Johnson",
+    author_email="sjohnson@axiomega.com",
+    description="An asynchronous noVNC client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shawrkbait/novncbot",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "selenium",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
