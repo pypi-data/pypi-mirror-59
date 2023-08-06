a017496b29669946588ff183754633556dc0fd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:17 +0000

    Add package vicorrect/0.0.6/vicorrect-0.0.6.tar.gz

---
+++ packages/vicorrect/vicorrect-0.0.6.tar.gz/vicorrect-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vicorrect",
+    version="0.0.6",
+    author="Hanh.Le Van",
+    author_email="lvhanh.270597@gmail.com",
+    description="Helpful tool to guess correct vietnamese sentence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lvhanh270597/correct_vietnamese_sentence",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nltk',
+        'faiss'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
