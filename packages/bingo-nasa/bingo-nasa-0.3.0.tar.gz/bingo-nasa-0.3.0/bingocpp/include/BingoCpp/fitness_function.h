ac34920c41b9070cf3a40a053aab6ef943e36401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:58 +0000

    Add package bingo-nasa/0.3.0/bingo-nasa-0.3.0.tar.gz

---
+++ packages/bingo-nasa/bingo-nasa-0.3.0.tar.gz/bingo-nasa-0.3.0/bingocpp/include/BingoCpp/fitness_function.h
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2018 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * The Bingo Mini-app platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+#ifndef BINGOCPP_INCLUDE_BINGOCPP_FITNESS_FUNCTION_H_
+#define BINGOCPP_INCLUDE_BINGOCPP_FITNESS_FUNCTION_H_
+
+#include <stdexcept>
+#include <string>
+#include <unordered_set>
+
+#include "BingoCpp/agraph.h"
+#include "BingoCpp/equation.h"
+#include "BingoCpp/training_data.h"
+
+namespace bingo {
+
+const std::unordered_set<std::string> kMeanAbsoluteError = {
+  "mean_absolute_error",
+  "mae"
+};
+
+const std::unordered_set<std::string> kMeanSquaredError = {
+  "mean_squared_error",
+  "mse"
+};
+
+const std::unordered_set<std::string> kRootMeanSquaredError = {
+  "root_mean_squared_error",
+  "rmse"
+};
+
+class FitnessFunction {
+ public:
+  inline FitnessFunction(TrainingData *training_data = nullptr) :
+    eval_count_(0), training_data_(training_data) { }
+
+  virtual ~FitnessFunction() { }
+
+  virtual double EvaluateIndividualFitness(const Equation &individual) const = 0;
+
+ protected:
+  mutable int eval_count_;
+  TrainingData* training_data_;
+};
+
+inline bool metric_found(const std::unordered_set<std::string> &set,
+                         std::string metric) {
+  return set.find(metric) != set.end();
+}
+
+class VectorBasedFunction : public FitnessFunction {
+ public:
+  typedef double (VectorBasedFunction::* MetricFunctionPointer)(const Eigen::ArrayXXd&);
+  VectorBasedFunction(TrainingData *training_data = nullptr,
+                      std::string metric = "mae") :
+      FitnessFunction(training_data) {
+    metric_function_ = GetMetric(metric);
+  }
+
+  virtual ~VectorBasedFunction() { }
+
+  double EvaluateIndividualFitness(const Equation &individual) const {
+    Eigen::ArrayXXd fitness_vector = EvaluateFitnessVector(individual);
+    return (const_cast<VectorBasedFunction*>(this)->*metric_function_)(fitness_vector);
+  }
+
+  virtual Eigen::ArrayXXd
+  EvaluateFitnessVector(const Equation &individual) const = 0;
+
+ protected:
+  double mean_absolute_error(const Eigen::ArrayXXd &fitness_vector) {
+    return fitness_vector.abs().mean();
+  }
+
+  double root_mean_square_error(const Eigen::ArrayXXd &fitness_vector) {
+    return sqrt(fitness_vector.square().mean());
+  }
+
+  double mean_squared_error(const Eigen::ArrayXXd &fitness_vector) {
+    return fitness_vector.square().mean();
+  }
+
+  MetricFunctionPointer GetMetric(std::string metric) {
+    if (metric_found(kMeanAbsoluteError, metric)) {
+      return &VectorBasedFunction::mean_absolute_error;
+    } else if (metric_found(kMeanSquaredError, metric)) {
+      return &VectorBasedFunction::mean_squared_error;
+    } else if (metric_found(kRootMeanSquaredError, metric)) {
+      return &VectorBasedFunction::root_mean_square_error;
+    } else {
+      throw std::invalid_argument("Invalid metric for Fitness Function");
+    }
+  }
+
+ private:
+  mutable MetricFunctionPointer metric_function_;
+};
+} // namespace bingo
+
+#endif // BINGOCPP_INCLUDE_BINGOCPP_FITNESS_FUNCTION_H_
---
+++ packages/bingo-nasa/bingo-nasa-0.3.0.tar.gz/bingo-nasa-0.3.0/bingocpp/include/BingoCpp/implicit_regression.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * The Bingo Mini-app platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+#ifndef BINGOCPP_INCLUDE_BINGOCPP_IMPLICIT_REGRESSION_H_
+#define BINGOCPP_INCLUDE_BINGOCPP_IMPLICIT_REGRESSION_H_
+
+#include <Eigen/Dense>
+
+#include "BingoCpp/equation.h"
+#include "BingoCpp/fitness_function.h"
+#include "BingoCpp/training_data.h"
+
+#include "BingoCpp/utils.h"
+
+namespace bingo {
+
+struct ImplicitTrainingData : TrainingData {
+ public:
+  Eigen::ArrayXXd x;
+
+  Eigen::ArrayXXd dx_dt;
+
+  ImplicitTrainingData(const Eigen::ArrayXXd &input) {
+    InputAndDeriviative input_and_deriv = CalculatePartials(input);
+    x = input_and_deriv.first;
+    dx_dt = input_and_deriv.second;
+  }
+
+  ImplicitTrainingData(const Eigen::ArrayXXd &input,
+                       const Eigen::ArrayXXd &derivative) {
+    x = input; 
+    dx_dt = derivative;
+  }
+
+  ImplicitTrainingData(ImplicitTrainingData &other) {
+    x = other.x;
+    dx_dt = other.dx_dt;
+  } 
+
+  ImplicitTrainingData* GetItem(int item);
+
+  ImplicitTrainingData* GetItem(const std::vector<int> &items);
+
+  int Size() { 
+    return x.rows();
+  }
+};
+
+class ImplicitRegression : public VectorBasedFunction {
+ public:
+  ImplicitRegression(ImplicitTrainingData *training_data, 
+                     int required_params = kNoneRequired,
+                     bool normalize_dot = false,
+                     std::string metric="mae") :
+      VectorBasedFunction(new ImplicitTrainingData(*training_data), metric) {
+    required_params_ = required_params;
+    normalize_dot_ = normalize_dot;
+  }
+
+  ~ImplicitRegression() {
+    delete training_data_;
+  }
+
+  Eigen::ArrayXXd EvaluateFitnessVector(const Equation &equation) const;
+
+ private:
+  int required_params_;
+  bool normalize_dot_;
+  static const int kNoneRequired = -1;
+};
+
+class ImplicitRegressionSchmidt : VectorBasedFunction {
+
+};
+} // namespace bingo
+#endif //BINGOCPP_INCLUDE_BINGOCPP_IMPLICIT_REGRESSION_H_
