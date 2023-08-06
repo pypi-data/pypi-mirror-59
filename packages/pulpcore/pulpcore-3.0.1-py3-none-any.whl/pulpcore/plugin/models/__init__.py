732e8fee9f94b211664d151dab6fac660049d91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:54 +0000

    Add package pulpcore/3.0.1/pulpcore-3.0.1-py3-none-any.whl

---
+++ packages/pulpcore/pulpcore-3.0.1-py3-none-any.whl/pulpcore/plugin/models/__init__.py
@@ -0,0 +1,27 @@
+# Models are exposed selectively in the versioned plugin API.
+# Any models defined in the pulpcore.plugin namespace should probably be proxy models.
+
+from pulpcore.app.models import (  # noqa
+    Artifact,
+    BaseDistribution,
+    BaseModel,
+    Content,
+    ContentArtifact,
+    ContentGuard,
+    CreatedResource,
+    FileSystemPublicationExporter,
+    FileSystemRepositoryVersionExporter,
+    MasterModel,
+    ProgressReport,
+    Publication,
+    PublicationDistribution,
+    PublishedArtifact,
+    PublishedMetadata,
+    Repository,
+    Remote,
+    RemoteArtifact,
+    RepositoryContent,
+    RepositoryVersion,
+    RepositoryVersionDistribution,
+    Task,
+)
