4380602070c834aa5ccae6a5000159e604ba6844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:08 +0000

    Add package cachet-url-monitor/0.6.2/cachet-url-monitor-0.6.2.tar.gz

---
+++ packages/cachet-url-monitor/cachet-url-monitor-0.6.2.tar.gz/cachet-url-monitor-0.6.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='cachet-url-monitor',
+      version='0.6.2',
+      description='Cachet URL monitor plugin',
+      author='Mitsuo Takaki',
+      author_email='mitsuotakaki@gmail.com',
+      url='https://github.com/mtakaki/cachet-url-monitor',
+      packages=['cachet_url_monitor'],
+      license='MIT',
+      requires=[
+          'requests',
+          'yaml',
+          'schedule',
+      ],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"]
+      )
