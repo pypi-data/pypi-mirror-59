f15cc366d0a135edf6849a2e9bad8b79d7a3c743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:10 +0000

    Add package swapper/1.1.2.post1/swapper-1.1.2.post1.tar.gz

---
+++ packages/swapper/swapper-1.1.2.post1.tar.gz/swapper-1.1.2.post1/setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = """
+The unofficial Django swappable models API.
+"""
+
+
+def readme():
+    try:
+        readme = open('README.md')
+    except IOError:
+        return LONG_DESCRIPTION
+    else:
+        return readme.read()
+
+
+setup(
+    name='swapper',
+    version='1.1.2.post1',
+    author='S. Andrew Sheppard',
+    author_email='andrew@wq.io',
+    url='https://github.com/wq/django-swappable-models',
+    license='MIT',
+    packages=['swapper'],
+    description=LONG_DESCRIPTION.strip(),
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+    ],
+    tests_require=['django>=1.6'],
+    test_suite='tests',
+    setup_requires=[
+        'setuptools_scm',
+    ],
+)
