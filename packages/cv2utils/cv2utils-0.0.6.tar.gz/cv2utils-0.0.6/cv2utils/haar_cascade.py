dd3647ab95674439a321fbc60f6c61dc9174149f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:52 +0000

    Add package cv2utils/0.0.6/cv2utils-0.0.6.tar.gz

---
+++ packages/cv2utils/cv2utils-0.0.6.tar.gz/cv2utils-0.0.6/cv2utils/haar_cascade.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+from pkg_resources import Requirement, resource_filename
+
+
+class HaarCascadeFace(object):
+
+    def __init__(self,
+                 weights_file: str = None,
+                 scaleFactor: float = 1.1,
+                 minNeighbors: int = 5,
+                 minSize: tuple = (1, 1)):
+
+        if weights_file is None:
+            weights_file = resource_filename(
+                Requirement.parse('cv2utils'), 'cv2utils' + os.path.sep + 'data' + os.path.sep +
+                'haarcascade_frontalface_default.xml')
+
+        if type(scaleFactor) is not float:
+            raise ValueError("Must be a float")
+
+        if type(minNeighbors) is not int:
+            raise ValueError("Must be a integer")
+
+        if len(minSize) > 2:
+            raise ValueError("Must be a tuple of two integers values")
+
+        if any([True if type(i) is not int else False for i in minSize]):
+            raise ValueError("Must be a tuple of two integers values")
+
+        self.scale_factor = scaleFactor
+        self.min_neighbors = minNeighbors
+        self.min_size = minSize
+        self.faceCascade = cv2.CascadeClassifier(weights_file)
+
+    def detect_face(self, image):
+
+        if image is None or not hasattr(image, "shape"):
+            raise ValueError("Image not valid.")
+
+        faces = self.faceCascade.detectMultiScale(
+            image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY),
+            scaleFactor=self.scale_factor,
+            minNeighbors=self.min_neighbors,
+            minSize=self.min_size)
+
+        return faces
---
+++ packages/cv2utils/cv2utils-0.0.6.tar.gz/cv2utils-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cv2utils',
+    version='0.0.6',
+    description='Face detector wrapper',
+    long_description='Face detector wrapper',
+    url='https://github.com/luizcarloscf/cv2utils.git',
+    author='Luiz Carlos Cosmi Filho',
+    author_email='luizcarloscosmifilho@gmail.com',
+    license='MIT',
+    packages=['cv2utils'],
+    zip_safe=False,
+    install_requires=[
+        'opencv-python==4.1.2.*',
+        'numpy==1.16.1',
+    ],
+    include_package_data=True,
+)
