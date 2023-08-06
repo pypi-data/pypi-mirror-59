6afdf314be5b6d3d4c0b4d82650b706803dd1b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:19 +0000

    Add package littlewitch/0.1.0/littlewitch-0.1.0.tar.gz

---
+++ packages/littlewitch/littlewitch-0.1.0.tar.gz/littlewitch-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2019 bluelief.
+# This source code is licensed under the MIT license.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="littlewitch",
+    version="0.1.0",
+    author="Bluelief",
+    description="Python project.",
+    license="MIT",
+    keywords="python",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
