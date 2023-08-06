77e0ee97c6bc6a9c1095adc2351a7ca606cd8114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package aero-astro-calc/2019.0.1/aero_astro_calc-2019.0.1.tar.gz

---
+++ packages/aero-astro-calc/aero_astro_calc-2019.0.1.tar.gz/aero_astro_calc-2019.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aero_astro_calc",  # Replace with your own username
+    version="2019.0.1",
+    author="Anson Biggs",
+    author_email="anson@ansonbiggs.com",
+    description="A small python library with functions to assist engineers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/MisterBiggs/aero-astro-calc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # python_requires=">=3.6",
+)
