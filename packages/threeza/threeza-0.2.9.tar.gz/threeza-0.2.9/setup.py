c6586267f93e1e46607f5530ed18a41dc606f6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:36 +0000

    Add package threeza/0.2.9/threeza-0.2.9.tar.gz

---
+++ packages/threeza/threeza-0.2.9.tar.gz/threeza-0.2.9/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="threeza",
+    version="0.2.9",
+    description="Minimalist conventions for www.3za.org experiment",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/threeza",
+    author="microprediction",
+    author_email="info@3za.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["threeza"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "threeza=threeza.__main__:main",
+        ]
+     },
+     )
