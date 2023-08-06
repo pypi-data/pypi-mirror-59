518abd9780fa54fae511b86ce79b94e4debc5e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:27 +0000

    Add package omikuji/0.3.1/omikuji-0.3.1-cp35-cp35m-win_amd64.whl

---
+++ packages/omikuji/omikuji-0.3.1-cp35-cp35m-win_amd64.whl/omikuji/_libomikuji.py
@@ -0,0 +1,8 @@
+# auto-generated file
+__all__ = ['lib', 'ffi']
+
+import os
+from omikuji._libomikuji__ffi import ffi
+
+lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), '_libomikuji__lib.cp35-win_amd64.pyd'), 0)
+del os
