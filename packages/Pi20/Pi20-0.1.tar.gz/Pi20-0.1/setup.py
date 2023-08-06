ef40582b75f6ed5072d6e6cc06b3fb5ca5cc895e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:00 +0000

    Add package Pi20/0.1/Pi20-0.1.tar.gz

---
+++ packages/Pi20/Pi20-0.1.tar.gz/Pi20-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import subprocess
+
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+
+setup(
+    name = "Pi20",
+    version = "0.1",
+    author = "Andy Emre Kocak, Rana Taki",
+    author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr",
+    description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
+    packages = ["Pi20"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["Development Status :: 4 - Beta"],
+    install_requires=[
+        'picamera',
+        'pygame',
+        'RPi.GPIO',
+        'wiringpi',
+        'numpy',
+        'opencv-contrib-python'
+    ]
+
+)
