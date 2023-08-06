87961c16e60a1c291a03f95a6e0b05223c3bc944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:26 +0000

    Add package Kaggler/0.8.9/Kaggler-0.8.9.tar.gz

---
+++ packages/Kaggler/Kaggler-0.8.9.tar.gz/Kaggler-0.8.9/kaggler/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+from .categorical import OneHotEncoder, LabelEncoder, TargetEncoder, EmbeddingEncoder
+from .numerical import Normalizer, QuantileEncoder
+
+__all__ = ['OneHotEncoder', 'LabelEncoder', 'TargetEncoder', 'EmbeddingEncoder',
+           'Normalizer', 'QuantileEncoder']
