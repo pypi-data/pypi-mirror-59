7fb28af45716c46b9d150dda39d4b4f11d87f67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:35 +0000

    Add package sermos-utils/0.7.1/sermos-utils-0.7.1.tar.gz

---
+++ packages/sermos-utils/sermos-utils-0.7.1.tar.gz/sermos-utils-0.7.1/sermos_utils/constants.py
@@ -0,0 +1,7 @@
+""" Constants used throughout utils.
+"""
+
+ENV_VAR_DEPLOY_KEY = "SERMOS_DEPLOY_KEY"
+ENV_VAR_PKG_NAME = "SERMOS_CLIENT_PKG_NAME"
+DEFAULT_DEPLOY_URL = "https://admin.sermos.ai/api/v1/deploy"
+DEFAULT_YAML_NAME = "sermos.yaml"
