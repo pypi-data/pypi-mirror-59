4a81fa2d837e5ce227662042dbdffaee92fe122f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:51 +0000

    Add package scrapi/0.3.1/scrapi-0.3.1.tar.gz

---
+++ packages/scrapi/scrapi-0.3.1.tar.gz/scrapi-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="scrapi",
+    version="0.3.1",
+    packages=[
+        "scrap",
+        "scrap.tags",
+        "scrap.chromium",
+        "scrap.plotting",
+        "scrap.dataset",
+        "scrap.utils"
+    ],
+    entry_points={
+        "console_scripts": [
+            "scrap-preprocess=scrap.dataset.preprocess:preprocess",
+            "scrap-init-ws=scrap.dataset.initialize:initialize"
+        ]
+    },
+    install_requires=[
+        "pandas",
+        "pepars",
+        "sparsedat",
+        "numpy",
+        "plotly",
+        "h5py",
+        "sklearn",
+        "scipy",
+        "statsmodels"
+    ]
+)
