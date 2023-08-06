35bdf2c1f09c30178d25582d246de9221f24a1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:25 +0000

    Add package mallu/0.1.2/mallu-0.1.2.tar.gz

---
+++ packages/mallu/mallu-0.1.2.tar.gz/mallu-0.1.2/mallu.py
@@ -0,0 +1,15 @@
+import model
+import santa
+
+def about():
+	santa.title('MALLU','*')
+	print()
+	santa.title('ABOUT THE AUTHOR','=')
+	santa.title('MALLU module is written by Santo K Thomas, Kerala, India')
+	santa.title('Email:santokalayil@gmail.com')
+	santa.title('-')
+	print()
+	santa.title('HOW TO USE THE MODULE','=')
+	santa.title('import mallu.santa as mls',' ')
+	santa.title('import mallu.model as mlm',' ')
+
