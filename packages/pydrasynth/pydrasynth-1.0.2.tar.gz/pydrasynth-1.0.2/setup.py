4162a635a87775a6b92199bb9ed7a144e62d206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:47 +0000

    Add package pydrasynth/1.0.2/pydrasynth-1.0.2.tar.gz

---
+++ packages/pydrasynth/pydrasynth-1.0.2.tar.gz/pydrasynth-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'pydrasynth',         
+  packages = ['pydrasynth'],   
+  version = '1.0.2',
+  license='MIT',        
+  description = 'A visual synthesizer written in Python',   
+  author = 'Daan Klijn',                   
+  author_email = 'daanklijn0@gmail.com',      
+  url = 'https://github.com/daanklijn/pydra',   
+  download_url = 'https://github.com/daanklijn/pydra/archive/1.0.2.tar.gz',    
+  keywords = ['python', 'synthesizer', 'visual'],   
+  install_requires=[            
+          'numpy',
+          'scipy',
+          'pygame',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
