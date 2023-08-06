1d637b10311b2329af97145ce725c64f99e069f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:54 +0000

    Add package vup/1.1.3/vup-1.1.3.tar.gz

---
+++ packages/vup/vup-1.1.3.tar.gz/vup-1.1.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+setuptools.setup(
+    name='vup',
+    version='1.1.3',
+    entry_points={
+        'console_scripts': [
+            'vup = vup.main:main'
+        ],
+    },
+    packages=setuptools.find_packages(),
+
+    description='VUP: Version UPdater: Version management system',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    author='SiLeader',
+    url='https://github.com/SiLeader/vup',
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Code Generators',
+    ],
+
+    license='GPLv3.0'
+)
