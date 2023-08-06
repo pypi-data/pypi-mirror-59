689d6cb398672fe657b545494e260ac08e3814f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:42 +0000

    Add package mesh-to-sdf/0.0.2/mesh_to_sdf-0.0.2-py3-none-any.whl

---
+++ packages/mesh-to-sdf/mesh_to_sdf-0.0.2-py3-none-any.whl/mesh_to_sdf/surface_point_cloud.py
@@ -0,0 +1,106 @@
+from mesh_to_sdf.scan import Scan
+
+import trimesh
+import logging
+logging.getLogger("trimesh").setLevel(9000)
+import numpy as np
+from sklearn.neighbors import KDTree
+import math
+import pyrender
+
+class SurfacePointCloud:
+    def __init__(self, mesh, points, normals=None, scans=None):
+        self.mesh = mesh
+        self.points = points
+        self.normals = normals
+        self.scans = scans
+
+        self.kd_tree = KDTree(points)
+
+    def get_random_surface_points(self, count, use_scans=True):
+        if use_scans:
+            indices = np.random.choice(self.points.shape[0], count)
+            return self.points[indices, :]
+        else:
+            return self.mesh.sample(count)
+
+    def get_sdf(self, query_points, use_depth_buffer=False, sample_count=11):
+        if use_depth_buffer:
+            distances, _ = self.kd_tree.query(query_points)
+            distances = distances.astype(np.float32).reshape(-1) * -1
+            distances[self.is_outside(query_points)] *= -1
+            return distances
+        else:
+            distances, indices = self.kd_tree.query(query_points, k=sample_count)
+            distances = distances.astype(np.float32)
+
+            closest_points = self.points[indices]
+            direction_to_surface = query_points[:, np.newaxis, :] - closest_points
+            inside = np.einsum('ijk,ijk->ij', direction_to_surface, self.normals[indices]) < 0
+            inside = np.sum(inside, axis=1) > sample_count * 0.5
+            distances = distances[:, 0]
+            distances[inside] *= -1
+            return distances
+
+    def get_sdf_in_batches(self, query_points, use_depth_buffer=False, sample_count=11, batch_size=1e6):
+        if query_points.shape[0] <= batch_size:
+            return self.get_sdf(query_points, use_depth_buffer=use_depth_buffer, sample_count=sample_count)
+        
+        result = np.zeros(query_points.shape[0])
+        for i in range(int(math.ceil(query_points.shape[0] / batch_size))):
+            start = i * batch_size
+            end = min(result.shape[0], (i + 1) * batch_size)
+            result[start:end] = self.get_sdf(query_points[start:end, :], use_depth_buffer=use_depth_buffer, sample_count=sample_count)
+        return result
+
+    def show(self):
+        scene = pyrender.Scene()
+        scene.add(pyrender.Mesh.from_points(self.points, normals=self.normals))
+        pyrender.Viewer(scene, use_raymond_lighting=True, point_size=2)
+        
+    def is_outside(self, points):
+        result = None
+        for scan in self.scans:
+            if result is None:
+                result = scan.is_visible(points)
+            else:
+                result = np.logical_or(result, scan.is_visible(points))
+        return result
+
+def get_equidistant_camera_angles(count):
+    increment = math.pi * (3 - math.sqrt(5))
+    for i in range(count):
+        theta = math.asin(-1 + 2 * i / (count - 1))
+        phi = ((i + 1) * increment) % (2 * math.pi)
+        yield phi, theta
+
+def create_from_scans(mesh, bounding_radius=1, scan_count=100, scan_resolution=400, calculate_normals=True):
+    scans = []
+
+    for phi, theta in get_equidistant_camera_angles(scan_count):
+        scans.append(Scan(mesh,
+            rotation_x=phi,
+            rotation_y=theta,
+            bounding_radius=bounding_radius,
+            resolution=scan_resolution,
+            calculate_normals=calculate_normals
+        ))
+
+    return SurfacePointCloud(mesh, 
+        points=np.concatenate([scan.points for scan in scans], axis=0),
+        normals=np.concatenate([scan.normals for scan in scans], axis=0) if calculate_normals else None,
+        scans=scans
+    )
+
+def sample_from_mesh(mesh, sample_point_count=10000000, calculate_normals=True):
+    if calculate_normals:
+        points, face_indices = mesh.sample(sample_point_count, return_index=True)
+        normals = mesh.face_normals[face_indices]
+    else:
+        points = mesh.sample(sample_point_count, return_index=False)
+
+    return SurfacePointCloud(mesh, 
+        points=points,
+        normals=normals if calculate_normals else None,
+        scans=None
+    )
