a366fe79471ce82645340a89b8a6dc10d0a26d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:10 +0000

    Add package haihonglicom-test-package/1.0/haihonglicom-test-package-1.0.tar.gz

---
+++ packages/haihonglicom-test-package/haihonglicom-test-package-1.0.tar.gz/haihonglicom-test-package-1.0/README.rst
@@ -0,0 +1,7 @@
+haihonglicom-test-package
+-------------------------
+
+This is a simple test package. To use it, type::
+
+  from haihonglicom_test_package import test
+  test.run()
---
+++ packages/haihonglicom-test-package/haihonglicom-test-package-1.0.tar.gz/haihonglicom-test-package-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+      name="haihonglicom-test-package",
+      packages=setuptools.find_packages(),
+      version="1.0",
+      description="Test Package",
+      author="haihong",
+      author_email="haihongli@qq.com",
+      url="https://github.com/haihonglicom/test-package",
+      download_url="https://github.com/haihonglicom/test-package/tarball/1.0",
+      keywords=["test", "pythonb"],
+      classifiers=[]
+)
