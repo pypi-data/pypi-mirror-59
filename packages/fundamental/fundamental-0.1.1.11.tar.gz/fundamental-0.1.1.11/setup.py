e6a16a509eaebbae65598ad0a36579833af9acf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package fundamental/0.1.1.11/fundamental-0.1.1.11.tar.gz

---
+++ packages/fundamental/fundamental-0.1.1.11.tar.gz/fundamental-0.1.1.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+   name='fundamental',
+   version='0.1.1.11',
+   description='Dataframe downloader for fundamental financials',
+   long_description=long_description,
+   url= 'https://github.com/steven2K19/fundamental',
+   author='Steven Wang',
+   author_email='steven.wang0619@gmail.com',
+   license='MIT',
+   packages=find_packages(),
+   install_requires=['numpy', 'pandas','yfinance','datetime', 'lxml',
+                     'pandas_market_calendars']
+)
+
+
+
+
+
+
+
