c26093eff721084d6b67f3de0791c157eef525b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:17 +0000

    Add package mayan-edms/3.3.9/mayan_edms-3.3.9-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-3.3.9-py2.py3-none-any.whl/mayan/apps/templating/widgets.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, unicode_literals
+
+from collections import OrderedDict
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.common.classes import ModelProperty
+from mayan.apps.common.widgets import NamedMultiWidget
+
+
+class TemplateWidget(NamedMultiWidget):
+    class Media:
+        js = ('templating/js/template_widget.js',)
+
+    def __init__(self, attrs=None, **kwargs):
+        widgets = OrderedDict()
+
+        widgets['model_property'] = forms.widgets.Select(
+            attrs={'data-template-fields': 'model_property'}
+        )
+        widgets['template'] = forms.widgets.Textarea(
+            attrs={'rows': 5, 'data-template-fields': 'template'}
+        )
+        super(TemplateWidget, self).__init__(
+            widgets=widgets, attrs=attrs, **kwargs
+        )
+
+    def get_context(self, name, value, attrs):
+        result = super(TemplateWidget, self).get_context(name, value, attrs)
+        result['widget']['subwidgets'][0]['attrs']['required'] = False
+        return result
+
+    def decompress(self, value):
+        choices = ModelProperty.get_choices_for(
+            model=self.attrs['model']
+        )
+        self.widgets['model_property'].choices = (
+            [('', _('<Model property choices>'))] + choices
+        )
+        return {
+            'model_property': None, 'template': value
+        }
+
+    def value_from_datadict(self, querydict, files, name):
+        template = querydict.get('{}_template'.format(name))
+
+        return template
