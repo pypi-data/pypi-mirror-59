919c1057a7c682c417656f956be5c673369c6f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:55 +0000

    Add package MISP-maltego/1.4.3/MISP_maltego-1.4.3.tar.gz

---
+++ packages/MISP-maltego/MISP_maltego-1.4.3.tar.gz/MISP_maltego-1.4.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='MISP_maltego',
+    author='Christophe Vandeplas',
+    # also update version in util.py
+    version='1.4.3',
+    author_email='christophe@vandeplas.com',
+    maintainer='Christophe Vandeplas',
+    url='https://github.com/MISP/MISP-maltego',
+    description='Maltego transform for interacting with a MISP Threat Sharing community and with MITRE ATT&CK.',
+    license='AGPLv3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ],
+    package_data={
+        '': ['*.gif', '*.png', '*.conf', '*.mtz', '*.machine']  # list of resources
+    },
+    python_requires='>=3.5',
+    install_requires=[
+        'canari>=3.3.10,<4',
+        'PyMISP>=2.4.114'
+    ],
+    dependency_links=[
+        # custom links for the install_requires
+    ]
+)
