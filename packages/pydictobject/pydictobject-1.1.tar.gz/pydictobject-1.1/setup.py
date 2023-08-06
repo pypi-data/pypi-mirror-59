03496e1ce284da1a435e8e9b221d99b52d1a1ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:00 +0000

    Add package pydictobject/1.1/pydictobject-1.1.tar.gz

---
+++ packages/pydictobject/pydictobject-1.1.tar.gz/pydictobject-1.1/setup.py
@@ -0,0 +1,14 @@
+"""Setup file for dictobject"""
+
+from setuptools import setup
+
+setup(
+    name='pydictobject',
+    version='v1.01',
+    description='DictObject - a dictionary that is accessible like an object',
+    packages=['pydictobject'],
+    author='Peter Harris',
+    author_email='pdrharris@gmail.com',
+    url='https://github.com/pdrharris/pydictobject',
+    download_url='https://github.com/pdrharris/pydictobject/archive/1.01.tar.gz'
+)
