a774674a6da1b635031dbe1e25fb831a0eac4153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:13 +0000

    Add package topsis-ppruthi-101883058/1.0.5/topsis-ppruthi-101883058-1.0.5.tar.gz

---
+++ packages/topsis-ppruthi-101883058/topsis-ppruthi-101883058-1.0.5.tar.gz/topsis-ppruthi-101883058-1.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsis-ppruthi-101883058", 
+    version="1.0.5",
+    author="Pritpal Singh Pruthi",
+    author_email="ppruthi_be17@thapar.edu",
+    description="A python package to identify the best model out of different models using TOPSIS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    License="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    packages=["topsis_python"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={"console_scripts":["topsis=topsis_python.topsis:main"]},    
+)
