33c95512fa8c1923c5d4cebc232d09dbd5d89149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package e3-core/21.0.0/e3_core-21.0.0-py3-none-any.whl

---
+++ packages/e3-core/e3_core-21.0.0-py3-none-any.whl/e3/store/__init__.py
@@ -0,0 +1,10 @@
+
+import stevedore
+
+
+def load_store(name, configuration, cache):
+    plugin = stevedore.DriverManager(
+        'e3.store',
+        name)
+
+    return plugin.driver(configuration, cache)
---
+++ packages/e3-core/e3_core-21.0.0-py3-none-any.whl/tests/tests_e3/anod/test_buildspace.py
@@ -0,0 +1,48 @@
+
+
+import os
+
+from e3.anod.buildspace import BuildSpace
+from e3.fs import mkdir
+from e3.os.fs import touch
+
+import pytest
+
+
+def test_reset():
+    """Verify that BuildSpace reset() delete expected content."""
+    bs = BuildSpace(root_dir=os.getcwd())
+    bs.create()
+    for name in bs.dirs:
+        touch(os.path.join(bs.subdir(name), 'a'))
+
+    # Reset delete all dirs except log and result by default
+    bs.reset()
+    for name in bs.dirs:
+        exists = os.path.exists(
+            os.path.join(bs.subdir(name), 'a'))
+        if name in ('log', 'results'):
+            assert exists
+        else:
+            assert not exists
+
+
+def test_subdir():
+    """Check error handling of subdir."""
+    bs = BuildSpace(root_dir=os.getcwd())
+    with pytest.raises(ValueError):
+        bs.subdir('foo')
+
+
+def test_reset_tmp_dir():
+    """Check that the tmp_dir is reset when the build space is created.
+
+    REQ-EC19.
+    """
+    bs = BuildSpace(root_dir=os.getcwd())
+    marker = os.path.join(bs.subdir(name='tmp'), 'deleteme')
+    mkdir(bs.tmp_dir)
+    touch(marker)
+    assert os.path.exists(marker)
+    bs.create()
+    assert not os.path.exists(marker)
