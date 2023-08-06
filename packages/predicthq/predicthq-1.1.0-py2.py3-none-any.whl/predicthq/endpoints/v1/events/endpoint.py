28b3770570f82b4aaee047d9a6400bab9485e82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package predicthq/1.1.0/predicthq-1.1.0-py2.py3-none-any.whl

---
+++ packages/predicthq/predicthq-1.1.0-py2.py3-none-any.whl/predicthq/endpoints/v1/events/endpoint.py
@@ -0,0 +1,21 @@
+from predicthq.endpoints.base import UserBaseEndpoint
+from predicthq.endpoints.decorators import accepts, returns
+from .schemas import SearchParams, EventResultSet, Count, CalendarParams, CalendarResultSet
+
+
+class EventsEndpoint(UserBaseEndpoint):
+
+    @accepts(SearchParams)
+    @returns(EventResultSet)
+    def search(self, **params):
+        return self.client.get(self.build_url('v1', 'events'), params=params)
+
+    @accepts(SearchParams)
+    @returns(Count)
+    def count(self, **params):
+        return self.client.get(self.build_url('v1', 'events/count'), params=params)
+
+    @accepts(CalendarParams)
+    @returns(CalendarResultSet)
+    def calendar(self, **params):
+        return self.client.get(self.build_url('v1', 'events/calendar'), params=params)
