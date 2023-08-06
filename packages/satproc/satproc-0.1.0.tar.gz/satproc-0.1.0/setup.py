1da628bdad8b9a12f3cc03946187fc3be0e55786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package satproc/0.1.0/satproc-0.1.0.tar.gz

---
+++ packages/satproc/satproc-0.1.0.tar.gz/satproc-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='satproc',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python package for satellite data download and processing.',
+    author='Jacob Høxbroe Jeppesen',
+    license='MIT',
+)
