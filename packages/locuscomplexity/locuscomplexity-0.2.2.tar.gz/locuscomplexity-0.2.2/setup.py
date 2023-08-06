6f48e508a377b151f0586015181f96f46a9005ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:25 +0000

    Add package locuscomplexity/0.2.2/locuscomplexity-0.2.2.tar.gz

---
+++ packages/locuscomplexity/locuscomplexity-0.2.2.tar.gz/locuscomplexity-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='locuscomplexity',
+    version='0.2.2',
+    packages=['locuscomplexity'],
+    url='https://github.com/LocusAnalytics/EconCmplx',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pandas', 'networkx', 'matplotlib', ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
