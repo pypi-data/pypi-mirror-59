723b14b0728449086fc1631ea3a0779180fd7a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package pyhealthbox/0.0.3/pyhealthbox-0.0.3.tar.gz

---
+++ packages/pyhealthbox/pyhealthbox-0.0.3.tar.gz/pyhealthbox-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyhealthbox",
+    version="0.0.3",
+    description="Python Client to interact with the Renson Healthbox 3.0 Ventilation system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdiepend/pyhealthbox",
+    author="Stijn Diependaele",
+    author_email="sdiepend@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="client api library renson healthbox ventilation",
+    project_urls={
+        "Documentation": "https://github.com/sdiepend/pyhealthbox/blob/master/README.md"
+    },
+    py_modules=["pyhealthbox"],
+    install_requires=[
+        "requests"
+    ],
+    python_requires=">=3.5",
+)
