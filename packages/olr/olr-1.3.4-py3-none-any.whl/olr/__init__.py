a0e6e275647606347cf7d14c0a5b46a7fd6383e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package olr/1.3.4/olr-1.3.4-py3-none-any.whl

---
+++ packages/olr/olr-1.3.4-py3-none-any.whl/olr/__init__.py
@@ -0,0 +1,6 @@
+from . olr_function import olr
+from . olr_function import olrmodels
+from . olr_function import olrformulas
+from . olr_function import olrformulasorder
+from . olr_function import adjr2list
+from . olr_function import r2list
