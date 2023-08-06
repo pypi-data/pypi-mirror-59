8bfc75a3f4d974ca058180d240bf1389a270c08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:13 +0000

    Add package jarviscore/0.1.1.25/jarviscore-0.1.1.25.tar.gz

---
+++ packages/jarviscore/jarviscore-0.1.1.25.tar.gz/jarviscore-0.1.1.25/README.md
@@ -0,0 +1,86 @@
+#Jarvis 
+
+**Status**
+[![Build status](https://dev.azure.com/cubbei/JarvisCore/_apis/build/status/JarvisCore-PiP%20Publish)](https://dev.azure.com/cubbei/JarvisCore/_build/latest?definitionId=1)
+
+
+This is the repository for Jarvis, the twitch bot.
+
+##Getting Started
+
+The simplest way to get started is to create a new file, with the basic code below:
+
+```python
+from jarviscore.client import Client
+
+jarvis = Client(nick="yourbotsname", 
+    token="yourbotstoken",
+    channels=["a list", "of channels", "to connect to"])
+jarvis.start()
+```
+
+As an alternative, better practice would be to make use of a config file to store your settings and loading them into the bot when you start.  
+Use the following code for your bot as a starter.
+```python
+from jarviscore.client import Client
+from jarviscore import Settings
+
+setting = Settings()
+
+jarvis = Client(nick=setting.get_setting("nick"), 
+    token=setting.get_setting("token"),
+    channels=setting.get_setting("channels"))
+jarvis.start()
+```
+Next, create a file called `config.json` and use the following template to get started
+```json
+{
+    "name": "yourbotsname",
+    "token": "yourbotstoken",
+    "channels": [
+        "a list", "of channels", "to connect to"
+    ]
+}
+```
+If you prefer, you may use a `config.yaml` file instead.
+
+##Custom Commands
+
+You can create your own custom commands and interactions for your bot using the Jarvis Core. 
+Create a folder called `Commands` in the same location as your bot file like so,
+```
++-- root
+|   |-- bot.py
+|   |-- config.json
+|   +-- Commands
+|       |-- command1.py
+|       +-- command2.py
+```
+
+Then, copy the following boiler plate text to get started. This example implements a simple ping command.
+**Note:** All Commands need to implement `setup()` and `teardown()`, both take channel as a parameter.
+
+```python
+from jarviscore import Command, Log
+from jarviscore.message import CommandMessage
+
+log = Log("CORE:Ping", verbose="log")
+class Ping(Command):
+
+    def __init__(self, channel):
+        Command.__init__(self, "Ping")
+        self.channel = channel
+    
+    def on_command(self, data: CommandMessage):
+        if "ping" == data.KEYWORD:
+            self.channel.send("pong")
+
+
+def setup(channel):
+    channel.load_command(Ping(channel))
+    log.log(f"[{channel.name}]: Loaded Module Ping")
+
+def teardown(channel):
+    log.log(f"[{channel.name}]: Removed Module Ping")
+
+```
---
+++ packages/jarviscore/jarviscore-0.1.1.25.tar.gz/jarviscore-0.1.1.25/jarviscore/socket.py
@@ -0,0 +1,292 @@
+import socket
+from time import sleep
+
+
+from .log import Log
+# from .bridge import Bridge
+from threading import Thread
+from .message import RawMessage
+
+
+from .messageparser import parse_line
+
+
+class Socket(Thread):
+    
+    # bridge: Bridge
+    socket: socket.socket
+    buffer: str
+    active: bool
+    ready: bool
+    channel_list: list
+    log: Log
+
+
+    # def __init__(self, bridge: Bridge = None):
+    def __init__(self, nick: str, token: str, verbose=False):
+        # self.bridge = bridge
+        Thread.__init__(self)
+        self.nick = nick
+        self.token = token
+        self.active = True
+        self.verbose = verbose
+        self.buffer = ""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.channel_list = []
+        
+        
+
+    def __connect(self):
+        self.socket.connect(("irc.chat.twitch.tv", 6667))
+        self._send_raw(f"PASS {self.token}")
+        self._send_raw(f"NICK {self.nick}")
+        self._send_raw("CAP REQ :twitch.tv/membership")
+        self._send_raw("CAP REQ :twitch.tv/tags")
+        self._send_raw("CAP REQ :twitch.tv/commands")
+        pass
+
+    def disconnect(self):
+        if self.verbose:
+            print("departing channels")
+        for channel in self.channel_list:
+            self._send_raw("PART #"+ channel.name.lower() +" ")
+        try:
+            self.socket.close()
+        except Exception:
+            pass
+
+    def reconnect(self):
+        if self.verbose:
+            print("Reconnect detected!")
+        self.disconnect()
+        if self.verbose:
+            print("Waiting to reconnect.")
+        sleep(10)
+        self.activate(self.channel_list)
+
+    def connect_to_channel(self, channel: str):
+        self._send_raw("JOIN #"+ channel.lower() +" ")
+        # self.channel_list.append(channel)
+        if self.verbose:
+            print(f"connecting to '{channel}'")
+
+    def disconnect_from_channel(self, channel: str):
+        self._send_raw("PART #"+ channel.lower() +" ")
+        # self.__clearchannel(channel)
+        if self.verbose:
+            print(f"departing from '{channel}'")
+
+    def __clearchannel(self, channel: str):
+        counter = 0
+        for chn in self.channel_list:
+            if chn.name == channel:
+                self.channel_list.pop(counter)
+                return
+            counter += 1
+
+    def activate(self, channels):
+        self.channel_list = channels
+        self.__connect()
+        for channel in self.channel_list:
+            self._send_raw("JOIN #"+ channel.name.lower() +" ")
+
+    def run(self):
+        while self.active:
+            self.__process_stream_data()
+            sleep(0.00001)
+        try:
+            self.socket.close()
+        except Exception:
+            pass
+
+    def close(self):
+        if self.verbose:
+            print(f"({self.name}) Closing Socket")
+        self.active = False
+        self.socket.close()
+        
+
+
+    def _send_raw(self, message: str):
+        try:
+            self.socket.send((f"{message}\r\n").encode('utf-8'))
+            if self.verbose:
+                if message[:4] == "PASS":
+                    print(f"({self.name}) < PASS ****")
+                else:
+                    print(f"({self.name}) < {message}")
+        except OSError:
+            self.log.error(f"Socket is closed and must be reopened to send the message '{message}'")
+
+    def __process_stream_data(self):
+        try:
+            self.buffer = self.buffer + self.socket.recv(1024).decode()
+        except ConnectionAbortedError:
+            self.log.info("Socket connection has Closed")
+        temp = self.buffer.split("\n")
+        self.buffer = temp.pop()
+        for line in temp:
+            
+            if ("PING :tmi.twitch.tv" in line): # Keep Alive Mechanism
+                self._send_raw("PONG :tmi.twitch.tv")
+            self.on_socket_data(line)
+
+
+    def on_socket_data(self, line: str):
+        raise NotImplementedError("`on_socket_data` not implemented")
+
+
+
+
+
+
+class ReadSocket(Socket):
+
+    def __init__(self, nick: str, token: str):
+        Socket.__init__(self, nick, token)
+        self.name = "Thread-ReadSocket"
+        self.log = Log("ReadSocket")
+
+
+
+    def on_socket_data(self, line: str):
+        print(line)
+        self.on_data(parse_line(line))
+
+    def on_data(self, data):
+        # print(f" >", data.line)
+        for channel in self.channel_list:
+            channel.on_raw(data)
+        
+        if data.inner == "Message":
+            print(f"Message: {data.message}")
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_message(data)
+        elif data.inner == "Join":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_join(data)
+        elif data.inner == "Mode":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_mode(data)
+        elif data.inner == "Names":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_names(data)
+        elif data.inner == "Part":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_part(data)
+        elif data.inner == "ClearChat":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_clearchat(data)
+        elif data.inner == "ClearMessage":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_clearmessage(data)
+        elif data.inner == "HostTarget":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_hosttarget(data)
+        elif data.inner == "Notice":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_notice(data)
+        elif data.inner == "Reconnect":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_reconnect(data)
+        elif data.inner == "RoomState":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_roomstate(data)
+        elif data.inner == "UserState":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_userstate(data)
+        elif data.inner == "GlobalUserState":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_globaluserstate(data)
+        elif data.inner == "UserNotice":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_usernotice(data)
+        elif data.inner == "RitualUserNotice":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_ritual_usernotice(data)
+        elif data.inner == "BitBadgeUpgradeUserNotice":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_bitbadgeupgrade_usernotice(data)
+        elif data.inner == "RaidUserNotice":
+            for channel in self.channel_list:
+                if channel.name in data.line:
+                    channel.on_raid_usernotice(data)
+        elif data.inner == "SubscriberUserNotice":
+            if data.display_name.lower() != self.nick.lower():
+                for channel in self.channel_list:
+                    if channel.name == data.channel:
+                        channel.on_subscriber_usernotice(data)
+        elif data.inner == "GiftedSubscriberUserNotice":
+            if data.display_name.lower() != self.nick.lower():
+                for channel in self.channel_list:
+                    if channel.name == data.channel:
+                        channel.on_giftedsubscriber_usernotice(data)
+        elif data.inner == "PrivateMessage":
+            if data.display_name.lower() != self.nick.lower():
+                for channel in self.channel_list:
+                    if channel.name == data.channel:
+                        self.log.chat(data.message_text, data.channel, data.display_name)
+                        channel.on_privmessage(data)
+        elif data.inner == "CommandMessage":
+            if data.display_name.lower() != self.nick.lower():
+                for channel in self.channel_list:
+                    if channel.name == data.channel:
+                        self.log.chat(f"[CMD] {data.message_text}",data.channel, data.display_name)
+                        channel.on_command(data)
+    
+
+class SendSocket(Socket):
+
+    def __init__(self, nick: str, token: str):
+        Socket.__init__(self, nick, token)
+        self.name = "Thread-SendSocket"
+        self.log = Log("SendSocket")
+
+    def join(self, channel: str):
+        send = f"JOIN #{channel.lower()}"
+        self._send_raw(send)
+
+    def send_message(self, channel: str, message: str):
+        send = f"PRIVMSG #{channel.lower()} :{message}"
+        self._send_raw(send)
+        self.log.sent(message, channel.lower())
+    
+    def send_action_message(self, channel: str, message: str):
+        send = f"PRIVMSG #{channel.lower()} :/me {message}"
+        self._send_raw(send)
+
+    def send_whisper(self, user: str, message: str):
+        send = f"PRIVMSG #{self.nick} :/w {user} {message}"
+        self._send_raw(send)
+        self.log.whisper(message, user.lower())
+
+    def timeout_user(self, user: str, channel: str, timeout=1):
+        send = f"PRIVMSG #{channel} :/timeout {user} {timeout}"
+        self._send_raw(send)
+
+    def clear_message(self, channel: str, message_id: str):
+        send = f"PRIVMSG #{channel} :/delete {message_id}"
+        self._send_raw(send)
+
+    def ban_user(self, user: str, channel: str):
+        send = f"PRIVMSG #{channel} :/ban {user}"
+        self._send_raw(send)
+
+    def on_socket_data(self, line: str):
+        pass
