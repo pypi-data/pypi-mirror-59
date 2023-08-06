386a78953288059d444d337327fd5988c8acb7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package fireflyai/0.0.40/fireflyai-0.0.40.tar.gz

---
+++ packages/fireflyai/fireflyai-0.0.40.tar.gz/fireflyai-0.0.40/fireflyai/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from logging import NullHandler
+
+# Set default logging handler to avoid "No handler found" warnings.
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
+token = None
+api_base = 'https://api.firefly.ai'
+
+from fireflyai import enums
+from fireflyai.auth import authenticate
+from fireflyai.resources import *
+
+
