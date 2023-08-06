34ee51c9bf86dd0225e1e49b1a64b60b37be1c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:57 +0000

    Add package adonai-client/0.1.7/adonai-client-0.1.7.tar.gz

---
+++ packages/adonai-client/adonai-client-0.1.7.tar.gz/adonai-client-0.1.7/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['adonai_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.11.1,<0.12.0', 'sgqlc>=9.1,<10.0']
+
+setup_kwargs = {
+    'name': 'adonai-client',
+    'version': '0.1.7',
+    'description': 'Client for adonai server',
+    'long_description': None,
+    'author': 'Aleksander Lavrov',
+    'author_email': 'egnod@ya.ru',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
