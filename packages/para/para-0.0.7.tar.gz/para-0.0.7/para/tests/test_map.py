4d8db99ea0655dd12cfb39c3297f9acd3b793609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:36 +0000

    Add package para/0.0.7/para-0.0.7.tar.gz

---
+++ packages/para/para-0.0.7.tar.gz/para-0.0.7/para/tests/test_map.py
@@ -0,0 +1,46 @@
+import sys
+
+from ..map import map
+
+
+def test_map():
+    words = ["foo", "bar", "baz"]
+
+    def get_the_a(word):
+        for i, char in enumerate(word):
+            if char == 'a':
+                yield word, i
+
+    set_of_a = set(map(get_the_a, words))
+
+    assert set_of_a == \
+        {
+            ("bar", 1),
+            ("baz", 1)
+        }
+
+
+def test_output_queue_size():
+    words = ["foo", "bar", "baz"]
+
+    def get_the_a(word):
+        for i, char in enumerate(word):
+            if char == 'a':
+                yield word, i
+
+    set_of_a = set(map(get_the_a, words, output_queue_size=1))
+
+    assert set_of_a == \
+        {
+            ("bar", 1),
+            ("baz", 1)
+        }
+
+
+def test_map_single():
+    paths = [sys.stdin]
+
+    def is_a_file(path):
+        yield hasattr(path, "read")
+
+    assert sum(map(is_a_file, paths)) == 1
