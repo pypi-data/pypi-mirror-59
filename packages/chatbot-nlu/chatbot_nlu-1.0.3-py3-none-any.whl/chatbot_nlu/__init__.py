150cb7eec51730573985ad39568c87ff9dc5274c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package chatbot-nlu/1.0.3/chatbot_nlu-1.0.3-py3-none-any.whl

---
+++ packages/chatbot-nlu/chatbot_nlu-1.0.3-py3-none-any.whl/chatbot_nlu/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+@Author  :   Xu
+ 
+@Software:   PyCharm
+ 
+@File    :   __init__.py.py
+ 
+@Time    :   2019-09-20 16:34
+ 
+@Desc    :
+ 
+'''
+
+from .version import __version__
---
+++ packages/chatbot-nlu/chatbot_nlu-1.0.3-py3-none-any.whl/chatbot_nlu/training_data/formats/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from chatbot_nlu.training_data.formats.dialogflow import DialogflowReader
+from chatbot_nlu.training_data.formats.luis import LuisReader
+from chatbot_nlu.training_data.formats.wit import WitReader
+from chatbot_nlu.training_data.formats.markdown import MarkdownWriter, MarkdownReader
+from chatbot_nlu.training_data.formats.rasa import RasaReader, RasaWriter
