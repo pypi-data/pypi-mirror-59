709214c355229c1275c3b987c132391cffad63a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:19 +0000

    Add package stellvia/0.1.0/stellvia-0.1.0.tar.gz

---
+++ packages/stellvia/stellvia-0.1.0.tar.gz/stellvia-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stellvia",
+    version="0.1.0",
+    author="Bluelief",
+    description="Python project.",
+    license="MIT",
+    keywords="python",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
