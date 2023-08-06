0a03788500f6681a8fe41e0cf3d61977c1b5d8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:04 +0000

    Add package mlpiper/1.0.1/mlpiper-1.0.1-py2.py3-none-any.whl

---
+++ packages/mlpiper/mlpiper-1.0.1-py2.py3-none-any.whl/components/csv_loader.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from parallelm.components import ConnectableComponent
+
+
+class CsvLoader(ConnectableComponent):
+
+    def __init__(self, engine):
+        super(self.__class__, self).__init__(engine)
+
+    def _materialize(self, parent_data_objs, user_data):
+        dataset_path = self._params.get('path', "dataset/10k_diabetes.csv")
+
+        data = pd.read_csv(dataset_path)
+
+        self._logger.info(data.head())
+
+        return [data]
