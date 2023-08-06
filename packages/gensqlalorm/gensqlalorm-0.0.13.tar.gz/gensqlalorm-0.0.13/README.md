4d868ab844c7bab82017863e58bb19c13eb3a957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:35 +0000

    Add package gensqlalorm/0.0.13/gensqlalorm-0.0.13.tar.gz

---
+++ packages/gensqlalorm/gensqlalorm-0.0.13.tar.gz/gensqlalorm-0.0.13/README.md
@@ -0,0 +1,2 @@
+# Generate Sqlalchemy Orm
+> 自动生成Sqlalchemy所需要的ORM文件
---
+++ packages/gensqlalorm/gensqlalorm-0.0.13.tar.gz/gensqlalorm-0.0.13/setup.py
@@ -0,0 +1,29 @@
+#! -*-coding:utf8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='gensqlalorm',
+      version='0.0.13',
+      description='a generate sqlarchemy models utils',
+      url='https://github.com/SUT-GC/gen-sqla-orm',
+      author='gouchao',
+      author_email='sutgouc@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'PyMySQL>=0.9.3',
+          'PyYAML>=5.2',
+      ],
+      entry_points={
+          'console_scripts': [
+              'gensqlalorm = gensqlalorm:gen_console',
+
+          ]
+      },
+      zip_safe=True)
