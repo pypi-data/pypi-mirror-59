3c315e96b7b74da730e3db2bff08c08834580fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package NeuroSkyPy/1.2/NeuroSkyPy-1.2.tar.gz

---
+++ packages/NeuroSkyPy/NeuroSkyPy-1.2.tar.gz/NeuroSkyPy-1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+ 
+setup(
+      name="NeuroSkyPy",
+      packages=['NeuroSkyPy'],
+      version="1.2",
+      license="GNU General Public License v3.0",
+      description="Library for interfacing with Neurosky's Mindwave EEG headset",
+      author="Jorge Lopez Marcos",
+      author_email="jlomar2005@hotmail.com",
+      url="https://github.com/Jor-G-ete/NeuroSkyPy",
+      download_url="https://github.com/Jor-G-ete/NeuroSkyPy/archive/v1.2.tar.gz",
+      keyword=["python3.7", "NeuroSky", "graphics"],
+      classifiers=[
+            "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            "Intended Audience :: Developers",  # Define that your audience are developers
+            "Topic :: Software Development :: Build Tools",
+            # "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",  # Again, pick a license
+            "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+            "Programming Language :: Python :: 3.7",
+            ],
+      )
