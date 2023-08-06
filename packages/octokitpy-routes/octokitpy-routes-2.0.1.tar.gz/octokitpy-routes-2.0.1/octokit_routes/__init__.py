d8a9421e1752223ff50c3ce7b41ed12bc20e67c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:08 +0000

    Add package octokitpy-routes/2.0.1/octokitpy-routes-2.0.1.tar.gz

---
+++ packages/octokitpy-routes/octokitpy-routes-2.0.1.tar.gz/octokitpy-routes-2.0.1/octokit_routes/__init__.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'octokit_routes'))
+
+
+def _load_spec(spec_name):
+    with open(os.path.join(__location__, spec_name, 'index.json'), 'r') as f:
+        return json.load(f)
+
+
+specs = [d.name for d in os.scandir(__location__) if d.is_dir() and not d.name == '__pycache__']
+specifications = {spec: _load_spec(spec) for spec in specs}
