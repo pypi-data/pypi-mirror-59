75725b521d56d8c20116b6ce712440906b0ed922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:50 +0000

    Add package echidna/0.2.1/echidna-0.2.1.tar.gz

---
+++ packages/echidna/echidna-0.2.1.tar.gz/echidna-0.2.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="echidna",
+    version="0.2.1",
+    author="1ntegrale9",
+    author_email="1ntegrale9uation@gmail.com",
+    description="BOT and CLI framework for handling multiple services in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/1ntegrale9/echidna",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy >= 1.6",
+        "discord.py >= 1.2.5"
+    ],
+)
