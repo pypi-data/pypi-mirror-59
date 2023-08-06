0b18d4663ce024f7a5f4d353a8acddbf709703ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:38 +0000

    Add package django-su/0.9.0/django-su-0.9.0.tar.gz

---
+++ packages/django-su/django-su-0.9.0.tar.gz/django-su-0.9.0/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import codecs
+
+from setuptools import setup, find_packages
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('django_su')
+    try:
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if 'sdist' in sys.argv:
+            raise
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = os.path.join(os.path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='django-su',
+    version=read('VERSION'),
+    license='MIT License',
+
+    install_requires=[
+        'django>=1.11',
+    ],
+    requires=[
+        'Django (>=1.11)',
+    ],
+
+    description="Login as any user from the Django admin interface, then switch back when done",
+    long_description=read('README.rst'),
+
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
+
+    maintainer='Basil Shubin',
+    maintainer_email='basil.shubin@gmail.com',
+
+    url='http://github.com/adamcharnock/django-su',
+    download_url='https://github.com/adamcharnock/django-su/zipball/master',
+
+    packages=find_packages(exclude=('example*', '*.tests*')),
+    include_package_data=True,
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
