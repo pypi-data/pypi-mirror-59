d36901d6eb4f087e98888eb8d5ac858c128d3688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package bootstrap-builder/0.0.4/bootstrap-builder-0.0.4.tar.gz

---
+++ packages/bootstrap-builder/bootstrap-builder-0.0.4.tar.gz/bootstrap-builder-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+try:
+    with open("README.md") as a:
+        long_description = a.read()
+except FileNotFoundError:
+    long_description = ""
+
+setuptools.setup(
+    name="bootstrap-builder",
+    version="0.0.4",
+    author="Callum Bartlett",
+    author_email="bartlett.c@outlook.com",
+    description="A small small package I use to turn some objects into Bootstrap HTML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/4Kaylum/BootstrapHTMLBuilder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
