d542e4548936cc357858468bf80d2ab3e39d7af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package CHESS-python-nightly/1.1.1.dev40/CHESS-python-nightly-1.1.1.dev40.tar.gz

---
+++ packages/CHESS-python-nightly/CHESS-python-nightly-1.1.1.dev40.tar.gz/CHESS-python-nightly-1.1.1.dev40/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='-'.join(['CHESS', 'python'] + (['nightly'] if os.getenv('NIGHTLY', None) else [])),
+    use_scm_version={
+        'local_scheme': lambda version: '',
+    },
+    setup_requires=['setuptools_scm'],
+    packages=['chess'],
+    url='https://github.com/nishaq503/CHESS',
+    license='',
+    author='Najib Ishaq',
+    author_email='',
+    description='Clustered Hierarchical Entropy-Scaling Search',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'scipy'],
+    python_requires='>=3.6',
+)
