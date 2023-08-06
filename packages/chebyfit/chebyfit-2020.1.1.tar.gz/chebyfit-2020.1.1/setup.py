bec7a38dbfd8b34426dd1d2bc82939a1a898549c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:35 +0000

    Add package chebyfit/2020.1.1/chebyfit-2020.1.1.tar.gz

---
+++ packages/chebyfit/chebyfit-2020.1.1.tar.gz/chebyfit-2020.1.1/setup.py
@@ -0,0 +1,77 @@
+# chebyfit/setup.py
+
+"""Chebyfit package setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+with open('chebyfit/chebyfit.py') as fh:
+    code = fh.read()
+
+version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
+
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
+                    re.MULTILINE | re.DOTALL).groups()[0]
+
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+
+class build_ext(_build_ext):
+    """Delay import numpy until build."""
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+setup(
+    name='chebyfit',
+    version=version,
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@uci.edu',
+    url='https://www.lfd.uci.edu/~gohlke/',
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.14.5'],
+    setup_requires=['setuptools>=18.0', 'numpy>=1.14.5'],
+    cmdclass={'build_ext': build_ext},
+    packages=['chebyfit'],
+    ext_modules=[Extension('chebyfit._chebyfit', ['chebyfit/chebyfit.c'])],
+    package_data={'chebyfit': ['examples/*.py']},
+    license='BSD',
+    zip_safe=False,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
