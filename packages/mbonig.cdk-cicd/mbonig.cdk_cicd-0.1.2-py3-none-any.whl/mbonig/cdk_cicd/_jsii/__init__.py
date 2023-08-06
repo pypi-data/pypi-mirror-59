984258043034ce87ccfdbbc79752e4413f8e1c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:46 +0000

    Add package mbonig.cdk-cicd/0.1.2/mbonig.cdk_cicd-0.1.2-py3-none-any.whl

---
+++ packages/mbonig.cdk-cicd/mbonig.cdk_cicd-0.1.2-py3-none-any.whl/mbonig/cdk_cicd/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_codebuild
+import aws_cdk.aws_codepipeline
+import aws_cdk.aws_codepipeline_actions
+import aws_cdk.aws_events
+import aws_cdk.aws_events_targets
+import aws_cdk.aws_s3
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
