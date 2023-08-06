29e5afab0e2edf4f7b28867879740bd7740c0ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:43 +0000

    Add package hek-pool/1.4.0/hek_pool-1.4.0-py3-none-any.whl

---
+++ packages/hek-pool/hek_pool-1.4.0-py3-none-any.whl/hek_pool/__init__.py
@@ -0,0 +1,8 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#           YYYY.MM.DD
+__date__ = "2020.01.11"
+__version__ = (1, 4, 0)
+__website__ = "https://github.com/MosesofEgypt/hek_pool"
