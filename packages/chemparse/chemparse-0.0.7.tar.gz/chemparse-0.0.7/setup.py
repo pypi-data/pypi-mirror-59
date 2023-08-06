f67c0e1d2b3ec25fc6ea0ed41bb4a0ab1e5369e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:27 +0000

    Add package chemparse/0.0.7/chemparse-0.0.7.tar.gz

---
+++ packages/chemparse/chemparse-0.0.7.tar.gz/chemparse-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chemparse",
+    version="0.0.7",
+    author="Grayson Boyer",
+    author_email="gmboyer@asu.edu",
+    description="Chemical formula parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['chemparse'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False
+)
