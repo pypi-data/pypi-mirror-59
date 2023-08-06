dca68e614eef9c3f2003feff5cac4c4f2ca9ee46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package forest-puller/1.0.1/forest_puller-1.0.1.tar.gz

---
+++ packages/forest-puller/forest_puller-1.0.1.tar.gz/forest_puller-1.0.1/forest_puller/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC biomass Project.
+Unit D1 Bioeconomy.
+"""
+
+# Special variables #
+__version__ = '1.0.1'
+
+# Built-in modules #
+import os, sys
+
+# First party modules #
+from autopaths    import Path
+from plumbing.git import GitRepo
+
+# Constants #
+project_name = 'forest_puller'
+project_url  = 'https://gitlab.com/bioeconomy/puller/forest_puller'
+
+# Get paths to module #
+self       = sys.modules[__name__]
+module_dir = Path(os.path.dirname(self.__file__))
+
+# The repository directory #
+repos_dir = module_dir.directory
+
+# The module is maybe in a git repository #
+git_repo = GitRepo(repos_dir, empty=True)
+
+# Determine where to cache things #
+env_var_name = "FOREST_PULLER_CACHE"
+
+# If it is specified by user #
+if env_var_name in os.environ:
+    cache_dir = os.environ[env_var_name]
+    if not cache_dir.endswith('/'): cache_dir += '/'
+
+# If it is not specified by user #
+else:
+    import tempfile
+    cache_dir = tempfile.gettempdir() + '/forest_puller/'
+    import warnings
+    message = ("\n\n The cache location for forest puller's data is not defined in"
+               " the '%s' environment variable.\n In this case it will default"
+               " to:\n\n '%s',\n which might lead to re-caching after every startup.\n")
+    warnings.warn(message % (env_var_name, cache_dir))
+
+# Guarantee it exists #
+cache_dir = Path(cache_dir)
+cache_dir.create_if_not_exists()
---
+++ packages/forest-puller/forest_puller-1.0.1.tar.gz/forest_puller-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'forest_puller',
+        version          = '1.0.1',
+        description      = 'A package for retrieving data concerning forests on the European continent.',
+        long_description = open('README.md').read(),
+        license          = 'MIT',
+        url              = 'https://gitlab.com/bioeconomy/puller/forest_puller',
+        author           = 'Lucas Sinclair',
+        author_email     = 'lucas.sinclair@me.com',
+        packages         = find_packages(),
+        install_requires = ['pandas', 'numpy', 'autopaths', 'plumbing', 'lxml', 'requests', 'tqdm'],
+)
