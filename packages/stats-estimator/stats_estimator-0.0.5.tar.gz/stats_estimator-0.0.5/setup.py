e60c41d67c16765f2f2b79ee62ff5b615fb8d4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:30 +0000

    Add package stats-estimator/0.0.5/stats_estimator-0.0.5.tar.gz

---
+++ packages/stats-estimator/stats_estimator-0.0.5.tar.gz/stats_estimator-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name ="stats_estimator",
+    version = "0.0.5",
+    description = " Files to get values of gaussian and binomial distribution",
+    packages = ["stats_estimator"],
+    author= "irahul",
+    zip_safe= False 
+)
