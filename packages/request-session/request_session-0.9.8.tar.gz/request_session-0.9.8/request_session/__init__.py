7f206f70bc41c9a170e11ab9d50ce6e23ef31027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:46 +0000

    Add package request-session/0.9.8/request_session-0.9.8.tar.gz

---
+++ packages/request-session/request_session-0.9.8.tar.gz/request_session-0.9.8/request_session/__init__.py
@@ -0,0 +1,4 @@
+from requests import HTTPError, RequestException
+
+from .request_session import RequestSession, Timeout
+from .utils import InvalidUserAgentString, RequestSessionException, UserAgentComponents
