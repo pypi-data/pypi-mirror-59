73886e80253d7d5ab225fa9a8807f3fc04f21126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:40 +0000

    Add package gregorian/0.0.9/gregorian-0.0.9.tar.gz

---
+++ packages/gregorian/gregorian-0.0.9.tar.gz/gregorian-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gregorian",
+    version="0.0.9",
+    author="david.schenck@outlook.com",
+    author_email="david.schenck@outlook.com",
+    description="A calendar wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dschenck/gregorian",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['python-dateutil', 'sortedcontainers']
+)
