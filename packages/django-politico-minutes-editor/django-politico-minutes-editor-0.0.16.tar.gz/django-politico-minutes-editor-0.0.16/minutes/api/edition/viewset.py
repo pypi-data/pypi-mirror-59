477cd98852a9c40f958c10f760c872e8ac3e46a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:04 +0000

    Add package django-politico-minutes-editor/0.0.16/django-politico-minutes-editor-0.0.16.tar.gz

---
+++ packages/django-politico-minutes-editor/django-politico-minutes-editor-0.0.16.tar.gz/django-politico-minutes-editor-0.0.16/minutes/api/edition/viewset.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from minutes.models import Edition, Vertical
+from .serializers.edition import EditionSerializer
+from ..common.viewsets.base import BaseApiReadOnlyViewset
+
+
+class EditionViewset(BaseApiReadOnlyViewset):
+    queryset = Edition.objects.all()
+    serializer_class = EditionSerializer
+
+    def list(self, request):
+        vertical_slug = request.GET.get("v", None)
+        v = get_object_or_404(Vertical, slug=vertical_slug)
+
+        return Response(
+            self.serializer_class(Edition.objects.latest_live(v)).data
+        )
