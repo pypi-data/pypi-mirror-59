20f3909630df61bd5bfdd6cefe509d5643619636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:48 +0000

    Add package SaeedMatrixCalculation/0.0.1/SaeedMatrixCalculation-0.0.1.tar.gz

---
+++ packages/SaeedMatrixCalculation/SaeedMatrixCalculation-0.0.1.tar.gz/SaeedMatrixCalculation-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "SaeedMatrixCalculation",
+     version = '0.0.1',
+     description = 'Add, Subtract, Multiply and Invert Matrices',
+     packages = ['SaeedMatrixCalculation'],
+     author = 'Saeed AlSarhi',
+     author_email = 'saeedalsarhi@gmail.com',
+     zip_safe = False)
