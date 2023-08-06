53eead755b6acb7b69ecd34515f7ab142972cb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:41 +0000

    Add package ident/0.0.1/ident-0.0.1.tar.gz

---
+++ packages/ident/ident-0.0.1.tar.gz/ident-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='ident',
+    version='0.0.1',
+    description='Identify with challenge messsage and SSH key.',
+    url='https://github.com/mindey/ident',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='MIT',
+    packages=['ident'],
+    install_requires=[
+        "cryptography"
+    ],
+    entry_points={
+        'console_scripts': [
+            'ident=ident.cli:verify',
+        ],
+    },
+    zip_safe=False
+)
