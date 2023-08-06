f771c65ffc82668b9856e9b4e1d35701999a16b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:12 +0000

    Add package mongo-queue-service/0.0.3/mongo_queue_service-0.0.3.tar.gz

---
+++ packages/mongo-queue-service/mongo_queue_service-0.0.3.tar.gz/mongo_queue_service-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mongo_queue_service",
+    version="0.0.3",
+    author="Amit Chotaliya",
+    author_email="amit@shunyeka.com",
+    description="Queue service built on top of mongo.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shunyeka/mongo_queue/",
+    download_url='https://github.com/shunyeka/mongo_queue/archive/v0.0.1.tar.gz',
+    packages=setuptools.find_packages(),
+    keywords=['mongo', 'queue', 'priority queue', 'task queue'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pymongo'
+    ],
+    python_requires='>=3.6',
+)
