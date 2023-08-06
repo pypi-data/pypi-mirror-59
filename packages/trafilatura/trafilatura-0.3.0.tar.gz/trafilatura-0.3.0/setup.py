568dd7f083c2b6a1abed3af251a4b8b43f8b654d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package trafilatura/0.3.0/trafilatura-0.3.0.tar.gz

---
+++ packages/trafilatura/trafilatura-0.3.0.tar.gz/trafilatura-0.3.0/setup.py
@@ -0,0 +1,83 @@
+"""
+Scrapes the main text of web pages while preserving some structure
+http://github.com/adbar/trafilatura
+"""
+
+# workaround for open() with encoding=''
+from codecs import open
+
+from os import path
+from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+# some problems with installation solved this way
+extras = {
+    'all': [
+        'cchardet >= 2.0.0',
+        'htmldate >= 0.6.0',
+        'langid >= 1.1.6',
+    ],
+    'metadata': [
+        'htmldate >= 0.6.0',
+    ]
+}
+
+
+def readme():
+    with open(path.join(here, 'README.rst'), 'r', 'utf-8') as readmefile:
+        return readmefile.read()
+
+
+setup(
+    name='trafilatura',
+    version='0.3.0',
+    description='Downloads web pages, scrapes main text and comments while preserving some structure, and converts to TXT, CSV, XML & TEI-XML',
+    long_description=readme(),
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    keywords=['html2text', 'nlp', 'scraper', 'tei-xml', 'text-extraction', 'webscraping', 'web-scraping'],
+    url='http://github.com/adbar/trafilatura',
+    author='Adrien Barbaresi',
+    author_email='barbaresi@bbaw.de',
+    license='GPLv3+',
+    packages=['trafilatura'],
+    package_data={'trafilatura': ['data/tei-schema.pickle']},
+    include_package_data=True,
+    python_requires='>=3.5',
+    install_requires=[
+        'justext >= 2.2.0',
+        'lxml >= 4.4.1',
+        'requests >= 2.22.0',
+    ],
+    extras_require=extras,
+    entry_points = {
+        'console_scripts': ['trafilatura=trafilatura.cli:main'],
+    },
+    # platforms='any',
+    tests_require=['pytest', 'tox'],
+    zip_safe=False,
+)
