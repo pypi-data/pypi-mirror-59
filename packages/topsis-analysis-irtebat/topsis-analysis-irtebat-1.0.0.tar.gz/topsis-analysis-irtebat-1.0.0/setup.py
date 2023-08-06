3496aab4fc008afecab8efdba90169fcbbc1da9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:11 +0000

    Add package topsis-analysis-irtebat/1.0.0/topsis-analysis-irtebat-1.0.0.tar.gz

---
+++ packages/topsis-analysis-irtebat/topsis-analysis-irtebat-1.0.0.tar.gz/topsis-analysis-irtebat-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import *
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="topsis-analysis-irtebat",
+    version="1.0.0",
+    description="Topsis analysis of a csv file",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/irtebat/topsis-analysis",
+    author="Irtebat",
+    author_email="irtebat.10@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TOPSIS_package"],
+    include_package_data=True,
+    install_requires=["numpy","pandas"],
+    entry_points={
+        "console_scripts": [
+            "topsis=TOPSIS_package.cli:main",
+        ]
+    },
+)
