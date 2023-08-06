23ccafc7bba179baee70e1e2a5a12e7ce92afdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:47 +0000

    Add package kth-build/0.0.1/kth-build-0.0.1.tar.gz

---
+++ packages/kth-build/kth-build-0.0.1.tar.gz/kth-build-0.0.1/kth-build.py
@@ -0,0 +1,27 @@
+# import struct
+# import cpuid_native
+
+def test():
+    print("test")
+    return (1, 2, 3, 4)
+
+
+
+# if __name__ == "__main__":
+    
+#     print("Vendor ID         : %s" % cpu_vendor())
+#     print("CPU name          : %s" % cpu_name())
+#     print("Microarchitecture : %s%s" % cpu_microarchitecture())
+#     print("Vector instructions supported:")
+#     print("SSE       : %s" % is_set(1, 3, 25))
+#     print("SSE2      : %s" % is_set(1, 3, 26))
+#     print("SSE3      : %s" % is_set(1, 2, 0))
+#     print("SSSE3     : %s" % is_set(1, 2, 9))
+#     print("SSE4.1    : %s" % is_set(1, 2, 19))
+#     print("SSE4.2    : %s" % is_set(1, 2, 20))
+#     print("SSE4a     : %s" % is_set(0x80000001, 2, 6))
+#     print("AVX       : %s" % is_set(1, 2, 28))
+#     print("AVX2      : %s" % is_set(7, 1, 5))
+#     print("BMI1      : %s" % is_set(7, 1, 3))
+#     print("BMI2      : %s" % is_set(7, 1, 8))
+
