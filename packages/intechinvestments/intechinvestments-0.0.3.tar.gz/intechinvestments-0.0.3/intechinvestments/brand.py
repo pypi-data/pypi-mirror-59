bcd02ae37ff85180cf04cfd900133ae2cb7d352f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package intechinvestments/0.0.3/intechinvestments-0.0.3.tar.gz

---
+++ packages/intechinvestments/intechinvestments-0.0.3.tar.gz/intechinvestments-0.0.3/intechinvestments/brand.py
@@ -0,0 +1,13 @@
+
+
+INTECH_YELLOW = [249,200,9]
+INTECH_PURPLE = [126,40,87]
+intech_yellow = [float(c)/256. for c in INTECH_YELLOW ]
+intech_purple = [float(c)/256. for c in INTECH_PURPLE ]
+INTECH_LOGO_URL = "https://i.imgur.com/qeu37Dg.png"
+INTECH_LOGO_MARKDOWN = "[Imgur](https://i.imgur.com/qeu37Dg.png)"
+INTECH_LOGO_HTML = "<a href='https://imgur.com/qeu37Dg'><img src='https://i.imgur.com/qeu37Dg.png' title='Intech Investments' /></a>"
+
+
+INTECH_COLLIDER_SHORT_SPLASH_URL="https://vimeo.com/386060045"
+INTECH_COLLIDER_LONG_SPLASH_URL="https://vimeo.com/386060045"
