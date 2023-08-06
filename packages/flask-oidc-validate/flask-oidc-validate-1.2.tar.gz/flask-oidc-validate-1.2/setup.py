9384730772635897ce330f5b4834774d801ba8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:50 +0000

    Add package flask-oidc-validate/1.2/flask-oidc-validate-1.2.tar.gz

---
+++ packages/flask-oidc-validate/flask-oidc-validate-1.2.tar.gz/flask-oidc-validate-1.2/setup.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+#!/usr/bin/env python
+
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name="flask-oidc-validate",
+
+    version="1.2",
+
+    keywords='flask-oidc',
+
+    description='A flask-oidc to validate IdToken for python ',
+
+    author='lixin159',
+    author_email='785321021@qq.com',
+
+    url='https://github.com/785321021/oidc-client-validate.git',
+
+    long_description=open('README.md', encoding='utf-8').read(),
+
+    packages=find_packages(),
+
+    license='MIT'
+
+)
