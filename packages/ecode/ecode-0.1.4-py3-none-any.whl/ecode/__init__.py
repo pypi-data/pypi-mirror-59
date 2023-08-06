8395e9f34db53bfc7167ba5ae7ff0b42ada66c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:22 +0000

    Add package ecode/0.1.4/ecode-0.1.4-py3-none-any.whl

---
+++ packages/ecode/ecode-0.1.4-py3-none-any.whl/ecode/__init__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+from .ecode import (
+    Ecode,
+    EcodeAlign,
+    EcodeFlag,
+    EcodeFmt,
+    EcodeLocale,
+    EcodeSize,
+)
+from .ecode_decoder import EcodeDecoder
+from .ecode_encoder import EcodeEncoder
+
+__version__ = '0.1.4'
