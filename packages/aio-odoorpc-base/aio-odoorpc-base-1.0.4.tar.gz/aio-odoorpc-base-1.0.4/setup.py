fdd2cc402a5747391fe6a689b57bd7ed18686c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:20 +0000

    Add package aio-odoorpc-base/1.0.4/aio-odoorpc-base-1.0.4.tar.gz

---
+++ packages/aio-odoorpc-base/aio-odoorpc-base-1.0.4.tar.gz/aio-odoorpc-base-1.0.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aio_odoorpc_base']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'aio-odoorpc-base',
+    'version': '1.0.4',
+    'description': "Fast, simple Odoo RPC package with sync and async functions to pilot Odoo's jsonrpc API. Check aio-odoorpc for a higher-level, friendlier interface.",
+    'long_description': None,
+    'author': 'mbello',
+    'author_email': 'mbello@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mbello/aio-odoorpc-base',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
