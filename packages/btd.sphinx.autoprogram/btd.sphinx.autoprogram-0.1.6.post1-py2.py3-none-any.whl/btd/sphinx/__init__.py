f51529e5980ef45e3df66bc79410157287951e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package btd.sphinx.autoprogram/0.1.6.post1/btd.sphinx.autoprogram-0.1.6.post1-py2.py3-none-any.whl

---
+++ packages/btd.sphinx.autoprogram/btd.sphinx.autoprogram-0.1.6.post1-py2.py3-none-any.whl/btd/sphinx/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    btd.sphinx
+    ~~~~~~~~~~
+
+    This package is a namespace package that contains all extensions
+    distributed in the ``sphinx-contrib`` distribution.
+
+    :copyright: Copyright 2007-2014 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+__import__('pkg_resources').declare_namespace(__name__)
