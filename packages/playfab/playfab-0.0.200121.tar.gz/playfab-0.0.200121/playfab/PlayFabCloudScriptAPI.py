cdc5f347e898875d24fbe14e70866c854963afc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:07 +0000

    Add package playfab/0.0.200121/playfab-0.0.200121.tar.gz

---
+++ packages/playfab/playfab-0.0.200121.tar.gz/playfab-0.0.200121/playfab/PlayFabCloudScriptAPI.py
@@ -0,0 +1,23 @@
+import playfab.PlayFabErrors as PlayFabErrors
+import playfab.PlayFabHTTP as PlayFabHTTP
+import playfab.PlayFabSettings as PlayFabSettings
+
+""" API methods for executing CloudScript using an Entity Profile """
+
+
+def ExecuteEntityCloudScript(request, callback, customData = None, extraHeaders = None):
+    """
+    Cloud Script is one of PlayFab's most versatile features. It allows client code to request execution of any kind of
+    custom server-side functionality you can implement, and it can be used in conjunction with virtually anything.
+    https://docs.microsoft.com/rest/api/playfab/cloudscript/server-side-cloud-script/executeentitycloudscript
+    """
+    if not PlayFabSettings._internalSettings.EntityToken:
+        raise PlayFabErrors.PlayFabException("Must call GetEntityToken before calling this method")
+
+    def wrappedCallback(playFabResult, error):
+        if callback:
+            callback(playFabResult, error)
+
+    PlayFabHTTP.DoPost("/CloudScript/ExecuteEntityCloudScript", request, "X-EntityToken", PlayFabSettings._internalSettings.EntityToken, wrappedCallback, customData, extraHeaders)
+
+
