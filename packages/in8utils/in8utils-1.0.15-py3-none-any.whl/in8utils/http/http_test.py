d3466e3701668eab5f9da213973e84a89f8116c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:21 +0000

    Add package in8utils/1.0.15/in8utils-1.0.15-py3-none-any.whl

---
+++ packages/in8utils/in8utils-1.0.15-py3-none-any.whl/in8utils/http/http_test.py
@@ -0,0 +1,64 @@
+import requests
+
+
+class HttpClientForTesting:
+    def __init__(self):
+        self.__session = requests.session()
+        self.__verb = None
+        self.__url = None
+        self.__data = None
+        self.__json = None
+        self.__headers = {}
+        self.__proxies = None
+        self.__status_code = -1
+        self.__message = None
+
+    def get(self, url):
+        self.__url = url
+        self.__verb = "get"
+        return self
+
+    def post(self, url):
+        self.__url = url
+        self.__verb = "post"
+        return self
+
+    def data(self, data):
+        self.__data = data
+        return self
+
+    def json(self, json):
+        self.__json = json
+        return self
+
+    def header(self, key, value):
+        self.__headers[key] = value
+        return self
+
+    def headers(self, headers):
+        self.__headers = headers
+        return self
+
+    def proxies(self, proxies):
+        self.__proxies = proxies
+        return self
+
+    def ensure(self, status_code, message):
+        self.__status_code = status_code
+        self.__message = message
+        return self
+
+    def build(self) -> requests.Response:
+        self.__clean()
+        return self.__data \
+            if self.__data else self.__json
+
+    def __clean(self):
+        self.__verb = None
+        self.__url = None
+        self.__data = None
+        self.__json = None
+        self.__headers = {}
+        self.__proxies = None
+        self.__status_code = -1
+        self.__message = None
