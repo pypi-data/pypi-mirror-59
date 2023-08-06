0562c3db9ecf6a4d4dd5925ea49e13876d46785a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:32 +0000

    Add package better-lambda-deploy/0.1.1/better_lambda_deploy-0.1.1-py3-none-any.whl

---
+++ packages/better-lambda-deploy/better_lambda_deploy-0.1.1-py3-none-any.whl/bld/api_function.py
@@ -0,0 +1,48 @@
+from .lambda_function import LambdaFunction
+import json
+
+
+class APIFunction(LambdaFunction):
+    def run(self, event, context):
+        # Get the calling method and route appropriately.
+        method = event["httpMethod"].lower()
+
+        # TODO: Should use a more elegent method of routing here.
+        if method == "post":
+            response = self.post(event)
+
+        elif method == "get":
+            response = self.get(event)
+
+        elif method == "put":
+            response = self.put(event)
+
+        else:
+            raise Exception(f"Triggered with unexpected HTTP method {method}.")
+
+        return response
+
+    def create_return(self, status_code=200, body=None):
+        return_obj = {"statusCode": status_code}
+        if body:
+            return_obj["body"] = json.dumps(body)
+        return return_obj
+
+    def get_methods(self):
+        methods = []
+        # Check post method.
+        post = getattr(self, "post", None)
+        if callable(post):
+            methods.append("post")
+
+        # Check get method.
+        get = getattr(self, "get", None)
+        if callable(get):
+            methods.append("get")
+
+        # Check put method.
+        put = getattr(self, "put", None)
+        if callable(put):
+            methods.append("put")
+
+        return methods
