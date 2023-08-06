7a3e9706786af88378e29b99006b0dacba9d4f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:11 +0000

    Add package django-apar/1.1.6.9/django-apar-1.1.6.9.tar.gz

---
+++ packages/django-apar/django-apar-1.1.6.9.tar.gz/django-apar-1.1.6.9/aparnik/packages/shops/payments/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+from django.shortcuts import render, get_list_or_404
+
+from aparnik.settings import aparnik_settings
+from .models import Payment
+from aparnik.packages.bankgateways.utils import send_to_bank
+
+# Create your views here.
+def payment(request, uuid):
+    payments_obj = get_list_or_404(Payment.objects.all(), uuid=uuid)
+    pay_obj = payments_obj[0]
+
+    if pay_obj.order_obj.status == Payment.STATUS_WAITING and pay_obj.status == Payment.STATUS_WAITING:
+
+        if aparnik_settings.BANK_ACTIVE:
+            # (bank_obj, created) = BankAPI.objects.get_or_create(amount=pay_obj.transaction.amount, invoice_number=pay_obj.uuid.int)
+            return send_to_bank(request, pay_obj)
+            # return send_request(request, pay_obj)
+        else:
+            pay_obj.success()
+
+    if pay_obj.is_success():
+        return render(request=request, template_name='suit/paysuc.html', status=200, context={'obj': pay_obj})
+
+    return render(request=request, template_name='suit/payunsuc.html', status=400, context={'obj': pay_obj})
