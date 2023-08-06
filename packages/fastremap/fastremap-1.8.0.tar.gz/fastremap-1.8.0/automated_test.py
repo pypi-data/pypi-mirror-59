90dc6079f9bc8a57b705d68bb0bbead2559011c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:42 +0000

    Add package fastremap/1.8.0/fastremap-1.8.0.tar.gz

---
+++ packages/fastremap/fastremap-1.8.0.tar.gz/fastremap-1.8.0/automated_test.py
@@ -0,0 +1,285 @@
+import pytest
+
+import numpy as np
+
+import fastremap
+
+DTYPES = (
+  np.uint8, np.uint16, np.uint32, np.uint64,
+  np.int8, np.int16, np.int32, np.int64
+)
+
+def test_empty_renumber():
+  for dtype in DTYPES:
+    data = np.array([], dtype=dtype)
+    data2, remapdict = fastremap.renumber(data, preserve_zero=False)
+
+    assert np.all(data2 == [])
+    assert remapdict == {}
+
+def test_1d_renumber():
+  for dtype in DTYPES:
+    print(dtype)
+    data = np.arange(8).astype(dtype)
+    data = np.flip(data)
+
+    data2 = np.copy(data)
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=False)
+
+    assert np.all(data2 == np.arange(1,9))
+    assert len(remapdict) > 0
+
+    data2 = np.copy(data)
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=True)
+
+    assert data2[-1] == 0
+    assert np.all(data2 == [1,2,3,4,5,6,7,0])
+    assert len(remapdict) > 0
+
+  data = np.arange(8).astype(np.bool)
+  data = np.flip(data)
+
+  data2 = np.copy(data)
+  data2, remapdict = fastremap.renumber(data2, preserve_zero=False)
+
+  assert np.all(data2 == [1,1,1,1,1,1,1,2])
+  assert len(remapdict) > 0
+
+  data2 = np.copy(data)
+  data2, remapdict = fastremap.renumber(data2, preserve_zero=True)
+
+  assert np.all(data2 == [1,1,1,1,1,1,1,0])
+  assert len(remapdict) > 0
+
+def test_2d_renumber():
+  for dtype in DTYPES:
+    data = np.array([
+      [ 5,  5,  5, 2],
+      [ 3,  5,  5, 0],
+      [ 1,  2,  4, 1],
+      [20, 19, 20, 1],
+    ], dtype=dtype)
+
+    data2 = np.copy(data, order='C')
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=True)
+
+    assert np.all(data2 == [
+      [1, 1, 1, 2],
+      [3, 1, 1, 0],
+      [4, 2, 5, 4],
+      [6, 7, 6, 4],
+    ])
+
+    data2 = np.copy(data, order='F')
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=True)
+
+    assert np.all(data2 == [
+      [1, 1, 1, 5],
+      [2, 1, 1, 0],
+      [3, 5, 7, 3],
+      [4, 6, 4, 3],
+    ])
+
+def test_3d_renumber():
+  for dtype in DTYPES:
+    bits = np.dtype(dtype).itemsize * 8
+    big = (2 ** (bits - 1)) - 1 # cover ints and uints
+    data = np.array([
+      [
+        [big, 0],
+        [2, big],
+      ],
+      [
+        [big-5, big-1],
+        [big-7, big-3],
+      ],
+    ], dtype=dtype)
+
+    data2 = np.copy(data, order='C')
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=False)
+
+    assert np.all(data2 == [
+      [
+        [1, 2],
+        [3, 1]
+      ],
+      [ 
+        [4, 5],
+        [6, 7],
+      ],
+    ])
+
+    data2 = np.copy(data, order='F')
+    data2, remapdict = fastremap.renumber(data2, preserve_zero=False)
+
+    assert np.all(data2 == [
+      [
+        [1, 5],
+        [3, 1]
+      ],
+      [ 
+        [2, 6],
+        [4, 7],
+      ],
+    ])
+
+  big = np.random.randint(0, (2**64)-1, size=(512,512,100), dtype=np.uint64)
+  big, remapdict = fastremap.renumber(big, preserve_zero=True, in_place=True)
+  assert np.dtype(big.dtype).itemsize <= 4
+  assert np.dtype(big.dtype).itemsize > 1
+
+
+def test_remap_1d():
+  dtypes = list(DTYPES) + [ np.float32, np.float64 ]
+  for dtype in dtypes:
+    print(dtype)
+
+    empty = fastremap.remap([], {})
+    assert len(empty) == 0
+
+    data = np.array([1, 2, 2, 2, 3, 4, 5], dtype=dtype)
+    remap = {
+      1: 10,
+      2: 30,
+      3: 15,
+      4: 0,
+      5: 5,
+    }
+
+    result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=False)
+    assert np.all(result == [10, 30, 30, 30, 15, 0, 5])
+
+    del remap[2]
+    try:
+      result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=False)
+      assert False
+    except KeyError:
+      pass 
+
+    result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=True)
+    assert np.all(result == [10, 2, 2, 2, 15, 0, 5])
+
+def test_remap_2d():
+  for dtype in DTYPES:
+    print(dtype)
+    data = np.array([[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]], dtype=dtype)
+    remap = {
+      1: 10,
+      2: 30,
+      3: 15,
+      4: 0,
+      5: 5,
+    }
+
+    result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=False)
+    assert np.all(result == [[10, 30, 15, 0, 5], [5, 0, 15, 30, 10]])
+
+    del remap[2]
+    try:
+      result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=False)
+      assert False
+    except KeyError:
+      pass 
+
+    result = fastremap.remap(np.copy(data), remap, preserve_missing_labels=True)
+    assert np.all(result == [[10, 2, 15, 0, 5], [5, 0, 15, 2, 10]])
+
+def test_mask():
+  for dtype in DTYPES:
+    for in_place in (True, False):
+      print(dtype)
+      data = np.arange(100, dtype=dtype)
+      data = fastremap.mask(data, [5, 10, 15, 20], in_place=in_place)
+
+      labels, cts = np.unique(data, return_counts=True)
+      assert cts[0] == 5 
+      assert labels[0] == 0
+      assert np.all(cts[1:] == 1)
+      assert len(labels == 95)
+
+def test_mask_except():
+  for dtype in DTYPES:
+    for in_place in (True, False):
+      for value in (0, 7, np.iinfo(dtype).max):
+        print(dtype)
+        data = np.arange(100, dtype=dtype)
+        data = fastremap.mask_except(
+          data, [5, 10, 15, 20], 
+          in_place=in_place, value=value
+        )
+
+        labels, cts = np.unique(data, return_counts=True)
+        print(labels, cts)
+        res = { lbl: ct for lbl, ct in zip(labels, cts) }
+        assert res == {
+          value: 96,
+          5: 1,
+          10: 1,
+          15: 1, 
+          20: 1,
+        }
+
+def test_asfortranarray():
+  dtypes = list(DTYPES) + [ np.float32, np.float64, np.bool, np.complex64 ]
+  for dtype in dtypes:
+    print(dtype)
+    for dim in (1, 4, 7, 9, 27, 31, 100, 127, 200):
+      print(dim)
+      x = np.arange(dim**1).reshape((dim)).astype(dtype)
+      y = np.copy(x)
+      assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+      x = np.arange(dim**2).reshape((dim,dim)).astype(dtype)
+      y = np.copy(x)
+      assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+      x = np.arange(dim**3).reshape((dim,dim,dim)).astype(dtype)
+      y = np.copy(x)
+      assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+      x = np.arange(dim**2+dim).reshape((dim,dim+1)).astype(dtype)
+      y = np.copy(x)
+      assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+      x = np.arange(dim**3+dim*dim).reshape((dim,dim+1,dim)).astype(dtype)
+      y = np.copy(x)
+      assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+      if dim < 100:
+        x = np.arange(dim**4).reshape((dim,dim,dim,dim)).astype(dtype)
+        y = np.copy(x)
+        assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+        x = np.arange(dim**4 + dim*dim*dim).reshape((dim+1,dim,dim,dim)).astype(dtype)
+        y = np.copy(x)
+        assert np.all(np.asfortranarray(x) == fastremap.asfortranarray(y))
+
+
+def test_ascontiguousarray():
+  dtypes = list(DTYPES) + [ np.float32, np.float64, np.bool, np.complex64 ]
+  for dtype in dtypes:
+    for dim in (1, 4, 7, 9, 27, 31, 100, 127, 200):
+      x = np.arange(dim**2).reshape((dim,dim), order='F').astype(dtype)
+      y = np.copy(x, order='F')
+      assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
+
+      x = np.arange(dim**3).reshape((dim,dim,dim), order='F').astype(dtype)
+      y = np.copy(x, order='F')
+      assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
+
+      x = np.arange(dim**2+dim).reshape((dim,dim+1), order='F').astype(dtype)
+      y = np.copy(x, order='F')
+      assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
+
+      x = np.arange(dim**3+dim*dim).reshape((dim,dim+1,dim), order='F').astype(dtype)
+      y = np.copy(x, order='F')
+      assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
+
+      if dim < 100:
+        x = np.arange(dim**4).reshape((dim,dim,dim,dim)).astype(dtype)
+        y = np.copy(x, order='F')
+        assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
+
+        x = np.arange(dim**4 + dim*dim*dim).reshape((dim+1,dim,dim,dim)).astype(dtype)
+        y = np.copy(x, order='F')
+        assert np.all(np.ascontiguousarray(x) == fastremap.ascontiguousarray(y))
---
+++ packages/fastremap/fastremap-1.8.0.tar.gz/fastremap-1.8.0/fastremap.pyx
@@ -0,0 +1,692 @@
+"""
+Functions related to remapping image volumes.
+
+Renumber volumes into smaller data types, mask out labels
+or their complement, and remap the values of image volumes.
+
+This module also constains the facilities for performing
+and in-place matrix transposition for up to 4D arrays. This is 
+helpful for converting between C and Fortran order in memory
+constrained environments when format shifting.
+
+Author: William Silversmith
+Affiliation: Seung Lab, Princeton Neuroscience Institute
+Date: August 2018 - January 2020
+"""
+
+cimport cython
+from libc.stdint cimport (  
+  uint8_t, uint16_t, uint32_t, uint64_t,
+  int8_t, int16_t, int32_t, int64_t
+)
+from libcpp.unordered_map cimport unordered_map
+
+from functools import reduce
+import operator
+
+import numpy as np
+cimport numpy as cnp
+
+__version__ = '1.8.0'
+
+ctypedef fused UINT:
+  uint8_t
+  uint16_t
+  uint32_t
+  uint64_t
+
+ctypedef fused ALLINT:
+  UINT
+  int8_t
+  int16_t
+  int32_t
+  int64_t
+
+ctypedef fused NUMBER:
+  ALLINT
+  float
+  double
+
+ctypedef fused COMPLEX_NUMBER:
+  NUMBER
+  float complex 
+
+cdef extern from "ipt.hpp" namespace "pyipt":
+  cdef void _ipt2d[T](T* arr, int sx, int sy)
+  cdef void _ipt3d[T](
+    T* arr, int sx, int sy, int sz
+  )
+  cdef void _ipt4d[T](
+    T* arr, int sx, int sy, int sz, int sw
+  )
+
+def renumber(arr, start=1, preserve_zero=True, in_place=False):
+  """
+  renumber(arr, start=1, preserve_zero=True, in_place=False)
+
+  Given an array of integers, renumber all the unique values starting
+  from 1. This can allow us to reduce the size of the data width required
+  to represent it.
+
+  arr: A numpy array
+  start (default: 1): Start renumbering from this value
+  preserve_zero (default: True): Don't renumber zero.
+  in_place (default: False): Perform the renumbering in-place to avoid
+    an extra copy. This option depends on a fortran or C contiguous
+    array. A copy will be made if the array is not contiguous.
+
+  Return: a renumbered array, dict with remapping of oldval => newval
+  """
+  if arr.size == 0:
+    return arr, {}
+
+  if arr.dtype == np.bool and preserve_zero:
+    return arr, { 0: 0, 1: start }
+  elif arr.dtype == np.bool:
+    arr = arr.view(np.uint8)
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  shape = arr.shape
+  order = 'F' if arr.flags['F_CONTIGUOUS'] else 'C'
+  in_place = in_place and (arr.flags['F_CONTIGUOUS'] or arr.flags['C_CONTIGUOUS'])
+
+  if not in_place:
+    arr = np.copy(arr, order=order)
+
+  arr = np.lib.stride_tricks.as_strided(arr, shape=(arr.size,), strides=(nbytes,))
+  arr, remap_dict = _renumber(arr, <int64_t>start, preserve_zero)
+  arr = reshape(arr, shape, order)
+
+  return arr, remap_dict
+
+def reshape(arr, shape, order=None):
+  """
+  If the array is contiguous, attempt an in place reshape
+  rather than potentially making a copy.
+
+  Required:
+    arr: The input numpy array.
+    shape: The desired shape (must be the same size as arr)
+
+  Optional: 
+    order: 'C', 'F', or None (determine automatically)
+
+  Returns: reshaped array
+  """
+  if order is None:
+    if arr.flags['F_CONTIGUOUS']:
+      order = 'F'
+    elif arr.flags['C_CONTIGUOUS']:
+      order = 'C'
+    else:
+      return arr.reshape(shape)
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  if order == 'C':
+    strides = [ reduce(operator.mul, shape[i:]) * nbytes for i in range(1, len(shape)) ]
+    strides += [ nbytes ]
+    return np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
+  else:
+    strides = [ reduce(operator.mul, shape[:i]) * nbytes for i in range(1, len(shape)) ]
+    strides = [ nbytes ] + strides
+    return np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.nonecheck(False)
+def _renumber(cnp.ndarray[NUMBER, cast=True, ndim=1] arr, int64_t start=1, preserve_zero=True):
+  """
+  renumber(arr, int64_t start=1, preserve_zero=True)
+
+  Given an array of integers, renumber all the unique values starting
+  from 1. This can allow us to reduce the size of the data width required
+  to represent it.
+
+  arr: A numpy array
+  start (default: 1): Start renumbering from this value
+  preserve_zero (default ): 
+
+  Return: a renumbered array, dict with remapping of oldval => newval
+  """
+  cdef dict remap_dict = {}
+  if preserve_zero:
+    remap_dict[0] = 0
+
+  cdef NUMBER[:] arrview = arr
+
+  cdef NUMBER remap_id = start
+  cdef NUMBER elem
+
+  cdef NUMBER last_elem = 0
+  cdef NUMBER last_remap_id = 0
+  if not preserve_zero:
+    last_remap_id = start
+
+  cdef size_t size = arr.size
+  cdef size_t i = 0
+
+  for i in range(size):
+    elem = arrview[i]
+
+    if elem == last_elem:
+      arrview[i] = last_remap_id
+      continue
+
+    if elem in remap_dict:
+      arrview[i] = remap_dict[elem]
+    else:
+      arrview[i] = remap_id
+      remap_dict[elem] = remap_id
+      remap_id += 1
+
+    last_elem = elem 
+    last_remap_id = arrview[i]
+
+  if start < 0:
+    types = [ np.int8, np.int16, np.int32, np.int64 ]
+  else:
+    types = [ np.uint8, np.uint16, np.uint32, np.uint64 ]
+  
+  factor = max(abs(start), abs(remap_id))
+
+  if factor < 2 ** 8:
+    final_type = types[0]
+  elif factor < 2 ** 16:
+    final_type = types[1]
+  elif factor < 2 ** 32:
+    final_type = types[2]
+  else:
+    final_type = types[3]
+
+  if arr.dtype == final_type:
+    return arr, remap_dict
+  else:
+    return arr.astype(final_type), remap_dict
+
+def mask(arr, labels, in_place=False, value=0):
+  """
+  mask(arr, labels, in_place=False, value=0)
+
+  Mask out designated labels in an array with the
+  given value. 
+
+  Alternative implementation of:
+
+  arr[np.isin(labels)] = value
+
+  arr: an N-dimensional numpy array
+  labels: an iterable list of integers
+  in_place: if True, modify the input array to reduce
+    memory consumption.
+  value: mask value
+
+  Returns: arr with `labels` masked out
+  """
+  labels = { lbl: value for lbl in labels }
+  return remap(arr, labels, preserve_missing_labels=True, in_place=in_place)
+
+def mask_except(arr, labels, in_place=False, value=0):
+  """
+  mask_except(arr, labels, in_place=False, value=0)
+
+  Mask out all labels except the provided list.
+
+  Alternative implementation of:
+
+  arr[~np.isin(labels)] = value
+
+  arr: an N-dimensional numpy array
+  labels: an iterable list of integers
+  in_place: if True, modify the input array to reduce
+    memory consumption.
+  value: mask value
+
+  Returns: arr with all labels except `labels` masked out
+  """
+  shape = arr.shape 
+
+  if arr.flags['F_CONTIGUOUS']:
+    order = 'F'
+  else:
+    order = 'C'
+
+  if not in_place:
+    arr = np.copy(arr, order=order)
+
+  arr = reshape(arr, (arr.size,))
+  arr = _mask_except(arr, labels, value)
+  return reshape(arr, shape, order=order)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.nonecheck(False)          
+def _mask_except(cnp.ndarray[ALLINT] arr, list labels, ALLINT value):
+  cdef ALLINT[:] arrview = arr
+  cdef size_t i = 0
+  cdef size_t size = arr.size
+
+  cdef unordered_map[ALLINT, ALLINT] tbl 
+
+  for label in labels:
+    tbl[label] = label 
+
+  if value == 0:
+    for i in range(size):
+      arrview[i] = tbl[arrview[i]]
+  else:
+    for i in range(size):
+      if tbl.find(arrview[i]) == tbl.end():
+        arrview[i] = value
+
+  return arr
+
+
+def remap(arr, table, preserve_missing_labels=False, in_place=False):
+  """
+  remap(cnp.ndarray[COMPLEX_NUMBER] arr, dict table, 
+    preserve_missing_labels=False, in_place=False)
+
+  Remap an input numpy array in-place according to the values in the given 
+  dictionary "table".   
+
+  arr: an N-dimensional numpy array
+  table: { label: new_label_value, ... }
+  preserve_missing_labels: If an array value is not present in "table"...
+    True: Leave it alone.
+    False: Throw a KeyError.
+  in_place: if True, modify the input array to reduce
+    memory consumption.
+
+  Returns: remapped array
+  """
+  if type(arr) == list:
+    arr = np.array(arr)
+
+  shape = arr.shape 
+
+  if arr.flags['F_CONTIGUOUS']:
+    order = 'F'
+  else:
+    order = 'C'
+
+  if not in_place:
+    arr = np.copy(arr, order=order)
+
+  arr = reshape(arr, (arr.size,))
+  arr = _remap(arr, table, preserve_missing_labels)
+  return reshape(arr, shape, order=order)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+@cython.nonecheck(False)          
+def _remap(cnp.ndarray[NUMBER] arr, dict table, uint8_t preserve_missing_labels):
+  cdef NUMBER[:] arrview = arr
+  cdef size_t i = 0
+  cdef size_t size = arr.size
+  cdef NUMBER elem = 0
+
+  if size == 0:
+    return arr
+
+  cdef unordered_map[NUMBER, NUMBER] tbl 
+
+  for k, v in table.items():
+    tbl[k] = v 
+
+  cdef NUMBER last_elem = arrview[0]
+  cdef NUMBER last_remap_id = 0
+  
+  with nogil:
+    if tbl.find(last_elem) == tbl.end():
+      if not preserve_missing_labels:
+        raise KeyError("{} was not in the remap table.".format(last_elem))  
+    else:
+      arrview[0] = tbl[last_elem]
+      last_remap_id = arrview[0]
+
+    for i in range(1, size):
+      elem = arrview[i]
+
+      if elem == last_elem:
+        arrview[i] = last_remap_id
+        continue
+
+      if tbl.find(elem) == tbl.end():
+        if preserve_missing_labels:
+          last_elem = elem
+          last_remap_id = elem
+          continue
+        else:
+          raise KeyError("{} was not in the remap table.".format(elem))  
+      else:
+        arrview[i] = tbl[elem]
+      
+      last_elem = elem
+      last_remap_id = arrview[i]
+
+  return arr
+
+@cython.boundscheck(False)
+def remap_from_array(cnp.ndarray[UINT] arr, cnp.ndarray[UINT] vals):
+  """
+  remap_from_array(cnp.ndarray[UINT] arr, cnp.ndarray[UINT] vals)
+  """
+  cdef UINT[:] valview = vals
+  cdef UINT[:] arrview = arr
+
+  cdef size_t i = 0
+  cdef size_t size = arr.size 
+  cdef size_t maxkey = vals.size - 1
+  cdef UINT elem
+
+  with nogil:
+    for i in range(size):
+      elem = arr[i]
+      if elem < 0 or elem > maxkey:
+        continue
+      arrview[i] = vals[elem]
+
+  return arr
+
+@cython.boundscheck(False)
+def remap_from_array_kv(cnp.ndarray[ALLINT] arr, cnp.ndarray[ALLINT] keys, cnp.ndarray[ALLINT] vals):
+  """
+  remap_from_array_kv(cnp.ndarray[ALLINT] arr, cnp.ndarray[ALLINT] keys, cnp.ndarray[ALLINT] vals)
+  """
+  cdef ALLINT[:] keyview = keys
+  cdef ALLINT[:] valview = vals
+  cdef ALLINT[:] arrview = arr
+  cdef unordered_map[ALLINT, ALLINT] remap_dict
+
+  assert keys.size == vals.size
+
+  cdef size_t i = 0
+  cdef size_t size = keys.size 
+  cdef ALLINT elem
+
+  with nogil:
+    for i in range(size):
+      remap_dict[keys[i]] = vals[i]
+
+  i = 0
+  size = arr.size 
+
+  with nogil:
+    for i in range(size):
+      elem = arr[i]
+      if remap_dict.find(elem) == remap_dict.end():
+        continue
+      else:
+          arrview[i] = remap_dict[elem]
+
+  return arr
+
+def transpose(arr):
+  """
+  asfortranarray(arr)
+
+  For up to four dimensional matrices, perform in-place transposition. 
+  Square matrices up to three dimensions are faster than numpy's out-of-place
+  algorithm. Default to the out-of-place implementation numpy uses for cases
+  that aren't specially handled.
+
+  Returns: transposed numpy array
+  """
+  if not arr.flags['F_CONTIGUOUS'] and not arr.flags['C_CONTIGUOUS']:
+    arr = np.copy(arr, order='C')
+
+  shape = arr.shape
+  strides = arr.strides
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  dtype = arr.dtype
+  if arr.dtype == np.bool:
+    arr = arr.view(np.uint8)
+
+  if arr.ndim == 2:
+    arr = ipt2d(arr)
+    return arr.view(dtype)
+  elif arr.ndim == 3:
+    arr = ipt3d(arr)
+    return arr.view(dtype)
+  elif arr.ndim == 4:
+    arr = ipt4d(arr)
+    return arr.view(dtype)
+  else:
+    return arr.T
+
+def asfortranarray(arr):
+  """
+  asfortranarray(arr)
+
+  For up to four dimensional matrices, perform in-place transposition. 
+  Square matrices up to three dimensions are faster than numpy's out-of-place
+  algorithm. Default to the out-of-place implementation numpy uses for cases
+  that aren't specially handled.
+
+  Returns: transposed numpy array
+  """
+  if arr.flags['F_CONTIGUOUS']:
+    return arr
+  elif not arr.flags['C_CONTIGUOUS']:
+    return np.asfortranarray(arr)
+  elif arr.ndim == 1:
+    return arr 
+
+  shape = arr.shape
+  strides = arr.strides
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  dtype = arr.dtype
+  if arr.dtype == np.bool:
+    arr = arr.view(np.uint8)
+
+  if arr.ndim == 2:
+    arr = ipt2d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=(nbytes, shape[0] * nbytes))
+    return arr.view(dtype)
+  elif arr.ndim == 3:
+    arr = ipt3d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=(nbytes, shape[0] * nbytes, shape[0] * shape[1] * nbytes))
+    return arr.view(dtype)
+  elif arr.ndim == 4:
+    arr = ipt4d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, 
+      strides=(
+        nbytes, 
+        shape[0] * nbytes, 
+        shape[0] * shape[1] * nbytes, 
+        shape[0] * shape[1] * shape[2] * nbytes
+      ))
+    return arr.view(dtype)
+  else:
+    return np.asfortranarray(arr)
+
+def ascontiguousarray(arr):
+  """
+  ascontiguousarray(arr)
+
+  For up to four dimensional matrices, perform in-place transposition. 
+  Square matrices up to three dimensions are faster than numpy's out-of-place
+  algorithm. Default to the out-of-place implementation numpy uses for cases
+  that aren't specially handled.
+
+  Returns: transposed numpy array
+  """
+  if arr.flags['C_CONTIGUOUS']:
+    return arr
+  elif not arr.flags['F_CONTIGUOUS']:
+    return np.ascontiguousarray(arr)
+  elif arr.ndim == 1:
+    return arr 
+
+  shape = arr.shape
+  strides = arr.strides
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  dtype = arr.dtype
+  if arr.dtype == np.bool:
+    arr = arr.view(np.uint8)
+
+  if arr.ndim == 2:
+    arr = ipt2d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=(shape[1] * nbytes, nbytes))
+    return arr.view(dtype)
+  elif arr.ndim == 3:
+    arr = ipt3d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=(
+        shape[2] * shape[1] * nbytes, 
+        shape[2] * nbytes, 
+        nbytes,
+      ))
+    return arr.view(dtype)
+  elif arr.ndim == 4:
+    arr = ipt4d(arr)
+    arr = np.lib.stride_tricks.as_strided(arr, shape=shape, 
+      strides=(
+        shape[3] * shape[2] * shape[1] * nbytes,
+        shape[3] * shape[2] * nbytes, 
+        shape[3] * nbytes, 
+        nbytes, 
+      ))
+    return arr.view(dtype)
+  else:
+    return np.ascontiguousarray(arr)
+
+def ipt2d(cnp.ndarray[COMPLEX_NUMBER, cast=True, ndim=2] arr):
+  cdef COMPLEX_NUMBER[:,:] arrview = arr
+
+  cdef int sx
+  cdef int sy
+
+  if arr.flags['F_CONTIGUOUS']:
+    sx = arr.shape[0]
+    sy = arr.shape[1]
+  else:
+    sx = arr.shape[1]
+    sy = arr.shape[0]
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  # ipt doesn't do anything with values, 
+  # just moves them around, so only bit width matters
+  # int, uint, float, bool who cares
+  if nbytes == 1:
+    _ipt2d[uint8_t](
+      <uint8_t*>&arrview[0,0],
+      sx, sy
+    )
+  elif nbytes == 2:
+    _ipt2d[uint16_t](
+      <uint16_t*>&arrview[0,0],
+      sx, sy
+    )
+  elif nbytes == 4:
+    _ipt2d[uint32_t](
+      <uint32_t*>&arrview[0,0],
+      sx, sy
+    )
+  else:
+    _ipt2d[uint64_t](
+      <uint64_t*>&arrview[0,0],
+      sx, sy
+    )
+
+  return arr
+
+def ipt3d(cnp.ndarray[COMPLEX_NUMBER, cast=True, ndim=3] arr):
+  cdef COMPLEX_NUMBER[:,:,:] arrview = arr
+
+  cdef int sx
+  cdef int sy
+  cdef int sz
+
+  if arr.flags['F_CONTIGUOUS']:
+    sx = arr.shape[0]
+    sy = arr.shape[1]
+    sz = arr.shape[2]
+  else:
+    sx = arr.shape[2]
+    sy = arr.shape[1]
+    sz = arr.shape[0]
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  # ipt doesn't do anything with values, 
+  # just moves them around, so only bit width matters
+  # int, uint, float, bool who cares
+  if nbytes == 1:
+    _ipt3d[uint8_t](
+      <uint8_t*>&arrview[0,0,0],
+      sx, sy, sz
+    )
+  elif nbytes == 2:
+    _ipt3d[uint16_t](
+      <uint16_t*>&arrview[0,0,0],
+      sx, sy, sz
+    )
+  elif nbytes == 4:
+    _ipt3d[uint32_t](
+      <uint32_t*>&arrview[0,0,0],
+      sx, sy, sz
+    )
+  else:
+    _ipt3d[uint64_t](
+      <uint64_t*>&arrview[0,0,0],
+      sx, sy, sz
+    )    
+
+  return arr
+
+def ipt4d(cnp.ndarray[COMPLEX_NUMBER, cast=True, ndim=4] arr):
+  cdef COMPLEX_NUMBER[:,:,:,:] arrview = arr
+
+  cdef int sx
+  cdef int sy
+  cdef int sz
+  cdef int sw
+
+  if arr.flags['F_CONTIGUOUS']:
+    sx = arr.shape[0]
+    sy = arr.shape[1]
+    sz = arr.shape[2]
+    sw = arr.shape[3]
+  else:
+    sx = arr.shape[3]
+    sy = arr.shape[2]
+    sz = arr.shape[1]
+    sw = arr.shape[0]
+
+  cdef int nbytes = np.dtype(arr.dtype).itemsize
+
+  # ipt doesn't do anything with values, 
+  # just moves them around, so only bit width matters
+  # int, uint, float, bool who cares
+  if nbytes == 1:
+    _ipt4d[uint8_t](
+      <uint8_t*>&arrview[0,0,0,0],
+      sx, sy, sz, sw
+    )
+  elif nbytes == 2:
+    _ipt4d[uint16_t](
+      <uint16_t*>&arrview[0,0,0,0],
+      sx, sy, sz, sw
+    )
+  elif nbytes == 4:
+    _ipt4d[uint32_t](
+      <uint32_t*>&arrview[0,0,0,0],
+      sx, sy, sz, sw
+    )
+  else:
+    _ipt4d[uint64_t](
+      <uint64_t*>&arrview[0,0,0,0],
+      sx, sy, sz, sw
+    )
+
+  return arr
+
