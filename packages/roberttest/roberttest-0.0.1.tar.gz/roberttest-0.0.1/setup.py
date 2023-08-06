97f3f3a8c5db0c12cf270d6ab570a2a0a0eafa53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:37 +0000

    Add package roberttest/0.0.1/roberttest-0.0.1.tar.gz

---
+++ packages/roberttest/roberttest-0.0.1.tar.gz/roberttest-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+name='roberttest',
+version='0.0.1',
+author='robertliu',
+author_email='ljm_jxau@163.com',
+url='',
+description='test',
+long_description='hello',
+packages=[],
+install_requires=[])
