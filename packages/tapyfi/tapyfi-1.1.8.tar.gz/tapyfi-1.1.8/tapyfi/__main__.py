b23e92e52f7762351d797f3d822591abf6392a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:02 +0000

    Add package tapyfi/1.1.8/tapyfi-1.1.8.tar.gz

---
+++ packages/tapyfi/tapyfi-1.1.8.tar.gz/tapyfi-1.1.8/tapyfi/__main__.py
@@ -0,0 +1,12 @@
+import tapyfi.Indicators
+import tapyfi.Helpers
+
+
+def main():
+    """Indıcator Lib For Trader"""
+    pass
+    # Read URL of the Real Python feed from config file
+
+if __name__ == "__main__":
+    main()
+
