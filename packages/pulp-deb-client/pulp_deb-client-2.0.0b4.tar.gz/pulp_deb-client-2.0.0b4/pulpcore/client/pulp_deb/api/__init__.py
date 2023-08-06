2425e29198256ba9b8fe5dbc5706474dfee5eb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package pulp-deb-client/2.0.0b4/pulp_deb-client-2.0.0b4.tar.gz

---
+++ packages/pulp-deb-client/pulp_deb-client-2.0.0b4.tar.gz/pulp_deb-client-2.0.0b4/pulpcore/client/pulp_deb/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_deb.api.content_generic_contents_api import ContentGenericContentsApi
+from pulpcore.client.pulp_deb.api.content_installer_file_index_api import ContentInstallerFileIndexApi
+from pulpcore.client.pulp_deb.api.content_installer_packages_api import ContentInstallerPackagesApi
+from pulpcore.client.pulp_deb.api.content_package_index_api import ContentPackageIndexApi
+from pulpcore.client.pulp_deb.api.content_packages_api import ContentPackagesApi
+from pulpcore.client.pulp_deb.api.content_release_files_api import ContentReleaseFilesApi
+from pulpcore.client.pulp_deb.api.distributions_apt_api import DistributionsAptApi
+from pulpcore.client.pulp_deb.api.publications_apt_api import PublicationsAptApi
+from pulpcore.client.pulp_deb.api.publications_verbatim_api import PublicationsVerbatimApi
+from pulpcore.client.pulp_deb.api.remotes_apt_api import RemotesAptApi
+from pulpcore.client.pulp_deb.api.repositories_apt_api import RepositoriesAptApi
+from pulpcore.client.pulp_deb.api.repositories_deb_versions_api import RepositoriesDebVersionsApi
