ee8973db06f78a5b6562fd176df894f9aeb77945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:16 +0000

    Add package OpenNMT-tf/2.5.1/OpenNMT-tf-2.5.1.tar.gz

---
+++ packages/OpenNMT-tf/OpenNMT-tf-2.5.1.tar.gz/OpenNMT-tf-2.5.1/opennmt/models/__init__.py
@@ -0,0 +1,28 @@
+"""Module defining models."""
+
+from opennmt.models.catalog import GPT2Small
+from opennmt.models.catalog import ListenAttendSpell
+from opennmt.models.catalog import LstmCnnCrfTagger
+from opennmt.models.catalog import LuongAttention
+from opennmt.models.catalog import Transformer as TransformerBase
+from opennmt.models.catalog import TransformerBig
+from opennmt.models.catalog import TransformerBigRelative
+from opennmt.models.catalog import TransformerRelative
+
+from opennmt.models.language_model import LanguageModel
+from opennmt.models.language_model import LanguageModelInputter
+
+from opennmt.models.model import Model
+from opennmt.models.model import SequenceGenerator
+
+from opennmt.models.sequence_classifier import ClassInputter
+from opennmt.models.sequence_classifier import SequenceClassifier
+
+from opennmt.models.sequence_tagger import SequenceTagger
+from opennmt.models.sequence_tagger import TagsInputter
+
+from opennmt.models.sequence_to_sequence import EmbeddingsSharingLevel
+from opennmt.models.sequence_to_sequence import SequenceToSequence
+from opennmt.models.sequence_to_sequence import SequenceToSequenceInputter
+
+from opennmt.models.transformer import Transformer
