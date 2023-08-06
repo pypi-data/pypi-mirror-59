180dd9a4cadf34fff94867639322c026387129e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:59 +0000

    Add package experiment-ltt/0.0.5/experiment_ltt-0.0.5-py3-none-any.whl

---
+++ packages/experiment-ltt/experiment_ltt-0.0.5-py3-none-any.whl/experiment/experiment.py
@@ -0,0 +1,67 @@
+import yaml
+
+def _create_default_instance_table(conn):
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE instance (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(512),
+            kind VARCHAR(512))''')
+    # Add unique constraint for all fields
+    query = '''ALTER TABLE instance ADD UNIQUE unique_index (name,kind) '''
+    cur.execute(query)
+    conn.commit()
+
+def _create_default_method_table(conn):
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE method(
+            id INT AUTO_INCREMENT PRIMARY KEY)''')
+
+def _create_default_iteration_table(conn):
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE iteration(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            best DOUBLE,
+            mean DOUBLE,
+            std DOUBLE,
+            best_solution MEDIUMBLOB,
+            num_iteration INT,
+            num_evaluation INT,
+            seed INT)''')
+
+def _create_summary_table(conn):
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE summary(
+            method_id INT REFERENCES method(id),
+            instance_id INT REFERENCES instance(id),
+            iteration_id INT REFERENCES iteration(id))''')
+
+def create_experiment(conn, config):
+    '''Create database and tables for the experiment.
+    Create a table "experiment" that contain the experimental
+    summarization
+
+    Parameters:
+        - conn (object): mysql connection
+        - config (dict): contain db name
+    '''
+    cur = conn.cursor()
+    cur.execute('CREATE DATABASE IF NOT EXISTS %s' % config['db'])
+    conn.select_db(config['db'])
+    _create_default_instance_table(conn)
+    _create_default_method_table(conn)
+    _create_default_iteration_table(conn)
+    _create_summary_table(conn)
+    if config['verbose']:
+        print('[experiment|ok] create_experiment(%s)' % config['db'])
+
+def drop_experiment(conn, config):
+    '''Drop database contain experimental results
+
+    Parameters:
+        - conn (object): mysql connection
+        - config (dict): contain db name
+    '''
+    cur = conn.cursor()
+    cur.execute('DROP DATABASE IF EXISTS %s' % config['db'])
+    if config['verbose']:
+        print('[experiment|ok] drop_experiment(%s)' % config['db'])
