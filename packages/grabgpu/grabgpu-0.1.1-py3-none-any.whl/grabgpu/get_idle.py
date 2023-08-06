764d18e044fe1acd736b3d295f362f12f0aad83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:02 +0000

    Add package grabgpu/0.1.1/grabgpu-0.1.1-py3-none-any.whl

---
+++ packages/grabgpu/grabgpu-0.1.1-py3-none-any.whl/grabgpu/get_idle.py
@@ -0,0 +1,19 @@
+import gpuinfo
+import time
+
+def get_idle(device_ids=[]):
+    for device_id in device_ids:
+        gpu_status = gpuinfo.get(device_id=device_id)
+        if gpu_status['memory_used'] < 100:
+            return device_id
+
+    return None
+
+def wait_until_available(device_ids=[]):
+    while True:
+        idle_device = get_idle(device_ids)
+        if idle_device is not None:
+            print(idle_device)
+            return idle_device
+
+        time.sleep(3)
