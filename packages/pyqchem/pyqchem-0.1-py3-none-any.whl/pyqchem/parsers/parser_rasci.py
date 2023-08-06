2d088aaefaa09ba56631b3febd86f7602c8f1987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:32 +0000

    Add package pyqchem/0.1/pyqchem-0.1-py3-none-any.whl

---
+++ packages/pyqchem/pyqchem-0.1-py3-none-any.whl/pyqchem/parsers/parser_rasci.py
@@ -0,0 +1,155 @@
+__author__ = 'Abel Carreras'
+
+import re
+import operator
+from pyqchem.utils import standardize_vector
+from pyqchem.structure import Structure
+
+
+def _read_simple_matrix(header, output, maxchar=10000, foot='-------'):
+    matrix_list = []
+    for m in re.finditer(header, output):
+        section_state = output[m.end():m.end() + maxchar]  # 10000: assumed to max of section
+        section_state = section_state[:section_state.find(foot)]
+        dim = len(section_state.split('\n')[1].split())
+        matrix = section_state.split('\n')[1:dim + 1]
+        matrix = [[float(n) for n in l.split()] for l in matrix]
+        matrix_list.append(matrix)
+
+    return matrix_list
+
+
+def rasci(output):
+    """
+    Parser for RAS-CI calculations
+    Include:
+    - Diabatization scheme data
+    - Structure
+    - Adiabatic states
+
+    :param output:
+    :return:
+    """
+
+    data_dict = {}
+    # Molecule
+    n = output.find('$molecule')
+    n2 = output[n:].find('$end')
+
+    molecule_region = output[n:n+n2-1].replace('\t', ' ').split('\n')[1:]
+    charge, multiplicity = [int(num) for num in molecule_region[0].split()]
+    coordinates = [[float(l) for l in line.split()[1:4]] for line in molecule_region[1:]]
+    symbols = [line.split()[0].capitalize() for line in molecule_region[1:]]
+    n_atoms = len(symbols)
+
+    # structure not stored
+    data_dict['structure'] = Structure(coordinates=coordinates,
+                                       atomic_elements=symbols,
+                                       charge=charge,
+                                       multiplicity=multiplicity)
+
+    # scf energy
+    enum = output.find('SCF   energy in the final basis set')
+    scf_energy = float(output[enum:enum+100].split()[8])
+
+    # total energy
+    # enum = output.find('Total energy in the final basis set')
+    # total_energy = float(output[enum:enum+100].split()[8])
+
+    # Diabatization scheme
+    done_diabat = bool(output.find('RASCI DIABATIZATION')+1)
+    if done_diabat:
+        rot_matrix = _read_simple_matrix('showmatrix final adiabatic -> diabatic', output)[-1]
+        adiabatic_matrix = _read_simple_matrix('showing H in adiabatic representation: NO coupling elements', output)[-1]
+        diabatic_matrix = _read_simple_matrix('showing H in diabatic representation: WITH coupling elements', output)[-1]
+
+        mulliken = []
+        enum = output.find('showing H in diabatic representation')
+        for m in re.finditer('Mulliken Analysis of Diabatic State', output[enum:]):
+            section_mulliken = output[m.end() + enum: m.end() + 10000 + enum]  # 10000: assumed to max of section
+            section_mulliken = section_mulliken[:section_mulliken.find('Natural Orbitals stored in FCHK')]
+            section_attachment = section_mulliken.split('\n')[9+n_atoms:9+n_atoms*2]
+
+            mulliken.append({'attach': [float(l.split()[1]) for l in section_attachment],
+                             'detach': [float(l.split()[2]) for l in section_attachment],
+                             'total': [float(l.split()[3]) for l in section_attachment]})
+
+        data_dict['diabatization'] = {'rot_matrix': rot_matrix,
+                                      'adiabatic_matrix': adiabatic_matrix,
+                                      'diabatic_matrix': diabatic_matrix,
+                                      'mulliken_analysis': mulliken}
+
+    # excited states data
+    excited_states = []
+    for m in re.finditer('RAS-CI total energy for state', output):
+        # print('ll found', m.start(), m.end())
+
+        section_state = output[m.end():m.end() + 10000]  # 10000: assumed to max of section
+        section_state = section_state[:section_state.find('********')]
+
+        enum = section_state.find('RAS-CI total energy for state')
+        section_state = section_state[:enum]
+
+        # energies
+        tot_energy = float(section_state.split()[1])
+        exc_energy_units = section_state.split()[4][1:-1]
+        exc_energy = float(section_state.split()[6])
+        mul = section_state.split()[8]
+
+        # dipole moment
+        enum = section_state.find('Dipole Moment')
+        dipole_mom = [float(section_state[enum:].split()[2]) + 0.0,
+                      float(section_state[enum:].split()[4]) + 0.0,
+                      float(section_state[enum:].split()[6]) + 0.0]
+
+        # Transition moment
+        enum = section_state.find('Trans. Moment')
+        if enum > -1:
+            trans_mom = [float(section_state[enum:].split()[2]) + 0.0,
+                         float(section_state[enum:].split()[4]) + 0.0,
+                         float(section_state[enum:].split()[6]) + 0.0]
+            trans_mom = standardize_vector(trans_mom)
+            strength = float(section_state[enum:].split()[10])
+        else:
+            trans_mom = None
+            strength = None
+
+        # amplitudes table
+        enum = section_state.find('AMPLITUDE')
+        enum2 = section_state.find('Contributions')
+        section_table = section_state[enum: enum2].split('\n')[2:-2]
+
+        # ' HOLE  | ALPHA | BETA  | PART | AMPLITUDE'
+
+        table = []
+        for row in section_table:
+            table.append({'hole': row.split('|')[1].strip(),
+                          'alpha': row.split('|')[2].strip(),
+                          'beta': row.split('|')[3].strip(),
+                          'part': row.split('|')[4].strip(),
+                          'amplitude': float(row.split('|')[5]) + 0.0})
+        table = sorted(table, key=operator.itemgetter('hole', 'alpha', 'beta', 'part'))
+
+        # Contributions RASCI wfn
+        contributions_section = section_state[enum2:]
+        contributions = {'active' : float(contributions_section.split()[4]),
+                         'hole': float(contributions_section.split()[6]),
+                         'part': float(contributions_section.split()[8])}
+
+        # complete dictionary
+        tot_energy_units = 'au'
+        excited_states.append({'total_energy': tot_energy,
+                               'total energy units': tot_energy_units,
+                               'excitation_energy': exc_energy,
+                               'excitation energy units': exc_energy_units,
+                               'multiplicity': mul,
+                               'dipole_moment': dipole_mom,
+                               'transition_moment': trans_mom,
+                               'oscillator_strength': strength,
+                               'amplitudes': table,
+                               'contributions_fwn': contributions})
+
+    data_dict.update({'scf energy': scf_energy,
+                      'excited states rasci': excited_states})
+
+    return data_dict
---
+++ packages/pyqchem/pyqchem-0.1-py3-none-any.whl/pyqchem/symmetry.py
@@ -0,0 +1,214 @@
+from wfnsympy import WfnSympy
+import numpy as np
+from scipy.optimize import leastsq
+from pyqchem.utils import standardize_vector
+
+
+def get_wf_symmetry(structure,
+                    basis,
+                    mo_coeff,
+                    center=None,
+                    orientation=(0., 0., 1.),
+                    group='C2h'):
+
+    """
+    Simplified interface between pyqchem anf wfnsympy
+
+    :param structure: molecular geometry (3xn) array
+    :param basis: dictionary containing the basis
+    :param mo_coeff: molecular orbitals coefficients
+    :param center: center of the molecule
+    :param orientation: unit vector perpendicular to the plane of the molecule
+    :param group: point symmetry group
+    :return molsym: wfnsympy object
+    """
+
+    alpha_mo_coeff = np.array(mo_coeff['alpha']).tolist()
+    if 'beta' in mo_coeff:
+        beta_mo_coeff = np.array(mo_coeff['beta']).tolist()
+    else:
+        beta_mo_coeff = None
+
+    molsym = WfnSympy(coordinates=structure.get_coordinates(),
+                      symbols=structure.get_atomic_elements(),
+                      basis=basis,
+                      center=center,
+                      VAxis=orientation,
+                      alpha_mo_coeff=alpha_mo_coeff,
+                      beta_mo_coeff=beta_mo_coeff,
+                      charge=structure.charge,
+                      multiplicity=structure.multiplicity,
+                      group=group)
+    return molsym
+
+
+def get_orbital_classification(fchk_data,
+                               center=(0., 0., 0.),
+                               orientation=(0., 0., 1.)):
+    """
+    Classify orbitals in sigma/pi in planar molecules
+
+    :param fchk_data: dictionary containing fchk data
+    :param orientation: unit vector perpendicular to the plane of the molecule
+    :return: list of labels 'sigma'/'pi' according to the order of the orbitals
+    """
+    molsym = get_wf_symmetry(fchk_data['structure'],
+                             fchk_data['basis'],
+                             fchk_data['coefficients'],
+                             center=center,
+                             orientation=orientation)
+
+    sh_index = molsym.SymLab.index('s_h')
+    orbital_type_alpha = []
+    for i, overlap in enumerate(molsym.mo_SOEVs_a[:, sh_index]):
+        overlap = overlap / molsym.mo_SOEVs_a[i, molsym.SymLab.index('E')]  # normalize
+        if overlap < 0:
+            orbital_type_alpha.append(['pi', np.abs(overlap)])
+        else:
+            orbital_type_alpha.append(['sigma', np.abs(overlap)])
+
+    if 'beta' in fchk_data['coefficients']:
+        orbital_type_beta = []
+        for i, overlap in enumerate(molsym.mo_SOEVs_b[:, sh_index]):
+            if overlap < 0:
+                orbital_type_beta.append(['pi', np.abs(overlap)])
+            else:
+                orbital_type_beta.append(['sigma', np.abs(overlap)])
+        return orbital_type_alpha, orbital_type_beta
+    else:
+        return orbital_type_alpha
+
+
+def set_zero_coefficients(basis, mo_coeff, range_atoms):
+    """
+    set 0.0 to coefficients of functions centered in atoms 'range_atoms'
+
+    :param basis: full basis dictionary
+    :param mo_coeff: full molecular orbitals coefficients to be modiffied
+    :param range_atoms: list containing the atom numbers whose coefficients will be set to zero
+    :return:
+    """
+
+    functions_to_atom = []
+    nat = len(basis['atoms'])
+    for i in range(0, nat):
+        nf = 0
+        for shell in basis['atoms'][i]['shells']:
+            nf += shell['functions']
+        functions_to_atom.append(nf)
+    functions_to_atom = functions_to_atom
+
+    # print(funtions_to_atom)
+    mo_coeff_a = np.array(mo_coeff['alpha'])
+    for i in range_atoms:
+        ini = np.sum(functions_to_atom[:i], dtype=int)
+        fin = np.sum(functions_to_atom[:i+1], dtype=int)
+        # print('ini', ini, 'fin', fin)
+        mo_coeff_a[:, ini: fin] *= 0.0
+    mo_coeff_zero = {'alpha': mo_coeff_a.tolist()}
+
+    if 'beta' in mo_coeff:
+        mo_coeff_b = np.array(mo_coeff['beta'])
+        for i in range_atoms:
+            ini = np.sum(functions_to_atom[:i], dtype=int)
+            fin = np.sum(functions_to_atom[:i + 1], dtype=int)
+            # print('ini', ini, 'fin', fin)
+            mo_coeff_b[:, ini: fin] *= 0.0
+        mo_coeff_zero['beta'] = mo_coeff_b.tolist()
+
+    return mo_coeff_zero
+
+
+if __name__ == '__main__':
+    from pyqchem.qchem_core import get_output_from_qchem, create_qchem_input
+    from pyqchem.structure import Structure
+    from pyqchem.parsers.parser_fchk import parser_fchk
+    from pyqchem.file_io import build_fchk
+
+    benzene_coordinates = [[ 0.00000,  1.40272, 0.00000],
+                           [ 0.00000,  2.49029, 0.00000],
+                           [-1.21479,  0.70136, 0.00000],
+                           [-2.15666,  1.24515, 0.00000],
+                           [-1.21479, -0.70136, 0.00000],
+                           [-2.15666, -1.24515, 0.00000],
+                           [ 0.00000, -1.40272, 0.00000],
+                           [ 0.00000, -2.49029, 0.00000],
+                           [ 1.21479, -0.70136, 0.00000],
+                           [ 2.15666, -1.24515, 0.00000],
+                           [ 1.21479,  0.70136, 0.00000],
+                           [ 2.15666,  1.24515, 0.00000]]
+
+    symbols = ['C','H','C','H','C','H','C','H','C','H','C','F']
+
+    benzene_coordinates = np.array(benzene_coordinates)
+    molecule = Structure(coordinates=benzene_coordinates,
+                         atomic_elements=symbols,
+                         charge=0)
+
+    parameters = {'jobtype': 'sp',
+                  'exchange': 'hf',
+                  'basis': '6-31G'}
+
+    qc_input = create_qchem_input(molecule, **parameters)
+
+    _, _, fchk_data = get_output_from_qchem(qc_input, processors=4, force_recalculation=True,
+                                            read_fchk=True)
+
+    from pyqchem.file_io import build_fchk
+    open('test.fchk', 'w').write(build_fchk(fchk_data))
+
+    alpha_mo_coeff = set_zero_coefficients(fchk_data['basis'],
+                                           fchk_data['coefficients']['alpha'],
+                                           range(6, 12))
+    fchk_data['coefficients']['alpha'] = alpha_mo_coeff
+
+    structure = fchk_data['structure']
+    print(structure.get_xyz())
+
+    txt_fchk = build_fchk(fchk_data)
+    open('test2.fchk', 'w').write(txt_fchk)
+
+    z_dist = structure.get_coordinates()[0][2]
+
+    orbital_type = get_orbital_classification(fchk_data, center=[0.0, 0.0, z_dist])
+
+    print('  {:5} {:5} {:5}'.format('num', 'type', 'overlap'))
+    for i, ot in enumerate(orbital_type):
+        print('{:5}:  {:5} {:5.2f}'.format(i+1, ot[0], ot[1]))
+
+
+def get_plane(coords, direction=None):
+    """Generate initial guess"""
+
+    coords = np.array(coords)
+    p0 = np.cross(coords[0] - coords[2], coords[0] - coords[-1]) / np.linalg.norm(np.cross(coords[0] - coords[2],
+                                                                                           coords[0] - coords[-1]))
+
+    # Fitting function to a plane
+    def fitfunc(p, coords):
+        average = np.average(coords, axis=0)
+        return np.array([np.dot(p, average - c) for c in coords])
+
+    # Error function (including force norm(normal) = 1)
+    errfunc = lambda p, x: fitfunc(p, x)**2 + (np.linalg.norm(p) - 1.0)**2
+
+    p1, flag = leastsq(errfunc, p0, args=(coords,))
+
+
+    # Check final result
+    point = np.average(coords, axis=0).tolist()
+    normal = np.array(p1)/np.linalg.norm(p1)
+
+    if direction is not None:
+        vector = coords[direction[1]] - coords[direction[0]]
+        # proj = vector - np.dot(vector, normal)*normal
+        projected = np.cross(normal,  np.cross(vector, normal))
+        projected /= np.linalg.norm(projected)
+
+        normal = standardize_vector(normal)
+        projected = standardize_vector(projected)
+
+        return point, normal, projected
+
+    normal = standardize_vector(normal)
+    return point, normal
