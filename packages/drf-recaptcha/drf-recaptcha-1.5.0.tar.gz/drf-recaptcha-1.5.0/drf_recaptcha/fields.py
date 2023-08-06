ee3bfc2464ffa3a808a322b1234e446c3219b760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:13 +0000

    Add package drf-recaptcha/1.5.0/drf-recaptcha-1.5.0.tar.gz

---
+++ packages/drf-recaptcha/drf-recaptcha-1.5.0.tar.gz/drf-recaptcha-1.5.0/drf_recaptcha/fields.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from rest_framework.serializers import CharField
+
+from drf_recaptcha.constants import DEFAULT_V3_SCORE
+from drf_recaptcha.validators import ReCaptchaV2Validator, ReCaptchaV3Validator
+
+
+class ReCaptchaV2Field(CharField):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.write_only = True
+
+        validator = ReCaptchaV2Validator(secret_key=settings.DRF_RECAPTCHA_SECRET_KEY)
+        self.validators.append(validator)
+
+
+class ReCaptchaV3Field(CharField):
+    def __init__(self, action: str, required_score: float = None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.write_only = True
+
+        scores_from_settings = getattr(settings, "DRF_RECAPTCHA_ACTION_V3_SCORES", {})
+
+        self.required_score = (
+            required_score
+            or (
+                isinstance(scores_from_settings, dict)
+                and scores_from_settings.get(action, None)
+            )
+            or getattr(settings, "DRF_RECAPTCHA_DEFAULT_V3_SCORE", None)
+            or DEFAULT_V3_SCORE
+        )
+
+        validator = ReCaptchaV3Validator(
+            action=action,
+            required_score=self.required_score,
+            secret_key=settings.DRF_RECAPTCHA_SECRET_KEY,
+        )
+        self.validators.append(validator)
