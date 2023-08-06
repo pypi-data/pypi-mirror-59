57ea4437ef375a6e8538c849dbfa998c8d375764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:54 +0000

    Add package cwinpy/0.2.1/cwinpy-0.2.1.tar.gz

---
+++ packages/cwinpy/cwinpy-0.2.1.tar.gz/cwinpy-0.2.1/test/test_utils.py
@@ -0,0 +1,37 @@
+"""
+Test script for utils.py function.
+"""
+
+import pytest
+import numpy as np
+
+from cwinpy.utils import logfactorial, gcd_array
+
+
+def test_logfactorial():
+    """
+    Test log factorial function
+    """
+
+    a = 3
+    assert logfactorial(a) == np.log(3 * 2 * 1)
+
+    a = 3.0
+    assert logfactorial(a) == np.log(3 * 2 * 1)
+
+
+def test_gcd_array():
+    """
+    Test greatest common divisor function.
+    """
+
+    a = 1  # non-list value
+    with pytest.raises(TypeError):
+        gcd_array(a)
+
+    a = [1]  # single value
+    with pytest.raises(ValueError):
+        gcd_array(a)
+
+    a = [5, 25, 90]
+    assert gcd_array(a) == 5
