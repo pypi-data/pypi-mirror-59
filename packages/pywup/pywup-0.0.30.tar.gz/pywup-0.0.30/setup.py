1c3072b12d5401d99b3ba94b16e32fa64ee67c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:46 +0000

    Add package pywup/0.0.30/pywup-0.0.30.tar.gz

---
+++ packages/pywup/pywup-0.0.30.tar.gz/pywup-0.0.30/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywup",
+    version="0.0.30",
+    author="Diego Souza",
+    author_email="contact@wespa.com.br",
+    description="A small set of tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+        url="https://github.com/diegofps/pywup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['wup=pywup.wup:wup'],
+    },
+    install_requires=[
+        'colorcet', 'tqdm', 'PyYAML'
+    ]
+)
