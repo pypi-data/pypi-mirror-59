45952f8b01d2146b660a059a59319dfc3fe9d32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:27 +0000

    Add package kube-manifest-lint/0.1.0/kube-manifest-lint-0.1.0.tar.gz

---
+++ packages/kube-manifest-lint/kube-manifest-lint-0.1.0.tar.gz/kube-manifest-lint-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kube_manifest_lint']
+
+package_data = \
+{'': ['*'], 'kube_manifest_lint': ['schemas/v1.17.0/*']}
+
+install_requires = \
+['jsonschema>=3.2.0,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['kube-manifest-lint = kube_manifest_lint:main.main']}
+
+setup_kwargs = {
+    'name': 'kube-manifest-lint',
+    'version': '0.1.0',
+    'description': 'Kubernetes manifest linter using JSON Schema',
+    'long_description': None,
+    'author': 'Henning Jacobs',
+    'author_email': 'henning@jacobs1.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
