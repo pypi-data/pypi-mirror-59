cd70aacf916ab7cdbdf9f1a7fe241ca2cf9a19c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:24 +0000

    Add package baizhanMath3/1.0/baizhanMath3-1.0.tar.gz

---
+++ packages/baizhanMath3/baizhanMath3-1.0.tar.gz/baizhanMath3-1.0/baizhanMath3/demo1.py
@@ -0,0 +1,6 @@
+#cdoing=utf-8
+
+print("run in demo1")
+
+def add(a,b):
+    return a+b
---
+++ packages/baizhanMath3/baizhanMath3-1.0.tar.gz/baizhanMath3-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name="baizhanMath3",
+    version="1.0",
+    description="这是对外发布的数学模块，用于测试",
+    author="ypm",
+    author_email="86436284@qq.com",
+    py_modules=['baizhanMath3.demo1','baizhanMath3.demo2']
+)
