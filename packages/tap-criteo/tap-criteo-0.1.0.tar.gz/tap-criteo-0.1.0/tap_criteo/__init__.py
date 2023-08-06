4c68ef134451d1bf39579f227721896b03e0233c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package tap-criteo/0.1.0/tap-criteo-0.1.0.tar.gz

---
+++ packages/tap-criteo/tap-criteo-0.1.0.tar.gz/tap-criteo-0.1.0/tap_criteo/__init__.py
@@ -0,0 +1,36 @@
+"""Singer Tap to pull data from Criteo Marketing API."""
+import json
+
+import singer
+from singer import utils
+from tap_criteo.discover import do_discover
+from tap_criteo.sync import do_sync
+
+
+REQUIRED_CONFIG_KEYS = ["start_date", "client_id", "client_secret"]
+
+LOGGER = singer.get_logger()
+
+
+@utils.handle_top_exception(LOGGER)
+def main():
+    """CLI for Singer Tap."""
+    # Parse command line arguments
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+
+    # If discover flag was passed, run discovery mode and dump output to stdout
+    if args.discover:
+        catalog = do_discover()
+        print(json.dumps(catalog, indent=2))
+    # Otherwise run in sync mode
+    else:
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = do_discover()
+
+        do_sync(args.config, args.state, catalog)
+
+
+if __name__ == "__main__":
+    main()
