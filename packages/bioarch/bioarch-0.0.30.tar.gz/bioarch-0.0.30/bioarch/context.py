dac4101023dec4c771360b98c2eff828ffa02cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:41 +0000

    Add package bioarch/0.0.30/bioarch-0.0.30.tar.gz

---
+++ packages/bioarch/bioarch-0.0.30.tar.gz/bioarch-0.0.30/bioarch/context.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+
+import enum
+from enum import Enum
+import functools
+import logging
+from typing import Any, cast, Dict, Optional, Union
+
+
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+
+
+logger = logging.getLogger(__name__)
+
+
+@functools.total_ordering
+@enum.unique
+class CompassBearing(Enum):
+    NORTH      = 0  # noqa: E221
+    NORTH_EAST = 1
+    EAST       = 2  # noqa: E221
+    SOUTH_EAST = 3
+    SOUTH      = 4  # noqa: E221
+    SOUTH_WEST = 5
+    WEST       = 6  # noqa: E221
+    NORTH_WEST = 7
+
+    @staticmethod
+    def parse(value: Any) -> Optional['CompassBearing']:
+        if value is None:
+            return None
+        if type(value) == CompassBearing:  # pylint: disable=C0123
+            return cast(CompassBearing, value)
+        if not isinstance(value, str):
+            raise ValueError(f'Failed to parse {CompassBearing.__name__}: "{value}"')
+        value = value.upper()
+        for bearing in CompassBearing:
+            if value == bearing.name:
+                return bearing
+            if value == bearing.to_short_code():
+                return bearing
+        raise ValueError(f'Failed to parse {CompassBearing.__name__}: "{value}"')
+
+    def to_short_code(self):
+        return ''.join([p[0] for p in str(self.name).split('_')])
+
+    def __lt__(self, other):
+        if other is None:
+            return False
+        if isinstance(other, int):
+            other = CompassBearing(other)
+        if type(other) != type(self):  # pylint: disable=C0123
+            logger.warning('Attempt to compare: %s with %s', self, other)
+            raise NotImplementedError
+        return (self.value < other.value)  # pylint: disable=C0325,W0143
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}: {self}'
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def dtype():
+        return CategoricalDtype(categories=[s.name for s in CompassBearing], ordered=True)
+
+
+def parse_value(value):
+    if value is None:
+        return None
+    if isinstance(value, str) and value.upper() == 'NA':
+        return None
+    if value in (1, '1'):
+        return True
+    if value in (0, '0'):
+        return False
+    raise ValueError(f'Failed to parse Boolean value "{value}" for context')
+
+
+def parse_position(value):
+    if value is None:
+        return None
+    if isinstance(value, str) and value.upper() == 'NA':
+        return None
+    if value in ('0', 0):
+        return 'supine'
+    if value in ('1', 1):
+        return 'supine with flexed legs'
+    if value in ('2', 2):
+        return 'crouched'
+    if value in ('3', 3):
+        return 'crouched on left side'
+    if value in ('4', 4):
+        return 'crouched on right side'
+    if value in ('5', 5):
+        return 'laid on their stomach'
+    raise ValueError(f'Failed to parse position value "{value}" for context')
+
+
+KNOWN_GROUPS = {
+    'utilitarian'  : set(['knife', 'whetstone', 'awl', 'scissors', 'vessel', 'pot_sherd', 'flint', 'flakes', 'flint_flakes']),  # noqa: E203
+    'textile'      : set(['textile', 'needle', 'spindle_whorl']),  # noqa: E203
+    'equestrian'   : set(['equestrian', 'horse_equipment']),  # noqa: E203
+    'economic'     : set(['coins', 'scales']),  # noqa: E203
+    'organic_material': set(['animal_remains', 'charcoal', 'flower', 'shell', 'burned_bones']),
+    'appearance'   : set(['brooch', 'cloak ring', 'bracelet', 'beads', 'comb', 'buckle', 'mounts', 'cloak_fastener', 'iron_ring']),  # noqa: E203
+    'burial_container': set(['coffin', 'coffin_substitute']),
+    'weapons'      : set(['sword', 'axe', 'shield_boss', 'spear']),  # noqa: E203
+    'iron_fragment': set(['iron frafments', 'nails', 'iron_object', 'iron_nail/rivets']),
+    'miscellaneous': set(['lock', 'keys', 'thors_hammer', 'bronze_item', 'bronze_disk', 'quartz', 'unidentified_bronze', 'iron_pole']),
+}
+
+
+class Context(object):
+    """docstring for Context"""
+
+    def __init__(self, body_position, body_orientation: Optional[CompassBearing], tags: Dict[str, Optional[Union[str, int, bool, float]]]):
+        self.body_position = parse_position(body_position)
+
+        if body_orientation is not None and not isinstance(body_orientation, CompassBearing):
+            raise ValueError(f'Invalid body_orientation: "{body_orientation}"')
+        self.body_orientation = body_orientation
+
+        self.tags = {k: parse_value(v) for k, v in tags.items()}
+
+    @staticmethod
+    def empty():
+        return Context(None, None, {})
+
+    @staticmethod
+    def group(value):
+        value = value.lower()
+        groups = set()
+        for group_name, group in KNOWN_GROUPS.items():
+            if value in group:
+                groups.add(group_name)
+        return groups
+
+    def _to_pd_series(self, prefix, tags):  # pylint: disable=R0201
+        labels = [f'{prefix}{label}' for label in tags.keys()]
+        s = pd.Series([val for val in tags.values()], index=labels, copy=True)  # pylint: disable=R1721
+        s = s.append(pd.Series([s.max()], index=[f'{prefix}present']))
+        return s
+
+    def to_pd_data_frame(self, index):
+        simple_data = {
+            'id': pd.Series([index]),
+            'body_position': pd.Series([self.body_position]),
+        }
+
+        if self.body_orientation:
+            simple_data['body_orientation_cat'] = pd.Series([self.body_orientation.name], copy=True, dtype=CompassBearing.dtype())
+            simple_data['body_orientation_val'] = pd.Series([self.body_orientation.value], copy=True, dtype='Int64')
+
+        group_series = []
+        group_series.append(self._to_pd_series(f'all_', self.tags))
+        for group_name, group in KNOWN_GROUPS.items():
+            group_series.append(self._to_pd_series(f'{group_name}_', {k: v for k, v in self.tags.items() if k.lower() in group}))
+        groups_df = pd.DataFrame.from_dict({index: pd.concat(group_series)}, orient='index')
+
+        return pd.DataFrame.from_dict(simple_data).set_index('id').join(groups_df, on='id', how='outer')
+
+
+if __name__ == "__main__":
+    raise RuntimeError('No main available')
---
+++ packages/bioarch/bioarch-0.0.30.tar.gz/bioarch-0.0.30/bioarch/context_test.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+
+import unittest
+
+
+from .context import CompassBearing, Context
+
+
+class CompassBearingTest(unittest.TestCase):
+    def test_order(self):
+        self.assertEqual(sorted([CompassBearing.SOUTH, CompassBearing.NORTH]), [CompassBearing.NORTH, CompassBearing.SOUTH])
+        # None is sorted first
+        self.assertEqual(sorted([CompassBearing.SOUTH, CompassBearing.NORTH, None]), [None, CompassBearing.NORTH, CompassBearing.SOUTH])
+        # Numbers are sorted as if they where CompassBearing.value
+        self.assertEqual(sorted([0, 3, CompassBearing.EAST]), [0, CompassBearing.EAST, 3])
+        # Invalid numbers fail
+        with self.assertRaises(ValueError):
+            [CompassBearing.SOUTH, -1].sort()
+
+    def test_to_short_code(self):
+        self.assertEqual(CompassBearing.NORTH.to_short_code(), 'N')
+        self.assertEqual(CompassBearing.NORTH_EAST.to_short_code(), 'NE')
+        self.assertEqual(CompassBearing.SOUTH.to_short_code(), 'S')
+
+    def test_parse(self):
+        self.assertEqual(CompassBearing.parse('NORTH'), CompassBearing.NORTH)
+        self.assertEqual(CompassBearing.parse('N'), CompassBearing.NORTH)
+        self.assertEqual(CompassBearing.parse(None), None)
+
+
+class ContextTest(unittest.TestCase):
+    def test_constructor_position(self):
+        self.assertEqual(Context(0, None, {}).body_position, 'supine')
+
+        self.assertEqual(Context(None, None, {}).body_position, None)
+        self.assertEqual(Context('NA', None, {}).body_position, None)
+
+        with self.assertRaises(ValueError):
+            Context('foo', None, {})
+
+    def test_constructor_orientation(self):
+        self.assertEqual(Context(None, CompassBearing.NORTH, {}).body_orientation, CompassBearing.NORTH)
+        self.assertEqual(Context(None, None, {}).body_orientation, None)
+
+        with self.assertRaises(ValueError):
+            Context(None, 'NORTH', {})
+        with self.assertRaises(ValueError):
+            Context(None, 1, {})
+
+    def test_constructor_tags(self):
+        self.assertEqual(Context(None, None, {'thing': True}).tags['thing'], True)
+        self.assertEqual(Context(None, None, {'thing': False}).tags['thing'], False)
+
+        self.assertEqual(Context(None, None, {'thing': None}).tags['thing'], None)
+        self.assertEqual(Context(None, None, {'thing': 'NA'}).tags['thing'], None)
+        self.assertEqual(Context(None, None, {'thing': 'na'}).tags['thing'], None)
+
+        with self.assertRaises(ValueError):
+            Context(None, None, {'spear': 'foo'})
+
+        with self.assertRaises(ValueError):
+            Context(None, None, {'spear': 10})
+
+    def test_to_pd_data_frame(self):
+        context = Context(1,
+                          CompassBearing.NORTH,
+                          {'spear': True,
+                           'pots': 'NA',
+                           'knife': None})
+
+        df = context.to_pd_data_frame('id1')
+        self.assertEqual(df.to_json(orient='records'), '[{"body_position":"supine with flexed legs","body_orientation_cat":"NORTH","body_orientation_val":0,"all_spear":true,"all_pots":null,"all_knife":null,"all_present":true,"utilitarian_knife":null,"utilitarian_present":null,"textile_present":null,"equestrian_present":null,"economic_present":null,"organic_material_present":null,"appearance_present":null,"burial_container_present":null,"weapons_spear":true,"weapons_present":true,"iron_fragment_present":null,"miscellaneous_present":null}]')
+
+        context = Context(1,
+                          None,
+                          {'spear': True,
+                           'pots': 'NA',
+                           'knife': None})
+
+        df = context.to_pd_data_frame('id1')
+        self.assertEqual(df.to_json(orient='records'), '[{"body_position":"supine with flexed legs","all_spear":true,"all_pots":null,"all_knife":null,"all_present":true,"utilitarian_knife":null,"utilitarian_present":null,"textile_present":null,"equestrian_present":null,"economic_present":null,"organic_material_present":null,"appearance_present":null,"burial_container_present":null,"weapons_spear":true,"weapons_present":true,"iron_fragment_present":null,"miscellaneous_present":null}]')
+
+    def test_known_context_to_group(self):
+        known_context_keys = {'knife': set(['utilitarian']),  # Should not be a weapons
+                              'Whetstone': set(['utilitarian']),
+                              'Awl': set(['utilitarian']),
+                              'SCISSORS': set(['utilitarian']),
+                              'iron_object': set(['iron_fragment']),
+                              'textile': set(['textile']),
+                              'spindle_whorl': set(['textile']),
+                              'brooch': set(['appearance']),
+                              'bracelet': set(['appearance']),
+                              'lock': set(['miscellaneous']),
+                              'keys': set(['miscellaneous']),
+                              'Sword': set(['weapons']),
+                              'axe': set(['weapons']),
+                              'Iron_pole': set(['miscellaneous']),  # Should not be iron_fragment or weapons
+                              'spear': set(['weapons']),
+                              'shield_boss': set(['weapons']),
+                              'cloak_fastener': set(['appearance']),
+                              'iron_ring': set(['appearance']),  # SHould not be iron_fragment
+                              'iron_nail/rivets': set(['iron_fragment']),
+                              'transport': set(),
+                              'Horse_equipment': set(['equestrian']),
+                              'coffin_substitute': set(['burial_container']),
+                              'coffin': set(['burial_container']),
+                              'Mounts': set(['appearance']),
+                              'thors_hammer': set(['miscellaneous']),
+                              'beads': set(['appearance']),
+                              'unidentified_bronze': set(['miscellaneous']),
+                              'Bronze_disk': set(['miscellaneous']),
+                              'SCALES': set(['economic']),
+                              'coins': set(['economic']),
+                              'VESSEL': set(['utilitarian']),
+                              'comb': set(['appearance']),
+                              'buckle': set(['appearance']),
+                              'pot_sherd': set(['utilitarian']),
+                              'FLINT_FLAKES': set(['utilitarian']),
+                              'charcoal': set(['organic_material']),
+                              'burned_bones': set(['organic_material']),
+                              'quartz': set(['miscellaneous']),
+                              'shell': set(['organic_material']),
+                              'needle': set(['textile']),
+                              'Flower': set(['organic_material']),
+                              'animal_remains': set(['organic_material']),
+                              'Body_Position': set(),
+                              'disturbed': set(),
+                              'decapitation': set(),
+                              'double_grave': set(),
+                              'Body_orientation': set(),
+                              'Stone_layer': set()}
+        for known_key, known_groups in known_context_keys.items():
+            actual_groups = Context.group(known_key)
+            self.assertEqual(actual_groups, known_groups, msg=f'{known_key} -> {actual_groups} != {known_groups}')
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
