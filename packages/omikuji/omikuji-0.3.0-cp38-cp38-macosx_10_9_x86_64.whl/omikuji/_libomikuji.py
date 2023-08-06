383bb5eb38e7e366af40108a73f74145b93f438f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:09 +0000

    Add package omikuji/0.3.0/omikuji-0.3.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/omikuji/omikuji-0.3.0-cp38-cp38-macosx_10_9_x86_64.whl/omikuji/_libomikuji.py
@@ -0,0 +1,8 @@
+# auto-generated file
+__all__ = ['lib', 'ffi']
+
+import os
+from omikuji._libomikuji__ffi import ffi
+
+lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), '_libomikuji__lib.so'), 130)
+del os
