67772540eee43d318622e783c92ffd1b082fcdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package algokit/0.1.0/algokit-0.1.0.tar.gz

---
+++ packages/algokit/algokit-0.1.0.tar.gz/algokit-0.1.0/README.md
@@ -0,0 +1,13 @@
+# algokit
+a python module published by Pypl
+
+#### check
+`python setup.py check`
+
+#### package
+`python setup.py sdist bdist_wheel`
+
+#### upload to pip
+`python setup.py register sdist upload  (deprecated)`    
+or  
+`twine upload dist/*`
---
+++ packages/algokit/algokit-0.1.0.tar.gz/algokit-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algokit",
+    version="0.1.0",
+    keywords=["pip", "algokit", "algorithm"],
+    description="common utilities and algorithm",
+    long_description="common utilities and algorithm",
+
+    url="https://github.com/wkgreat/algokit",
+    author="ke wang",
+    author_email="wkgreat@outlook.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['pandas','geographiclib'],
+    data_files=[("algokit/geo", ["algokit/geo/adcodes.csv"])]
+)
