50d121443cf0ce9ba5d9ef7289d36ead1adf7e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:56 +0000

    Add package freshsalessdk/0.0.4/freshsalessdk-0.0.4-py3-none-any.whl

---
+++ packages/freshsalessdk/freshsalessdk-0.0.4-py3-none-any.whl/freshsalessdk/freshsalessdk.py
@@ -0,0 +1,153 @@
+import requests
+import json
+import time
+import copy
+
+class APIBase:
+    def __init__(self, resource_type, domain, api_key, resource_type_singular=None, default_params={}):
+        self.resource_type = resource_type
+        self.resource_type_singular = resource_type_singular
+        if self.resource_type_singular is None:
+        # best guess is to remove last letter
+            self.resource_type_singular = self.resource_type[0:-1]
+        self.domain = domain
+        self.api_key = api_key
+        self.default_params = default_params
+
+    def _get(self, path, params={}):
+        """Create a HTTP GET request.
+
+        Parameters:
+            params (dict): HTTP GET parameters for the wanted API.
+            path (str): path for the wanted API. Should start with a '/'
+
+        Returns:
+            A response from the request (dict).
+        """
+        assert path is not None
+        assert path.startswith('/')
+
+        api_headers = {'Authorization': f'Token token={self.api_key}'}
+        api_params = copy.deepcopy(self.default_params)
+
+        for k in params:
+            # ignore all unused params
+            if not params[k] is None:
+                p = params[k]
+
+                # convert boolean to lowercase string
+                if isinstance(p, bool):
+                    p = str(p).lower()
+
+                api_params[k] = p
+
+#        print(f'passing params {api_params}')
+        response = requests.get(
+            f'https://{self.domain}.freshsales.io{path}', 
+            headers=api_headers, 
+            params=api_params
+        )
+        # raise exception if not 200
+        response.raise_for_status()
+
+        result = json.loads(response.text)
+#        print(f'result = {result}')
+        return result
+
+    def _get_views(self):
+        return self._get(path=f'/{self.resource_type}/filters')['filters']
+
+    @staticmethod
+    def _find_obj_by_id(objs, id):
+#        print(f'trying to find {id} in {objs}')
+        for o in objs:
+            if o['id'] == id:
+                return o
+        return None
+
+    @staticmethod
+    def _normalize(obj, container):
+        users = []
+        sales_accounts = []
+        deal_stages = []
+        if 'users' in container:
+            users = container['users']
+        if 'sales_accounts' in container:
+            sales_accounts = container['sales_accounts']
+        if 'deal_stages' in container:
+            deal_stages = container['deal_stages']
+        if 'owner_id' in obj:
+            owner = APIBase._find_obj_by_id(objs=users, id=obj['owner_id'])
+            obj['owner'] = owner
+        if 'sales_account_id' in obj:
+            sales_account = APIBase._find_obj_by_id(objs=sales_accounts, id=obj['sales_account_id'])
+            obj['sales_account'] = sales_account
+        if 'deal_stage_id' in obj:
+            deal_stage = APIBase._find_obj_by_id(objs=deal_stages, id=obj['deal_stage_id'])
+            obj['deal_stage'] = deal_stage
+
+
+    def _get_all_generator(self, view_id):
+        page = 1
+        while True:
+            start_time = time.time()
+            params = {'page': page}
+            res = self._get(path=f'/{self.resource_type}/view/{view_id}', params=params)
+            total_pages = res['meta']['total_pages']
+            end_time = time.time()
+#            print(f'got page {page} of {total_pages} in {end_time-start_time} seconds')
+        
+            objs = res[self.resource_type]
+            for obj in objs:
+                APIBase._normalize(obj=obj, container=res)
+                yield obj
+
+            page = page + 1
+            if page > total_pages:
+                break
+
+    def _get_by_id(self, id):
+        res = self._get(path=f'/{self.resource_type}/{id}')
+        v = res[self.resource_type_singular]
+        APIBase._normalize(obj=v, container=res)
+        return v
+
+    def get_views(self):
+        return self._get_views()
+
+    def get_all_generator(self, view_id):
+        return self._get_all_generator(view_id=view_id)
+
+    def get_all(self, view_id):
+        return list(self.get_all_generator(view_id=view_id))
+    
+    def get(self, id):
+        return self._get_by_id(id=id)
+
+
+class Contacts(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'sales_accounts,appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='contacts', default_params=default_params)
+
+    def get_activities(self, id):
+        return self._get(f'/contacts/{id}/activities')['activities']
+
+
+class Accounts(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'appointments,owner', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='sales_accounts', default_params=default_params)
+
+
+class Deals(APIBase):
+    def __init__(self, domain, api_key):
+        default_params = {'include': 'sales_account,appointments,owner,deal_stage', 'sort': 'updated_at', 'sort_type': 'desc'}
+        super().__init__(domain=domain, api_key=api_key, resource_type='deals', default_params=default_params)
+
+
+class FreshsalesSDK:
+    def __init__(self, domain, api_key):
+        self.contacts = Contacts(domain=domain, api_key=api_key)
+        self.accounts = Accounts(domain=domain, api_key=api_key)
+        self.deals = Deals(domain=domain, api_key=api_key)
