e70c318f683823c0fec8b9223c8b24e33d48c966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:10 +0000

    Add package skillbridge/1.0.11/skillbridge-1.0.11.tar.gz

---
+++ packages/skillbridge/skillbridge-1.0.11.tar.gz/skillbridge-1.0.11/skillbridge/client/objects.py
@@ -0,0 +1,103 @@
+from typing import Any, List
+
+from .hints import SkillCode
+from .channel import Channel
+from .extract import method_map
+from .functions import RemoteFunction, RemoteMethod
+from . import translator
+
+
+def is_jupyter_magic(attribute: str) -> bool:
+    ignore = {
+        '_ipython_canary_method_should_not_exist_',
+        '_ipython_display_',
+        '_repr_mimebundle_',
+        '_repr_html_',
+        '_repr_markdown_',
+        '_repr_svg_',
+        '_repr_png_',
+        '_repr_pdf_',
+        '_repr_jpeg_',
+        '_repr_latex_',
+        '_repr_json_',
+        '_repr_javascript_',
+        '_rapped',
+        '__wrapped__',
+        '__call__',
+    }
+    return attribute in ignore
+
+
+class RemoteObject:
+    _attributes = {'_channel', '_variable', '_methods'}
+    _method_map = method_map()
+
+    def __init__(self, channel: Channel, variable: str) -> None:
+        self._channel = channel
+        self._variable = SkillCode(variable)
+
+        object_type, _ = variable[5:].split('_', maxsplit=1)
+        self._methods = RemoteObject._method_map.get(object_type, {})
+
+    @property
+    def skill_id(self) -> str:
+        return self._variable.split('_')[-1]
+
+    def _replicate(self, variable: str) -> 'RemoteObject':
+        return RemoteObject(self._channel, variable)
+
+    def __repr_skill__(self) -> SkillCode:
+        return SkillCode(self._variable)
+
+    def _send(self, command: SkillCode) -> Any:
+        return self._channel.send(command).strip()
+
+    def __str__(self) -> str:
+        type_, address = self._variable[5:].split('_', maxsplit=1)
+        if type_.startswith('db'):
+            type_ = self.obj_type or type_
+        elif type_.startswith('dd'):
+            dd_type = self.type
+            type_ = dd_type.name[2:-4] if dd_type else type_
+        return f"<remote {type_}@{address}>"
+
+    __repr__ = __str__
+
+    def __dir__(self) -> List[str]:
+        response = self._send(translator.skill_help(self._variable))
+        attributes = translator.skill_help_to_list(response)
+        return attributes
+
+    def __getattr__(self, key: str) -> Any:
+        if is_jupyter_magic(key):
+            raise AttributeError(key)
+
+        if key in self._methods:
+            func = RemoteFunction(self._channel, self._methods[key], self._replicate)
+            return RemoteMethod(self, func)
+
+        result = self._send(translator.skill_getattr(self._variable, key))
+        return translator.skill_value_to_python(result, self._replicate)
+
+    def __setattr__(self, key: str, value: Any) -> None:
+        if key in RemoteObject._attributes:
+            return super().__setattr__(key, value)
+
+        if key in self._methods:
+            raise TypeError("remote method is readonly")
+
+        result = self._send(translator.skill_setattr(self._variable, key, value))
+        translator.skill_value_to_python(result, self._replicate)
+
+    def getdoc(self) -> str:
+        return "Properties:\n- " + '\n- '.join(dir(self))
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, RemoteObject):
+            return self._variable == other._variable
+        return NotImplemented
+
+    def __ne__(self, other: Any) -> bool:
+        if isinstance(other, RemoteObject):
+            return self._variable != other._variable
+        return NotImplemented
---
+++ packages/skillbridge/skillbridge-1.0.11.tar.gz/skillbridge-1.0.11/skillbridge/client/translator.py
@@ -0,0 +1,130 @@
+from typing import NoReturn, Any, List, Iterable, cast
+from re import sub
+from json import dumps
+from warnings import warn_explicit
+
+from .hints import Replicator, SkillCode, Skill, Symbol
+
+
+class ParseError(Exception):
+    pass
+
+
+def _raise_error(message: str) -> NoReturn:
+    raise ParseError(message)
+
+
+def _show_warning(message: str, result: Any) -> Any:
+    for i, line in enumerate(message.splitlines(keepends=False)):
+        warn_explicit(line.lstrip("*WARNING*"), UserWarning, "Skill response", i)
+
+    return result
+
+
+def skill_value_to_python(string: str, replicate: Replicator) -> Skill:
+    return eval(  # type: ignore
+        string,
+        {'Remote': replicate, 'Symbol': Symbol, 'error': _raise_error, 'warning': _show_warning},
+    )
+
+
+def snake_to_camel(snake: str) -> str:
+    if snake.startswith('_') or '_' not in snake:
+        return snake
+    return snake[0] + snake.replace('_', ' ').title().replace(' ', '')[1:]
+
+
+def camel_to_snake(camel: str) -> str:
+    if camel[0].isupper():
+        return camel
+
+    return sub(r'(?<=[a-z])([A-Z])|([A-Z][a-z])', r'_\1\2', camel).lower()
+
+
+def python_value_to_skill(value: Skill) -> SkillCode:
+    try:
+        return value.__repr_skill__()  # type: ignore
+    except AttributeError:
+        pass
+
+    if isinstance(value, dict):
+        items = ' '.join(f"'{key} {python_value_to_skill(value)}" for key, value in value.items())
+        return SkillCode(f'list(nil {items})')
+
+    if value is False or value is None:
+        return SkillCode('nil')
+
+    if value is True:
+        return SkillCode('t')
+
+    if isinstance(value, (int, float, str)):
+        return SkillCode(dumps(value))
+
+    if isinstance(value, (list, tuple)):
+        inner = ' '.join(python_value_to_skill(item) for item in value)
+        return SkillCode(f'(list {inner})')
+
+    type_ = type(value).__name__
+    raise RuntimeError(f"Cannot convert object {type_!r} to skill.") from None
+
+
+def _not_implemented(string: str) -> Replicator:
+    def inner(_name: str) -> NoReturn:
+        raise NotImplementedError(f"Failed to parse skill literal {string!r}")
+
+    return inner
+
+
+def skill_help(obj: SkillCode) -> SkillCode:
+    parts = ' '.join((f'{obj}->?', f'{obj}->systemHandleNames', f'{obj}->userHandleNames'))
+    code = f'mapcar(lambda((attr) sprintf(nil "%s" attr)) nconc({parts}))'
+    return SkillCode(code)
+
+
+def skill_help_to_list(code: str) -> List[str]:
+    attributes = skill_value_to_python(code, _not_implemented("help list"))
+    return [camel_to_snake(attr) for attr in cast(List[str], attributes)]
+
+
+def skill_getattr(obj: SkillCode, key: str) -> SkillCode:
+    return build_skill_path([obj, key])
+
+
+def skill_setattr(obj: SkillCode, key: str, value: Any) -> SkillCode:
+    code = build_skill_path([obj, key])
+    value = python_value_to_skill(value)
+    return SkillCode(f'{code} = {value}')
+
+
+def call(func_name: str, *args: Skill, **kwargs: Skill) -> SkillCode:
+    args_code = ' '.join(map(python_value_to_skill, args))
+    kw_keys = map(snake_to_camel, kwargs)
+    kw_values = map(python_value_to_skill, kwargs.values())
+    kwargs_code = ' '.join(f'?{key} {value}' for key, value in zip(kw_keys, kw_values))
+    return SkillCode(f'{func_name}({args_code} {kwargs_code})')
+
+
+def build_skill_path(components: Iterable[str]) -> SkillCode:
+    it = iter(components)
+    path = snake_to_camel(str(next(it)))
+
+    for component in it:
+        if isinstance(component, int):
+            path = f'(nth {component} {path})'
+        else:
+            path = f'{path}->{snake_to_camel(component)}'
+
+    return SkillCode(path)
+
+
+def build_python_path(components: Iterable[str]) -> SkillCode:
+    it = iter(components)
+    path = str(next(it))
+
+    for component in it:
+        if isinstance(component, int):
+            path = f'{path}[{component}]'
+        else:
+            path = f'{path}.{component}'
+
+    return SkillCode(path)
