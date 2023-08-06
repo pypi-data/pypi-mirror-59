a8278d45b5cf7675e8a97866612fe6523449516c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:48 +0000

    Add package dna-designer/0.3.3/dna_designer-0.3.3.tar.gz

---
+++ packages/dna-designer/dna_designer-0.3.3.tar.gz/dna_designer-0.3.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dna_designer",
+    version="0.3.3",
+    author="Keoni Gandall",
+    author_email="koeng101@gmail.com",
+    description="A package that assists in designing DNA",
+    package_data={'': ['./dna_designer/data','./dna_designer/data/*']},
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
