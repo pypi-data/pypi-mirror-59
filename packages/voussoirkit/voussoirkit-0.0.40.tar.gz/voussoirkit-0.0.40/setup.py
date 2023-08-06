b15dee14db78ad8b9217ce222b74cfb843c85dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package voussoirkit/0.0.40/voussoirkit-0.0.40.tar.gz

---
+++ packages/voussoirkit/voussoirkit-0.0.40.tar.gz/voussoirkit-0.0.40/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='voussoirkit',
+    packages=setuptools.find_packages(),
+    version='0.0.40',
+    author='voussoir',
+    author_email='pypi@voussoir.net',
+    description='voussoir\'s toolkit',
+    url='https://github.com/voussoir/voussoirkit',
+    install_requires=[
+        'pyperclip',
+        'pywin32;platform_system=="Windows"',
+        'winshell;platform_system=="Windows"',
+    ]
+)
