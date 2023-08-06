de4402be38210fc3c48906e14422f5c95b74c35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:36 +0000

    Add package TOPSIS-ANALYSIS-kriti/1.0.3/TOPSIS_ANALYSIS_kriti-1.0.3.tar.gz

---
+++ packages/TOPSIS-ANALYSIS-kriti/TOPSIS_ANALYSIS_kriti-1.0.3.tar.gz/TOPSIS_ANALYSIS_kriti-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="TOPSIS_ANALYSIS_kriti",
+    version="1.0.3",
+    description="A Python package to evaluate ranks of a Multiple Criteria Decision Making Problem(MCDM) using Technique for Order of Preference by Similarity to Ideal Solution(TOPSIS)",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Kriti Pandey",
+    author_email="kritip105@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TOPO_101703292"],
+    include_package_data=True,
+    install_requires=["numpy","pandas"],
+    entry_points={
+        "console_scripts": [
+            "TOPO-101703292=TOPO_101703292.3292:main",
+        ]
+    },
+)
