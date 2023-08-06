0bd7a6f824ecb94b32290a68682d8ca5c60c3eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:59 +0000

    Add package nlg/0.1.2rc1/nlg-0.1.2rc1.tar.gz

---
+++ packages/nlg/nlg-0.1.2rc1.tar.gz/nlg-0.1.2rc1/nlg/__init__.py
@@ -0,0 +1,16 @@
+import sys
+
+__version__ = '0.1.2rc1'
+
+try:
+    __NLG_SETUP__
+except NameError:
+    __NLG_SETUP__ = False
+
+
+if __NLG_SETUP__:
+    sys.stderr.write('Partial import of nlg during the build process.\n')
+else:
+    from .search import templatize  # NOQA: F401
+    from .narrative import Narrative  # NOQA: F401
+    __all__ = ['templatize', 'Narrative']
