96666eaaa7e61b9a4dee367316b574d5f5662a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:31 +0000

    Add package kragle/0.1.1/kragle-0.1.1.tar.gz

---
+++ packages/kragle/kragle-0.1.1.tar.gz/kragle-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['kragle']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'kragle',
+    'version': '0.1.1',
+    'description': 'glue and tape',
+    'long_description': '# kragle\n',
+    'author': 'Arni Inaba Kjartansson',
+    'author_email': 'arni@inaba.is',
+    'url': 'https://github.com/arni-inaba/kragle',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
