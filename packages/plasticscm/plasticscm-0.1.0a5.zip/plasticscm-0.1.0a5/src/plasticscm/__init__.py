d30932e37be00463c438e2bf2728125515c72676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:53 +0000

    Add package plasticscm/0.1.0a5/plasticscm-0.1.0a5.zip

---
+++ packages/plasticscm/plasticscm-0.1.0a5.zip/plasticscm-0.1.0a5/src/plasticscm/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2019-2020 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/zlib
+
+from .__about__  import *  # noqa
+from .plastic    import *  # noqa
+from .exceptions import *  # noqa
+del __about__, plastic, config, model
+#print(dir())
