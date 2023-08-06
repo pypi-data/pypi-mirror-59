66c42f3c1c4c526e50bf9ff6809326eaf56d9f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:53 +0000

    Add package EleganceReport/0.1.9/EleganceReport-0.1.9.tar.gz

---
+++ packages/EleganceReport/EleganceReport-0.1.9.tar.gz/EleganceReport-0.1.9/EleganceReport/__init__.py
@@ -0,0 +1,12 @@
+"""
+@Version: 1.0
+@Project: EleganceReport
+@Author: andy
+@Data: 2019/11/16 
+@File: __init__.py
+"""
+
+from .EleganceReport import EleganceReport
+
+
+__all__ = ['EleganceReport']
