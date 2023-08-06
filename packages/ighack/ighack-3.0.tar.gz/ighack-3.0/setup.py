3a205e5ff97a5fd084b698b11e441eef28e143ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:08 +0000

    Add package ighack/3.0/ighack-3.0.tar.gz

---
+++ packages/ighack/ighack-3.0.tar.gz/ighack-3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+name='ighack',
+version='3.0',
+author='Nasir Ali',
+author_email='nasiralis1731@gmail.com',
+#author_url='https://facebook.com/nasir.xo',
+packages=['ighack'],
+scripts=['bin/ighack',],
+install_requires=['igramscraper',],
+)
+
+
+
+
