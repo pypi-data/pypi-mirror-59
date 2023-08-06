07f7fbc5d1b89f0dd9b37205e22ac73acdfdb506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package dophon-properties/0.1.4.post4/dophon_properties-0.1.4.post4-py3-none-any.whl

---
+++ packages/dophon-properties/dophon_properties-0.1.4.post4-py3-none-any.whl/dophon_properties/cloud/client/default_properties.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+from ..utils import DictObject
+
+# 此处为微服务服务端配置
+center_type = 'DOPHON'
+cloud_debug_trace = False
+client = DictObject({
+    'center': ','.join(['http://127.0.0.1:8761/eureka']),
+    'host': '127.0.0.1',
+    'prefer_ip': False
+})
+
+# 日志打印组件
+log_types = 'DOPHON'
---
+++ packages/dophon-properties/dophon_properties-0.1.4.post4-py3-none-any.whl/dophon_properties/dophon/default_properties.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import os
+
+"""
+配置集合
+author:CallMeE
+date:2018-06-01
+"""
+
+project_root = os.getcwd()
+
+# 服务器相关配置
+server_threaded = False  # 服务器多线程开关
+server_processes = 1  # 服务器多进程处理
+
+debug_trace = False  # 调试跟踪记录
+
+# 此为开启ip防火墙模式(1秒不超过50次请求,60秒解冻)
+ip_count = False
+
+# 此处为服务器配置
+host = '127.0.0.1'
+port = 88
+ssl_context = 'adhoc'
+
+# 此处为路由文件夹配置
+blueprint_path = ['/routes']  # route model dir path
+
+# 此处为日志配置
+if debug_trace:
+    logger_config = {
+        # 'filename': 'app.log',
+        # 'level': 'logging.DEBUG',
+        'format': '%(levelname)s : <%(module)s> (%(asctime)s) ==> %(filename)s {%(funcName)s} [line:%(lineno)d] ::: %(message)s',
+        'datefmt': '%Y-%m-%d %H:%M:%S'
+    }
+
+# 错误信息类型配置
+error_info = 'HTML'
+
+# 组件路径
+components_path = ['/']
+
+# 日志打印组件
+log_types = 'DOPHON'
