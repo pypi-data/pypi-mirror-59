db4d9bfbbc7edb6442f8afd20edc5a4ba02f199e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:46 +0000

    Add package LeanTurenester/1.1.0/LeanTurenester-1.1.0.tar.gz

---
+++ packages/LeanTurenester/LeanTurenester-1.1.0.tar.gz/LeanTurenester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'LeanTurenester',
+	version = '1.1.0',
+	py_modules = ['LeanTurenester'],
+	author = 'taewankim',
+	author_email = "kingwan1101@gmail.com",
+	url = 'http://wani111.github.io/web1',
+	description = 'A simple printer of nested lists',
+)
