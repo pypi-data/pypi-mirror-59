24a1d03b3993f43b3b6175cd3be9cb41b138aee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:50 +0000

    Add package polyaxon-schemas/0.6.0/polyaxon-schemas-0.6.0.tar.gz

---
+++ packages/polyaxon-schemas/polyaxon-schemas-0.6.0.tar.gz/polyaxon-schemas-0.6.0/README.md
@@ -0,0 +1,14 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE)
+[![Build Status](https://travis-ci.org/polyaxon/polyaxon-schemas.svg?branch=master)](https://travis-ci.org/polyaxon/polyaxon-schemas)
+[![PyPI version](https://badge.fury.io/py/polyaxon-schemas.svg)](https://badge.fury.io/py/polyaxon-schemas)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/3bf5388afaa141b19e4cfc4fe58876c0)](https://www.codacy.com/app/polyaxon/polyaxon-schemas?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=polyaxon/polyaxon-schemas&amp;utm_campaign=Badge_Grade)
+[![Slack](https://img.shields.io/badge/chat-on%20slack-aadada.svg?logo=slack&longCache=true)](https://join.slack.com/t/polyaxon/shared_invite/enQtMzQ0ODc2MDg1ODc0LWY2ZTdkMTNmZjBlZmRmNjQxYmYwMTBiMDZiMWJhODI2ZTk0MDU4Mjg5YzA5M2NhYzc5ZjhiMjczMDllYmQ2MDg)
+
+# Polyaxon-schemas
+
+Schema definitions and validation for Polyaxon.
+
+
+# License
+
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Fpolyaxon%2Fpolyaxon-schemas.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2Fpolyaxon%2Fpolyaxon-schemas?ref=badge_large)
---
+++ packages/polyaxon-schemas/polyaxon-schemas-0.6.0.tar.gz/polyaxon-schemas-0.6.0/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+def read_readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='polyaxon-schemas',
+      version='0.6.0',
+      description='Schema definitions and validation for Polyaxon.',
+      long_description=read_readme(),
+      long_description_content_type="text/markdown",
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mourad@polyaxon.com',
+      author='Mourad Mourafiq',
+      author_email='mourad@polyaxon.com',
+      url='https://github.com/polyaxon/polyaxon-schemas',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'tensorFlow',
+          'deep-learning',
+          'machine-learning',
+          'data-science',
+          'neural-networks',
+          'artificial-intelligence',
+          'ai',
+          'reinforcement-learning',
+          'kubernetes',
+      ],
+      install_requires=[
+          'hestia==0.6.0',
+          'Jinja2~=2.10.1',
+          'marshmallow==3.0.0rc5',
+          'numpy>=1.15.2',
+          'python-dateutil>=2.7.3',
+          'pytz>=2018.9',
+          'rhea==0.5.5'
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest})
