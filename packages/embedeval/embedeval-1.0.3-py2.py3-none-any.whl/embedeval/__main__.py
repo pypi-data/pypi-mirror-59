0b45734ddeccbd58c6c680a306b9bf591c615d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:12 +0000

    Add package embedeval/1.0.3/embedeval-1.0.3-py2.py3-none-any.whl

---
+++ packages/embedeval/embedeval-1.0.3-py2.py3-none-any.whl/embedeval/__main__.py
@@ -0,0 +1,15 @@
+"""
+embedeval
+~~~~~~~~~
+
+NLP Embedding Evaluation Tool
+
+:copyright: (c) 2019 by Timo Furrer <tuxtimo@gmail.com>
+:license: MIT, see LICENSE for more details.
+"""
+
+from embedeval.cli import cli  # pragma: no cover
+
+
+if __name__ == "__main__":
+    cli()
