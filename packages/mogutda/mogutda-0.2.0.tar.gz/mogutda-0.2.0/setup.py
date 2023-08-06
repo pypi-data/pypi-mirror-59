02d60ee64d211fa519a98efccd24cfc89e31014d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:38 +0000

    Add package mogutda/0.2.0/mogutda-0.2.0.tar.gz

---
+++ packages/mogutda/mogutda-0.2.0.tar.gz/mogutda-0.2.0/setup.py
@@ -0,0 +1,48 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def package_description():
+    text = open('README.md', 'r').read()
+    startpos = text.find('## Introduction')
+    return text[startpos:]
+
+
+setup(name='mogutda',
+      version="0.2.0",
+      description="Topological Data Analysis in Python",
+      long_description=package_description(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Education",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research"
+      ],
+      keywords="mogutda numerics topology data",
+      url="https://github.com/stephenhky/MoguTDA",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['mogutda',],
+      install_requires=[
+          'numpy>=1.16.0', 'scipy>=1.2.0', 'networkx>=2.0',
+      ],
+      tests_require=[
+          'unittest2',
+      ],
+      include_package_data=True,
+      test_suite="test",
+      zip_safe=False)
+
