cfed3047c47a33d6c27a236d17e6468830dba5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:32 +0000

    Add package mwcli/0.0.3/mwcli-0.0.3-py2.py3-none-any.whl

---
+++ packages/mwcli/mwcli-0.0.3-py2.py3-none-any.whl/mwcli/about.py
@@ -0,0 +1,10 @@
+__name__ = "mwcli"
+__version__ = "0.0.3"
+__author__ = "Aaron Halfaker"
+__author_email__ = "aaron.halfaker@gmail.com"
+__description__ = "Utilities for processing MediaWiki on the command line."
+__license__ = "MIT"
+__url__ = "https://github.com/mediawiki-utilities/python-mwcli"
+
+all = [__name__, __version__, __author__, __author_email__, __description__,
+       __license__, __url__]
---
+++ packages/mwcli/mwcli-0.0.3-py2.py3-none-any.whl/mwcli/files/__init__.py
@@ -0,0 +1,5 @@
+from .functions import (concat, reader, writer, output_dir_path,
+                        normalize_path, normalize_dir)
+
+__all__ = (concat, reader, writer, output_dir_path,
+           normalize_path, normalize_dir)
