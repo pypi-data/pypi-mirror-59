0711d5065e8f2888201004c6c5fef5d8d4e43c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:21 +0000

    Add package django-elasticsearch-model-binder/0.0.1/django_elasticsearch_model_binder-0.0.1-py3-none-any.whl

---
+++ packages/django-elasticsearch-model-binder/django_elasticsearch_model_binder-0.0.1-py3-none-any.whl/django_elasticsearch_model_binder/__init__.py
@@ -0,0 +1,6 @@
+from django_elasticsearch_model_binder.mixins import (
+    ESModelBinderMixin, ESQuerySetMixin
+)
+from django_elasticsearch_model_binder.utils import ExtraModelFieldBase
+
+__all__ = ['ESModelBinderMixin', 'ESQuerySetMixin', 'ExtraModelFieldBase']
