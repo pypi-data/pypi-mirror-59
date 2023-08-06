1f192cedc14a073decfaeecdb48b56ae5184d381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:21 +0000

    Add package sspo-db/4.3.2/sspo_db-4.3.2-py3-none-any.whl

---
+++ packages/sspo-db/sspo_db-4.3.2-py3-none-any.whl/sspo_db/application/process/application.py
@@ -0,0 +1,21 @@
+from sspo_db.model.process.models import Sprint
+from sspo_db.service.process.service import SprintService
+
+class ApplicationSprint():
+
+    def __init__(self):
+        self.sprint_service = SprintService()
+    
+    def create (self, name, start_date, end_date, scrum_process):
+        
+        sprint = Sprint()
+        sprint.name = name
+        sprint.description = name
+        sprint.startDate = start_date
+        sprint.endDate = end_date
+        sprint.scrum_process = scrum_process
+        
+        self.sprint_service.create(sprint)
+        
+        return sprint
+
