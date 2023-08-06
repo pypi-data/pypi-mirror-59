90af5f6897bc648516e73a0383c67fb816157d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:17 +0000

    Add package photo-grid/0.2.47/photo_grid-0.2.47-py3-none-any.whl

---
+++ packages/photo-grid/photo_grid-0.2.47-py3-none-any.whl/grid/__init__.py
@@ -0,0 +1,40 @@
+__author__ = "Chunpeng James Chen"
+__version__ = "0.2.0"
+__update__ = "Oct 29, 2019"
+
+from sys import platform
+
+# self imports
+from .grid import *
+
+print("=========*=========*=========*=========*=========*=========")
+print("                     Welcome to GRID!")
+print("    Try 'python -m grid' in Terminel to launch GRID GUI,")
+print("             since CLT version is not ready yet.")
+print("=========*=========*=========*=========*=========*=========")
+
+"""
+Update Log
+
+- Oct 29, 2019 (0.2.0)
+    * Add progress bars
+    * Enhance the support for low resolution monitors
+    * Improve UI
+ 
+- Oct 22, 2019 (0.1.3)
+    * Optimize default setting of refining parameters
+    * Fix wrong angle detection
+    * Minor bug fixes
+
+- Oct 19, 2019 (0.1.2)
+    * Support rhombus field layout
+    * Bug fixes
+
+- Sep 17, 2019 (0.0.16)
+    * Improve memory efficiency on Windows OS
+
+- Sep 12, 2019 (0.0.15)
+    * Fix problems wiht fixed segmentation
+    * Organize file structure
+    * Add dark mode
+"""
