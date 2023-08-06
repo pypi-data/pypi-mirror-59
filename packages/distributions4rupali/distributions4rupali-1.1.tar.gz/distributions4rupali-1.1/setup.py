a1a0cd090b0e5e7cb2d8165e21ce7ad566aebbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:46 +0000

    Add package distributions4rupali/1.1/distributions4rupali-1.1.tar.gz

---
+++ packages/distributions4rupali/distributions4rupali-1.1.tar.gz/distributions4rupali-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions4rupali',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions4rupali'],
+      author='Rupali Sinha',
+      email='rupalisinha23@gmail.com',
+      zip_safe=False)
