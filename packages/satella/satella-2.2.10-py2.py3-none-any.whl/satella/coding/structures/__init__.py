b532d2a65ed3119e5adfb08fba7944428aeaf0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package satella/2.2.10/satella-2.2.10-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.2.10-py2.py3-none-any.whl/satella/coding/structures/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    'typednamedtuple',
+    'Heap',
+    'TimeBasedHeap',
+    'OmniHashableMixin',
+    'Singleton',
+    'DictObject',
+    'apply_dict_object',
+    'Immutable'
+]
+
+from .dict_object import *
+from .immutable import Immutable
+from .singleton import Singleton
+from .structures import *
+from .typednamedtuple import *
