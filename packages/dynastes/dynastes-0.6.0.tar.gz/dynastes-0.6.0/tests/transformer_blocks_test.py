487475d39f5017558b6ab12550b54879b088ede9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:58 +0000

    Add package dynastes/0.6.0/dynastes-0.6.0.tar.gz

---
+++ packages/dynastes/dynastes-0.6.0.tar.gz/dynastes-0.6.0/tests/transformer_blocks_test.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.layers as tfkl
+
+from dynastes.blocks.attention_blocks import SelfAttentionBlock1D
+from dynastes.blocks.transformer_blocks import PointWiseFeedForwardBlock, EncoderBlock, EncoderBlockStack
+
+
+def _test_grads(testCase: tf.test.TestCase, func, input):
+    _, grads = tf.test.compute_gradient(func, input)
+    for grad in grads:
+        testCase.assertNotAllClose(grad, np.zeros_like(grad))
+        testCase.assertAllInRange(grad, -400., 400)
+
+
+to_tensor = tf.convert_to_tensor
+normal = np.random.normal
+
+
+class EncoderBlockTest(tf.test.TestCase):
+    def test_simple(self):
+        d_model = 16
+        num_heads = 4
+        dff = 32
+        length = 4096
+        mask_len = 32
+        sablock = SelfAttentionBlock1D(d_model, d_model, num_heads=num_heads,
+                                       attention_type='PseudoBlockSparseAttention1D',
+                                       block_length=1024,
+                                       multiquery_attention=True, )
+        norm = tfkl.LayerNormalization(epsilon=1e-6)
+        df_net = PointWiseFeedForwardBlock(dff=dff, d_model=d_model)
+        enc_block = EncoderBlock(sa_layer=sablock, norm0=norm, ffn=df_net, norm1=norm)
+
+        stack = EncoderBlockStack([enc_block] * 3)
+
+        test_input = tf.convert_to_tensor(normal(size=(1, length, d_model)).astype(np.float32))
+        mask = to_tensor(([True] * (length - mask_len)) + ([False] * (mask_len)))
+        mask = tf.expand_dims(mask, axis=0)
+        enc_out = enc_block(test_input, training=None, mask=mask)
+        out = stack(test_input, training=None, mask=mask)
+        out_mask = enc_block.compute_mask(test_input, mask=mask)
+        comp_out_shape = stack.compute_output_shape(test_input.shape)
+
+        self.assertEqual(out.shape, comp_out_shape)
+        self.assertEqual(enc_out.shape, comp_out_shape)
