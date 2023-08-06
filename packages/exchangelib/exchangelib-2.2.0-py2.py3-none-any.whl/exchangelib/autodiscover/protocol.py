2d031dd8777dcc7f9b7cf4bbefa4dea67990fe42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:48 +0000

    Add package exchangelib/2.2.0/exchangelib-2.2.0-py2.py3-none-any.whl

---
+++ packages/exchangelib/exchangelib-2.2.0-py2.py3-none-any.whl/exchangelib/autodiscover/protocol.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from future.utils import python_2_unicode_compatible
+
+from ..protocol import BaseProtocol
+
+
+@python_2_unicode_compatible
+class AutodiscoverProtocol(BaseProtocol):
+    """Protocol which implements the bare essentials for autodiscover"""
+    TIMEOUT = 10  # Seconds
+
+    def __str__(self):
+        return '''\
+Autodiscover endpoint: %s
+Auth type: %s''' % (
+            self.service_endpoint,
+            self.auth_type,
+        )
