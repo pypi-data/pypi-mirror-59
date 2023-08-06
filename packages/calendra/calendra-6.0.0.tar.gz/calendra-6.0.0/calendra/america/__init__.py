46becbfb11194287f1b3a2a185e8effec2f661ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:39 +0000

    Add package calendra/6.0.0/calendra-6.0.0.tar.gz

---
+++ packages/calendra/calendra-6.0.0.tar.gz/calendra-6.0.0/calendra/america/__init__.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from .barbados import Barbados
+from .brazil import (
+    Brazil, BrazilAcre, BrazilAlagoas, BrazilAmapa, BrazilAmazonas,
+    BrazilBahia, BrazilCeara, BrazilDistritoFederal, BrazilEspiritoSanto,
+    BrazilGoias, BrazilMaranhao, BrazilMatoGrosso, BrazilMatoGrossoDoSul,
+    BrazilPara, BrazilParaiba, BrazilPernambuco, BrazilPiaui,
+    BrazilRioDeJaneiro, BrazilRioGrandeDoNorte, BrazilRioGrandeDoSul,
+    BrazilRondonia, BrazilRoraima, BrazilSantaCatarina, BrazilSaoPauloState,
+    BrazilSaoPauloCity, BrazilSergipe, BrazilTocantins, BrazilVitoriaCity,
+    BrazilVilaVelhaCity, BrazilCariacicaCity, BrazilGuarapariCity,
+    BrazilSerraCity, BrazilBankCalendar
+)
+from .canada import (
+    Canada, Ontario, Quebec, BritishColumbia, Alberta, Saskatchewan, Manitoba,
+    NewBrunswick, NovaScotia, PrinceEdwardIsland, Newfoundland, Yukon,
+    NorthwestTerritories, Nunavut
+)
+from .chile import Chile
+from .colombia import Colombia
+from .mexico import Mexico
+from .panama import Panama
+from .paraguay import Paraguay
+
+
+__all__ = (
+    # Brazil & its states.
+    'Brazil',
+    'BrazilAcre',
+    'BrazilAlagoas',
+    'BrazilAmapa',
+    'BrazilAmazonas',
+    'BrazilBahia',
+    'BrazilCeara',
+    'BrazilDistritoFederal',
+    'BrazilEspiritoSanto',
+    'BrazilGoias',
+    'BrazilMaranhao',
+    'BrazilMatoGrosso',
+    'BrazilMatoGrossoDoSul',
+    'BrazilPara',
+    'BrazilParaiba',
+    'BrazilPernambuco',
+    'BrazilPiaui',
+    'BrazilRioDeJaneiro',
+    'BrazilRioGrandeDoNorte',
+    'BrazilRioGrandeDoSul',
+    'BrazilRondonia',
+    'BrazilRoraima',
+    'BrazilSantaCatarina',
+    'BrazilSaoPauloState',
+    'BrazilSaoPauloCity',
+    'BrazilSergipe',
+    'BrazilTocantins',
+    'BrazilVitoriaCity',
+    'BrazilVilaVelhaCity',
+    'BrazilCariacicaCity',
+    'BrazilGuarapariCity',
+    'BrazilSerraCity',
+    'BrazilBankCalendar',
+    # Canada
+    'Canada',
+    'Ontario',
+    'Quebec',
+    'BritishColumbia',
+    'Alberta',
+    'Saskatchewan',
+    'Manitoba',
+    'NewBrunswick',
+    'NovaScotia',
+    'PrinceEdwardIsland',
+    'Newfoundland',
+    'Yukon',
+    'NorthwestTerritories',
+    'Nunavut',
+    # Other american countries
+    'Barbados',
+    'Chile',
+    'Colombia',
+    'Mexico',
+    'Panama',
+    'Paraguay',
+)
