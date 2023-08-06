128e8aedae7bde4fc56d411faed1e1dac9be2dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:13 +0000

    Add package spade-client/4.2.3/spade_client-4.2.3.tar.gz

---
+++ packages/spade-client/spade_client-4.2.3.tar.gz/spade_client-4.2.3/spade_client/__init__.py
@@ -0,0 +1,9 @@
+from .Spade import Spade, FatalError
+from .BundlesFunction import BundlesFunction
+from .ItemsFunction import ItemsFunction
+from .TicketsFunction import TicketsFunction
+
+import spade_cli
+def main():
+    """Entry point for the application script"""
+    spade_cli.main()
