db40fb45a398165579524da4568b32e877b0d512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:52 +0000

    Add package alchemlyb/0.3.1/alchemlyb-0.3.1.tar.gz

---
+++ packages/alchemlyb/alchemlyb-0.3.1.tar.gz/alchemlyb-0.3.1/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+"""Setuptools-based setup script for alchemlyb.
+
+For a basic installation just type the command::
+
+  python setup.py install
+
+"""
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(name='alchemlyb',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='the simple alchemistry library',
+      author='David Dotson',
+      author_email='dotsdl@gmail.com',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      license='BSD',
+      long_description=open('README.rst').read(),
+      tests_require = ['pytest', 'alchemtest'],
+      install_requires=['numpy', 'pandas>=0.23.0', 'pymbar>=3.0.5', 'scipy', 'scikit-learn']
+      )
