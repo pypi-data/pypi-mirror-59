1e2c6b49323841ef0aa21d1bf38be8bbde9cbe78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:15 +0000

    Add package pantab/1.0.0/pantab-1.0.0-cp36-cp36m-win_amd64.whl

---
+++ packages/pantab/pantab-1.0.0-cp36-cp36m-win_amd64.whl/pantab/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.0.0"
+
+
+from ._reader import frame_from_hyper, frames_from_hyper
+from ._writer import frame_to_hyper, frames_to_hyper
+
+__all__ = [
+    "__version__",
+    "frame_from_hyper",
+    "frames_from_hyper",
+    "frame_to_hyper",
+    "frames_to_hyper",
+]
