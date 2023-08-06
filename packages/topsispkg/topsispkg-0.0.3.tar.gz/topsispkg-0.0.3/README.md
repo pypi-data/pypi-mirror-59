09f8cf55f190067b51fbf8bc24ca022450bdea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:10 +0000

    Add package topsispkg/0.0.3/topsispkg-0.0.3.tar.gz

---
+++ packages/topsispkg/topsispkg-0.0.3.tar.gz/topsispkg-0.0.3/README.md
@@ -0,0 +1,6 @@
+# Example Package
+This is topsis package (topsispkg) UCS633. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
+
+
---
+++ packages/topsispkg/topsispkg-0.0.3.tar.gz/topsispkg-0.0.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 19:09:01 2020
+
+@author: Home
+"""
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsispkg", # Replace with your own username
+    version="0.0.3",
+    author="Priyanshu Gupta",
+    author_email="priyanshugupta1904@gmail.com.com",
+    description="UCS633/psrana:This package will rank your input on the basis of topsis",
+    url="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
