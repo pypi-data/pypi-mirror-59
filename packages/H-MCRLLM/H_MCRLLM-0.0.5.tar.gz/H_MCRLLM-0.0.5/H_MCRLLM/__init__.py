906a06f6c65e492aeede99beae838eda8de65e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package H-MCRLLM/0.0.5/H_MCRLLM-0.0.5.tar.gz

---
+++ packages/H-MCRLLM/H_MCRLLM-0.0.5.tar.gz/H_MCRLLM-0.0.5/H_MCRLLM/__init__.py
@@ -0,0 +1,6 @@
+from H_MCRLLM.fct_H_MCR_LLM import * 
+from H_MCRLLM.fct_half_MCR import * 
+from H_MCRLLM.fct_pointselector import * 
+from H_MCRLLM.S1_call_H_MCRLLM import * 
+from H_MCRLLM.S2_PCA_on_H_spectra import * 
+from H_MCRLLM.S3_finalize_halfMCR import * 
---
+++ packages/H-MCRLLM/H_MCRLLM-0.0.5.tar.gz/H_MCRLLM-0.0.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="H_MCRLLM",
+    version="0.0.5",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    packages=["H_MCRLLM"],
+    description="H MCRLLM: Hierarchical Multivariate Curve Resolution by Log-Likelihood Maximization",
+    long_description="H MCRLLM: Hierarchical Multivariate Curve Resolution by Log-Likelihood Maximization",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
