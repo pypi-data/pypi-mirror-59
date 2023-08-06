20a5c6d15d9d87aa19e7b9afc4be1d773a6a683a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:58 +0000

    Add package request_limiter/0.1.1/request_limiter-0.1.1-py2.py3-none-any.whl

---
+++ packages/request_limiter/request_limiter-0.1.1-py2.py3-none-any.whl/request_limiter/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for Request Limiter."""
+from request_limiter.decorators import RequestLimiterDecorator, django_request_limiter
+from request_limiter.exceptions import LimitException
+from request_limiter.strategy import LimitStrategy, LimitedIntervalStrategy
+
+__author__ = """Mati Bekuma Terefe"""
+__email__ = 'matibek@gmail.com'
+__version__ = '0.1.1'
+
+request_limiter = RequestLimiterDecorator
+
+__all__ = [
+    'request_limiter',
+    'django_request_limiter',
+    'LimitStrategy',
+    'LimitedIntervalStrategy',
+    'LimitException'
+]
