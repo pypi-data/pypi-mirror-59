d4787a83702001b752f476d2dd2100eeae15d652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package ecoe/0.0.15/ecoe-0.0.15.tar.gz

---
+++ packages/ecoe/ecoe-0.0.15.tar.gz/ecoe-0.0.15/ecoe/__init__.py
@@ -0,0 +1,9 @@
+﻿__all__= ['econ','hydro','geo','modeling']
+
+# print("Loading ecoe package 1...")
+
+# Load subpackage contents into their respective namespaces 
+from ecoe.econ import *
+from ecoe.geo import *
+from ecoe.hydro import *
+from ecoe.modeling import *
