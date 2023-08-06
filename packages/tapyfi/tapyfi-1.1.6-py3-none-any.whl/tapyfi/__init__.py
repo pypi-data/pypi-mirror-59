8ab27c43cb9c69442d7251e7c97f0e6419feb4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:50 +0000

    Add package tapyfi/1.1.6/tapyfi-1.1.6-py3-none-any.whl

---
+++ packages/tapyfi/tapyfi-1.1.6-py3-none-any.whl/tapyfi/__init__.py
@@ -0,0 +1,10 @@
+import setuptools
+# Version of the realpython-reader package
+__version__ = "1.1.6"
+
+
+setuptools.setup(
+    # ...
+    packages=['tapify', 'tapify.Helpers', "tapify.Indicators"],
+    # ...
+)
