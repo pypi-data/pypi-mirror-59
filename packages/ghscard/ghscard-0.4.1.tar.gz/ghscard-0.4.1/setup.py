10dfc524b5655807fd9e758b27bd65abf30bbdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:25 +0000

    Add package ghscard/0.4.1/ghscard-0.4.1.tar.gz

---
+++ packages/ghscard/ghscard-0.4.1.tar.gz/ghscard-0.4.1/setup.py
@@ -0,0 +1,95 @@
+import os.path
+import sys
+from typing import Dict, List  # noqa
+
+import setuptools
+
+
+MODULE_NAME = "ghscard"
+REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
+REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info = {}  # type: Dict[str, str]
+
+
+def pytest_runner_requires() -> List[str]:
+    if set(["pytest", "test", "ptr"]).intersection(sys.argv):
+        return ["pytest-runner"]
+
+    return []
+
+
+def get_release_command_class():
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with open("README.rst", encoding=ENCODING) as f:
+    long_description = f.read()
+
+with open(os.path.join("docs", "pages", "introduction", "summary.txt"), encoding=ENCODING) as f:
+    summary = f.read().strip()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
+    docs_requires = [line.strip() for line in f if line.strip()]
+
+SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description=summary,
+    include_package_data=True,
+    keywords=["GitHub", "JavaScript", "Widget"],
+    license=pkg_info["__license__"],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    project_urls={
+        "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
+        "Source": REPOSITORY_URL,
+        "Tracker": "{:s}/issues".format(REPOSITORY_URL),
+    },
+    install_requires=SETUPTOOLS_REQUIRES + install_requires,
+    setup_requires=SETUPTOOLS_REQUIRES + pytest_runner_requires(),
+    tests_require=tests_requires,
+    extras_require={
+        "dev": ["releasecmd>=0.2.0,<1", "twine", "wheel"] + docs_requires + tests_requires,
+        "test": tests_requires,
+    },
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Widget Sets",
+    ],
+    entry_points={"console_scripts": ["ghscard=ghscard.ghscard:cmd"]},
+    cmdclass=get_release_command_class(),
+)
