301dc95d8d79eed707d7f514fed00b51517607ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:25 +0000

    Add package momo-ma/0.1.1/momo_ma-0.1.1.tar.gz

---
+++ packages/momo-ma/momo_ma-0.1.1.tar.gz/momo_ma-0.1.1/tests/test_01.py
@@ -0,0 +1,15 @@
+import unittest
+from momo.cli import br
+
+class TestMOMO(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_lucher(self):
+        pass
+
+    def tearDown(self):
+        pass
+
