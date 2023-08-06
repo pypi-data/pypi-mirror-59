681dae16ec1cffec7734a9184a262b1ea590accb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:34 +0000

    Add package zstat-cli/0.1.3/zstat-cli-0.1.3.tar.gz

---
+++ packages/zstat-cli/zstat-cli-0.1.3.tar.gz/zstat-cli-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='zstat-cli',
+    version='0.1.3',
+    description='Bone-simple tool for generating basic numerical stats from stdin',
+    long_description=readme,
+    author='Zachary Wolfe',
+    author_email='wolfe.zach@gmail.com',
+    url='https://github.com/zdwolfe/zstat',
+    license="MIT",
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': ['zstat=zstat.command_line:main'],
+    }
+)
+
