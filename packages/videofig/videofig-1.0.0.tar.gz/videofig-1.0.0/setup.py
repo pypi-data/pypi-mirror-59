af5ad80addd99ad57beae1dd6989ee9a095df571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:09 +0000

    Add package videofig/1.0.0/videofig-1.0.0.tar.gz

---
+++ packages/videofig/videofig-1.0.0.tar.gz/videofig-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='videofig',
+      version='1.0.0',
+      description='Lightweight image sequence visualization utility based on matplotlib',
+      url="https://github.com/bilylee/videofig",
+      author='Bily Lee',
+      author_email='bily.lee@qq.com',
+      license='MIT',
+      packages=['videofig'],
+      install_requires=['matplotlib>=2.0.0'],
+      zip_safe=False)
