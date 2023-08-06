6ef6b1e554848cc06b61486a468b746ba7c282f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:08 +0000

    Add package pydcd/0.0.6/PyDCD-0.0.6.tar.gz

---
+++ packages/pydcd/PyDCD-0.0.6.tar.gz/pydcd-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydcd", 
+    version="0.0.6",
+    author="Kunpeng Zhang & Shaokun Fan & Bruce Golden",
+    author_email="shaokunfan@gmail.com",
+    description="PyDCD: A Deep Learning-Based Community Detection Software in Python for Large-scale Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kpzhang/deepcommunitydetection",
+    # package_data={
+    #     'pydcd': ['data/*']
+    # },
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    py_modules = ["pydcd"],
+    install_requires = ['numpy', 'networkx', 'sklearn', 'scipy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',
+)
