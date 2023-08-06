a2ce9c7546e16f40317ddeb3c55fc625edb177ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:24 +0000

    Add package topsis-1.0.0/1.0.0/topsis-1.0.0-1.0.0.tar.gz

---
+++ packages/topsis-1.0.0/topsis-1.0.0-1.0.0.tar.gz/topsis-1.0.0-1.0.0/README.md
@@ -0,0 +1,4 @@
+
+
+This is a simple implementation of topsis in python.
+ You can use Github-flavored Markdown to make decision for your data.
---
+++ packages/topsis-1.0.0/topsis-1.0.0-1.0.0.tar.gz/topsis-1.0.0-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="topsis-1.0.0", # Replace with your own username
+    version="1.0.0",
+    author="Gurpreet_Kaur",
+    author_email="gur01052000@gmail.com",
+    description="Topsis",
+    #long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gur01052000/git/blob/master/topsis_python.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
