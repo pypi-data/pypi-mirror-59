73aae1d90f51df66de53959fb8cbae8454b246c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:27 +0000

    Add package djdt-flamegraph/0.2.13/djdt_flamegraph-0.2.13.tar.gz

---
+++ packages/djdt-flamegraph/djdt_flamegraph-0.2.13.tar.gz/djdt_flamegraph-0.2.13/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+setup(
+    name='djdt_flamegraph',
+    version='0.2.13',
+    description='Flamegraphs for Django Debug Toolbar',
+    long_description=readme + '\n\n' + history,
+    author='Bo Lopker',
+    author_email='blopker@23andme.com',
+    url='https://github.com/23andme/djdt-flamegraph',
+    packages=[
+        'djdt_flamegraph',
+    ],
+    package_dir={'djdt_flamegraph':
+                 'djdt_flamegraph'},
+    license='MIT',
+    zip_safe=False,
+    include_package_data=True,
+    keywords='djdt_flamegraph',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
