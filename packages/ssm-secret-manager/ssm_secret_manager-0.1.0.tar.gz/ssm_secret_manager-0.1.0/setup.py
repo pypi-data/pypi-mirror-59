0cd01ab26a068c30a2b91817ed95c77ecc54aea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:32 +0000

    Add package ssm-secret-manager/0.1.0/ssm_secret_manager-0.1.0.tar.gz

---
+++ packages/ssm-secret-manager/ssm_secret_manager-0.1.0.tar.gz/ssm_secret_manager-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from os import path
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ssm_secret_manager',
+    version='0.1.0',
+    url='https://github.com/bklim5/aws-ssm-secret-manager',
+    description='SecretManager class to retrieve secrets from AWS System Manager Parameter Store',
+    author='BK Lim',
+    author_email='bklim5@hotmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'boto3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
