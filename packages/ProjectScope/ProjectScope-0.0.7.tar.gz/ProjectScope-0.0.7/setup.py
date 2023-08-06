efd9ab0a5301d8ee63721f8810361f2a69fa194f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package ProjectScope/0.0.7/ProjectScope-0.0.7.tar.gz

---
+++ packages/ProjectScope/ProjectScope-0.0.7.tar.gz/ProjectScope-0.0.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ProjectScope",
+    version="0.0.7",
+    license='MIT',
+    author="Corey Bird",
+    author_email="birdcorey@gmail.com",
+    description="A small project management tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/CoreyBird/projectscope/src/master/",
+    download_url="https://bitbucket.org/CoreyBird/projectscope/src/master/dist/ProjectScope-0.0.7.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'wxPython',
+        'Panda3D',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+    package_data={'': ['version_info.txt']},
+)
