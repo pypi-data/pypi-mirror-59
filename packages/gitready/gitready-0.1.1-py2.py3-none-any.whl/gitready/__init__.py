620c97ad01aba02a70173f145f9f2ad225fa5aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package gitready/0.1.1/gitready-0.1.1-py2.py3-none-any.whl

---
+++ packages/gitready/gitready-0.1.1-py2.py3-none-any.whl/gitready/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__all__ = []
+
+__title__ = "gitready"
+__summary__ = "Initializes a git repo for a python project"
+__url__ = "https://github.com/aluttik/gitready"
+
+__version__ = "0.1.1"
+
+__author__ = "Arie van Luttikhuizen"
+__email__ = "aluttik@gmail.com"
+
+__license__ = "Apache 2.0"
