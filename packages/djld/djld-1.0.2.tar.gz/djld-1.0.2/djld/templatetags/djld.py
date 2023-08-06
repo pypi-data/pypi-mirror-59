c858bbe99b15ff31fa3db64d1dc29b56c80319ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:27 +0000

    Add package djld/1.0.2/djld-1.0.2.tar.gz

---
+++ packages/djld/djld-1.0.2.tar.gz/djld-1.0.2/djld/templatetags/djld.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from django import template
+from django.conf import settings
+from django.template import Template, Context
+from django.utils.safestring import mark_safe
+
+from djld.errors import DjLdException
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def structured_data(context, template, data_context={}):
+    if type(template) == dict:
+        rendered = json.dumps(template, indent=2)
+    else:
+        data_context['request_context'] = context
+        ld_files_path = getattr(
+            settings, 'LD_JSON_PATH',
+            os.path.join(settings.BASE_DIR, 'ldjson')
+        )
+        with open(os.path.join(ld_files_path, template)) as template_file:
+            tempalte_content = template_file.read()
+            tmpl = Template(tempalte_content)
+            rendered = tmpl.render(Context(data_context))
+            rendered = rendered.replace('\n', '').replace('\r', '')
+            rendered = rendered.replace('\t', '  ').replace('\0', '')
+            try:
+                json.loads(rendered)
+            except ValueError:
+                raise DjLdException('Invalid JSON content in template')
+    rendered = '<script type="application/ld+json">\n' + rendered + '\n</script>'
+    return mark_safe(rendered)
