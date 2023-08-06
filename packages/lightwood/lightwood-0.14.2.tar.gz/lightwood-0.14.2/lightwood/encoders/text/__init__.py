44a539f2a8949eeea43b82115e66a05edd20a8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:22 +0000

    Add package lightwood/0.14.2/lightwood-0.14.2.tar.gz

---
+++ packages/lightwood/lightwood-0.14.2.tar.gz/lightwood-0.14.2/lightwood/encoders/text/__init__.py
@@ -0,0 +1,6 @@
+from lightwood.encoders.text.rnn import RnnEncoder
+from lightwood.encoders.text.infersent import InferSentEncoder
+from lightwood.encoders.text.distilbert import DistilBertEncoder
+
+default = DistilBertEncoder
+#default = InferSentEncoder
