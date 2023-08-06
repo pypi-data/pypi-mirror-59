d273b2d733c0b4b4147e09827965c7c190c57a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package topsis-101703325/1.0.0/topsis-101703325-1.0.0.tar.gz

---
+++ packages/topsis-101703325/topsis-101703325-1.0.0.tar.gz/topsis-101703325-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="topsis-101703325",
+    version="1.0.0",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Maninder Singh",
+    author_email="manii.sandhuu@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_python"],
+    include_package_data=True,
+    install_requires=['scipy',
+                      'tabulate',
+                      'numpy',
+                      'pandas'
+     ],
+     entry_points={
+        "console_scripts": [
+            "topsis=topsis_python.topsis:main",
+        ]
+     },
+)
