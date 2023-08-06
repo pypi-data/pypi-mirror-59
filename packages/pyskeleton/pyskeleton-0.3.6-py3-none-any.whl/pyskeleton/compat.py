f1e6a34df6329d246a80acfe53fa47446806c12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:03 +0000

    Add package pyskeleton/0.3.6/pyskeleton-0.3.6-py3-none-any.whl

---
+++ packages/pyskeleton/pyskeleton-0.3.6-py3-none-any.whl/pyskeleton/compat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+
+import sys
+
+if sys.version_info >= (3, 3):
+    from os import replace
+else:
+    # POSIX rename() is always atomic
+    from os import rename as replace
