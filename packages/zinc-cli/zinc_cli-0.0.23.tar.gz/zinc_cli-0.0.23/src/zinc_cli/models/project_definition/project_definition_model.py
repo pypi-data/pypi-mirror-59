f6114640a084c29d4b5c8ef38def34a98d1dc467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:05 +0000

    Add package zinc-cli/0.0.23/zinc_cli-0.0.23.tar.gz

---
+++ packages/zinc-cli/zinc_cli-0.0.23.tar.gz/zinc_cli-0.0.23/src/zinc_cli/models/project_definition/project_definition_model.py
@@ -0,0 +1,28 @@
+import os
+import yaml
+from models.base_model import BaseModel
+
+
+class ProjectDefinitionModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.project_name: str = "MyProject"
+        self.model_path: str = ""
+
+    def serialize(self):
+        # Get serialized to a JSON Object.
+        payload = {
+            "project_name": self.project_name
+        }
+        return payload
+
+    def get_local_path(self):
+        return os.path.join(self.model_path, "project_definition.yaml")
+        pass
+
+    def save_to_local(self):
+        # Saves the definition file locally.
+        payload = self.serialize()
+        path = self.get_local_path()
+        with open(path, "w") as file:
+            yaml.dump(payload, file)
