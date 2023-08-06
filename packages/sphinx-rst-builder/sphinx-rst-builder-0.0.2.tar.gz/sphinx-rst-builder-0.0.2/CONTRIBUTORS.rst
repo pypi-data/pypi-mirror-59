b3bdfa7864f943d668eca7ee9f0fca164de2ce4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:08 +0000

    Add package sphinx-rst-builder/0.0.2/sphinx-rst-builder-0.0.2.tar.gz

---
+++ packages/sphinx-rst-builder/sphinx-rst-builder-0.0.2.tar.gz/sphinx-rst-builder-0.0.2/CONTRIBUTORS.rst
@@ -0,0 +1,10 @@
+Contributors
+============
+
+Freek Dijkstra (macfreek)
+G. Nicholas d'Andrea (gnidan)
+Jeffrey Lo (jeffrey_lo)
+Matthew Planchard (mplanchard)
+Nicola Musatti (nmusatti)
+David Fritzsche (davidfritzsche)
+Fabio Bonelli (bfabio)
---
+++ packages/sphinx-rst-builder/sphinx-rst-builder-0.0.2.tar.gz/sphinx-rst-builder-0.0.2/src/sphinx_rst_builder/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: BSD-2-Clause
+"""Sphinx extension to output reST files.
+
+"""
+
+from sphinx.application import Sphinx
+from sphinx.builders import Builder
+from sphinx.writers.text import STDINDENT
+
+from ._builder import RstBuilder
+from ._writer import RstWriter
+
+
+__version__ = "0.0.2"
+
+
+def setup(app: Sphinx):
+    """Setup this extension"""
+    app.require_sphinx('2.0')
+    app.add_builder(RstBuilder)
+    app.add_config_value('rst_file_suffix', ".rst", False)
+    """This is the file name suffix for reST files"""
+    app.add_config_value('rst_link_suffix', None, False)
+    """The is the suffix used in internal links. By default, takes the same value as rst_file_suffix"""
+    app.add_config_value('rst_file_transform', None, False)
+    """Function to translate a docname to a filename. By default, returns docname + rst_file_suffix."""
+    app.add_config_value('rst_link_transform', None, False)
+    """Function to translate a docname to a (partial) URI. By default, returns docname + rst_link_suffix."""
+    app.add_config_value('rst_indent', STDINDENT, False)
+    app.add_config_value('rst_preserve_code_block_flags', False, 'env')
