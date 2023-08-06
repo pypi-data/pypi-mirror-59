220b1fc05339bbc879f51b856a5839d3e469a1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:08 +0000

    Add package Quasar/0.7.1/Quasar-0.7.1.tar.gz

---
+++ packages/Quasar/Quasar-0.7.1.tar.gz/Quasar-0.7.1/installer/windows/specs/update-conda-spec.bat
@@ -0,0 +1,17 @@
+setlocal EnableDelayedExpansion
+
+if "%CONDA%" == "" (
+    set "%CONDA%"=conda
+)
+
+"%CONDA%" config --append channels conda-forge
+"%CONDA%" config --append channels https://quasar.codes/conda/
+
+rem %CONDA% remove -y -n orange --all
+
+"%CONDA%" create -y -n env python=3.6.* quasar=0.7.1 ^
+ orange3=3.24.1 orange-spectroscopy=0.4.9 opusFC=1.2.* ^
+ keyring scipy=1.2.* numpy=1.16.* pyqt=5.12.* ^
+ scikit-learn=0.22.* ^
+ colorcet h5py || exit /b !ERRORLEVEL!
+"%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
