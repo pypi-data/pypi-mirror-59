b1f014fa1099067bb2d6d65a3ab3eaa319b76e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:47 +0000

    Add package graphviz2drawio/0.2.0/graphviz2drawio-0.2.0-py3-none-any.whl

---
+++ packages/graphviz2drawio/graphviz2drawio-0.2.0-py3-none-any.whl/graphviz2drawio/__main__.py
@@ -0,0 +1,48 @@
+from sys import stderr
+from .graphviz2drawio import convert
+from .models import Arguments
+from .version import __version__
+
+
+def main():
+
+    args = Arguments(__version__).parse_args()
+
+    if not args.stdout:
+        stderr.write("This is beta software, please report issues to:\n")
+        stderr.write("https://github.com/hbmartin/graphviz2drawio/issues\n")
+
+    try:
+        output = convert(args.to_convert, args.program)
+    except BaseException:
+        stderr.write("Something went wrong, please report\n")
+        stderr.write(
+            "An automatic crash report can be sent to the developer (no personal or graph information)\n"
+        )
+        permission = input("Type 'no' to cancel report, press enter to send: ")
+        if permission != "no":
+            from raven import Client
+
+            client = Client(
+                dsn="https://f1fce21b51864819a26ea116ff4e5b7f:9f61501e4891465ea53e0416e5f402b1@sentry.io/1266157",
+                release=__version__,
+            )
+            client.captureException()
+        raise
+
+    if args.stdout:
+        print(output)
+        return
+
+    if args.outfile is not None:
+        outfile = args.outfile
+    else:
+        base = args.to_convert.split(".")
+        outfile = ".".join(base[:-1] + ["xml"])
+    with open(outfile, "w") as fd:
+        fd.write(output)
+    stderr.write("Converted file: " + outfile + "\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/graphviz2drawio/graphviz2drawio-0.2.0-py3-none-any.whl/graphviz2drawio/mx/Text.py
@@ -0,0 +1,37 @@
+from graphviz2drawio.models import DotAttr
+from graphviz2drawio.mx import MxConst
+from .Styles import Styles
+
+
+class Text:
+    def __init__(self, anchor, family, size, text, color):
+        self.anchor = anchor
+        self.family = family
+        self.size = size
+        self.text = text
+        self.color = color
+
+    def get_mx_style(self):
+        align = MxConst.CENTER if self.anchor == DotAttr.MIDDLE else MxConst.START
+        # TODO: add right
+        margin = (
+            "margin-top:4px;" if self.anchor == DotAttr.MIDDLE else "margin-left:4px;"
+        )
+        rescaled_size = 10.0 * (self.size / 14.0)
+        return Styles.TEXT.format(
+            align=align,
+            margin=margin,
+            size=rescaled_size,
+            family=self.family or MxConst.DEFAULT_FONT_FAMILY,
+            color=self.color or MxConst.DEFAULT_FONT_COLOR,
+        )
+
+    @staticmethod
+    def from_svg(t):
+        return Text(
+            text=t.text.replace("<", "&lt;").replace(">", "&gt;"),
+            anchor=t.attrib.get("text-anchor", None),
+            family=t.attrib.get("font-family", MxConst.DEFAULT_FONT_FAMILY),
+            size=float(t.attrib.get("font-size", MxConst.DEFAULT_TEXT_SIZE)),
+            color=t.attrib.get("fill", None),
+        )
