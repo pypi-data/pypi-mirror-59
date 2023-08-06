fb6b1ecd534d48f1d133795800c24ea1d2a63f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:38 +0000

    Add package wbplot/1.0.3/wbplot-1.0.3.tar.gz

---
+++ packages/wbplot/wbplot-1.0.3.tar.gz/wbplot-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+requirements = ["numpy>=1", "nibabel==2.1.0", "pillow>=7", "matplotlib>=2"]
+
+setup(
+    name="wbplot",
+    version="1.0.3",
+    author="Joshua Burt",
+    author_email="joshua.burt@yale.edu",
+    include_package_data=True,
+    description="A package for automated plotting of neuroimaging maps using Connectome Workbench.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jbburt/wbplot",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
