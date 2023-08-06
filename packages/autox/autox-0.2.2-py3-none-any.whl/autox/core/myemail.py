37f3e49d37d4abe40a9f85270e13bfe235c7e232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:30 +0000

    Add package autox/0.2.2/autox-0.2.2-py3-none-any.whl

---
+++ packages/autox/autox-0.2.2-py3-none-any.whl/autox/core/myemail.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 基础包：邮件服务
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from autox import constants as cs
+import autox.core.mylog as log
+
+logging = log.track_log()
+
+
+def email(reportfile):
+
+    att = MIMEText(reportfile, 'html', 'utf-8')
+    att['ContentType'] = 'application/octet-stream'
+    att['Content-Disposition'] = 'attachment:filename="测试报告'
+
+    message = MIMEMultipart('related')
+    message['From'] = cs.MAIL_SENDER
+    message['To'] = ','.join(cs.MAIL_RECEIVER)
+    message['Subject'] = Header('测试报告', 'utf-8')
+    message.attach(att)
+
+    try:
+        smtp = smtplib.SMTP()
+        smtp.connect(cs.MAIL_HOST, 25)
+        smtp.login(cs.MAIL_USER, cs.MAIL_PASS)
+        smtp.sendmail(cs.MAIL_SENDER, message['To'].split(','), message.as_string())
+        logging.info("**********发送邮件成功**********")
+    except smtplib.SMTPException as e:
+        logging.error("**********发送邮件失败:**********\n %s", e)
+    finally:
+        smtp.quit()
+
---
+++ packages/autox/autox-0.2.2-py3-none-any.whl/autox/core/myrequest.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 基础包：请求服务，目前只支持带token的请求
+
+import requests
+import traceback
+import autox.core.mylog as log
+import os
+
+logging = log.track_log()
+
+result = ''
+results = ''
+
+response_result = {
+    "status_result": None,
+    "status_code": None
+}
+
+timeout_Result= {
+        "code": -1,
+        "message": "请求超时",
+        "result": None,
+        "run_time": 20000.000
+    }
+
+
+def api(method, url, data, headers):
+    """
+    定义一个请求接口的方法和参数
+    :param method: 请求类型
+    :param url: 请求地址
+    :param data: 请求参数
+    :param headers: 请求头信息
+    :return: code码
+    """
+    global result
+
+    try:
+        if method.upper() == "POST":
+            result = requests.post(url, data, headers=headers)
+        if method.upper() == "GET":
+            result = requests.get(url, headers=headers)
+        response = result.json()
+        return response
+    except Exception as e:
+        logging.error("请求失败%s" % e)
+        traceback.print_exc(file=open(os.getcwd()+'/log/error.log', 'w+'))
+
+
+def get_message(method, url, data, headers):
+    """
+    定义一个请求接口的方法和参数
+    :param method: 请求类型
+    :param url: 请求地址
+    :param data: 请求参数
+    :param headers: 请求头信息
+    :return: message信息
+    """
+    global result, results
+
+    try:
+        if method.upper() == "POST":
+            result = requests.post(url, data, headers=headers, timeout=15)
+        if method.upper() == "GET":
+            result = requests.get(url, data, headers=headers, timeout=15)
+        if method.upper() == "PUT":
+            result = requests.put(url, data, headers=headers)
+        if method.upper() == "PATCH":
+            result = requests.patch(url, data, headers=headers)
+        if method.upper() == "OPTIONS":
+            result = requests.options(url, headers=headers)
+        run_time = result.elapsed.total_seconds()
+        response_code = result.status_code
+        if response_code > 200:
+            response_result['status_code'] = response_code
+            response_result['status_result'] = result.text
+            response_result['run_time'] = round(run_time * 1000, 3)
+            return response_result
+        elif response_code == 200 and result is None:
+            return result
+        response = result.json()
+        response['run_time'] = round(run_time * 1000, 3)
+        return response
+    except requests.ReadTimeout:
+        traceback.print_exc(file=open(os.getcwd()+'/log/error.log', 'a+'))
+        return timeout_Result
+    except Exception as e:
+        logging.error("请求发生异常\n %s" % e)
+        traceback.print_exc(file=open(os.getcwd()+'/log/error.log', 'a+'))
+
+
+def get_mfd(method, url, data, headers):
+    """
+    定义一个请求multipart/form-data接口的方法和参数
+    :param url: 请求地址
+    :param method: 请求方法
+    :param data: 请求文件
+    :param headers: 请求头信息
+    :return: result信息
+    """
+    global result
+    try:
+        if method == ('post' or 'POST'):
+            result = requests.post(url, data=data, headers=headers)
+        response = result.json()
+        return response
+    except Exception as e:
+        logging.error("请求失败%s" % e)
+        traceback.print_exc(file=open(os.getcwd()+'/log/error.log', 'a+'))
+
