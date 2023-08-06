907cd3acfa8abd195d533f71578481e1705ebc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:39 +0000

    Add package base-api/0.0.0/base-api-0.0.0.tar.gz

---
+++ packages/base-api/base-api-0.0.0.tar.gz/base-api-0.0.0/baseapi/__init__.py
@@ -0,0 +1,4 @@
+from .client import Client  # noqa
+from .utils import File  # noqa
+
+__version__ = '0.0.0'
---
+++ packages/base-api/base-api-0.0.0.tar.gz/base-api-0.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from baseapi import __version__
+
+setup(
+    name='base-api',
+    version=__version__,
+    author='Luke Hodkinson',
+    author_email='furious.luke@gmail.com',
+    maintainer='Luke Hodkinson',
+    maintainer_email='furious.luke@gmail.com',
+    description='Easily create maintainable API clients.',
+    long_description='',
+    url='https://github.com/furious.luke/baseapi',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
+    install_requires=[
+        'requests'
+    ],
+    extras_require={
+    },
+    entry_points={
+    },
+    zip_safe=True
+)
