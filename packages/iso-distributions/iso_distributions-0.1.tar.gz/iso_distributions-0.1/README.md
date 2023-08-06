248a0044a7393147b0b5d86768368703f651e2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:05 +0000

    Add package iso-distributions/0.1/iso_distributions-0.1.tar.gz

---
+++ packages/iso-distributions/iso_distributions-0.1.tar.gz/iso_distributions-0.1/README.md
@@ -0,0 +1,12 @@
+# isa-distribuions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
+Explanation of how installation works
+
---
+++ packages/iso-distributions/iso_distributions-0.1.tar.gz/iso_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='iso_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['iso_distributions'],
+      author = 'Samuel Agbanagba',
+      author_email = 'agbanagba.samuel@gmail.com',
+      zip_safe=False)
