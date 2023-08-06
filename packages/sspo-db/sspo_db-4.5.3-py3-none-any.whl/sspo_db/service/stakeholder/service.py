75a185b27cac7c8ccfe8d34fed2b788091f28863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package sspo-db/4.5.3/sspo_db-4.5.3-py3-none-any.whl

---
+++ packages/sspo-db/sspo_db-4.5.3-py3-none-any.whl/sspo_db/service/stakeholder/service.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sspo_db.model.stakeholder.models import *
+from sspo_db.service.base_service import BaseService
+
+class PersonService(BaseService):
+    
+    def __init__(self):
+        super(PersonService,self).__init__(Person)
+    
+class TeamMemberService(BaseService):
+    def __init__(self):
+        super(TeamMemberService,self).__init__(TeamMember)
+
+class DeveloperService(BaseService):
+    def __init__(self):
+        super(DeveloperService,self).__init__(Developer)
+
+class ScrumMasterService(BaseService):
+    def __init__(self):
+        super(ScrumMasterService,self).__init__(ScrumMaster)
+
+class ProductOwnerService(BaseService):
+    def __init__(self):
+        super(ProductOwnerService,self).__init__(ProductOwner)
+
+class ClientService(BaseService):
+    def __init__(self):
+        super(ClientService,self).__init__(Client)
