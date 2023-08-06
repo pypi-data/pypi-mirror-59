c1a87f6215badd25c5e3465b9145bc0669356ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:33 +0000

    Add package vbml/0.5.92/vbml-0.5.92.tar.gz

---
+++ packages/vbml/vbml-0.5.92.tar.gz/vbml-0.5.92/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['vbml',
+ 'vbml.patcher',
+ 'vbml.patcher.loader',
+ 'vbml.patcher.pattern',
+ 'vbml.patcher.standart']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vbml',
+    'version': '0.5.92',
+    'description': 'Way to check',
+    'long_description': None,
+    'author': 'timoniq',
+    'author_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
