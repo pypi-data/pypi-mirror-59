e164c4a5f8c1f908f637a737cd4109474d3cf7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:53 +0000

    Add package vowcon-removal-helper/0.1.1/vowcon_removal_helper-0.1.1.tar.gz

---
+++ packages/vowcon-removal-helper/vowcon_removal_helper-0.1.1.tar.gz/vowcon_removal_helper-0.1.1/README.md
@@ -0,0 +1,2 @@
+# vowcon_removal_helper
+A collection of functions that remove vowels or a consonant from a word. 
---
+++ packages/vowcon-removal-helper/vowcon_removal_helper-0.1.1.tar.gz/vowcon_removal_helper-0.1.1/vowcon_helper_code/lecture.py
@@ -0,0 +1,14 @@
+"""
+
+lambdata - A collection of data science helper functions
+
+"""
+
+import pandas as pd 
+import numpy as np
+
+# Sample Code
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
