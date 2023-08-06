a997204a65e87273cea48bb59f542e28a735bd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:51 +0000

    Add package pyFaceTrace/2.0/pyFaceTrace-2.0.tar.gz

---
+++ packages/pyFaceTrace/pyFaceTrace-2.0.tar.gz/pyFaceTrace-2.0/setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+
+setup(
+        name='pyFaceTrace',   
+        version='2.0',   
+        description='easy Face Recognition fo python',   
+        author='funny4875',  
+        author_email='funny4875@gmail.com',  
+        url='https://github.com/funny4875/pyFaceTrace.git',      
+        packages=['pyFaceTrace'],       
+)
