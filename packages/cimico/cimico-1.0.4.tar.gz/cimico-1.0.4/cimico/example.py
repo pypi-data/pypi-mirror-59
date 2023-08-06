349a6454f36386a408c233fe8aba2f7da4662787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:47 +0000

    Add package cimico/1.0.4/cimico-1.0.4.tar.gz

---
+++ packages/cimico/cimico-1.0.4.tar.gz/cimico-1.0.4/cimico/example.py
@@ -0,0 +1,82 @@
+def main(y, x, a):
+    x += y
+    y += x
+    z = x * y
+    b = z + y + x + a
+    print("text")
+    arr = [4,5,6,72,79, 3, 7, 9, 3, 34]
+    for i in range(len(arr)):
+        b *= 2
+        y += 1
+        print(arr[i])
+    t = 10
+    while t > 0:
+        t -= 1
+    strval = ""
+    actualval = 0   
+    print(strval)
+    b += 5
+    c = x + y + z + b + a + t + actualval    
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    b -= 1
+    b += 1
+    b -= 1
+    b += 1
+    b -= 1
+    return b
+
+
+def testfunction(a, b, curr):
+    if(curr == 3):
+        return a+b
+    curr+=1
+    test = "sfwef"
+    a += 1
+    b += 5
+    n = 0
+    while n<10:
+        n+=1
+    test += "e"
+    c = abs(a - b)
+    d = a * b * c
+    e = a + b + d + c
+    test += "gsagar"
+    return e + testfunction(d, e, curr)
