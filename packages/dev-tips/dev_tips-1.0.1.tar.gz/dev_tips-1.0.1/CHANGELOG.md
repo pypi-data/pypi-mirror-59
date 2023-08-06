5d58719fa23c8faa5eff16493502fa1d120f411b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:09 +0000

    Add package dev-tips/1.0.1/dev_tips-1.0.1.tar.gz

---
+++ packages/dev-tips/dev_tips-1.0.1.tar.gz/dev_tips-1.0.1/CHANGELOG.md
@@ -0,0 +1,18 @@
+# [](https://github.com/igoryamamoto/dev-tips/compare/v1.0.0...v) (2020-01-21)
+
+
+### Bug Fixes
+
+* Add package data to setup.py ([34392aa](https://github.com/igoryamamoto/dev-tips/commit/34392aa3250981caeb1feea7c52fcb039bafa5e9))
+
+
+
+#  (2020-01-21)
+
+
+### Features
+
+* Add functions to list tips and to print a random one ([a94a196](https://github.com/igoryamamoto/dev-tips/commit/a94a196a2030178e2bb68f239f22b19aa7448b7e))
+
+
+
---
+++ packages/dev-tips/dev_tips-1.0.1.tar.gz/dev_tips-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dev_tips",
+    version="1.0.1",
+    author="Ígor Yamamoto",
+    author_email="hello@igoryamamoto.com",
+    description="CLI version of 97 Things Every Programmer Should Know",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/igoryamamoto/dev-tips",
+    packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.md"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
