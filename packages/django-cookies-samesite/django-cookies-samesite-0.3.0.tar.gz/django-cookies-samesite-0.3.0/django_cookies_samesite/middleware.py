6de3cae74cf2c13d54eb9a6e4e7213ff79d3b9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:33 +0000

    Add package django-cookies-samesite/0.3.0/django-cookies-samesite-0.3.0.tar.gz

---
+++ packages/django-cookies-samesite/django-cookies-samesite-0.3.0.tar.gz/django-cookies-samesite-0.3.0/django_cookies_samesite/middleware.py
@@ -0,0 +1,65 @@
+# Cookie library has moved to http in python3
+try:
+    import Cookie
+except ImportError:
+    import http.cookies as Cookie
+
+import warnings
+
+import django
+
+from distutils.version import LooseVersion
+
+from django.conf import settings
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+
+
+Cookie.Morsel._reserved['samesite'] = 'SameSite'
+
+
+class CookiesSameSite(MiddlewareMixin):
+    """
+    Support for SameSite attribute in Cookies is implemented in Django 2.1 and won't
+    be backported to Django 1.11.x.
+    This middleware will be obsolete when your app will start using Django 2.1.
+    """
+    def process_response(self, request, response):
+        if LooseVersion(django.__version__) >= LooseVersion('2.1.0'):
+            raise DeprecationWarning(
+                'Your version of Django supports SameSite flag in the cookies mechanism. '
+                'You should remove django-cookies-samesite from your project.'
+            )
+
+        protected_cookies = getattr(
+            settings,
+            'SESSION_COOKIE_SAMESITE_KEYS',
+            set()
+        ) or set()
+
+        if not isinstance(protected_cookies, (list, set, tuple)):
+            raise ValueError('SESSION_COOKIE_SAMESITE_KEYS should be a list, set or tuple.')
+
+        protected_cookies = set(protected_cookies)
+        protected_cookies |= {settings.SESSION_COOKIE_NAME, settings.CSRF_COOKIE_NAME}
+
+        samesite_flag = getattr(
+            settings,
+            'SESSION_COOKIE_SAMESITE',
+            None
+        )
+
+        if not samesite_flag:
+            return response
+
+        if samesite_flag.lower() not in {'lax', 'none', 'strict'}:
+            raise ValueError('samesite must be "lax", "none", or "strict".')
+
+        for cookie in protected_cookies:
+            if cookie in response.cookies:
+                response.cookies[cookie]['samesite'] = samesite_flag.lower()
+
+        return response
