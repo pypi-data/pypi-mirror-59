3a003e409bb54bc5478056dbea338c72364d930a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package fsttest/0.2.1/fsttest-0.2.1-py3-none-any.whl

---
+++ packages/fsttest/fsttest-0.2.1-py3-none-any.whl/fsttest/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+FST test -- test your Foma finite-state transducers!
+"""
+
+from .__version__ import VERSION as __version__
+from ._fst import FST
+from .exceptions import FSTTestError, TestCaseDefinitionError
+from .fsttest import TestResults, execute_test_case, load_fst, run_tests
+
+__all__ = [
+    "FST",
+    "FSTTestError",
+    "TestCaseDefinitionError",
+    "TestResults",
+    "execute_test_case",
+    "run_tests",
+]
