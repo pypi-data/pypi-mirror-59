585203af6d449ebf5156b15170093619389428c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:13 +0000

    Add package fuguier/2.0.0/fuguier-2.0.0-py3-none-any.whl

---
+++ packages/fuguier/fuguier-2.0.0-py3-none-any.whl/cn/hn/hx/Message.py
@@ -0,0 +1,14 @@
+'''
+@Author: your name
+@Date: 2020-01-16 23:52:12
+@LastEditTime : 2020-01-17 15:47:33
+@LastEditors  : Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \MyPython\cn\hn\hx\Message.py
+'''
+def get_msg():
+   # print("__name__: %s" % (__name__))
+    return "富贵儿"
+def _hello():
+    print("world")
+_hi = "hi"
---
+++ packages/fuguier/fuguier-2.0.0-py3-none-any.whl/cn/hn/hx/__init__.py
@@ -0,0 +1,9 @@
+'''
+@Author: your name
+@Date: 2020-01-17 15:35:43
+@LastEditTime: 2020-01-17 15:48:28
+@LastEditors: your name
+@Description: In User Settings Edit
+@FilePath: \MyPython\cn\hn\hx\__init__.py
+'''
+__all__ = ["Message", "Information"]
