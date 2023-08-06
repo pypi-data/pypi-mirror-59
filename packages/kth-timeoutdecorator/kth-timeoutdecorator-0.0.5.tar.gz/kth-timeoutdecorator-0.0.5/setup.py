f1d275fd3160ef5a4fb0acec6861894417356471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:14 +0000

    Add package kth-timeoutdecorator/0.0.5/kth-timeoutdecorator-0.0.5.tar.gz

---
+++ packages/kth-timeoutdecorator/kth-timeoutdecorator-0.0.5.tar.gz/kth-timeoutdecorator-0.0.5/setup.py
@@ -0,0 +1,36 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='kth-timeoutdecorator',
+    version='0.0.5',
+    description='timeout decorator for python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Angel Hair',
+    author_email='asdwsl@foxmail.com',
+    url='https://github.com/Angel-Hair/kth-timeoutdecorator',
+    packages=['kth_timeoutdecorator'],
+    install_requires=[],
+    classifiers=CLASSIFIERS)
