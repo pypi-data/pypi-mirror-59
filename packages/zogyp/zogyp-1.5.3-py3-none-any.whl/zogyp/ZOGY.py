bc4b261e21f6541474f6ce9f74913a88b2fe4ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:54 +0000

    Add package zogyp/1.5.3/zogyp-1.5.3-py3-none-any.whl

---
+++ packages/zogyp/zogyp-1.5.3-py3-none-any.whl/zogyp/ZOGY.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+This script is to make the program executable in the shell
+"""
+
+import sys
+import glob
+import ntpath
+import time
+import shutil
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import numpy as np
+
+#ZOGY in Parallel routines
+from zogyp.zip import run_ZOGY
+
+
+def main():
+   run_ZOGY(sys.argv[1], sys.argv[2])
