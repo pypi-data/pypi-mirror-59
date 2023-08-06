77d13e0982fbe24bd2751b186477b3fe46eb27ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package pubtools-pulplib/2.4.0/pubtools-pulplib-2.4.0.tar.gz

---
+++ packages/pubtools-pulplib/pubtools-pulplib-2.4.0.tar.gz/pubtools-pulplib-2.4.0/pubtools/pulplib/_impl/model/__init__.py
@@ -0,0 +1,16 @@
+from .common import (
+    PulpObject,
+    DetachedException,
+    InvalidDataException,
+)
+from .repository import (
+    Repository,
+    YumRepository,
+    FileRepository,
+    ContainerImageRepository,
+    PublishOptions,
+)
+from .unit import Unit, FileUnit, RpmUnit, ModulemdUnit, ModulemdDefaultsUnit
+from .task import Task
+from .distributor import Distributor
+from .maintenance import MaintenanceReport, MaintenanceEntry
---
+++ packages/pubtools-pulplib/pubtools-pulplib-2.4.0.tar.gz/pubtools-pulplib-2.4.0/pubtools/pulplib/_impl/model/repository/yum.py
@@ -0,0 +1,20 @@
+from .base import Repository, repo_type
+from ..frozenlist import FrozenList
+from ..attr import pulp_attrib
+from ... import compat_attr as attr
+
+
+@repo_type("rpm-repo")
+@attr.s(kw_only=True, frozen=True)
+class YumRepository(Repository):
+    """A :class:`~pubtools.pulplib.Repository` for RPMs, errata and related content."""
+
+    # this class only overrides some defaults for attributes defined in super
+
+    type = pulp_attrib(default="rpm-repo", type=str, pulp_field="notes._repo-type")
+
+    mutable_urls = attr.ib(
+        default=attr.Factory(lambda: FrozenList(["repodata/repomd.xml"])),
+        type=list,
+        converter=FrozenList,
+    )
