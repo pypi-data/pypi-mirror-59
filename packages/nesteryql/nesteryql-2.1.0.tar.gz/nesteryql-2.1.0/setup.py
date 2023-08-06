5fa150707508eadf8d6f64b246a4b950c5ce0a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:34 +0000

    Add package nesteryql/2.1.0/nesteryql-2.1.0.tar.gz

---
+++ packages/nesteryql/nesteryql-2.1.0.tar.gz/nesteryql-2.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nesteryql',
+    version='2.1.0',
+    py_modules=['nester-yql'],
+    author='yinql',
+    author_email='yql1019@126.com',
+    url='http://www.yql.com',
+    description='A simple print list function',
+    )
