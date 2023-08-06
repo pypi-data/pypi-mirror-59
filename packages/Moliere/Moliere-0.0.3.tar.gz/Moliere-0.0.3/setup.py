5c8fb32feda14f2040cade54c7768d3d412cd39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:59 +0000

    Add package Moliere/0.0.3/Moliere-0.0.3.tar.gz

---
+++ packages/Moliere/Moliere-0.0.3.tar.gz/Moliere-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Moliere",
+    version="0.0.3",
+    author="Peter Menshih",
+    author_email="p.menshih@gmail.com",
+    description="Psycopg wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/pmenshih/moliere",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
