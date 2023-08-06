dd260c9ce1fc7bd9371a22a94bb18278e8b939e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:50 +0000

    Add package cocos/0.1.5/cocos-0.1.5-py3-none-any.whl

---
+++ packages/cocos/cocos-0.1.5-py3-none-any.whl/cocos/symbolic/__init__.py
@@ -0,0 +1,9 @@
+from ._lambdification import \
+    lambdify, \
+    LambdifiedArrayExpressions, \
+    LambdifiedMatrixExpressions, \
+    LambdifiedArrayExpression, \
+    LambdifiedMatrixExpression, \
+    LambdifiedVectorExpression
+
+from .utilities import find_length_of_state_vectors
