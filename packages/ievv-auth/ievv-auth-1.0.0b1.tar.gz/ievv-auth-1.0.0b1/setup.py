6ce99163106a97a6cf1cb5a409dbdd32272da734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package ievv-auth/1.0.0b1/ievv-auth-1.0.0b1.tar.gz

---
+++ packages/ievv-auth/ievv-auth-1.0.0b1.tar.gz/ievv-auth-1.0.0b1/setup.py
@@ -0,0 +1,35 @@
+import os
+import json
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'ievv_auth', 'version.json')) as f:
+    version = json.loads(f.read())
+
+
+setup(
+    name='ievv-auth',
+    description='Authentication modules for the Django framework.',
+    version=version,
+    author='Appresso developers',
+    author_email='post@appresso.no',
+    packages=find_packages(exclude=['manage']),
+    license='BSD',
+    url="https://github.com/appressoas/ievv_auth",
+    install_requires=[
+        'Django',
+        'PyJWT>=1.7.1',
+        'djangorestframework',
+        'psycopg2'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
