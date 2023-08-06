c59da3ee2b845b068af5099d1fda1ad2c32e9982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package torchio/0.11.6/torchio-0.11.6.tar.gz

---
+++ packages/torchio/torchio-0.11.6.tar.gz/torchio-0.11.6/torchio/transforms/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from .spatial.pad import Pad
+from .spatial.crop import Crop
+from .spatial.resample import Resample
+
+from .intensity.rescale import Rescale
+from .intensity.z_normalization import ZNormalization
+from .intensity.histogram_standardization import HistogramStandardization
