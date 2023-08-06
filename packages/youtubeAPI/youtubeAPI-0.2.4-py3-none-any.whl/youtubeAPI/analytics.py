a5019ece9c8ffa5adf827757979fdb155109f38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:06 +0000

    Add package youtubeAPI/0.2.4/youtubeAPI-0.2.4-py3-none-any.whl

---
+++ packages/youtubeAPI/youtubeAPI-0.2.4-py3-none-any.whl/youtubeAPI/analytics.py
@@ -0,0 +1,9 @@
+import pickle
+import calendar
+
+with open('.credentials/analytics.pkl', 'rb') as file:
+	analytics_service = pickle.load(file)
+
+def endMonth(year, month):
+	endDay = calendar.monthrange(year, month)[1]
+	return endDay
