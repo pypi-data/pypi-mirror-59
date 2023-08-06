29cbd24d81ae53c33ed553c81676a3fb98962c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:36 +0000

    Add package keios-dynabuffers-sol/0.2.0/keios-dynabuffers-sol-0.2.0.tar.gz

---
+++ packages/keios-dynabuffers-sol/keios-dynabuffers-sol-0.2.0.tar.gz/keios-dynabuffers-sol-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['keios_dynabuffers_sol', 'keios_dynabuffers_sol.optical_character_recognition']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'keios-dynabuffers-sol',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'fridayy',
+    'author_email': 'benjamin.krenn@leftshift.one',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
