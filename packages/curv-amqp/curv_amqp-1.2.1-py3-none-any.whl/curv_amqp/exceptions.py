4ac027c96fe639a7ffe263f5357a32d43250ec40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:50 +0000

    Add package curv-amqp/1.2.1/curv_amqp-1.2.1-py3-none-any.whl

---
+++ packages/curv-amqp/curv_amqp-1.2.1-py3-none-any.whl/curv_amqp/exceptions.py
@@ -0,0 +1,31 @@
+class AMQPError(Exception):
+    pass
+
+
+class ReconnectingToFastError(AMQPError):
+    pass
+
+
+class ClosedError(AMQPError):
+    pass
+
+
+class ConnectionClosedError(ClosedError):
+    pass
+
+
+class ChannelClosedError(ClosedError):
+    pass
+
+
+class PriorityLevelError(AMQPError):
+    pass
+
+
+class RequeueRetryCountError(AMQPError):
+    pass
+
+
+class MessageAutoAckError(AMQPError):
+    pass
+
