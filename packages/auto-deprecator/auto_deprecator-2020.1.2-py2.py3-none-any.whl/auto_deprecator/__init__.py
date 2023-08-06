e52965b473aa1379241ff9927f784782eb97588d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:35 +0000

    Add package auto-deprecator/2020.1.2/auto_deprecator-2020.1.2-py2.py3-none-any.whl

---
+++ packages/auto-deprecator/auto_deprecator-2020.1.2-py2.py3-none-any.whl/auto_deprecator/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Auto deprecator."""
+# flake8: noqa
+
+__author__ = """Gavin Chan"""
+__email__ = "gavincyi@gmail.com"
+__version__ = "2020.1.2"
+
+from auto_deprecator.deprecate import (
+    deprecate,
+    check_deprecation,
+)
