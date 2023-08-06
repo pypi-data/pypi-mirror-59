a1711665f7b9e43a3a7f93a503c92f3a90318f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:18 +0000

    Add package Autogovernance/0.7.1/Autogovernance-0.7.1.tar.gz

---
+++ packages/Autogovernance/Autogovernance-0.7.1.tar.gz/Autogovernance-0.7.1/autogovernance/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 12:32:13 2020
+
+@author: LP885RH
+"""
+from element import QR_object
+from element import BusinessTerm
+from element import Attributes
+from element import Element
+from . import tables
+from . import browser
+
---
+++ packages/Autogovernance/Autogovernance-0.7.1.tar.gz/Autogovernance-0.7.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 09:36:01 2020
+
+@author: LP885RH
+"""
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='Autogovernance',
+      version='0.7.1',
+      description='Package to update information in Governance',
+      url='https://github.com/JulioPestanaSalinas/AutoGovernance',
+      author='Julio Pestaña Salinas',
+      author_email='Julio.Pestana.Salinas@es.ey.com',
+      license='GNU',
+      packages=['autogovernance'],
+      zip_safe=False,
+      python_requires='>=3.6',
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
