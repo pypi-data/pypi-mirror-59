ee2ec59b99b512aa8b3bde660623448b313bc987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:01 +0000

    Add package crawltools/0.1.2/crawltools-0.1.2-py3-none-any.whl

---
+++ packages/crawltools/crawltools-0.1.2-py3-none-any.whl/crawltools/webtoon/pipelines.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+
+
+class WebtoonPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url, meta={'image_names': item["image_names"]})
+
+    def file_path(self, request, response=None, info=None):
+        return next(iter(request.meta['image_names']))
