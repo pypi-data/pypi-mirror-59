fe553cc20a28455b3d4ee79b8551fb414b400c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:22 +0000

    Add package pybsv/0.0.4/pybsv-0.0.4.tar.gz

---
+++ packages/pybsv/pybsv-0.0.4.tar.gz/pybsv-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# Read the contents of your README file to upload with PyPi package.
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pybsv",
+    version="0.0.4",
+    description="An easy to use Python BSV client.",
+    url="http://github.com/jannainm/pybsv",
+    author="jannainm",
+    author_email="jannainm@gmail.com",
+    license="MIT",
+    packages=["pybsv"],
+    install_requires=["bitsv<1"],
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
