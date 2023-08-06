f54c24c10622461448f06a8988bf7f937c540608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:45 +0000

    Add package textdirectory/0.3.1/textdirectory-0.3.1-py2.py3-none-any.whl

---
+++ packages/textdirectory/textdirectory-0.3.1-py2.py3-none-any.whl/textdirectory/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for textdirectory."""
+
+__author__ = """Ingo Kleiber"""
+__email__ = 'ingo@kleiber.me'
+__version__ = '0.3.1'
+
+from textdirectory import *
+from textdirectory.textdirectory import *
