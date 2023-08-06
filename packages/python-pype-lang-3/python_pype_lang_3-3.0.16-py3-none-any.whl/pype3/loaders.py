9bee96b6c7e2334c347eb34dabe5c641583818ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:47 +0000

    Add package python-pype-lang-3/3.0.16/python_pype_lang_3-3.0.16-py3-none-any.whl

---
+++ packages/python-pype-lang-3/python_pype_lang_3-3.0.16-py3-none-any.whl/pype3/loaders.py
@@ -0,0 +1,23 @@
+import json
+
+def dump_json(fileName,js):
+
+    with open(fileName,'w') as f:
+
+        f.write(json.dumps(js))
+
+
+def load_list_file(fileName):
+
+    with open(fileName,'r') as f:
+
+        return [ln for ln in f.read().splitlines()]
+
+
+def load_json(fileName):
+
+    with open(fileName,'r') as f:
+
+        js=json.loads(f.read())
+
+        return js
