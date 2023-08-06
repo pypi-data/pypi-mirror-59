1954bfcbdd10a529887fb28d80b0e14ac3e57759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:28 +0000

    Add package pybetter/0.2.0/pybetter-0.2.0.tar.gz

---
+++ packages/pybetter/pybetter-0.2.0.tar.gz/pybetter-0.2.0/pybetter/transformers/parenthesized_return.py
@@ -0,0 +1,17 @@
+import libcst as cst
+import libcst.matchers as m
+
+
+class RemoveParenthesesFromReturn(cst.CSTTransformer):
+    def leave_Return(
+        self, original_node: cst.Return, updated_node: cst.Return
+    ) -> cst.Return:
+        if not m.matches(original_node.value, m.Tuple()):
+            return original_node
+
+        if not original_node.value.lpar:
+            return original_node
+
+        changed_tuple = original_node.value.with_changes(lpar=[], rpar=[])
+
+        return updated_node.with_changes(value=changed_tuple)
