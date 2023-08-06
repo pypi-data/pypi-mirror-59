528f5ebf850c3053b1d1c675ccc320f31a1a0fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:47 +0000

    Add package magnet4c/0.0.1/magnet4c-0.0.1-py3-none-any.whl

---
+++ packages/magnet4c/magnet4c-0.0.1-py3-none-any.whl/magnet/parser.py
@@ -0,0 +1,45 @@
+import json
+from magnet.datastructs import jsonPos
+import copy
+
+class magnetParser:
+    def parseJson(self, jsonStr):
+        obj = json.loads(jsonStr)
+        corporas = []
+        pos = jsonPos()
+        pos.add(dict, 'blocks')
+        for i in range(len(obj['blocks'])):
+            b = obj['blocks'][i]
+            np = copy.deepcopy(pos)
+            np.add(list, i)
+            self.parseElement(b, corporas, np)
+        return corporas
+
+
+    def parseElement(self, eleObj, corporas, pos: jsonPos):
+        if type(eleObj) != dict:
+            if type(eleObj) == list:
+                for i in range(len(eleObj)):
+                    l = eleObj[i]
+                    np = copy.deepcopy(pos)
+                    np.add(list, i)
+                    self.parseElement(l, corporas, np)
+            else:
+                corporas.append((eleObj, pos))
+        else:
+            if 'c' not in eleObj.keys():
+                return
+            if 't' in eleObj.keys():
+                if eleObj['t'] == 'Image':
+                    return
+            if type(eleObj['c']) == list:
+                pos.add(dict, 'c')
+                for i in range(len(eleObj['c'])):
+                    c = eleObj['c'][i]
+                    np = copy.deepcopy(pos)
+                    np.add(list, i)
+                    self.parseElement(c, corporas, np)
+            else:
+                np = copy.deepcopy(pos)
+                np.add(dict, 'c')
+                self.parseElement(eleObj['c'], corporas, np)
