4d34a9536c1fcf220ed3299c38eeb42f7e09695c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:34 +0000

    Add package rh-doozer/0.5.39/rh_doozer-0.5.39-py2.py3-none-any.whl

---
+++ packages/rh-doozer/rh_doozer-0.5.39-py2.py3-none-any.whl/doozerlib/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from .runtime import Runtime
+from .pushd import Dir
+
+
+def version():
+    from os.path import abspath, dirname, join
+    filename = join(dirname(abspath(__file__)), 'VERSION')
+    return open(filename).read().strip()
