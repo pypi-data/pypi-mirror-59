cdac116b4c90534df20991b1bb2f5de2d5578a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:39 +0000

    Add package dato/0.0.1a1/dato-0.0.1a1.tar.gz

---
+++ packages/dato/dato-0.0.1a1.tar.gz/dato-0.0.1a1/dato/ml/preprocessing.py
@@ -0,0 +1,23 @@
+"""
+Matplotlib plotting functions.
+"""
+
+from ..base import Pipeable, use_first_arg_only
+from .base import _ModelSpec
+
+@Pipeable
+def InitModel(df, label, *args, **kwargs):
+    """Specify the dataframe and the label for a machine learning model, and set up the class object that will act as accumulator.
+    """
+    X_columns = [col for col in df.columns if col!=label]
+    newdf = df[X_columns + [label]]
+    m = _ModelSpec(newdf)
+    return m
+
+
+@Pipeable
+def TrainTestSplit(m, **kwargs):
+    train, test = m.train_test_split(**kwargs)
+    return m
+
+
