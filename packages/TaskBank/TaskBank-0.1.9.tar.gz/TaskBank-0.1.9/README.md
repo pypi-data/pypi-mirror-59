bddc3e4d3b7c389e7bc06358ef95b34ad096a2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:01 +0000

    Add package TaskBank/0.1.9/TaskBank-0.1.9.tar.gz

---
+++ packages/TaskBank/TaskBank-0.1.9.tar.gz/TaskBank-0.1.9/README.md
@@ -0,0 +1,2 @@
+To launch test, go to project root and type "ward" in terminal
+ test
---
+++ packages/TaskBank/TaskBank-0.1.9.tar.gz/TaskBank-0.1.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = ["TaskBank"]
+
+setuptools.setup(
+     name="TaskBank",
+     version="0.1.9",
+     author="Alexandre Kempf",
+     author_email="alexanre.kempf@cri-paris.org",
+     description="A library of machine learning tasks for HackDuck",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/AlexandreKempf/TaskBank",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],)
