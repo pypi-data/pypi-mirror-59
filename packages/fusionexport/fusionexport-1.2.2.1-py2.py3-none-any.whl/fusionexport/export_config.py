87c4237ecd51755188b787989a0fc677712fc56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package fusionexport/1.2.2.1/fusionexport-1.2.2.1-py2.py3-none-any.whl

---
+++ packages/fusionexport/fusionexport-1.2.2.1-py2.py3-none-any.whl/fusionexport/export_config.py
@@ -0,0 +1,186 @@
+import os
+import tempfile
+import json
+
+from .constants import Constants
+from .utils import Utils
+from .export_error import ExportError
+from .typings import typings
+from .converters import BooleanConverter, NumberConverter, ChartConfigConverter, EnumConverter, FileConverter, HtmlConverter, ObjectConverter
+
+class ExportConfig(object):
+    def __init__(self, config_dict=None):
+        self.__configs = {}
+
+        if config_dict is not None:
+            for config_name, config_value in config_dict.items():
+                self.set(config_name, config_value)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def set(self, config_name, config_value):
+        self.__configs[config_name] = self.__resolve_config_value(config_name, config_value)
+
+    def __resolve_config_value(self, config_name, config_value):
+        if config_name not in typings:
+            raise ExportError("Invalid export config: %s" % config_name)
+
+        if config_name == "template" or config_name == "templateFilePath":
+            if "templateFilePath" in self.__configs or "template" in self.__configs:
+                print("Both 'templateFilePath' and 'template' is provided. 'templateFilePath' will be ignored.");
+
+        converter = typings[config_name].get("converter", None)
+        if converter is not None:
+            if converter == "BooleanConverter":
+                return BooleanConverter.convert(config_value, config_name)
+            elif converter == "NumberConverter":
+                return NumberConverter.convert(config_value, config_name)
+            elif converter == "ChartConfigConverter":
+                return ChartConfigConverter.convert(config_value)
+            elif converter == "EnumConverter":
+                dataset = typings[config_name].get("dataset")
+                return EnumConverter.convert(config_value, dataset, config_name)
+            elif converter == "FileConverter":
+                return FileConverter.convert(config_value, config_name)
+            elif converter == "HtmlConverter":
+                return HtmlConverter.convert(config_value, config_name)
+            elif converter == "ObjectConverter":
+                return ObjectConverter.convert(config_value, config_name)
+            else:
+                raise ExportError("Unknown converter: %s" % converter)
+        elif typings[config_name]["type"] == "string":
+            return str(config_value)
+        else:
+            raise ExportError("Could not resolve the config name and config value")
+
+    def get(self, config_name):
+        return self.__configs[config_name]
+
+    def __delitem__(self, index):
+        del self.__configs[index]
+
+    def remove(self, config_name):
+        if config_name in self.__configs:
+            self.__configs.pop(config_name)
+            return True
+        else:
+            return False
+
+    def __contains__(self, key):
+        return key in self.__configs
+
+    def has(self, config_name):
+        return config_name in self.__configs
+
+    def clear(self):
+        self.__configs.clear()
+
+    def __len__(self):
+        return len(self.__configs)
+
+    def count(self):
+        return len(self.__configs)
+
+    def config_names(self):
+        return list(self.__configs)
+
+    def config_values(self):
+        return list(self.__configs.values())
+
+    def clone(self):
+        return ExportConfig(self.__configs)
+
+    def get_formatted_configs(self):
+        configs = self.__process_config_values()
+        configs.pop(Constants.EXPORT_CONFIG_NAME_RESOURCE_FILE_PATH, None)
+        return configs
+
+    def __process_config_values(self):
+        configs = self.__configs.copy()
+        zip_files_map = []
+        
+        configs["clientName"] = "Python"
+
+        if Constants.EXPORT_CONFIG_NAME_INPUTSVG in configs:
+            self.__resolve_zip_path_config(
+                configs,
+                zip_files_map,
+                Constants.EXPORT_CONFIG_NAME_INPUTSVG,
+                Constants.EXPORT_CONFIG_ZIP_PATH_INPUTSVG
+            )
+
+        if Constants.EXPORT_CONFIG_NAME_CALLBACK_FILE_PATH in configs:
+            self.__resolve_zip_path_config(
+                configs,
+                zip_files_map,
+                Constants.EXPORT_CONFIG_NAME_CALLBACK_FILE_PATH,
+                Constants.EXPORT_CONFIG_ZIP_PATH_CALLBACK_FILE_PATH
+            )
+        
+        if Constants.EXPORT_CONFIG_NAME_DASHBOARD_LOGO in configs:
+            ext = os.path.splitext(configs[Constants.EXPORT_CONFIG_NAME_DASHBOARD_LOGO])[1]
+            self.__resolve_zip_path_config(
+                configs,
+                zip_files_map,
+                Constants.EXPORT_CONFIG_NAME_DASHBOARD_LOGO,
+                Constants.EXPORT_CONFIG_ZIP_PATH_DASHBOARD_LOGO + ext
+            )
+
+        if Constants.EXPORT_CONFIG_NAME_OUTPUT_FILE_DEFINITION in configs:
+            self.__resolve_zip_path_config(
+                configs,
+                zip_files_map,
+                Constants.EXPORT_CONFIG_NAME_OUTPUT_FILE_DEFINITION,
+                Constants.EXPORT_CONFIG_ZIP_PATH_OUTPUT_FILE_DEFINITION
+            )
+
+        if Constants.EXPORT_CONFIG_NAME_TEMPLATE_FILE_PATH in configs:
+            fileContent = configs[Constants.EXPORT_CONFIG_NAME_TEMPLATE_FILE_PATH]
+            # If it is not a file but html content, then save the content to a temp file and set the path of that temp file
+            if (fileContent.startswith("<")):
+                tmp = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
+                tmp.writelines(fileContent)
+                tmp.close();
+                configs[Constants.EXPORT_CONFIG_NAME_TEMPLATE_FILE_PATH] = tmp.name
+
+            template_zip_files_map, prefixed_template_zip_path = Utils.create_template_zip_paths(
+                configs[Constants.EXPORT_CONFIG_NAME_TEMPLATE_FILE_PATH],
+                configs.get(Constants.EXPORT_CONFIG_NAME_RESOURCE_FILE_PATH, None)
+            )
+            configs[Constants.EXPORT_CONFIG_NAME_TEMPLATE_FILE_PATH] = prefixed_template_zip_path
+            zip_files_map.extend(template_zip_files_map)
+
+        if Constants.EXPORT_CONFIG_NAME_ASYNC_CAPTURE in configs:
+            bool_val = configs[Constants.EXPORT_CONFIG_NAME_ASYNC_CAPTURE]
+            configs[Constants.EXPORT_CONFIG_NAME_ASYNC_CAPTURE] = str(bool_val).lower()
+
+        if len(zip_files_map) > 0:
+            zip_file_path = Utils.generate_zip_file(zip_files_map)
+            configs[Constants.EXPORT_CONFIG_NAME_PAYLOAD] = zip_file_path
+        
+        return configs
+
+    def __resolve_zip_path_config(self, configs, zip_files_map, config_name, zip_path):
+        zip_files_map.append({
+            "zipPath": zip_path,
+            "localPath": configs[config_name]
+        })
+        configs[config_name] = zip_path
+
+    def __str__(self):
+        return str(self.__configs)
+
+    def __eq__(self, other):
+        if isinstance(other, ExportConfig):
+            return self.__configs == other.__configs
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
---
+++ packages/fusionexport/fusionexport-1.2.2.1-py2.py3-none-any.whl/fusionexport/utils.py
@@ -0,0 +1,179 @@
+import sys
+import base64
+import json
+import os
+import glob2
+import tempfile
+import shutil
+from bs4 import BeautifulSoup
+
+from .constants import Constants
+from .export_error import ExportError
+
+class Utils(object):
+    @staticmethod
+    def read_text_file(file_path):
+        with open(file_path, "r") as f:
+            return f.read()
+
+    @staticmethod
+    def json_parse(value):
+        try:
+            return json.loads(value)
+        except Exception:
+            return None
+
+    @staticmethod
+    def create_template_zip_paths(template_file_path, resource_file_path):
+        template_file_path = os.path.abspath(template_file_path)
+
+        ref_files = Utils.extract_ref_files_from_template(template_file_path)
+        base_path, res_paths = Utils.normalize_resource_file_paths(resource_file_path)
+        if base_path is None:
+            base_path = Utils.get_common_path([template_file_path] + ref_files)
+        
+        if base_path == "":
+            raise ExportError("Couldn't calculate the basepath of template resources")
+        
+        res_paths = list(filter(lambda path: Utils.is_within_path(path, base_path), res_paths))
+
+        zip_files_map = Utils.create_template_zip_files_map(
+            ref_files + res_paths + [template_file_path],
+            base_path
+        )
+
+        prefixed_template_file_zip_path = os.path.join(
+            Constants.TEMPLATE_ZIP_PREFIX,
+            Utils.get_rel_path_within_root(template_file_path, base_path)
+        )
+
+        return zip_files_map, prefixed_template_file_zip_path
+    
+    @staticmethod
+    def create_template_zip_files_map(paths, base_path):
+        return list(map(
+            lambda path: { "zipPath": Utils.create_prefixed_template_zip_path(path, base_path), "localPath": path },
+            paths
+        ))
+    
+    @staticmethod
+    def create_prefixed_template_zip_path(local_path, base_path):
+        rel_path = Utils.get_rel_path_within_root(local_path, base_path)
+        return os.path.join(Constants.TEMPLATE_ZIP_PREFIX, rel_path)
+    
+    @staticmethod
+    def generate_zip_file(zip_files_map):
+        temp_dir = tempfile.mkdtemp()
+        temp_write_dir = os.path.abspath(os.path.join(temp_dir, "files"))
+        os.makedirs(temp_write_dir)
+
+        for path_map in zip_files_map:
+            rel_path = path_map["zipPath"]
+            if rel_path is not None:
+                rel_path = rel_path.strip(os.sep)
+                temp_output_file_path = os.path.abspath(os.path.join(temp_write_dir, rel_path))
+                if not os.path.exists(os.path.dirname(temp_output_file_path)):
+                    os.makedirs(os.path.dirname(temp_output_file_path))
+                shutil.copyfile(path_map["localPath"], temp_output_file_path)
+
+        return shutil.make_archive(os.path.abspath(os.path.join(temp_dir, "archive")), 'zip', temp_write_dir)
+    
+    @staticmethod
+    def is_within_path(target_path, parent_path):
+        return target_path.startswith(parent_path + os.sep)
+
+    @staticmethod
+    def get_rel_path_within_root(path, root_path):
+        rel_path = os.path.relpath(path, root_path)
+        if not rel_path.startswith(os.pardir + os.sep):
+            return rel_path
+
+    @staticmethod
+    def get_common_path(paths):
+        return os.path.abspath(os.path.dirname(os.path.commonprefix(paths)))
+
+    @staticmethod
+    def extract_ref_files_from_template(template_file_path):
+        if template_file_path is None:
+            return []
+
+        ref_files = []
+        template_file_path = os.path.abspath(template_file_path)
+        template_file_dir = os.path.dirname(template_file_path)
+        html_template = Utils.read_text_file(template_file_path)
+        html_soup = BeautifulSoup(html_template, 'html.parser')
+
+        for link in html_soup.find_all('link'):
+            ref = Utils.resolve_template_ref(link.get("href"), template_file_dir)
+            if ref is not None:
+                ref_files.append(ref)
+
+        for script in html_soup.find_all('script'):
+            ref = Utils.resolve_template_ref(script.get("src"), template_file_dir)
+            if ref is not None:
+                ref_files.append(ref)
+
+        for img in html_soup.find_all('img'):
+            ref = Utils.resolve_template_ref(img.get("src"), template_file_dir)
+            if ref is not None:
+                ref_files.append(ref)
+
+        return ref_files
+
+    @staticmethod
+    def normalize_resource_file_paths(resource_file_path):
+        if resource_file_path is None:
+            return None, []
+
+        resource_file_path = os.path.abspath(resource_file_path)
+        resource_file_dir = os.path.dirname(resource_file_path)
+
+        rc_config = Utils.json_parse(Utils.read_text_file(resource_file_path))
+        if rc_config is None:
+            raise ExportError("Failed to parse the resource JSON file")
+
+        base_path = rc_config.get("basePath", None)
+        # Resolve base_path wrt resource_file_dir not cwd
+        base_path = os.path.abspath(os.path.join(resource_file_dir, base_path)) if base_path is not None else None
+
+        include_paths = Utils.glob_matched_paths(rc_config.get("include", None), resource_file_dir)
+        exclude_paths = Utils.glob_matched_paths(rc_config.get("exclude", None), resource_file_dir)
+
+        return base_path, list(set(include_paths).difference(set(exclude_paths)))
+
+    @staticmethod
+    def resolve_template_ref(ref, template_file_dir):
+        if ref is None or ref == "":
+            return None
+
+        if not Utils.is_url(ref):
+            return os.path.abspath(os.path.join(template_file_dir, ref))
+
+    @staticmethod
+    def glob_matched_paths(glob_pats, root_path):
+        if glob_pats is None:
+            return []
+
+        root_path = os.path.abspath(root_path)
+        matched_paths = []
+
+        prev_cwd = os.getcwd()
+        os.chdir(root_path)
+
+        for glob_pat in glob_pats:
+            for path in glob2.iglob(glob_pat):
+                path = os.path.abspath(os.path.join(root_path, path))
+                if os.path.isfile(path):
+                    matched_paths.append(path)
+
+        os.chdir(prev_cwd)
+        return matched_paths
+
+    @staticmethod
+    def is_url(value):
+        value = str(value)
+        prefixes = ["//", "http://", "https://", "file://"]
+        for prefix in prefixes:
+            if value.startswith(prefix):
+                return True
+        return False
