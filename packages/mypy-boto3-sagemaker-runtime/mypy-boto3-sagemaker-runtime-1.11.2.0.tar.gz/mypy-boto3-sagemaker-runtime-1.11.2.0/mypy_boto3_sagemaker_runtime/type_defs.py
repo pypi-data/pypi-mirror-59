932d5f458264413dca8a39343e62bca936427e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:48 +0000

    Add package mypy-boto3-sagemaker-runtime/1.11.2.0/mypy-boto3-sagemaker-runtime-1.11.2.0.tar.gz

---
+++ packages/mypy-boto3-sagemaker-runtime/mypy-boto3-sagemaker-runtime-1.11.2.0.tar.gz/mypy-boto3-sagemaker-runtime-1.11.2.0/mypy_boto3_sagemaker_runtime/type_defs.py
@@ -0,0 +1,32 @@
+"""
+Main interface for sagemaker-runtime service type definitions.
+
+Usage::
+
+    from mypy_boto3.sagemaker_runtime.type_defs import ClientInvokeEndpointResponseTypeDef
+
+    data: ClientInvokeEndpointResponseTypeDef = {...}
+"""
+from __future__ import annotations
+
+import sys
+from botocore.response import StreamingBody
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("ClientInvokeEndpointResponseTypeDef",)
+
+ClientInvokeEndpointResponseTypeDef = TypedDict(
+    "ClientInvokeEndpointResponseTypeDef",
+    {
+        "Body": StreamingBody,
+        "ContentType": str,
+        "InvokedProductionVariant": str,
+        "CustomAttributes": str,
+    },
+    total=False,
+)
