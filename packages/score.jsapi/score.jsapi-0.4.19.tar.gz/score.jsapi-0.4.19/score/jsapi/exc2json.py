f53571200bb521af1ad9eb26a11a14c800e2fe5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:00 +0000

    Add package score.jsapi/0.4.19/score.jsapi-0.4.19.tar.gz

---
+++ packages/score.jsapi/score.jsapi-0.4.19.tar.gz/score.jsapi-0.4.19/score/jsapi/exc2json.py
@@ -0,0 +1,66 @@
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2018-2020 Necdet Can Ateşman <can@atesman.at>, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in
+# the file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district
+# the Licensee has his registered seat, an establishment or assets.
+
+
+import traceback
+
+
+def exc2json(excinfo, untrace=[]):
+    """
+    Converts exception info (as returned by :func:`sys.exc_info`) into a
+    3-tuple that can be converted into a json string by python's :mod:`json`
+    library. It will consist of the exception name, the message and the stack
+    trace as provided by :func:`traceback.extract_tb`::
+
+        {
+            type: 'ZeroDivisionError',
+            message: 'division by zero',
+            trace: [
+                [<filename>, <lineno>, <line>],
+                ...
+            ]
+        }
+
+    It is possible to omit the last value of *excinfo*, effectively reducing
+    the generated exception description to a class name and a message.
+
+    The optional parameter *untrace* contains file names that will be removed
+    from the beginning of the stack trace.
+    """
+    trace = None
+    if len(excinfo) > 2:
+        trace = traceback.extract_tb(excinfo[2])
+        untrace.append(__file__)
+        while trace and any(skip for skip in untrace if skip in trace[0][0]):
+            trace = trace[1:]
+        trace = list(map(
+            lambda frame: (frame[0], frame[1], frame[2], frame[3]), trace))
+    return {
+        'type': excinfo[0].__name__,
+        'message': str(excinfo[1]),
+        'trace': trace,
+    }
---
+++ packages/score.jsapi/score.jsapi-0.4.19.tar.gz/score.jsapi-0.4.19/setup.py
@@ -0,0 +1,92 @@
+# Copyright © 2015-2018 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2018-2020 Necdet Can Ateşman <can@atesman.at>, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in
+# the file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district
+# the Licensee has his registered seat, an establishment or assets.
+
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.jsapi',
+    version='0.4.19',
+    description='Javascript API generator of The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='score@strg.at',
+    url='http://score-framework.org',
+    keywords='score framework web javascript rpc api',
+    packages=['score', 'score.jsapi'],
+    namespace_packages=['score'],
+    package_dir={
+        'score.jsapi': 'score/jsapi',
+    },
+    package_data={
+        'score.jsapi': [
+            'tpl/umd/unified.js',
+            'tpl/umd/exception.js',
+            'tpl/umd/endpoint.js',
+            'tpl/umd/queue.js',
+            'tpl/umd/endpoint/url.js',
+            'tpl/umd/excformat.js',
+            'tpl/es6/unified.js',
+            'tpl/es6/exception.js',
+            'tpl/es6/queue.js',
+            'tpl/es6/endpoint/index.js',
+            'tpl/es6/endpoint/url.js',
+            'tpl/es6/endpoint/base.js',
+            'tpl/es6/excformat.js',
+        ]
+    },
+    zip_safe=False,
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=[
+        'score.init >= 0.3',
+        'score.ctx >= 0.3',
+    ],
+    entry_points={
+        'score.cli': [
+            'jsapi = score.jsapi.cli:main',
+        ],
+    },
+)
