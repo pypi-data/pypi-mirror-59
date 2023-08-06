5b06e45fb90a407f4c8677930b45a166b41e2952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:01 +0000

    Add package topsis-naveen/1.2/topsis-naveen-1.2.tar.gz

---
+++ packages/topsis-naveen/topsis-naveen-1.2.tar.gz/topsis-naveen-1.2/README.md
@@ -0,0 +1,3 @@
+# Topsis Package
+
+This is a simple package used for implementing topsis functionality easily.
---
+++ packages/topsis-naveen/topsis-naveen-1.2.tar.gz/topsis-naveen-1.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsis-naveen",
+    author="Naveen Budhwal",
+    version="1.2",
+    author_email="author@example.com",
+    description="A package used to implement topsis functionality easily",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
