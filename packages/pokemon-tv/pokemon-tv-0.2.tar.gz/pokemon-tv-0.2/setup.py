c3fada99f92294b3b8198a5ae20b74b1ffb6a88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:54 +0000

    Add package pokemon-tv/0.2/pokemon-tv-0.2.tar.gz

---
+++ packages/pokemon-tv/pokemon-tv-0.2.tar.gz/pokemon-tv-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import py2exe
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pokemon-tv',
+    version='0.2',
+    packages=['pokemon-tv'],
+    data_files=[('icon', ['pokemon-tv/icon.png'])],
+    url='https://github.com/davFaithid/pokemon-tv',
+    license='GPL-3.0',
+    author='davFaithid',
+    author_email='',
+    description='Watch Pokemon in python3.',
+    console=['pokemon-tv/__init__.py'],
+    options={
+            "py2exe":{
+                    "unbuffered": True,
+                    "optimize": 2,
+                    "excludes": ["email"]
+                }
+        }
+)
