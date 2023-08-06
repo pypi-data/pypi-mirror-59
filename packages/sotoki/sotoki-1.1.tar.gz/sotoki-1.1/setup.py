1083482fd9742e88a9f704e767d2d27f2fc6ee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package sotoki/1.1/sotoki-1.1.tar.gz

---
+++ packages/sotoki/sotoki-1.1.tar.gz/sotoki-1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sotoki',
+    version='1.1',
+    description="Make zimfile from stackexchange dump",
+    long_description=open('README.md').read(),
+    author='dattaz',
+    author_email='taz@dattaz.fr',
+    url='http://github.com/kiwix/sotoki',
+    keywords="kiwix zim stackexchange offline",
+    license="GPL",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        'Jinja2==2.8',
+        'lxml==3.4.4',
+        'MarkupSafe==0.23',
+        'docopt==0.6.2',
+        'slugify==0.0.1',
+        'pydenticon==0.2',
+        'bs4',
+        'envoy',
+        'subprocess32',
+        'filemagic',
+        'mistune'
+        ],
+    zip_safe=False,
+    platforms='Linux',
+    include_package_data=True,
+    entry_points={
+            'console_scripts': ['sotoki=sotoki.sotoki:run'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
