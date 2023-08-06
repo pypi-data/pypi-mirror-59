518e72f97bfef4dab39fedaae70e345f49f8e44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package pychemia/0.20.1.21/pychemia-0.20.1.21.tar.gz

---
+++ packages/pychemia/pychemia-0.20.1.21.tar.gz/pychemia-0.20.1.21/pychemia/code/abinit/__init__.py
@@ -0,0 +1,19 @@
+"""
+Set of classes and functions to manipulate
+
+ABINIT '.in' input files
+ABINIT '.files' files
+ABINIT '_OUT.nc' output files
+
+"""
+from . import task
+
+from .abifiles import AbiFiles
+from .input import AbinitInput, xyz2input
+from .abinit import AbinitJob
+from .output import AbinitOutput
+from .run import AbinitRun
+from .utils import psp_name
+from .parser import parser
+
+# __all__ = filter(lambda s: not s.startswith('_'), dir())
---
+++ packages/pychemia/pychemia-0.20.1.21.tar.gz/pychemia-0.20.1.21/pychemia/version.py
@@ -0,0 +1,17 @@
+
+# THIS FILE IS GENERATED FROM PYCHEMIA SETUP.PY
+short_version = '0.20.1.21'
+version = '0.20.1.21'
+full_version = '0.20.1.21.dev0+2d8dd12'
+git_revision = '2d8dd12ecf87129b04dbc6d36119581a50467922'
+name = 'pychemia'
+description = 'Python Framework for Materials Discovery and Design'
+url = 'https://materialsdiscovery.github.io/PyChemia'
+author = 'Guillermo Avendaño Franco'
+email = 'gufranco@mail.wvu.edu'
+status = 'Development'
+copyright = 'Copyright 2020'
+date = 'January 21, 2020'
+release = False
+if not release:
+    version = full_version
