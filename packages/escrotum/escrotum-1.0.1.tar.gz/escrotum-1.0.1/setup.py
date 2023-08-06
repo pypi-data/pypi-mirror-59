49a5cd7c2facf0b98fc6961344f3672d34c325ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package escrotum/1.0.1/escrotum-1.0.1.tar.gz

---
+++ packages/escrotum/escrotum-1.0.1.tar.gz/escrotum-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="escrotum",
+    description="Minimalist screenshot capture and screen recording program inspired by scrot.",
+    long_description=long_description,
+    version="1.0.1",
+    author='Roger Duran',
+    author_email='rogerduran@gmail.com',
+    url='https://github.com/Roger/escrotum',
+    download_url='https://github.com/Roger/escrotum/archive/1.0.1.tar.gz',
+    keywords=['screenshot', 'screen-recording', 'scrot', 'cli'],
+    packages=["escrotum"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'gobject',
+      'xcffib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'escrotum = escrotum.main:run',
+        ],
+    }
+)
