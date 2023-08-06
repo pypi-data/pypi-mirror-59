5114acff6f60e21fdd1fc9539be4833ed953e485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:33 +0000

    Add package MyQRcode/2.7.5/MyQRcode-2.7.5.tar.gz

---
+++ packages/MyQRcode/MyQRcode-2.7.5.tar.gz/MyQRcode-2.7.5/MyQRcode/mylibs/draw.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+import os
+import requests
+from io import BytesIO
+import urllib.request
+
+
+def draw_qrcode(abspath, qrmatrix, qrcolor):
+    unit_len = 3
+    x = y = 4*unit_len
+    pic = Image.new(mode='RGB', size=[
+                    (len(qrmatrix)+8)*unit_len]*2, color='#ffffff')
+    bg = urllib.request.urlretrieve('http://placehold.it/200x200', 'bg.jpg')
+    im = Image.open('bg.jpg', 'r')
+
+    coord = a, b = 10, 10
+
+    pix_val = list(im.getdata())
+    # pix_val_flat = [x for sets in pix_val for x in sets]
+
+    print(pix_val)
+
+    for line in qrmatrix:
+        for module in line:
+            if module:
+                draw_a_black_unit(pic, x, y, unit_len, qrcolor)
+            x += unit_len
+        x, y = 4*unit_len, y+unit_len
+
+    saving = os.path.join(abspath, 'qrcode.png')
+    pic.save(saving)
+    return saving
+
+
+def draw_a_black_unit(p, x, y, ul, color):
+    for i in range(ul):
+        for j in range(ul):
+            coord = a, b = x+i, y+j
+            # pixcolor = background.getpixel(coord)
+            p.putpixel((x+i, y+j), color)
