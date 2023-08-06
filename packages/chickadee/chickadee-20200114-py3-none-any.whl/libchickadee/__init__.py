00527ead113288b8485020257812b582dea8fb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:37 +0000

    Add package chickadee/20200114/chickadee-20200114-py3-none-any.whl

---
+++ packages/chickadee/chickadee-20200114-py3-none-any.whl/libchickadee/__init__.py
@@ -0,0 +1,9 @@
+"""Libchickadee
+
+Yet another GeoIP resolution tool.
+"""
+
+__author__ = 'Chapin Bryce'
+__date__ = 20200107
+__license__ = 'GPLv3 Copyright 2019 Chapin Bryce'
+__desc__ = '''Yet another GeoIP resolution tool.'''
