afeb996f33a44e5ca819ba5e740a4bb19c975ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:01 +0000

    Add package pynamix/0.0.3/pynamix-0.0.3-py2.7.egg

---
+++ packages/pynamix/pynamix-0.0.3-py2.7.egg/pynamix/io.py
@@ -0,0 +1,144 @@
+import os, json, glob
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm, Normalize
+from matplotlib.cm import inferno
+from imageio import imwrite
+
+def load_seq(filename,varian=False):
+    """Load an SEQ file and related logfile, reshaping the file into a 3D array if a logfile is present.
+
+    Args:
+        filename (str): location of SEQ/log file to load. Can be the full path to the SEQ or log file including or excluding the file format.
+        varian (bool): Was this recorded using the Varian software?
+
+    Returns:
+        A 3D array with dimensions [nt,nx,ny]
+    """
+    if filename[-3:] == 'seq': filename = filename[:-4] # strip seq file ending if present
+    elif filename[-3:] == 'log': filename = filename[:-4] # strip seq file ending if present
+
+    with open(filename + '.seq','rb') as f:
+        if varian:
+            data = np.memmap(f,dtype="H",mode='r',offset=2048)
+            try: data = data.reshape(-1,960*2,768*2)
+            except:
+                try: data = data.reshape(-1,960,768)
+                except: pass
+            logfile = {} # TODO
+        else:
+            data = np.memmap(f,dtype="H",mode='r')
+            if os.path.exists(filename + '.log'):
+                with open(filename + '.log') as g:
+                    try:
+                        logfile = json.load(g)
+                        nt = len(logfile['frames'])
+                        ny = logfile['resolution']['width']
+                        nx = logfile['resolution']['height']
+                        try:
+                            data = data.reshape(nt,ny,nx)
+                        except:
+                            data = data.reshape(-1,ny,nx)
+                            print('WARNING: REMOVE THIS TRY STATEMENT AFTER GETTING REAL TEST DATA')
+                    except:
+                        # for line in g:
+                            # if line.find('frames') != -1: nt = 0
+                        try: data = data.reshape(-1,960,768)
+                        except: pass
+                        print("WARNING: Haven't implemented old log file checking, assumed no ROI and 2x2 binning")
+                        logfile = {} # TODO
+
+            else:
+                raise Exception('No log file found!')
+    return data, logfile
+
+def save_as_tiffs(foldername,data,vmin=0,vmax=65535,angle=0,colour=False,logscale=False,tmin=0,tmax=None,tstep=1):
+    """Convert an appropriately shaped SEQ file into TIFF files. Optionally takes an angle to rotate the images by.
+
+    Args:
+        foldername (str): Location to save tiffs into. If this folder does not exist, it will be created for you. including or excluding the file format.
+        data (array): The data loaded as a 3D numpy array with dimensions [nt,nx,ny]
+        vmin (int): Minimum value to show in tiff
+        vmax (int): Maximum value to show in tiff
+        angle (float): Angle in degrees to rotate the images
+        colour (bool): Flag to save the image in colour instead of grayscale
+        logscale (bool): Flag to save the image using a logscale for the colours
+        tmin (int): First frame to save
+        tmax (int): Last frame to save
+    """
+    nt = data.shape[0]
+    if tmax == None: tmax = nt
+    if not os.path.exists(foldername): os.makedirs(foldername)
+    if logscale:
+        norm = LogNorm()
+    else:
+        norm = Normalize()
+    for t in range(tmin,tmax,tstep):
+        im = data[t].astype(np.float)
+
+        # Optionally rotate image
+        if angle != 0: im = np.rotate(im,angle=angle,mode='edge')
+
+        # Set correct color mapping
+        im = (im -vmin)/(vmax - vmin)*255.0
+        im[im<0] = 0
+        im[im>255] = 255
+
+        if not colour:
+            imwrite(foldername + '/' + str(t).zfill(5) + '.tiff',im.astype(np.uint8))
+        else:
+            plt.clf()
+            plt.imshow(im,
+    #                    norm = LogNorm(),
+                       cmap = inferno,
+                      )
+            plt.subplots_adjust(left=0,right=1,bottom=0,top=1)
+            plt.axis('off')
+            plt.savefig(foldername + '/' + str(t).zfill(5) + '.tiff',dpi=72)
+
+def load_PIVLab_txtfiles(foldername,start=0,end=None,tstep=1):
+    '''Load a folder full of PIVLab txtfiles and return the data as a series of arrays
+
+    Args:
+        foldername (str): Name of the folder that contains the text files to load.
+
+    Returns:
+        4 element tuple containing
+
+        - x (1D array): The `x` location of each patch
+        - y (1D array): The `y` location of each patch
+        - u (3D array): The horizontal velocity of each patch. Has the shape [nt,nx,y].
+        - v (3D array): The vertical velocity of each patch. Has the shape [nt,nx,y].
+    '''
+
+    files = glob.glob(foldername + '/*.txt')
+    files = sorted(files)
+    nt = len(files)
+    for i,f in enumerate(files[start:end:tstep]):
+        data = np.loadtxt(f,skiprows=3,delimiter=',')
+        if i == 0:
+            np = data.shape[0] # number of data points in total
+            x_all = data[:,0]
+            y_all = data[:,1]
+            u = np.zeros([nt,np])
+            v = np.zeros([nt,np])
+        # elif i % 500 == 0: print('Up to file ' + str(i))
+        u[i] = data[:,2]
+        v[i] = data[:,3]
+
+    x = np.unique(x_all)
+    y = np.unique(y_all)
+    nx = len(x)
+    ny = len(y)
+    u = u.reshape([nt,nx,ny])
+    v = v.reshape([nt,nx,ny])
+
+    return x,y,u,v
+
+# Testing area
+if __name__ == '__main__':
+    from pynamix.data import radiographs
+    data,logfile = radiographs()
+    save_as_tiffs('tt',data,tmin=1000,tmax=1050,tstep=10)
+
+    x,y,u,v = load_PIVLab_txtfiles('/Volumes/LTS/DynamiX/FRC/Bernie_Hazim/test_150_400_2.seq')
---
+++ packages/pynamix/pynamix-0.0.3-py2.7.egg/pynamix/measure.py
@@ -0,0 +1,241 @@
+import numpy as np
+from scipy.linalg import eig # NOT SURE WHY NUMPY ONE SEG FAULTS
+
+def main_direction(tensor):
+    """Calculate the principal orientation and orientation magnitude of a nematic order tensor.
+
+    Args:
+        X: 2 by 2 array represnting the nematic order tensor.
+
+    Returns:
+        Two values, one for the principal orientation in radians and one for the magnitude of the orientation on a scale of zero to one.
+    """
+    v,_ = eig(tensor)
+    v = np.real(v) # NOTE: NOT SURE WHY THIS IS NECESSARY
+    # idx=np.argmax(V)
+    # print(v))
+    angle=np.arctan2(v[1],v[0]) # HACK: CHECK THIS!!!!!
+    if (angle < 0): angle=angle + np.pi
+    elif (angle > np.pi): angle=angle - np.pi
+    dzeta=np.sqrt(np.sum(tensor ** 2))
+    return angle, dzeta
+
+def window_mask(patchw=32):
+    """Compute a radial hanning window.
+
+    Args:
+        patchw (int): The half width of the patch.
+
+    Returns:
+        The radial hanning window.
+    """
+    w = np.zeros([patchw*2,patchw*2])
+    for i in range(1,patchw * 2):
+        for j in range(1,patchw * 2):
+            dst=np.sqrt((i - 0.5 - patchw) ** 2 + (j - 0.5 - patchw) ** 2)
+            w[i,j]=0.5 * (np.cos(2 * np.pi * dst / (patchw * 2))) + 0.5
+            if (dst > patchw):
+                w[i,j]=0
+    return w
+
+def angular_binning(patchw=32,N=10000):
+    """
+    Use a Monte-Carlo method to compute the individual Q(k) coefficients for equation 4 in `Guillard et al. 2017 <https://www.nature.com/articles/s41598-017-08573-y>`_.
+
+    Angular binning definition
+
+    step_angle=5 / 180 * pi
+
+    bin_angle=range(0 - step_angle / 2,- 180,- step_angle)
+
+    n_angle=floor(pi / step_angle)
+
+    .. warning:: Fix this definition.
+
+    Args:
+        patchw (int): The half width of the patch.
+        N (int): Number of particle throws per iteration.
+
+    Returns:
+        4D array: An array n_maskQ that stores Q(k) coefficients in a 4D table such that Q(kx, ky)=n_maskQ(kx,ky,:,:)
+    """
+    K = np.zeros([N,2])
+    n_nbmaskQ = np.zeros([patchw * 2,patchw * 2])
+    n_maskQ   = np.zeros([patchw * 2,patchw * 2,2,2])
+    for j in range(1,1000): # Number of iteration in Monte-Carlo simulation
+        r = (np.random.rand(N,2) - 0.5) * 2 * patchw
+        K[:,0] = r[:,0] / (np.sqrt(r[:,0] ** 2 + r[:,1] ** 2))
+        K[:,1] = r[:,1] / (np.sqrt(r[:,0] ** 2 + r[:,1] ** 2))
+        x = np.floor(r + patchw).astype(np.int)
+        for i in range(1,N):
+            n_nbmaskQ[x[i,1],x[i,0]] += 1
+            n_maskQ[x[i,1],x[i,0],0,0] += K[i,0] * K[i,0]
+            n_maskQ[x[i,1],x[i,0],0,1] += K[i,0] * K[i,1]
+            n_maskQ[x[i,1],x[i,0],1,0] += K[i,1] * K[i,0]
+            n_maskQ[x[i,1],x[i,0],1,1] += K[i,1] * K[i,1]
+
+    # Final scaling for the n_maskQ coefficients.
+    n_maskQ[:,:,0,0] /= n_nbmaskQ
+    n_maskQ[:,:,0,1] /= n_nbmaskQ
+    n_maskQ[:,:,1,0] /= n_nbmaskQ
+    n_maskQ[:,:,1,1] /= n_nbmaskQ
+
+    return n_maskQ
+
+def orientation_map(data,start=0,end=None,tstep=1,xstep=32,ystep=32,patchw=32):
+    """
+    Calculate the principal orientation and orientation magnitude at a set of patches in images in a series.
+
+    Args:
+        data: The source data. Should be in the shape [nt,nx,ny]
+        start (int): First frame to analyse in the series
+        end (int): Last frame to analyse in the series
+        tstep (int): Spacing between frames to analyse
+        xstep (int): Spacing between patches in the x direction
+        ystep (int): Spacing between patches in the y direction
+        patchw (int): The half width of the patch.
+
+    Returns:
+        Two arrays of shape which describe the principal orientation and orientation magnitude for each patch
+    """
+    w = window_mask(patchw)
+    n_maskQ = angular_binning(patchw,N=100) # NOTE: LOW N FOR TESTING - REMOVE THIS LATER
+
+    nt,nx,ny = data.shape
+
+    gridx = range(patchw,nx-patchw,xstep) # locations of centres of patches in x direction
+    gridy = range(patchw,ny-patchw,ystep) # locations of centres of patches in y direction
+    if end is None: end = nt # optionally set end time
+
+    # Prepare thre result matrices (3D), first 2 indices are the grid, the last index is time
+    orient = np.nan * np.zeros([end-start,len(gridx),len(gridy)])
+    dzeta  = np.nan * np.zeros([end-start,len(gridx),len(gridy),end-start])
+    Q = np.zeros_like(n_maskQ)
+    Q2 = np.zeros([2,2,nx,ny,nt])
+
+    for t,ti in enumerate(range(start,end,tstep)): # Loop on the movie frames
+        for i,xi in enumerate(gridx): # Loop over the grid
+            for j,yj in enumerate(gridy):
+                patch = data[ti,xi-patchw:xi+patchw,yj-patchw:yj+patchw]
+
+                if np.std(patch) != 0: patch=(patch - np.mean(patch)) / np.std(patch)
+                else:                  patch=(patch - np.mean(patch))
+
+                S = np.fft.fftshift(np.abs(np.fft.fft2(patch*w) ** 2))
+
+                if np.sum(S) == 0:
+                    Q[:,:,0,0] = Q[:,:,0,1] = Q[:,:,1,0] = Q[:,:,1,1] = S
+                else:
+                    Q[:,:,0,0] = n_maskQ[:,:,0,0].dot(S) / np.sum(S)
+                    Q[:,:,0,1] = n_maskQ[:,:,0,1].dot(S) / np.sum(S)
+                    Q[:,:,1,0] = n_maskQ[:,:,1,0].dot(S) / np.sum(S)
+                    Q[:,:,1,1] = n_maskQ[:,:,1,1].dot(S) / np.sum(S)
+                Q2[:,:,i,j,t] = np.sum(np.sum(Q,0),0)
+                Q2[0,0,i,j,t] -= 0.5
+                Q2[1,1,i,j,t] -= 0.5
+                Q2[:,:,i,j,t] *= np.sqrt(2)
+                orient[t,i,j],dzeta[t,i,j] = main_direction(Q2[:,:,i,j,t])
+    return orient, dzeta
+
+def size_map(data,start=0,end=None,tstep=1,xstep=32,ystep=32,patchw=32):
+    """
+    Calculate the radial average of the 2D FFT at a set of patches in images in a series.
+
+    Args:
+        data: The source data. Should be in the shape [nt,nx,ny]
+        start (int): First frame to analyse in the series
+        end (int): Last frame to analyse in the series
+        tstep (int): Spacing between frames to analyse
+        xstep (int): Spacing between patches in the x direction
+        ystep (int): Spacing between patches in the y direction
+        patchw (int): The half width of the patch.
+
+    Returns:
+        Two arrays of shape which describe the principal orientation and orientation magnitude for each patch
+    """
+    w = window_mask(patchw)
+    n_maskQ = angular_binning(patchw,N=100) # NOTE: LOW N FOR TESTING - REMOVE THIS LATER
+
+    nt,nx,ny = data.shape
+
+    gridx = range(patchw,nx-patchw,xstep) # locations of centres of patches in x direction
+    gridy = range(patchw,ny-patchw,ystep) # locations of centres of patches in y direction
+    if end is None: end = nt # optionally set end time
+
+    # Prepare thre result matrices (3D), first 2 indices are the grid, the last index is time
+    orient = np.nan * np.zeros([end-start,len(gridx),len(gridy)])
+    dzeta  = np.nan * np.zeros([end-start,len(gridx),len(gridy),end-start])
+    Q = np.zeros_like(n_maskQ)
+    Q2 = np.zeros([2,2,nx,ny,nt])
+
+    for t,ti in enumerate(range(start,end,tstep)): # Loop on the movie frames
+        for i,xi in enumerate(gridx): # Loop over the grid
+            for j,yj in enumerate(gridy):
+                patch = data[ti,xi-patchw:xi+patchw,yj-patchw:yj+patchw]
+
+                if np.std(patch) != 0: patch=(patch - np.mean(patch)) / np.std(patch)
+                else:                  patch=(patch - np.mean(patch))
+
+                S = np.fft.fftshift(np.abs(np.fft.fft2(patch*w) ** 2))
+
+                if np.sum(S) == 0:
+                    Q[:,:,0,0] = Q[:,:,0,1] = Q[:,:,1,0] = Q[:,:,1,1] = S
+                else:
+                    Q[:,:,0,0] = n_maskQ[:,:,0,0].dot(S) / np.sum(S)
+                    Q[:,:,0,1] = n_maskQ[:,:,0,1].dot(S) / np.sum(S)
+                    Q[:,:,1,0] = n_maskQ[:,:,1,0].dot(S) / np.sum(S)
+                    Q[:,:,1,1] = n_maskQ[:,:,1,1].dot(S) / np.sum(S)
+                Q2[:,:,i,j,t] = np.sum(np.sum(Q,0),0)
+                Q2[0,0,i,j,t] -= 0.5
+                Q2[1,1,i,j,t] -= 0.5
+                Q2[:,:,i,j,t] *= np.sqrt(2)
+                orient[t,i,j],dzeta[t,i,j] = main_direction(Q2[:,:,i,j,t])
+    return size
+
+# Testing area
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    # w = window_mask()
+    # plt.imshow(w)
+    # plt.colorbar()
+    # plt.show()
+
+    # n_maskQ = angular_binning(N=1000)
+    # plt.subplot(221)
+    # plt.imshow(n_maskQ[:,:,0,0])
+    # plt.colorbar()
+    # plt.subplot(222)
+    # plt.imshow(n_maskQ[:,:,0,1])
+    # plt.colorbar()
+    # plt.subplot(223)
+    # plt.imshow(n_maskQ[:,:,1,0])
+    # plt.colorbar()
+    # plt.subplot(224)
+    # plt.imshow(n_maskQ[:,:,1,1])
+    # plt.colorbar()
+    # plt.show()
+
+    # m = np.array([[1,0],[1,0]])
+    # angle,dzeta = main_direction(m)
+    # print(np.degrees(angle),dzeta)
+    # m = np.array([[1,0],[0,1]])
+    # angle,dzeta = main_direction(m)
+    # print(np.degrees(angle),dzeta)
+    # m = np.array([[0,1],[1,0]])
+    # angle,dzeta = main_direction(m)
+    # print(np.degrees(angle),dzeta)
+
+    from pynamix.data import radiographs
+    data,logfile = radiographs()
+    orient, dzeta = orientation_map(data,start=1000,end=1002)
+    # size = size_map(data,start=1000,end=1002)
+
+    plt.subplot(131)
+    plt.imshow(data[1000])
+    plt.subplot(132)
+    plt.pcolormesh(orient[:,:,0])
+    plt.colorbar()
+    # plt.subplot(133)
+    # plt.pcolormesh(size[:,:,0])
+    # plt.colorbar()
+    plt.show()
