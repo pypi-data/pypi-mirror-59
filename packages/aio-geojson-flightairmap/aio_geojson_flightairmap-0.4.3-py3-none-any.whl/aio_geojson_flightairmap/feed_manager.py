f9459f56dbb017b5623866e444d03f6de3da4268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:33 +0000

    Add package aio-geojson-flightairmap/0.4.3/aio_geojson_flightairmap-0.4.3-py3-none-any.whl

---
+++ packages/aio-geojson-flightairmap/aio_geojson_flightairmap-0.4.3-py3-none-any.whl/aio_geojson_flightairmap/feed_manager.py
@@ -0,0 +1,26 @@
+"""Feed Manager for Flight Air Map Incidents feed."""
+from aio_geojson_client.feed_manager import FeedManagerBase
+from aiohttp import ClientSession
+
+from .feed import FlightAirMapFeed
+
+class FlightAirMapFeedManager(FeedManagerBase):
+    """Feed Manager for Flight Air Map feed."""
+
+    def __init__(self,
+                 websession: ClientSession,
+                 generate_callback,
+                 update_callback,
+                 remove_callback,
+                 coordinates=None,
+                 filter_radius=None):
+        """Initialize the Flight Air Map Manager."""
+        feed = FlightAirMapFeed(
+            websession,
+            coordinates=coordinates,
+            url="http://192.168.0.200/FlightAirMap/live/geojson",
+            filter_radius=filter_radius)
+        super().__init__(feed,
+                         generate_callback,
+                         update_callback,
+                         remove_callback)
