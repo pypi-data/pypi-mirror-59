338164817c32bead480141dd3e33d732dd64123e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:49 +0000

    Add package yglu/0.2.0/yglu-0.2.0.tar.gz

---
+++ packages/yglu/yglu-0.2.0.tar.gz/yglu-0.2.0/yglu/builder.py
@@ -0,0 +1,94 @@
+''' Transforms the parsed YAML structure into a tree '''
+
+from . import loader
+from .tree import (Scalar, Sequence, Mapping)
+from .expression import (Expression, Function, FunctionBlock, init_scope)
+from ruamel.yaml.nodes import (ScalarNode, SequenceNode, MappingNode)
+from ruamel.yaml.comments import TaggedScalar
+
+
+def build(source):
+    result = convert(loader.load(source))
+    init_scope(result)
+    return result
+
+
+def convert(node):
+    if isinstance(node, dict):
+        return Mapping([(k, convert(v)) for (k, v) in node.items()])
+    if isinstance(node, list):
+        return Sequence([convert(v) for v in node])
+    if isinstance(node, TaggedNode):
+        return node.create()
+    return Scalar(node)
+
+
+def construct_node(self, node):
+    if isinstance(node, ScalarNode):
+        return self.construct_scalar(node)
+    if isinstance(node, MappingNode):
+        return [c for c in self.construct_yaml_map(node)][0]
+    if isinstance(node, SequenceNode):
+        return self.construct_sequence(node)
+
+
+class TaggedNode:
+    def __init__(self, value):
+        self.value = value
+
+
+class InvisibleNode(TaggedNode):
+    def create(self):
+        if isinstance(self.value, str):
+            result = Expression(self.value)
+        else:
+            result = convert(self.value)
+        result.visible = False
+        return result
+
+
+def invisible_constructor(self, node):
+    return InvisibleNode(construct_node(self, node))
+
+
+loader.add_constructor('!-', invisible_constructor)
+
+
+class ExpressionNode(TaggedNode):
+    def create(self):
+        result = Expression(self.value)
+        return result
+
+
+def expression_constructor(self, node):
+    if isinstance(node, ScalarNode):
+        return ExpressionNode(self.construct_scalar(node))
+    assert False, 'Expression nodes must be scalar'
+
+
+loader.add_constructor('!?', expression_constructor)
+
+
+class FunctionNode(TaggedNode):
+    def create(self):
+        result = Function(self.value)
+        return result
+
+
+class FunctionBlockNode(TaggedNode):
+    def __init__(self, value, constructor):
+        self.value = value
+        self.constructor = constructor
+    def create(self):
+        result = FunctionBlock(self, lambda node: convert(construct_node(self.constructor, node)))
+        return result
+
+
+def function_constructor(self, node):
+    if isinstance(node, ScalarNode):
+        return FunctionNode(self.construct_scalar(node))
+    else:
+        return FunctionBlockNode(node, self)
+
+
+loader.add_constructor('!()', function_constructor)
---
+++ packages/yglu/yglu-0.2.0.tar.gz/yglu-0.2.0/yglu/expression.py
@@ -0,0 +1,81 @@
+''' Evaluation of YAQL expressions '''
+
+import yaql
+from .tree import Node
+from ruamel.yaml.nodes import (MappingNode, SequenceNode)
+
+engine = yaql.factory.YaqlFactory(allow_delegates=True).create(options={
+    'yaql.convertInputData': False,
+})
+scopes = []
+stack = []
+context = yaql.create_context(delegates=True)
+
+
+def init_scope(scope):
+    global scopes
+    global stack
+    scopes = [scope]
+    stack = []
+
+
+def push_stack(node):
+    if node in stack:
+        raise CircularReferenceException()
+    stack.append(node)
+
+
+def pop_stack():
+    stack.pop()
+
+
+def push_scope(scope):
+    scopes.append(scope)
+
+
+def pop_scope():
+    scopes.pop()
+
+
+class Expression(Node):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def create_content(self):
+        push_stack(self)
+        result = engine(self.expression).evaluate(data=scopes[-1], context=context)
+        pop_stack()
+        return result
+
+
+class Function(Node):
+    def __init__(self, expression):
+        self.expression = expression
+        self.visible = False
+
+    def eval(self, scope):
+        result = engine(self.expression).evaluate(data=scope, context=context)
+        return result
+
+    def create_content(self):
+        return self.eval
+
+
+class FunctionBlock(Node):
+    def __init__(self, node, constructor):
+        self.node = node.value
+        self.visible = False
+        self.constructor = constructor
+
+    def eval(self, scope):
+        push_scope(scope)
+        result = dict(self.constructor(self.node).items())
+        pop_scope()
+        return result
+
+    def create_content(self):
+        return self.eval
+
+
+class CircularReferenceException(Exception):
+    pass
