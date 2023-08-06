aa76494270fc143381550af3f8c5036b4224f44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package ambiance-client/0.0.2/ambiance_client-0.0.2.tar.gz

---
+++ packages/ambiance-client/ambiance_client-0.0.2.tar.gz/ambiance_client-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ambiance_client", # Replace with your own username
+    version="0.0.2",
+    author="Ben Klang",
+    author_email="bklang@wirehack.net",
+    description="A Python client for the Elixir Ambiance home automation API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/bklang/ambiance_client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+    ],
+)
