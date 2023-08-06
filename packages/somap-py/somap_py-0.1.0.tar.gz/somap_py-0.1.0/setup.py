663297512e2de4452c270bde34f4f400a1e63693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package somap-py/0.1.0/somap_py-0.1.0.tar.gz

---
+++ packages/somap-py/somap_py-0.1.0.tar.gz/somap_py-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    author='Doug Denu',
+    author_email='ddenu@uvm.edu',
+    url='https://bitbucket.org/dcdenu4/somap_py/src/master/',
+    name='somap_py',
+    version='0.1.0',
+    packages=['somap_py',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+)
