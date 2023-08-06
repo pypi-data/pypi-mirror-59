5ac563ca1143b0a241c017585c64ec428eadc5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:27 +0000

    Add package SCSA/0.1.7/SCSA-0.1.7.tar.gz

---
+++ packages/SCSA/SCSA-0.1.7.tar.gz/SCSA-0.1.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SCSA",
+    version="0.1.7",
+    author="Taous-Meriem Laleg-Kirati, Julio Mario Orozco Lopez, Evangelos Piliouras, Abderrazak chahid, Peihao Li",
+    author_email="taousmeriem.laleg@kaust.edu.sa ,jmorozcol@unal.edu.co, evangelos.piliouras@kaust.edu.sa, "
+                 "abderrazak.chahid@kaust.edu.sa, "
+                 "peihao.li@kaust.edu.sa",
+    description="A novel algorithm for signal representation and denoising",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://emang.kaust.edu.sa/Pages/Home.aspx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
