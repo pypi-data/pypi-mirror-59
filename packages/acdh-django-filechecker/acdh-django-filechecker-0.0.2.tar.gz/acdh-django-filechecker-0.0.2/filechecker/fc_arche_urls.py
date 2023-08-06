8fb7b74eb2ec0b81ff2857bc970c8576023bd0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:02 +0000

    Add package acdh-django-filechecker/0.0.2/acdh-django-filechecker-0.0.2.tar.gz

---
+++ packages/acdh-django-filechecker/acdh-django-filechecker-0.0.2.tar.gz/acdh-django-filechecker-0.0.2/filechecker/fc_arche_urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from . import arche_views
+
+app_name = "filechecker"
+
+urlpatterns = [
+    path(
+        'collection-and-resources/<pk>',
+        arche_views.children_as_arche_graph,
+        name='collection_and_resources_as_graph'
+    ),
+    path('<model_name>/<pk>', arche_views.res_as_arche_graph, name='res_as_arche_graph'),
+    path('<model_name>', arche_views.qs_as_arche_graph, name='qs_as_arche_graph'),
+]
