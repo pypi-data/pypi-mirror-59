3ece7da7eb588c0f9ad042a44e69a5d594af42e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package torchio/0.11.3/torchio-0.11.3-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.11.3-py2.py3-none-any.whl/torchio/torchio.py
@@ -0,0 +1,16 @@
+"""Main module."""
+
+__all__ = [
+    'INTENSITY',
+    'LABEL',
+    'SAMPLING_MAP',
+    'DATA',
+    'AFFINE',
+]
+
+INTENSITY = 'intensity'
+LABEL = 'label'
+SAMPLING_MAP = 'sampling_map'
+
+DATA = 'data'
+AFFINE = 'affine'
