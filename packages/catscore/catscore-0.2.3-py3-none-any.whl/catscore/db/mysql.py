80a6814a7a6bb6d111c9fe14d98bbaaa89569f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:44 +0000

    Add package catscore/0.2.3/catscore-0.2.3-py3-none-any.whl

---
+++ packages/catscore/catscore-0.2.3-py3-none-any.whl/catscore/db/mysql.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import json
+
+@dataclass(frozen=True)
+class MySQLConf:
+    host: str
+    port: str
+    user: str
+    pwd: str
+    db_name: str
+
+    def connection_uri(self, inteface:str):
+        if inteface == "jdbc":
+            uri = f"jdbc:mysql://{self.host}:{self.port}/{self.db_name}?user={self.user}&password={self.pwd}"
+        elif inteface == "pymysql":
+            uri =  'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (self.user, self.pwd, self.host + ":" + self.port, self.db_name)
+        print(uri)
+        return uri
+
+    @classmethod
+    def from_json(cls, path):
+        with open(path, "r") as f:
+            j = json.load(f)
+            m = MySQLConf(
+                host=j["host"],
+                port=j["port"],
+                user=j["user"],
+                pwd=j["pwd"],
+                db_name=j["db_name"])
+        return m
