0841266f2bf6a89a260b5f02845a6ffddcffce61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package YZ-distributions/0.1/YZ_distributions-0.1.tar.gz

---
+++ packages/YZ-distributions/YZ_distributions-0.1.tar.gz/YZ_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='YZ_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['YZ_distributions'],
+      author='Yan Zhang',
+      author_email='yanzhang0623@gmail.com',
+      zip_safe=False)
