c1f13c62b171f40e9c7d1c10dcef15ecc8309160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:27 +0000

    Add package mflux-ai/0.7.0/mflux_ai-0.7.0-py2.py3-none-any.whl

---
+++ packages/mflux-ai/mflux_ai-0.7.0-py2.py3-none-any.whl/mflux_ai/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+import mflux_ai.mflux_ai as mflux_ai
+from .core import (
+    init,
+    get_minio_client,
+    ensure_bucket_exists,
+    put_dataset,
+    get_dataset,
+    get_best_run,
+)
+
+"""Top-level package for mflux-ai."""
+
+__author__ = """AIA Science AS"""
+__email__ = "mflux.ai@aiascience.com"
+__version__ = "0.7.0"
+
+
+def set_env_vars(token):
+    warnings.warn(
+        "mflux_ai.set_env_vars() is deprecated. Please use mflux_ai.init() instead.",
+        DeprecationWarning,
+    )
+    return init(token)
