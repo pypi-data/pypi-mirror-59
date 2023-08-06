61a7b2948bd816463147fbbdabda9f8e50b3ac82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:40 +0000

    Add package pypub/1.5/pypub-1.5.tar.gz

---
+++ packages/pypub/pypub-1.5.tar.gz/pypub-1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pypub',
+    version='1.5',
+    packages=['pypub',],
+    package_data={'pypub': ['epub_templates/*',]},
+    author = 'William Cember',
+    author_email = 'wcember@gmail.com',
+    url = 'https://github.com/wcember/pypub',
+    license='MIT',
+    install_requires=[
+            'beautifulsoup4',
+            'jinja2',
+            'requests',
+            ],
+    description= "Create epub's using python. Pypub is a python library to create epub files quickly without having to worry about the intricacies of the epub specification.",
+)
