50a3f3d0a4f0ce7e08c5979763927a3c4e637085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:55 +0000

    Add package owutil/0.1.0/owutil-0.1.0.tar.gz

---
+++ packages/owutil/owutil-0.1.0.tar.gz/owutil-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['owutil']
+entry_points = \
+{'console_scripts': ['owutil = owutil.main']}
+
+setup_kwargs = {
+    'name': 'owutil',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'drys262',
+    'author_email': 'koeichavez17@gmail.com',
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
