307850654e8fe63716e0d3aa0a935a668b77c2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:30 +0000

    Add package ticts/0.3.5/ticts-0.3.5-py2.py3-none-any.whl

---
+++ packages/ticts/ticts-0.3.5-py2.py3-none-any.whl/ticts/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+import pandas as pd
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .timeseries import TimeSeries  # noqa: F401
+
+try:
+    _dist = get_distribution("ticts")
+
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+
+    if not here.startswith(os.path.join(dist_loc, "ticts")):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = "Version not found."
+else:
+    __version__ = _dist.version
+
+
+@pd.api.extensions.register_dataframe_accessor("to_ticts")
+class FromPandasDTtoTicTS(object):
+    def __init__(self, pandas_obj):
+        self.obj = pandas_obj
+
+    def __call__(self):
+        return TimeSeries(self.obj)
+
+
+@pd.api.extensions.register_series_accessor("to_ticts")
+class FromPandasSeriestoTicTS(object):
+    def __init__(self, pandas_obj):
+        self.obj = pandas_obj
+
+    def __call__(self):
+        return TimeSeries(self.obj)
---
+++ packages/ticts/ticts-0.3.5-py2.py3-none-any.whl/ticts/utils.py
@@ -0,0 +1,33 @@
+import operator
+
+import pandas as pd
+
+
+def timestamp_converter(ts, tz='UTC'):
+    try:  # in case ts is a timestamp (also called epoch)
+        ts = pd.to_datetime(float(ts), unit='ns')
+    except Exception:
+        ts = pd.Timestamp(ts)
+
+    if not ts.tz:
+        ts = ts.tz_localize(tz)
+    return ts
+
+
+MINTS = pd.Timestamp.min.tz_localize('UTC')
+MAXTS = pd.Timestamp.max.tz_localize('UTC')
+
+
+class NoDefault():
+    def __repr__(self):
+        return 'No default'
+
+
+NO_DEFAULT = NoDefault()
+
+
+def operation_factory(operation):
+    def fn_operation(self, other):
+        return self._operate(other, getattr(operator, operation))
+
+    return fn_operation
