8604469a4e0635e28be5a70020300c2bba27c6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package pyscr/0.1/pyscr-0.1.tar.gz

---
+++ packages/pyscr/pyscr-0.1.tar.gz/pyscr-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyscr',
+      version='0.1',
+      description='A Python library for RNAseq analysis',
+      url='https://github.com/aneeshpanoli/pyscr',
+      author='Aneesh aneeshpanoli',
+      author_email='topanoli@gmail.com',
+      license='MIT',
+      packages=['pyscr'],
+      zip_safe=False)
