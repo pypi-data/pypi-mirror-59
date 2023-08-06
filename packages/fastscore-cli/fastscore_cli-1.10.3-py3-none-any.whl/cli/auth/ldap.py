5188a7ec0a14f0d7a77d4caa7880156762e829e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:35 +0000

    Add package fastscore-cli/1.10.3/fastscore_cli-1.10.3-py3-none-any.whl

---
+++ packages/fastscore-cli/fastscore_cli-1.10.3-py3-none-any.whl/cli/auth/ldap.py
@@ -0,0 +1,9 @@
+from fastscore.suite import Connect
+from getpass import getpass
+
+def login_ldap(connect, verbose, username=None, password=None):
+    if username is None:
+        username = input("Username:")
+    if password is None:
+        password = getpass("Password:")
+    connect.ldap_login(username, password)
---
+++ packages/fastscore-cli/fastscore_cli-1.10.3-py3-none-any.whl/cli/monitor/stable.py
@@ -0,0 +1,30 @@
+
+from fastscore import Sensor
+
+class JetStable(object):
+
+    def __init__(self, engine):
+        self._engine = engine
+        self._sensors = {}
+
+    def track(self, sandbox):
+        tap1 = 'jet.' + sandbox + '.input.records.count'
+        tap2 = 'jet.' + sandbox + '.output.records.count'
+
+        sid1 = Sensor.prep(tap1).install(self._engine)
+        sid2 = Sensor.prep(tap2).install(self._engine)
+
+        self._sensors[sandbox] = [sid1,sid2]
+
+    def untrack(self, sandbox):
+        self._engine.clear()
+        for sid in self._sensors.pop(sandbox):
+            self._engine.active_sensors[sid].uninstall()
+
+    def untrackall(self):
+        self._engine.clear()
+        for ss in list(self._sensors.values()):
+            for sid in ss:
+                self._engine.active_sensors[sid].uninstall()
+        self._sensors = {}
+
