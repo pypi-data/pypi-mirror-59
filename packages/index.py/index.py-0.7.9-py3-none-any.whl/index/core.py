d52932dc0f9306c9ef676a1bac8d8a4fa340f3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:19 +0000

    Add package index.py/0.7.9/index.py-0.7.9-py3-none-any.whl

---
+++ packages/index.py/index.py-0.7.9-py3-none-any.whl/index/core.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import logging
+from typing import Optional
+
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.errors import ServerErrorMiddleware
+from starlette.exceptions import ExceptionMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+
+from .config import config
+from .autoreload import MonitorFile
+from .applications import Index, favicon
+
+logger = logging.getLogger(__name__)
+
+sys.path.insert(0, config.path)
+
+app = Index(debug=config.DEBUG)
+
+app.mount("/favicon.ico", favicon, "asgi")
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(config.path, "static"), check_dir=False),
+    "asgi",
+)
+
+if config.FORCE_SSL:
+    app.add_middleware(HTTPSRedirectMiddleware)
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.ALLOWED_HOSTS)
+
+if config.HOTRELOAD:
+    monitor: Optional[MonitorFile] = None
+
+    @app.on_event("startup")
+    async def check_on_startup() -> None:
+        # monitor file event
+        global monitor
+        monitor = MonitorFile(config.path)
+
+    @app.on_event("shutdown")
+    async def clear_check_on_shutdown() -> None:
+        global monitor
+        if monitor is not None:
+            monitor.stop()
+
+
+@app.on_event("startup")
+async def create_directories() -> None:
+    """
+    create directories for static & template
+    """
+    os.makedirs(os.path.join(config.path, "views"), exist_ok=True)
+    os.makedirs(os.path.join(config.path, "static"), exist_ok=True)
+    os.makedirs(os.path.join(config.path, "templates"), exist_ok=True)
+
+
+@app.on_event("shutdown")
+async def clear_directories() -> None:
+    """
+    if no files exist in the directory, delete them
+    """
+    os.rmdir(os.path.join(config.path, "views"))
+    os.rmdir(os.path.join(config.path, "static"))
+    os.rmdir(os.path.join(config.path, "templates"))
