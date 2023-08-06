8fde838c08319031ad83c403f300beb7f93f80dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:07 +0000

    Add package dato/0.0.1a2/dato-0.0.1a2.tar.gz

---
+++ packages/dato/dato-0.0.1a2.tar.gz/dato-0.0.1a2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dato',
+    version='0.0.1a2',
+    license='GPLv3',
+    description='Declarative syntactic sugar for data piping.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Joseph Moon',
+    author_email='joseph@opendataframe.com',
+    url='https://github.com/dataframehq/dato',
+    keywords=['pipe', 'declarative', 'python', 'data science'],
+    packages=setuptools.find_packages(),
+)
