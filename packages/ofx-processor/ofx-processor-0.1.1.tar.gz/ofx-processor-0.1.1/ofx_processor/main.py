abf8bebae3b9ed7999dec18d5b1e94a333a446cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:48 +0000

    Add package ofx-processor/0.1.1/ofx-processor-0.1.1.tar.gz

---
+++ packages/ofx-processor/ofx-processor-0.1.1.tar.gz/ofx-processor-0.1.1/ofx_processor/main.py
@@ -0,0 +1,60 @@
+import os
+import re
+from xml.etree import ElementTree
+
+import click
+from ofxtools.Parser import OFXTree
+from ofxtools.header import make_header
+
+
+@click.command()
+@click.argument("ofx_file")
+def main(ofx_file):
+    parser = OFXTree()
+    try:
+        parser.parse(ofx_file)
+    except FileNotFoundError:
+        print("Couldn't open ofx file")
+        return
+
+    ofx = parser.convert()
+
+    if ofx is None:
+        print("Couldn't open ofx file")
+        return
+    for transaction in ofx.statements[0].transactions:
+        if "CB****" in transaction.name:
+            print("Found following transaction:")
+            print("Name")
+            print(transaction.name)
+            print("Memo")
+            print(transaction.memo)
+
+            name = transaction.name
+            memo = transaction.memo
+            conversion = re.compile(r"\d+,\d{2}[a-zA-Z]{3}")
+            match = conversion.search(memo)
+            if match:
+                transaction.name = memo[: match.start() - 1]
+                transaction.memo = name + memo[match.start() - 1 :]
+            else:
+                transaction.name = memo
+                transaction.memo = name
+
+            print("\nMapping to:")
+            print("Name (should not contain CB****)")
+            print(transaction.name)
+            print("Memo")
+            print(transaction.memo)
+            print()
+
+    header = str(make_header(version=102))
+    root = ofx.to_etree()
+    data = ElementTree.tostring(root).decode()
+
+    with open(os.path.join(os.path.dirname(ofx_file), "processed.ofx"), "w") as f:
+        f.write(header + data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ofx-processor/ofx-processor-0.1.1.tar.gz/ofx-processor-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ofx_processor']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'ofxtools>=0.8.20,<0.9.0']
+
+entry_points = \
+{'console_scripts': ['ofxprocessor = ofx_processor.main:cli']}
+
+setup_kwargs = {
+    'name': 'ofx-processor',
+    'version': '0.1.1',
+    'description': 'Personal ofx processor',
+    'long_description': None,
+    'author': 'Gabriel Augendre',
+    'author_email': 'gabriel@augendre.info',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
