df3909968e9b03c944b7e11c4ff2c6f7e4ae1b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:21 +0000

    Add package simimgs-csv/0.0.5/simimgs-csv-0.0.5.tar.gz

---
+++ packages/simimgs-csv/simimgs-csv-0.0.5.tar.gz/simimgs-csv-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simimgs-csv",
+    version="0.0.5",
+    author="Yefei Li",
+    author_email='abc@gmail.com',
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liyefei737/simimgs",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'opencv-python', 'scikit-image'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
