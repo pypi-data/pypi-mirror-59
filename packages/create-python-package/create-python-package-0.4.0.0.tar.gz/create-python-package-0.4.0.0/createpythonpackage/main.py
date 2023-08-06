78f468a415d43811c00857460a7264721da95297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:54 +0000

    Add package create-python-package/0.4.0.0/create-python-package-0.4.0.0.tar.gz

---
+++ packages/create-python-package/create-python-package-0.4.0.0.tar.gz/create-python-package-0.4.0.0/createpythonpackage/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+from cookiecutter.main import cookiecutter  # type: ignore
+from click.exceptions import Abort
+
+__version__ = "0.4.0.0"
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create the file and folder structure for a Python package",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--template",
+        default="https://github.com/audreyr/cookiecutter-pypackage",
+        help="Template name or url.",
+    )
+    parser.add_argument("--version", action="store_true")
+    args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        exit(0)
+
+    print(f"Using template {args.template}")
+
+    try:
+        cookiecutter("cookiecutter-pypackage/")
+    except Abort:
+        print()
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
