7de26aed840a66e6642f6b69688a445c784462a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:53 +0000

    Add package ReportWriter/0.0.4/ReportWriter-0.0.4.tar.gz

---
+++ packages/ReportWriter/ReportWriter-0.0.4.tar.gz/ReportWriter-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ReportWriter", # Replace with your own username
+    version="0.0.4",
+    author="Benjamin Saljooghi",
+    author_email="benjamin.saljooghi@gmail.com",
+    description="A small class for building strings and writing reports.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/benjaminsaljooghi/reportwriter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
