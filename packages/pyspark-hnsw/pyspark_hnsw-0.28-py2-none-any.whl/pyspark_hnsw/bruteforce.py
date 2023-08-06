abf517b7acdd08391ffbfd7af0e762feb174ab95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:13 +0000

    Add package pyspark-hnsw/0.28/pyspark_hnsw-0.28-py2-none-any.whl

---
+++ packages/pyspark-hnsw/pyspark_hnsw-0.28-py2-none-any.whl/pyspark_hnsw/bruteforce.py
@@ -0,0 +1,69 @@
+from pyspark.ml.wrapper import JavaEstimator, JavaModel
+from pyspark.ml.param.shared import *
+from pyspark.mllib.common import inherit_doc
+from pyspark import keyword_only
+
+
+@inherit_doc
+class BruteForce(JavaEstimator):
+    @keyword_only
+    def __init__(self, identifierCol="id", vectorCol="vector", neighborsCol="neighbors",
+                 numPartitions=1, k=5, distanceFunction="cosine", excludeSelf=False, similarityThreshold=-1.0,
+                 outputFormat="full"):
+        super(BruteForce, self).__init__()
+        self._java_obj = self._new_java_obj("com.github.jelmerk.spark.knn.bruteforce.BruteForce", self.uid)
+
+        self.identifierCol = Param(self, "identifierCol", "the column name for the row identifier")
+        self.vectorCol = Param(self, "vectorCol", "the column name for the vector")
+        self.neighborsCol = Param(self, "neighborsCol", "column name for the returned neighbors")
+        self.numPartitions = Param(self, "numPartitions", "number of partitions")
+        self.k = Param(self, "k", "number of neighbors to find")
+        self.distanceFunction = Param(self, "distanceFunction",
+                                      "distance function, one of bray-curtis, canberra, cosine, correlation, "
+                                      "euclidean, inner-product, manhattan or the fully qualified classname of a "
+                                      "distance function")
+        self.excludeSelf = Param(self, "excludeSelf", "whether to include the row identifier as a candidate neighbor")
+        self.similarityThreshold = Param(self, "similarityThreshold",
+                                         "do not return neighbors further away than this distance")
+
+        self.outputFormat = Param(self, "outputFormat", "output format, one of full, minimal")
+        self.storageLevel = Param(self, "storageLevel",
+                                  "storageLevel for the indices. Pass in a string representation of StorageLevel")
+
+        self._setDefault(identifierCol="id", vectorCol="vector", neighborsCol="neighbors", numPartitions=1, k=5,
+                         distanceFunction="cosine", excludeSelf=False, similarityThreshold=-1.0, outputFormat="full",
+                         storageLevel="MEMORY_ONLY")
+
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, identifierCol="id", vectorCol="vector", neighborsCol="neighbors",
+                  numPartitions=1, k=5, distanceFunction="cosine", excludeSelf=False, similarityThreshold=-1.0,
+                  outputFormat="full", storageLevel="MEMORY_ONLY"):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def _create_model(self, java_model):
+        return BruteForceModel(java_model)
+
+
+class BruteForceModel(JavaModel):
+    """
+    Model fitted by BruteForce.
+    """
+    def __init__(self, java_model):
+        super(BruteForceModel, self).__init__(java_model)
+
+        # note: look at https://issues.apache.org/jira/browse/SPARK-10931 in the future
+
+        self.identifierCol = Param(self, "identifierCol", "the column name for the row identifier")
+        self.vectorCol = Param(self, "vectorCol", "the column name for the vector")
+        self.neighborsCol = Param(self, "neighborsCol", "column names for returned neighbors")
+        self.k = Param(self, "k", "number of neighbors to find")
+        self.excludeSelf = Param(self, "excludeSelf", "whether to include the row identifier as a candidate neighbor")
+        self.similarityThreshold = Param(self, "similarityThreshold",
+                                         "do not return neighbors further away than this distance")
+        self.outputFormat = Param(self, "outputFormat", "output format, one of full, minimal")
+
+        self._transfer_params_from_java()
---
+++ packages/pyspark-hnsw/pyspark_hnsw-0.28-py2-none-any.whl/pyspark_hnsw/hnsw.py
@@ -0,0 +1,77 @@
+from pyspark.ml.wrapper import JavaEstimator, JavaModel
+from pyspark.ml.param.shared import *
+from pyspark.mllib.common import inherit_doc
+from pyspark import keyword_only
+from pyspark.ml.util import JavaMLReadable, JavaMLWritable
+
+
+@inherit_doc
+class Hnsw(JavaEstimator, JavaMLReadable, JavaMLWritable):
+    """
+    Approximate nearest neighbour search.
+    """
+    @keyword_only
+    def __init__(self, identifierCol="id", vectorCol="vector", neighborsCol="neighbors",
+                 m=16, ef=10, efConstruction=200, numPartitions=1, k=5, distanceFunction="cosine",
+                 excludeSelf=False, similarityThreshold=-1.0, outputFormat="full"):
+        super(Hnsw, self).__init__()
+        self._java_obj = self._new_java_obj("com.github.jelmerk.spark.knn.hnsw.Hnsw", self.uid)
+
+        self.identifierCol = Param(self, "identifierCol", "the column name for the row identifier")
+        self.vectorCol = Param(self, "vectorCol", "the column name for the vector")
+        self.neighborsCol = Param(self, "neighborsCol", "column name for the returned neighbors")
+        self.m = Param(self, "m", "number of bi-directional links created for every new element during construction")
+        self.ef = Param(self, "ef", "size of the dynamic list for the nearest neighbors (used during the search)")
+        self.efConstruction = Param(self, "efConstruction",
+                                    "has the same meaning as ef, but controls the index time / index precision")
+        self.numPartitions = Param(self, "numPartitions", "number of partitions")
+        self.k = Param(self, "k", "number of neighbors to find")
+        self.distanceFunction = Param(self, "distanceFunction",
+                                      "distance function, one of bray-curtis, canberra, cosine, correlation, "
+                                      "euclidean, inner-product, manhattan or the fully qualified classname "
+                                      "of a distance function")
+        self.excludeSelf = Param(self, "excludeSelf", "whether to include the row identifier as a candidate neighbor")
+        self.similarityThreshold = Param(self, "similarityThreshold",
+                                         "do not return neighbors further away than this distance")
+        self.outputFormat = Param(self, "outputFormat", "output format, one of full, minimal")
+        self.storageLevel = Param(self, "storageLevel",
+                                  "storageLevel for the indices. Pass in a string representation of StorageLevel")
+
+        self._setDefault(identifierCol="id", vectorCol="vector", neighborsCol="neighbors",
+                         m=16, ef=10, efConstruction=200, numPartitions=1, k=5, distanceFunction="cosine",
+                         excludeSelf=False, similarityThreshold=-1.0, outputFormat="full", storageLevel="MEMORY_ONLY")
+
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, identifierCol="id", vectorCol="vector", neighborsCol="neighbors",
+                  m=16, ef=10, efConstruction=200, numPartitions=1, k=5, distanceFunction="cosine", excludeSelf=False,
+                  similarityThreshold=-1.0, outputFormat="full", storageLevel="MEMORY_ONLY"):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def _create_model(self, java_model):
+        return HnswModel(java_model)
+
+
+class HnswModel(JavaModel, JavaMLReadable, JavaMLWritable):
+    """
+    Model fitted by Hnsw.
+    """
+    def __init__(self, java_model):
+        super(HnswModel, self).__init__(java_model)
+
+        # note: look at https://issues.apache.org/jira/browse/SPARK-10931 in the future
+
+        self.identifierCol = Param(self, "identifierCol", "the column name for the row identifier")
+        self.vectorCol = Param(self, "vectorCol", "the column name for the vector")
+        self.neighborsCol = Param(self, "neighborsCol", "column names for returned neighbors")
+        self.k = Param(self, "k", "number of neighbors to find")
+        self.ef = Param(self, "ef", "size of the dynamic list for the nearest neighbors (used during the search)")
+        self.excludeSelf = Param(self, "excludeSelf", "whether to include the row identifier as a candidate neighbor")
+        self.similarityThreshold = Param(self, "similarityThreshold",
+                                         "do not return neighbors further away than this distance")
+        self.outputFormat = Param(self, "outputFormat", "output format, one of full, minimal")
+
+        self._transfer_params_from_java()
