7ebf80564c09824e6acad5654a743d639e77692d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:54 +0000

    Add package EOxServer/0.5.dev0/EOxServer-0.5.dev0.tar.gz

---
+++ packages/EOxServer/EOxServer-0.5.dev0.tar.gz/EOxServer-0.5.dev0/eoxserver/instance_template/project_name/urls.py
@@ -0,0 +1,56 @@
+#-------------------------------------------------------------------------------
+#
+# Project: EOxServer <http://eoxserver.org>
+# Authors: Stephan Krause <stephan.krause@eox.at>
+#          Stephan Meissl <stephan.meissl@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2012 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+"""
+URLs config for EOxServer's {{ project_name }} instance.
+
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from eoxserver.services.opensearch.urls import urlpatterns as opensearch
+from eoxserver.webclient.urls import urlpatterns as webclient
+from eoxserver.views import index
+
+
+admin.autodiscover()
+
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^ows', include("eoxserver.services.urls")),
+    url(r'^opensearch/', include(opensearch)),
+
+    # enable the client
+    url(r'^client/', include(webclient)),
+
+    # Enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+]
