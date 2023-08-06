670ba9cefaba1083846f6b3b7d66e4b0d9d6e916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:28 +0000

    Add package phorg/0.0.1/phorg-0.0.1.tar.gz

---
+++ packages/phorg/phorg-0.0.1.tar.gz/phorg-0.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['phorg']
+entry_points = \
+{'console_scripts': ['phorg = phorg:main']}
+
+setup_kwargs = {
+    'name': 'phorg',
+    'version': '0.0.1',
+    'description': "Command-line application to help reduce photography's organization time consumption.",
+    'long_description': None,
+    'author': 'NunoPalma',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '==3.8.0',
+}
+
+
+setup(**setup_kwargs)
