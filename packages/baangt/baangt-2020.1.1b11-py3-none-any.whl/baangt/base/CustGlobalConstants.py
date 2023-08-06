afa7752ddf03421c7fea08a21fd926de84273deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:46 +0000

    Add package baangt/2020.1.1b11/baangt-2020.1.1b11-py3-none-any.whl

---
+++ packages/baangt/baangt-2020.1.1b11-py3-none-any.whl/baangt/base/CustGlobalConstants.py
@@ -0,0 +1,12 @@
+CUST_TOASTS = "Toasts"
+# CUST_TOASTS_ERROR = "ToastsError" --> Replaced by GC.TESTCASEERRORLOG
+VIGOGFNUMMER = "VIGOGF#"
+SAPPOLNR = "SAP Polizzennr"
+PRAEMIE = "Prämie"
+POLNRHOST = "PolNR Host"
+VERMITTLER = "vermittler"
+MANDANT = "Mandant"
+DONAU = "DON"
+WSTV = "WSTV"
+
+CLASSES_TESTCASE = "baangtVIG.CustTestCaseMaster"
