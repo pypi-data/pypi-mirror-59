538bec4127bdba78ddfc968e09b6ea3953783ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:42 +0000

    Add package surya-distributions/0.1/surya_distributions-0.1.tar.gz

---
+++ packages/surya-distributions/surya_distributions-0.1.tar.gz/surya_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='surya_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['surya_distributions'],
+      author='Surya Sanchez',
+      author_email='hello@suryasanchez.com',
+      zip_safe=False)
