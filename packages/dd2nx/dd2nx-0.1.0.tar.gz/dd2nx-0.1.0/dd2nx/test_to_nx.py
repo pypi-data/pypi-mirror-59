a9424c6bd5424e2060d00515e00183eade292bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:07 +0000

    Add package dd2nx/0.1.0/dd2nx-0.1.0.tar.gz

---
+++ packages/dd2nx/dd2nx-0.1.0.tar.gz/dd2nx-0.1.0/dd2nx/test_to_nx.py
@@ -0,0 +1,26 @@
+from functools import reduce
+
+try:
+    from dd.cudd import BDD
+except ImportError:
+    from dd.autoref import BDD
+
+
+from dd2nx.to_nx import to_nx
+
+
+def xor(x, y):
+    return (x | y) & ~(x & y)
+
+
+def test_to_nx():
+    levels = {'x': 0, 'y': 1, 'z': 2, 'w': 3}
+    manager = BDD(levels)
+    manager.configure(reordering=False)
+
+    x, y, z, w = map(manager.var, "xyzw")
+    bexpr = reduce(xor, [x, y, z, w])
+
+    g, negated = to_nx(bexpr, pydot=True)
+    assert len(g.nodes) == 4 + 2
+    assert len(g.edges) == 2*4 + 1
---
+++ packages/dd2nx/dd2nx-0.1.0.tar.gz/dd2nx-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dd2nx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=19.3,<20.0',
+ 'dd>=0.5.4,<0.6.0',
+ 'funcy>=1.13,<2.0',
+ 'networkx>=2.4,<3.0']
+
+setup_kwargs = {
+    'name': 'dd2nx',
+    'version': '0.1.0',
+    'description': "Python library for converting the dd package's BDD to Multigraph in networkx.",
+    'long_description': "# dd2nx\nPython library for converting the dd package's BDD to Multigraph in networkx.\n",
+    'author': 'Marcell Vazquez-Chanlatte',
+    'author_email': 'mvc@linux.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mvcisback/dd2nx',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
