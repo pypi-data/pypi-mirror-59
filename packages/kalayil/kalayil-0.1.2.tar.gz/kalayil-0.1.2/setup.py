33efaa9a93d0a3676db24b8ae0b5395c4a12f4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:36 +0000

    Add package kalayil/0.1.2/kalayil-0.1.2.tar.gz

---
+++ packages/kalayil/kalayil-0.1.2.tar.gz/kalayil-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md",'r') as fh:
+	long_description = fh.read()
+
+setup(
+	name='kalayil',
+	version='0.1.2', # PEP440
+	description='Package for easier Machine Learning Workflow.',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	py_modules=['kalayil'], 
+	package_dir ={'':'src'},
+	url='https://github.com/santokalayil/kalayil',
+	author='Santo K Thomas',
+	author_email='santokalayil@gmail.com',
+	classifiers=[
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.7',
+			'Programming Language :: Python :: 3.8',
+			'Operating System :: OS Independent'
+	],
+	)
