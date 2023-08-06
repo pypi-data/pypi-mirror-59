d0d7367d3620529c6986c82cefa85e56d4c2ac70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:54 +0000

    Add package tensorflow-transform/0.21.0/tensorflow-transform-0.21.0.tar.gz

---
+++ packages/tensorflow-transform/tensorflow-transform-0.21.0.tar.gz/tensorflow-transform-0.21.0/tensorflow_transform/saved/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module level imports for tensorflow_transform.saved."""
+
+# TODO(b/35581165) All of the following dependencies introduce a cyclical
+# on dependency on tensorflow_transform.saved which dill is not being able to
+# handle.
+
+# from tensorflow_transform.saved.input_fn_maker import
+# build_parsing_transforming_serving_input_fn
+# from tensorflow_transform.saved.input_fn_maker import build_training_input_fn
+# from tensorflow_transform.saved.input_fn_maker import
+# build_transforming_training_input_fn
+# from tensorflow_transform.saved.saved_transform_io import
+# apply_saved_transform
