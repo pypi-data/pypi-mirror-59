f4260910201b0e544ca4696c2a58575a815e01ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:13 +0000

    Add package dynamixel-controller/0.3/dynamixel_controller-0.3.tar.gz

---
+++ packages/dynamixel-controller/dynamixel_controller-0.3.tar.gz/dynamixel_controller-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dynamixel_controller",
+    version=0.3,
+    packages=find_packages(),
+    author="Hunter Halloran (Jyumpp)",
+    author_email="hdh20267@uga.edu",
+    package_data={
+        '': ['*.json'],
+    },
+    install_requires=[
+        'setuptools',
+    ],
+    download_url='https://github.com/Jyumpp/dynamixel-controller/archive/v_01.tar.gz',
+    url="https://github.com/Jyumpp/dynamixel-controller",
+    keywords="dynamixel, dxl, dynamixel_sdk, motor controller",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
