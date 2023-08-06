15a4cddf737a76975de0800fea49ef6846eeef46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:10 +0000

    Add package hunter/3.1.2/hunter-3.1.2-cp38-cp38-win_amd64.whl

---
+++ packages/hunter/hunter-3.1.2-cp38-cp38-win_amd64.whl/hunter/const.py
@@ -0,0 +1,27 @@
+import collections
+import os
+import site
+import sys
+from distutils.sysconfig import get_python_lib
+
+SITE_PACKAGES_PATHS = set()
+if hasattr(site, 'getsitepackages'):
+    SITE_PACKAGES_PATHS.update(site.getsitepackages())
+if hasattr(site, 'getusersitepackages'):
+    SITE_PACKAGES_PATHS.add(site.getusersitepackages())
+SITE_PACKAGES_PATHS.add(get_python_lib())
+SITE_PACKAGES_PATHS = tuple(SITE_PACKAGES_PATHS)
+
+SYS_PREFIX_PATHS = {
+    '<frozen importlib._bootstrap>',
+    '<frozen importlib._bootstrap_external>',
+    sys.prefix,
+    sys.exec_prefix,
+    os.path.dirname(os.__file__),
+    os.path.dirname(collections.__file__),
+}
+for prop in 'real_prefix', 'real_exec_prefix', 'base_prefix', 'base_exec_prefix':
+    if hasattr(sys, prop):
+        SYS_PREFIX_PATHS.add(getattr(sys, prop))
+
+SYS_PREFIX_PATHS = tuple(sorted(SYS_PREFIX_PATHS, key=len, reverse=True))
