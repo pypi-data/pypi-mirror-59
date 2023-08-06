32c89f6ab3553e234b9ab9c0e3672634f0f74693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package LyricsPy/0.1.7.post2/LyricsPy-0.1.7.post2.tar.gz

---
+++ packages/LyricsPy/LyricsPy-0.1.7.post2.tar.gz/LyricsPy-0.1.7.post2/lyricspy/__init__.py
@@ -0,0 +1,60 @@
+from markdownify import markdownify as md
+from bs4 import BeautifulSoup
+import re
+import urllib3
+
+http = urllib3.PoolManager()
+
+def letra(query, **kwargs):
+    tr = 'a'
+    query = query.replace('www.letras', 'm.letras')
+    r = http.request("get",query, fields=dict(**kwargs))
+    soup = BeautifulSoup(r.data, "html.parser")
+    a = soup.find('div', "lyric-cnt g-1")
+    if a is None:
+        a = soup.find('div', "lyric-tra_l")
+        tr = soup.find('div', "lyric-tra_r")
+    b = ''
+    for i in a.find_all('p'):
+        b += md(str(i))
+    c = soup.find("div", "lyric-title g-1")
+    musica = c.find('h1').get_text()
+    autor = c.find('a').get_text()
+    ret = {'autor': autor, 'musica': musica, 'letra': b.replace('\n\n\n', '\n\n'), 'link': r.url}
+    if 'a' not in tr:
+        b = ''
+        for i in tr.find_all('p'):
+            b += md(str(i))
+        ret['traducao'] = b.replace('\n\n\n', '\n\n')
+
+    return ret
+
+
+def auto(query, limit=4):
+    result = []
+    n = 0
+    for i in search(q='site:letras.mus.br ' + query):
+        if re.match(r'^(https?://)?(letras\.mus.br/|(m\.|www\.)?letras\.mus\.br/).+', i) and '/traducao.html' not in i:
+            try:
+                a = letra(i)
+                result.append(a)
+                n += 1
+            except AttributeError:
+                pass
+        if n == limit:
+            break
+
+    return result
+
+def search(q, **kwargs):
+    a = []
+    rt = http.request("get","https://www.bing.com/search", fields=dict(q=q, **kwargs))
+    soupt = BeautifulSoup(rt.data, "html.parser")
+    results = soupt.find("ol", {"id":"b_results"})
+    lists = results.findAll("li", {"class":"b_algo"})
+    for item in lists:
+        item_text = item.find("a").text
+        item_href = item.find("a").attrs["href"]
+        if item_text and item_href:
+            a.append(item_href)
+    return a
