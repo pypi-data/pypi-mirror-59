1fb4dc60343ac199d7fbcd30fac9035495a696b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:48 +0000

    Add package trell-data-pipeline/0.1.1/trell_data_pipeline-0.1.1-py2.py3-none-any.whl

---
+++ packages/trell-data-pipeline/trell_data_pipeline-0.1.1-py2.py3-none-any.whl/pipeline/settings.py
@@ -0,0 +1,13 @@
+# system config
+LOG_DIR = None
+
+# LoRa server settings
+LORA_MQTT_TOPICS = None
+LORA_MQTT_URI = None
+LORA_DRIVER = None
+
+# Producer settings
+PRODUCER_DRIVER = None
+PRODUCER_TOPIC = None
+PRODUCER_URI = None
+PRODUCER_PORT = None
