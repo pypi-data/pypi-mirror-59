df26fc6644484dc9c8b15c584e9b166652554dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package pyeee/0.3/pyeee-0.3-py2.py3-none-any.whl

---
+++ packages/pyeee/pyeee-0.3-py2.py3-none-any.whl/pyeee/version.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import division, absolute_import, print_function
+"""
+Provide version number for pyeee library.
+
+This module was written by Matthias Cuntz while at Institut National
+de Recherche en Agriculture, Alimentation et Environnement (INRAE),
+Nancy, France.
+
+Copyright (c) 2019-2020 Matthias Cuntz - mc (at) macu (dot) de
+Released under the MIT License; see LICENSE file for details.
+
+* Written Oct 2019 by Matthias Cuntz (mc (at) macu (dot) de)
+* Make numpy doctsring format, Dec 2019, Matthias Cuntz
+* Set version number 0.2, Jan 2020, Matthias Cuntz
+* Set version number 0.3, Jan 2020, Matthias Cuntz
+
+.. moduleauthor:: Matthias Cuntz
+"""
+__version__ = "0.3"
