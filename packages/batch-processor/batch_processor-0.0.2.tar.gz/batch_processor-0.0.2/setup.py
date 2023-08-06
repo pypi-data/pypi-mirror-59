fc3accd0f9912e7e147a0163dcaea339482141d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package batch-processor/0.0.2/batch_processor-0.0.2.tar.gz

---
+++ packages/batch-processor/batch_processor-0.0.2.tar.gz/batch_processor-0.0.2/setup.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(name='batch_processor',
+      version='0.0.2',
+      description='This library is used to enable batch processing by implementing python multiprocessing',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Operating System :: OS Independent',
+      ],      
+      keywords='batch, multiprocessing, performance',
+      url='https://github.com/leon0707/batch_processor',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[],
+      include_package_data=True,
+      python_requires='>=3.5',
+      zip_safe=False
+)
