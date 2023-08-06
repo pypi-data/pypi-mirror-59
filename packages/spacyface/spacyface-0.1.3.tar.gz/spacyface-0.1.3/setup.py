b2e9d542c6f609ca53ecd18c1dfcfa4d487d666f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package spacyface/0.1.3/spacyface-0.1.3.tar.gz

---
+++ packages/spacyface/spacyface-0.1.3.tar.gz/spacyface-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'transformers>=2.3.0',
+    'h5py>=2.10.0',
+    'numpy>=1.17.4',
+    'regex>=2020.1.8',
+    'spacy>=2.2.3'
+]
+
+setup(
+    name="spacyface",
+    description="Aligner for spacy and huggingface tokenization",
+    packages=['spacyface'],
+    version='0.1.3',
+    license='MIT',
+    author="Ben Hoover",
+    author_email="benjamin.hoover@ibm.com",
+    url="https://github.com/bhoov/spacyface",
+    download_url = 'https://github.com/bhoov/spacyface/archive/v0.1.1.tar.gz',
+    keywords=["transformer", "pytorch", "spacy", "tokenize", "tokenization", "NLP", "Natural Language Processing",
+              "huggingface", "linguistic"],
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ]
+)
