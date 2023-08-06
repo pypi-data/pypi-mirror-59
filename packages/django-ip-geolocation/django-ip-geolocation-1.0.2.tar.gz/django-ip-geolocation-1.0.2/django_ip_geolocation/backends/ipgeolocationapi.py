ed59173ee4d38268c8956a7df0fa348f904487be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:33 +0000

    Add package django-ip-geolocation/1.0.2/django-ip-geolocation-1.0.2.tar.gz

---
+++ packages/django-ip-geolocation/django-ip-geolocation-1.0.2.tar.gz/django-ip-geolocation-1.0.2/django_ip_geolocation/backends/ipgeolocationapi.py
@@ -0,0 +1,38 @@
+import requests
+from django_ip_geolocation.backends import GeolocationBackend
+
+
+class IPGeolocationAPI(GeolocationBackend):
+
+    def geolocate(self):
+        res = requests.get('https://api.ipgeolocationapi.com/geolocate/{}'.format(self._ip))
+        if res.ok:
+            self._raw_data = res.json()
+
+        
+    def _parse(self):
+        """raw_data example:
+        {
+            "continent": "Europe",
+            "alpha2": "DE",
+            "country_code": "49",
+            "international_prefix": "00",
+            "name": "Germany",
+            "languages_spoken": [
+                "de"
+            ],
+            "geo": {
+                "latitude": 51.165691,
+                "longitude": 10.451526,
+            },
+            "currency_code": "EUR"
+        }
+        """
+        self._continent = self._raw_data.get('continent')
+        self._county = {
+            'code': self._raw_data.get('alpha2'),
+            'name': self._raw_data.get('name'),
+        }
+
+        self._geo_data = self._raw_data.get('geo')
+
