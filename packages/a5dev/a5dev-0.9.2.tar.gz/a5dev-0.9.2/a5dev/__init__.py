d32ff9656d090bcb25ceb07fe044de0706b72ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package a5dev/0.9.2/a5dev-0.9.2.tar.gz

---
+++ packages/a5dev/a5dev-0.9.2.tar.gz/a5dev-0.9.2/a5dev/__init__.py
@@ -0,0 +1,7 @@
+from . import deepfake
+from . import general
+from . import image
+from . import video
+from . import transfersh
+from .general import download_necessary_files
+download_necessary_files()
