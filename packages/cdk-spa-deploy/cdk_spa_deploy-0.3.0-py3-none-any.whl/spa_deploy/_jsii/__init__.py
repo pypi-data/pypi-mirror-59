4e93611e64c96247c991c3a8c9583f716a53059c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:49 +0000

    Add package cdk-spa-deploy/0.3.0/cdk_spa_deploy-0.3.0-py3-none-any.whl

---
+++ packages/cdk-spa-deploy/cdk_spa_deploy-0.3.0-py3-none-any.whl/spa_deploy/_jsii/__init__.py
@@ -0,0 +1,18 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_certificatemanager
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_deployment
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
