0351b78299f253b6d894743c60b6b0f8bb8407fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:57 +0000

    Add package probability/0.0.4/probability-0.0.4.tar.gz

---
+++ packages/probability/probability-0.0.4.tar.gz/probability-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name='probability',
+  packages=find_packages(),
+  version='0.0.4',
+  license='MIT',
+  description='Probability in Python 3',
+  author='Vahndi Minah',
+  url='https://github.com/vahndi/probability',
+  download_url='https://github.com/vahndi/probability/archive/v_0.0.3.tar.gz',
+  keywords=['python', 'probability'],
+  install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
