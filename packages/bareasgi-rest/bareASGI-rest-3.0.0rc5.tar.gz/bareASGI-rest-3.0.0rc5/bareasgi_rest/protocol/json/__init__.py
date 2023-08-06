571ca9d9e8679367ccb2bf2cfb3a4a8c3a99ba67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:36 +0000

    Add package bareasgi-rest/3.0.0rc5/bareASGI-rest-3.0.0rc5.tar.gz

---
+++ packages/bareasgi-rest/bareASGI-rest-3.0.0rc5.tar.gz/bareASGI-rest-3.0.0rc5/bareasgi_rest/protocol/json/__init__.py
@@ -0,0 +1,32 @@
+"""JSON Serialization"""
+
+from .coercion import (
+    from_json_value,
+    is_json_container,
+    is_json_literal,
+    camelize_object,
+    underscore_object
+)
+from .serialization import (
+    to_json,
+    from_json,
+    from_form_data,
+    from_query_string,
+    JSONEncoderEx,
+    json_to_python
+)
+
+__all__ = [
+    'from_json_value',
+    'is_json_container',
+    'is_json_literal',
+    'camelize_object',
+    'underscore_object',
+
+    'to_json',
+    'from_json',
+    'from_form_data',
+    'from_query_string',
+    'JSONEncoderEx',
+    'json_to_python'
+]
---
+++ packages/bareasgi-rest/bareASGI-rest-3.0.0rc5.tar.gz/bareASGI-rest-3.0.0rc5/bareasgi_rest/types.py
@@ -0,0 +1,35 @@
+"""Types"""
+
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    Generic,
+    TypeVar
+)
+
+Deserializer = Callable[[str, bytes, Dict[bytes, bytes], Any], Any]
+DictConsumes = Dict[bytes, Deserializer]
+
+Serializer = Callable[[Any], str]
+DictProduces = Dict[bytes, Serializer]
+
+RestCallback = Callable[..., Awaitable[Any]]
+
+T = TypeVar('T')
+
+
+class Body(Generic[T]):
+    """A wrapper for the body"""
+
+    def __init__(self, value: T) -> None:
+        """A wrapper for the body
+
+        Args:
+            value (T): The body value
+        """
+        self.value = value
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, type(self)) and self.value == other.value
