5905a702a5840745265ea31b20c07d4ebd515727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package sob/0.1.34/sob-0.1.34.tar.gz

---
+++ packages/sob/sob-0.1.34.tar.gz/sob-0.1.34/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sob',
+    version="0.1.34",
+    description=(
+        'A framework for serializing/deserializing JSON/YAML into python class'
+        ' instances and vice versa'
+    ),
+    url='https://github.com/davebelais/sob.git',
+    author='David Belais',
+    author_email='david@belais.me',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='rest api serialization serialize',
+    packages=find_packages(),
+    install_requires=[
+        "future>=0.18.2",
+        "pyyaml>=5.1.1",
+        "iso8601>=0.1.12"
+    ],
+    extras_require={
+        "testing": [
+            "setuptools_setup_versions>=0.0.29"
+        ]
+    }
+)
