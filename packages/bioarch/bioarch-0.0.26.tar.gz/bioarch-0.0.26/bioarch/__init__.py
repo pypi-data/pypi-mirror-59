66bd59c135fe3e74cd9aa7e4f80ee60664cd2c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:50 +0000

    Add package bioarch/0.0.26/bioarch-0.0.26.tar.gz

---
+++ packages/bioarch/bioarch-0.0.26.tar.gz/bioarch-0.0.26/bioarch/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+import sys
+
+
+from .age import AgeCategory, EstimatedAge
+from .context import Context
+from .individual import AgeSexStature, BurialInfo, Individual, LongBoneMeasurement, OsteologicalSex
+from .joints import JointCondition, Joints
+from .left_right import LeftRight
+from .mouth import Mouth, Tooth
+from .occupational_markers import EnthesialMarker, OccupationalMarkers
+from .sex import Sex
+from .trauma import Trauma, TraumaCategory
+
+
+if sys.version_info < (3, 7):
+    print('ERROR: Only Python 3.7 and above is supported')
+    sys.exit(-1)
+
+
+__title__ = 'bioarch'
+__version__ = '0.0.26'
+__author__ = 'Guy Taylor'
+
+__all__ = ['AgeCategory', 'EstimatedAge'] + ['Context'] + ['AgeSexStature', 'BurialInfo', 'Individual', 'LongBoneMeasurement', 'OsteologicalSex'] + \
+          ['JointCondition', 'Joints'] + ['EnthesialMarker', 'OccupationalMarkers'] + ['LeftRight'] + ['Mouth', 'Tooth'] + ['Sex'] + ['Trauma', 'TraumaCategory']
