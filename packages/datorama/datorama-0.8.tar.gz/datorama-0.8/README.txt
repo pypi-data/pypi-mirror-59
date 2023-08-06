c3f003740bc7f7a5ab96382e30ed7133c5d0a068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:19 +0000

    Add package datorama/0.8/datorama-0.8.tar.gz

---
+++ packages/datorama/datorama-0.8.tar.gz/datorama-0.8/README.txt
@@ -0,0 +1,5 @@
+datorama
+
+This is python package for using it in Datorama Python Connector.
+
+pip install datorama
---
+++ packages/datorama/datorama-0.8.tar.gz/datorama-0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='datorama',
+    version='0.8',
+    packages=['datorama'],
+    license='MIT',
+    description='Datorama Python Package',
+    url='https://github.com/datorama/python-connector-package',
+    author='Asaad Awesat',
+    author_email='asaad.awesat@datorama.com'
+)
