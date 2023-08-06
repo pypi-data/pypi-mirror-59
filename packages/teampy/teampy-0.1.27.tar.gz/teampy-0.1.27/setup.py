951904e8d6d8cac6657915bdb370f016307b9393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:53 +0000

    Add package teampy/0.1.27/teampy-0.1.27.tar.gz

---
+++ packages/teampy/teampy-0.1.27.tar.gz/teampy-0.1.27/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name='teampy',
+  packages=['teampy'],
+  version='0.1.27',
+  description='Tools for Team-Based Learning',
+  install_requires=['pyyaml', 'colorama', 'click', 'numpy', 'pandas', 'xlrd', 'openpyxl', 'progressbar2', 'latex'],
+  package_data={
+        '': ['*.tex', '*.pdf'],
+    },
+  include_package_data=True,
+  author='Frank Alexander Kraemer',
+  author_email='kraemer.frank@gmail.com',
+  license='GPLv3',
+  url='https://github.com/falkr/teampy',
+  download_url='https://github.com/falkr/teampy/archive/0.2.tar.gz',
+  keywords=['education'],
+  classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'],
+  entry_points = {'console_scripts': ['teampy=teampy.command_line_setup:teampy', 'rat=teampy.command_line_rat:rat']}
+)
