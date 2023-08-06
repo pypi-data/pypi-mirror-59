839e9776e7800a050e8f19a7c33cee5afc931de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:21 +0000

    Add package catscore/0.1.4/catscore-0.1.4-py3-none-any.whl

---
+++ packages/catscore/catscore-0.1.4-py3-none-any.whl/tests/http/test_web_driver.py
@@ -0,0 +1,34 @@
+import unittest
+from catscore.http.web_driver import CatsWebDriver
+import argparse
+import time
+
+class TestCatsWebDriver(unittest.TestCase):
+    binary_location = "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
+    executable_path = "/Users/rv/workspace/docker/finance_db/tools/chromedriver"
+    proxy = None
+    headless = True
+    
+    def test_simple_get(self):
+        """[python -m unittest tests.http.test_web_driver.TestCatsWebDriver.test_simple_get]
+        """
+        request:CatsRequest = CatsWebDriver(binary_location=self.binary_location, executable_path=self.executable_path, proxy=self.proxy, headless=self.headless)
+        html = request.move(url="https://yahoo.co.jp")
+        time.sleep(10)
+        request.close()
+    
+    def test_websocket(self):
+        """[python -m unittest tests.http.test_web_driver.TestCatsWebDriver.test_websocket]
+        """
+        request:CatsRequest = CatsWebDriver(binary_location=self.binary_location, executable_path=self.executable_path, proxy=self.proxy, headless=self.headless)
+        html = request.move(url="https://demo.click-sec.com/ixop/order.do")
+        time.sleep(10)
+        log = request.log()
+        with open("/Users/rv/Desktop/a.text", mode='w') as f:
+            for l in log:
+                f.write(l)
+        time.sleep(10)
+        request.close() 
+
+if __name__ == "__main__":
+    unittest.main()
