1727d36396b5818eeac35039611935028cceb0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:58 +0000

    Add package BIT-Online-Code-Helper/1.0.0/BIT%20Online%20Code%20Helper-1.0.0.tar.gz

---
+++ packages/BIT-Online-Code-Helper/BIT%20Online%20Code%20Helper-1.0.0.tar.gz/BIT Online Code Helper-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+description_file = open('README.md', 'r', encoding='utf-8')
+
+setuptools.setup(
+    name='BIT Online Code Helper',
+    version='1.0.0',
+    description='Test your code of "BIT Online coding homework" more easily and conveniently!',
+    long_description=description_file.read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Crawler995/BITOnlineCodeHelper',
+    author='crawler995',
+    author_email='zhang_995@foxmail.com',
+    keywords=['BIT', 'code', 'helper'],
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        'requests',
+        'tqdm'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bch=main:main'
+        ]
+    }
+)
