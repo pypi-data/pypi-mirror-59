dd01a1012bc1ae3b3bd4ef84cb6500c516b8e3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:05 +0000

    Add package flask-rest-error-handling/3.0.0/flask-rest-error-handling-3.0.0.tar.gz

---
+++ packages/flask-rest-error-handling/flask-rest-error-handling-3.0.0.tar.gz/flask-rest-error-handling-3.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="flask-rest-error-handling",
+    version="3.0.0",
+    packages=setuptools.find_packages(),
+    author="Reece Dunham",
+    author_email="me@rdil.rocks",
+    url="https://github.com/RDIL/bluejay",
+    description="Provides users with JSON responses for errors.",
+    install_requires=["Flask"],
+    license="MIT"
+)
