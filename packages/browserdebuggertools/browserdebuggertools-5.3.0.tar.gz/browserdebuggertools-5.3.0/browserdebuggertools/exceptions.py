f256f3acd22884134b298297b231095ce8c143ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:24 +0000

    Add package browserdebuggertools/5.3.0/browserdebuggertools-5.3.0.tar.gz

---
+++ packages/browserdebuggertools/browserdebuggertools-5.3.0.tar.gz/browserdebuggertools-5.3.0/browserdebuggertools/exceptions.py
@@ -0,0 +1,50 @@
+class DevToolsException(Exception):
+    pass
+
+
+class ProtocolError(DevToolsException):
+    pass
+
+
+class NotFoundError(DevToolsException):
+    pass
+
+
+class DevToolsTimeoutException(DevToolsException):
+    pass
+
+
+class TimerException(DevToolsException):
+    pass
+
+
+class TabNotFoundError(NotFoundError):
+    pass
+
+
+class DomainNotEnabledError(DevToolsException):
+    pass
+
+
+class MethodNotFoundError(ProtocolError, NotFoundError):
+    pass
+
+
+class ResultNotFoundError(NotFoundError):
+    pass
+
+
+class ResourceNotFoundError(NotFoundError):
+    pass
+
+
+class JavascriptDialogNotFoundError(NotFoundError):
+    pass
+
+
+class MaxRetriesException(DevToolsException):
+    pass
+
+
+class UnknownError(ProtocolError):
+    pass
