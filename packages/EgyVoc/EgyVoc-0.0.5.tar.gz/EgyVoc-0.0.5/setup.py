773caeb26f7787f622a58fd8f5d5e5d205e82148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:57 +0000

    Add package EgyVoc/0.0.5/EgyVoc-0.0.5.tar.gz

---
+++ packages/EgyVoc/EgyVoc-0.0.5.tar.gz/EgyVoc-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EgyVoc", # Replace with your own username
+    version="0.0.5",
+    author="Marwan Kilani",
+    author_email="kilani.edu@gmail.com",
+    description="Vocaliazer for Ancient Egyptian",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MKilani/EgyVoc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.',
+)
