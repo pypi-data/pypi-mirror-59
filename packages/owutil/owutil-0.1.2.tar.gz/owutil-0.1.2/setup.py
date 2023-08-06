c78ad7fb0184ea835eb8a2b82d218905861c7256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:46 +0000

    Add package owutil/0.1.2/owutil-0.1.2.tar.gz

---
+++ packages/owutil/owutil-0.1.2.tar.gz/owutil-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['owutil']
+entry_points = \
+{'console_scripts': ['owutil = owutil:main']}
+
+setup_kwargs = {
+    'name': 'owutil',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'drys262',
+    'author_email': 'koeichavez17@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
