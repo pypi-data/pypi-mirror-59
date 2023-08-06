0142a7269286e0675440764a1b50e938ec447547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:18 +0000

    Add package storedisagg/1.0.0/storedisagg-1.0.0.tar.gz

---
+++ packages/storedisagg/storedisagg-1.0.0.tar.gz/storedisagg-1.0.0/README.md
@@ -0,0 +1,12 @@
+
+# Storage operation disaggregator
+
+A module to disaggregate energy storage operation by time scales.
+The disaggregation only requires the charging and discharging power profiles and is agnostic with respect to electricity prices or other drivers.
+
+# Example
+
+Please consult the [example notebook](storedisagg/example/std_example.ipynb)
+
+
+
---
+++ packages/storedisagg/storedisagg-1.0.0.tar.gz/storedisagg-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import fnmatch
+from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py as build_py_orig
+
+with open("description_pypi.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="storedisagg",
+    version="1.0.0",
+    author="storedisagg contributors listed in AUTHORS",
+    author_email="m.c.soini@posteo.de",
+    description=("Ex-post disaggregation of storage operation by time scales"),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/mcsoini/storedisagg",
+    packages=find_packages(),
+    install_requires=['scipy', 'pandas', 'matplotlib', 'wrapt', 'tqdm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"],
+)
