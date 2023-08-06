49f393ef855a19447da0e3c48ab19d298673b888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:29 +0000

    Add package beaconscanner/1.1.1/beaconscanner-1.1.1.tar.gz

---
+++ packages/beaconscanner/beaconscanner-1.1.1.tar.gz/beaconscanner-1.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'beaconscanner',
+  packages = ['beaconscanner'],
+  version = '1.1.1',  
+  license='MIT',
+  description = 'Scan and decode ble data',
+  author = 'theBASTI0N',
+  author_email = 'theBASTI0Ncode@gmail.com',
+  url = 'https://github.com/theBASTI0N/beaconscanner',
+  download_url = 'https://github.com/theBASTI0N/beaconscanner/archive/1.1.1.tar.gz',
+  keywords = ['BLE', 'decode', 'iot'],
+  install_requires=['beacondecoder', 'uptime', 'pyserial'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
