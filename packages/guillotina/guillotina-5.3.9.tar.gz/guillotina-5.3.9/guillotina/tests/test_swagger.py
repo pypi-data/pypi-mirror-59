6e2b553a3a7a68644a12a887836d70da7b7fb51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:37 +0000

    Add package guillotina/5.3.9/guillotina-5.3.9.tar.gz

---
+++ packages/guillotina/guillotina-5.3.9.tar.gz/guillotina-5.3.9/guillotina/tests/test_swagger.py
@@ -0,0 +1,41 @@
+from openapi_spec_validator import validate_v3_spec
+
+import os
+import pytest
+
+
+SWAGGER_SETTINGS = {"applications": ["guillotina.contrib.swagger"]}
+
+
+@pytest.mark.app_settings(SWAGGER_SETTINGS)
+async def test_get_swagger_definition(container_requester):
+    async with container_requester as requester:
+        resp, status = await requester("GET", "/@swagger")
+        assert status == 200
+        assert "/" in resp["paths"]
+
+    async with container_requester as requester:
+        resp, status = await requester("GET", "/db/@swagger")
+        assert status == 200
+        assert "/db" in resp["paths"]
+
+    async with container_requester as requester:
+        resp, status = await requester("GET", "/db/guillotina/@swagger")
+        assert status == 200
+        assert "/db/guillotina" in resp["paths"]
+
+
+@pytest.mark.app_settings(SWAGGER_SETTINGS)
+async def test_get_swagger_index(container_requester):
+    async with container_requester as requester:
+        resp, status = await requester("GET", "/@docs")
+        assert status == 200
+
+
+@pytest.mark.app_settings(SWAGGER_SETTINGS)
+async def test_validate_swagger_definition(container_requester):
+    async with container_requester as requester:
+        for path in ("/", "/db", "/db/guillotina"):
+            resp, status = await requester("GET", os.path.join(path, "@swagger"))
+            assert status == 200
+            validate_v3_spec(resp)
