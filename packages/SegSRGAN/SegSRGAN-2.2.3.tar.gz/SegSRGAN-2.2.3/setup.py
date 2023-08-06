f255c910ae1088aab5bf6961588f8c97339acf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:20 +0000

    Add package SegSRGAN/2.2.3/SegSRGAN-2.2.3.tar.gz

---
+++ packages/SegSRGAN/SegSRGAN-2.2.3.tar.gz/SegSRGAN-2.2.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from os.path import abspath
+
+with open(abspath("README.md"), encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open(abspath("requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="SegSRGAN",
+    version="2.2.3",
+    author="Clément Cazorla", #Quentin Delannoy, Guillaume Dollé, Nicolas Passat, François Rousseau",
+    author_email="clement.cazorla@univ-reims.fr", # quentin.delannoy@univ-reims.fr, guillaume.dolle@univ-reims.fr, nicolas.passat@univ-reims.fr, francois.rousseau@imt-atlantique.fr",
+    description="Segmentation and super resolution GAN network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/koopa31/SegSRGAN",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+)
