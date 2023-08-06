5e7352fd50577c01b53804b63ff930941977d299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:05 +0000

    Add package torequests/4.8.21/torequests-4.8.21-py2-none-any.whl

---
+++ packages/torequests/torequests-4.8.21-py2-none-any.whl/torequests/__init__.py
@@ -0,0 +1,17 @@
+#! coding: utf-8
+import codecs
+import os
+
+from .main import *
+
+__all__ = [
+    "Pool",
+    "ProcessPool",
+    "NewFuture",
+    "Async",
+    "threads",
+    "get_results_generator",
+    "run_after_async",
+    "tPool",
+]
+__version__ = '4.8.21'
