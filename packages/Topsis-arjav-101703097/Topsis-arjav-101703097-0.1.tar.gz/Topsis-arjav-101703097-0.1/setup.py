1ac86ba05d928e9109c1292805b064e970a2a564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:06 +0000

    Add package Topsis-arjav-101703097/0.1/Topsis-arjav-101703097-0.1.tar.gz

---
+++ packages/Topsis-arjav-101703097/Topsis-arjav-101703097-0.1.tar.gz/Topsis-arjav-101703097-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis-arjav-101703097',
+  packages = ['Topsis'],
+  version = '0.1',
+  license='MIT',
+  description = 'library to perform topsis technique',
+  author = 'Arjav Jain',
+  author_email = 'arjavjain59998@gmail.com',
+  url = 'https://github.com/arjavjain59998',
+  download_url = 'https://github.com/arjavjain59998/topsis.git',
+  keywords = ['Topsis'],
+  install_requires=[
+        'python',
+        'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
