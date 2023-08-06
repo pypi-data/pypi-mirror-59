ccfe072fa56089bcdfc43841ec409c32bd0a58c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package data-science-types/0.2.2/data_science_types-0.2.2-py3-none-any.whl

---
+++ packages/data-science-types/data_science_types-0.2.2-py3-none-any.whl/pandas-stubs/_core.pyi
@@ -0,0 +1,32 @@
+from typing import Tuple, List, Union, IO, Optional, Any
+from typing_extensions import Literal
+from pathlib import Path
+import numpy as _np
+
+from pandas.core.frame import DataFrame
+
+from pandas.core.series import Series
+
+_AxisType = Literal["columns", "index"]
+
+def concat(
+    dataframes: List[DataFrame],
+    axis: _AxisType = ...,
+    sort: Optional[bool] = ...,
+    ignore_index: bool = ...,
+) -> DataFrame: ...
+def cut(arr: _np.ndarray, bins: int) -> Tuple[Union[Series, _np.ndarray], _np.ndarray]: ...
+def get_dummies(df: Union[DataFrame, Series]) -> DataFrame: ...
+def isnull(df: Union[DataFrame, Series]) -> _np.ndarray: ...
+def merge(left: DataFrame, right: DataFrame, on: str = ...) -> DataFrame: ...
+def read_csv(
+    filepath_or_buffer: Union[str, Path],
+    sep: str = ...,
+    header: Optional[int] = ...,
+    index_col: Optional[int] = ...,
+    delim_whitespace: bool = ...,
+    names: Optional[List[str]] = ...,
+    dtype: Optional[Any] = ...,
+) -> DataFrame: ...
+def read_feather(p: Union[Path, IO]) -> DataFrame: ...
+def unique(values: Series) -> _np.ndarray: ...
