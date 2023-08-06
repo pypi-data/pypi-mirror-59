0c4aebaf9c68e4d546d252dd27cd9f5e147f23a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package blastsight/0.2.0/blastsight-0.2.0.tar.gz

---
+++ packages/blastsight/blastsight-0.2.0.tar.gz/blastsight-0.2.0/blastsight/view/glprograms/tubeprogram.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#  Copyright (c) 2019-2020 Gabriel Sanhueza.
+#
+#  Distributed under the MIT License.
+#  See LICENSE for more info.
+
+from .shaderprogram import ShaderProgram
+
+
+class TubeProgram(ShaderProgram):
+    def __init__(self, widget):
+        super().__init__(widget)
+        self.base_name = 'Tube'
