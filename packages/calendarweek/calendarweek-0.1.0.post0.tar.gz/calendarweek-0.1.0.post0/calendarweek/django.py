83967a54c1316574619fdd8a12670f20e2cf9ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:45 +0000

    Add package calendarweek/0.1.0.post0/calendarweek-0.1.0.post0.tar.gz

---
+++ packages/calendarweek/calendarweek-0.1.0.post0.tar.gz/calendarweek-0.1.0.post0/calendarweek/django.py
@@ -0,0 +1,34 @@
+import locale
+from typing import Tuple
+
+from django.utils.encoding import DEFAULT_LOCALE_ENCODING
+from django.utils.functional import lazy
+from django.utils.translation import get_language, to_locale
+
+from .calendarweek import CalendarWeek
+
+
+def i18n_day_names() -> Tuple[Tuple[int, str]]:
+    """ Return an enumeration of day names for the current locale. """
+
+    loc = to_locale(get_language()) + "." + DEFAULT_LOCALE_ENCODING
+
+    try:
+        return CalendarWeek.day_names(loc)
+    except locale.Error:
+        return CalendarWeek.day_names()
+
+
+def i18n_day_abbrs() -> Tuple[Tuple[int, str]]:
+    """ Return an enumeration of day name abbreviations for the current locale. """
+
+    loc = to_locale(get_language()) + "." + DEFAULT_LOCALE_ENCODING
+
+    try:
+        return CalendarWeek.day_abbrs(loc)
+    except locale.Error:
+        return CalendarWeek.day_abbrs()
+
+
+i18n_day_names_lazy = lazy(i18n_day_names, tuple)
+i18n_day_abbrs_lazy = lazy(i18n_day_abbrs, tuple)
