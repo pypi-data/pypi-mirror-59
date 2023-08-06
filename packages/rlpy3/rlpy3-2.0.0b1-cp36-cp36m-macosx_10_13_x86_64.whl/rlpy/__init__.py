c20595c2dcc1231aa69e17464f24f39cb6140bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:01 +0000

    Add package rlpy3/2.0.0b1/rlpy3-2.0.0b1-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/rlpy3/rlpy3-2.0.0b1-cp36-cp36m-macosx_10_13_x86_64.whl/rlpy/__init__.py
@@ -0,0 +1,10 @@
+import rlpy.agents
+import rlpy.tools
+import rlpy.domains
+import rlpy.mdp_solvers
+import rlpy.experiments
+import rlpy.policies
+import rlpy.representations
+
+
+__version__ = "2.0.0b1"
