698994fa883fa87248f074b648fdea72e1c1691c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:40 +0000

    Add package firstApp-lal/0.1/firstApp_lal-0.1-py3-none-any.whl

---
+++ packages/firstApp-lal/firstApp_lal-0.1-py3-none-any.whl/myapp/greet.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ==================================================
+# @Time        : 2020/1/17 10:31
+# @Author      : 刘安龙
+# @Description : 
+# @Email       : liuanlong@deri.energy
+# @Site        : http://www.deri.energy/
+# @License     : Copyright(C), DERI
+# @IDE         : PyCharm
+# ==================================================
+ 
+# greet.py
+
+def hello():
+    print('Hello, welcome to setuptools!')
