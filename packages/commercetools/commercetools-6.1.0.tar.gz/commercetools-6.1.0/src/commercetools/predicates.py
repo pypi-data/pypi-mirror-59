be2e27b014da3dde3e9a7f1cbc87d730faf1fe1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:34 +0000

    Add package commercetools/6.1.0/commercetools-6.1.0.tar.gz

---
+++ packages/commercetools/commercetools-6.1.0.tar.gz/commercetools-6.1.0/src/commercetools/predicates.py
@@ -0,0 +1,78 @@
+import json
+from collections.abc import Collection
+from decimal import Decimal
+from functools import reduce
+
+
+class QueryPredicate:
+    AND = "AND"
+    OR = "OR"
+
+    _operators = {
+        "exact": "=",
+        "gte": ">=",
+        "in": "in",
+        "lte": "=<",
+        "lt": "<",
+        "gt": ">",
+        "is_defined": ("is defined", "is not defined"),
+        "contains_all": "contains all",
+        "contains_any": "contains any",
+    }
+
+    def __init__(self, **filters: str) -> None:
+        self._sibling_connector = filters.pop("_sibling_connector", self.AND)
+        self._sibling = filters.pop("_sibling", None)
+        self._filters = filters
+
+    def __str__(self) -> str:
+        result = []
+
+        for key, value in self._filters.items():
+            fields = key.split("__")
+            operator = fields.pop()
+            if operator not in self._operators:
+                fields.append(operator)
+                operator = "exact"
+
+            lhs = fields.pop()
+            val = self._clause(lhs, operator, value)
+            fields.append(val)
+            result.append(reduce(lambda x, y: f"{y}({x})", fields[::-1]))
+
+        val = " AND ".join(result)
+        if self._sibling:
+            return "(%s) %s (%s)" % (val, self._sibling_connector, self._sibling)
+        return val
+
+    def __or__(self, other):
+        return self.__class__(**self._filters, _sibling_connector=self.OR, _sibling=other)
+
+    def __and__(self, other):
+        return self.__class__(**self._filters, _sibling_connector=self.AND, _sibling=other)
+
+    def _clause(self, lhs, operator, rhs) -> str:
+        assert operator in self._operators
+
+        if isinstance(rhs, dict):
+            rhs = self.__class__(**rhs)
+            return "%s(%s)" % (lhs, rhs)
+
+        if isinstance(rhs, self.__class__):
+            return "%s(%s)" % (lhs, rhs)
+
+        op = self._operators[operator]
+        if isinstance(op, tuple):
+            return "%s %s" % (lhs, op[0 if rhs else 1])
+        else:
+            rhs = self._escape_value(rhs)
+            return "%s %s %s" % (lhs, op, rhs)
+
+    def _escape_value(self, value) -> str:
+        if isinstance(value, self.__class__):
+            return "(%s)" % value
+        if isinstance(value, Decimal):
+            return str(value)
+        if not isinstance(value, str) and isinstance(value, Collection):
+            return "(%s)" % (", ".join(self._escape_value(v) for v in value))
+        return json.dumps(value)
