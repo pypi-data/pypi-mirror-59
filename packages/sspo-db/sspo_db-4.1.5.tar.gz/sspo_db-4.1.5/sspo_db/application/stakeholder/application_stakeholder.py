86d618a3d7023d47807342026afb4a7db032112f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:40 +0000

    Add package sspo-db/4.1.5/sspo_db-4.1.5.tar.gz

---
+++ packages/sspo-db/sspo_db-4.1.5.tar.gz/sspo_db-4.1.5/sspo_db/application/stakeholder/application_stakeholder.py
@@ -0,0 +1,49 @@
+from sspo_db.model.stakeholder.models import Person, Developer
+from sspo_db.service.stakeholder.service import PersonService, DeveloperService
+from sspo_db.application.core.application_application_reference import ApplicationApplicationReference
+
+class ApplicationPerson():
+    
+    def __init__(self):
+        self.person_service = PersonService()
+        self.application_reference_service = ApplicationApplicationReference()
+    
+    def retrive_by_uuid(self, uuid):
+        return self.person_service.get_by_uuid(uuid)
+    
+    def retrive_person_by_external_uuid (self, external_uuid):
+        
+        application_reference = self.application_reference_service.get_by_external_uuid(external_uuid)
+        if application_reference:
+            return self.person_service.get_by_uuid(application_reference.internal_uuid)
+        return None
+
+    def create_person(self, name, email, organization, description = ""):
+        
+        person = Person()
+        person.name = name
+        person.description = description
+        person.email = email 
+        person.organization = organization
+
+        self.person_service.create(person)
+
+        return person
+        
+
+class ApplicationStakeholder():
+
+    def __ini__(self):
+        self.develope_service = DeveloperService()
+    
+    def create_developer(self, person, team, team_role = ""):
+        
+        developer = Developer()
+        developer.person = person
+        developer.team = team 
+        developer.team_role = team_role
+
+        self.develope_service.create(developer)
+
+    
+    
