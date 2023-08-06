3235899642290444e028990e13f3c9038f987554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:24 +0000

    Add package canaryd/0.6.7/canaryd-0.6.7.tar.gz

---
+++ packages/canaryd/canaryd-0.6.7.tar.gz/canaryd-0.6.7/canaryd/plugins/integrity.py
@@ -0,0 +1,111 @@
+from glob import glob
+from hashlib import sha1
+from os import path, stat
+
+from canaryd_packages import six
+
+from canaryd.plugin import Plugin
+
+# Attempt to import pwd/grp - only supported on *nix
+try:
+    import pwd
+    import grp
+except ImportError:
+    pwd = None
+    grp = None
+
+
+BUFFER_SIZE = 65536
+MAX_SIZE = 1024 ** 3  # 1GB
+PATHS_TO_TRACK = (
+    '/etc/*',
+    '/boot/*',
+    '/dev/*',
+
+    '/lib/*',
+    '/usr/lib/*',
+    '/usr/local/lib/*',
+    '/lib64/*',
+    '/usr/lib64/*',
+
+    '/bin/*',
+    '/sbin/*',
+    '/usr/bin/*',
+    '/usr/sbin/*',
+    '/usr/local/bin/*',
+    '/usr/local/sbin/*',
+    '/opt/bin/*',
+    '/opt/sbin/*',
+)
+PATHS_TO_IGNORE = (
+    '/etc/adjtime',
+)
+
+
+def get_file_hash(filename):
+    sha = sha1()
+
+    with open(filename, 'rb') as f:
+        while True:
+            data = f.read(BUFFER_SIZE)
+            if not data:
+                break
+
+            sha.update(data)
+    return sha.hexdigest()
+
+
+class Integrity(Plugin):
+    '''
+    Tracks the integrity of system files by checking their owner, permissions
+    and checksum hashes.
+    '''
+
+    spec = ('file', {
+        'sha1_hash': six.text_type,
+        'size': int,
+        'user': six.text_type,
+        'group': six.text_type,
+        'permissions': six.text_type,
+    })
+
+    is_slow = True
+
+    def prepare(self, settings):
+        return True
+
+    def get_state(self, settings):
+        file_hashes = {}
+
+        for path_to_track in PATHS_TO_TRACK:
+            for filename in glob(path_to_track):
+                if (
+                    filename in PATHS_TO_IGNORE
+                    or not path.isfile(filename)
+                ):
+                    continue
+
+                stat_data = stat(filename)
+
+                if stat_data.st_size > MAX_SIZE:
+                    continue
+
+                file_data = {
+                    'size': stat_data.st_size,
+                    'permissions': format(stat_data.st_mode, 'o'),
+                }
+
+                if pwd:
+                    file_data['user'] = pwd.getpwuid(stat_data.st_uid).pw_name
+
+                if grp:
+                    file_data['group'] = grp.getgrgid(stat_data.st_gid).gr_name
+
+                try:
+                    file_data['sha1_hash'] = get_file_hash(filename)
+                except (OSError, IOError):
+                    pass
+
+                file_hashes[filename] = file_data
+
+        return file_hashes
