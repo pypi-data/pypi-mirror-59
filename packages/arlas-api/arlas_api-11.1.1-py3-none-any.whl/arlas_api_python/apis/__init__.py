f9a494282bb49fc747bed348918b21afaa4b30c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:15 +0000

    Add package arlas-api/11.1.1/arlas_api-11.1.1-py3-none-any.whl

---
+++ packages/arlas-api/arlas_api-11.1.1-py3-none-any.whl/arlas_api_python/apis/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .collections_api import CollectionsApi
+from .explore_api import ExploreApi
+from .write_api import WriteApi
