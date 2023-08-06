798369e063b5f41e8a901741fce8d641c91968ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package cotede/0.21.0/cotede-0.21.0-py2.py3-none-any.whl

---
+++ packages/cotede/cotede-0.21.0-py2.py3-none-any.whl/cotede/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Guilherme Castelao'
+__email__ = 'guilherme@castelao.net'
+__version__ = '0.21.0'
+
+from cotede import qc
---
+++ packages/cotede/cotede-0.21.0-py2.py3-none-any.whl/cotede/utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from .utils import *
+from cotede.utils.profilescollection import ProfilesQCCollection
+from cotede.utils.profilescollection import ProfilesQCPandasCollection
+from .config import load_cfg
+
+import logging
