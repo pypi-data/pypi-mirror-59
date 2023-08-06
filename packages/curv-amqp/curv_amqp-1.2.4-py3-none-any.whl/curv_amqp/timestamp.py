da501d9112183072415cc2630f58fc94fb7fec4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:15 +0000

    Add package curv-amqp/1.2.4/curv_amqp-1.2.4-py3-none-any.whl

---
+++ packages/curv-amqp/curv_amqp-1.2.4-py3-none-any.whl/curv_amqp/timestamp.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import Optional
+
+
+def get_timestamp_in_milliseconds(now: Optional[float] = None) -> int:
+    if now is None:
+        now = datetime.utcnow().timestamp()
+    return int(round(now * 1000))
+
+
+def get_timestamp_difference_in_seconds(
+    timestamp: int, now: Optional[float] = None
+) -> float:
+    """returns the difference between a timestamp and now in seconds."""
+    if now is None:
+        now = datetime.utcnow().timestamp()
+    _timestamp = timestamp / 1000
+    return now - _timestamp
