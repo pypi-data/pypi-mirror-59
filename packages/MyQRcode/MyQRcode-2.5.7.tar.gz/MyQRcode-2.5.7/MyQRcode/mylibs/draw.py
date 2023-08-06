c0069fcaeb5a939e419c333c13dac2c908591605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:48 +0000

    Add package MyQRcode/2.5.7/MyQRcode-2.5.7.tar.gz

---
+++ packages/MyQRcode/MyQRcode-2.5.7.tar.gz/MyQRcode-2.5.7/MyQRcode/mylibs/draw.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+import os
+import requests
+from io import BytesIO
+
+
+def draw_qrcode(abspath, qrmatrix, qrcolor):
+    unit_len = 3
+    x = y = 4*unit_len
+    pic = Image.new(mode='RGB', size=[
+                    (len(qrmatrix)+8)*unit_len]*2, color='#ffffff')
+    response = requests.get('http://placehold.it/200x200')
+    bg = Image.open(BytesIO(response.content))
+
+    for line in qrmatrix:
+        for module in line:
+            if module:
+                draw_a_black_unit(pic, x, y, unit_len, qrcolor, bg)
+            x += unit_len
+        x, y = 4*unit_len, y+unit_len
+
+    saving = os.path.join(abspath, 'qrcode.png')
+    pic.save(saving)
+    return saving
+
+
+def draw_a_black_unit(p, x, y, ul, color, background):
+    for i in range(ul):
+        for j in range(ul):
+            pixcolor = background.getpixel((x+i, y+j))
+            p.putpixel((x+i, y+j), color)
