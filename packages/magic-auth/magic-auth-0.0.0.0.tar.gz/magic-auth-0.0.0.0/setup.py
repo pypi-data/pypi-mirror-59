6922b458931056210e003a592a432321de20ab9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:58 +0000

    Add package magic-auth/0.0.0.0/magic-auth-0.0.0.0.tar.gz

---
+++ packages/magic-auth/magic-auth-0.0.0.0.tar.gz/magic-auth-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='magic-auth',
+    version='0.0.0.0',
+    description='Troll Goat',
+    url='https://troll_goat.TM',
+    author='Troll Goat',
+    author_email='fei.yang.jen@gmail.com',
+    license='MIT',
+    packages=['magic-auth'],
+    zip_safe=False,
+)
