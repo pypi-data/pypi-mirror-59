417f279da9c4d7c62133f78286ee0d356f7c5e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:50 +0000

    Add package dvc/0.81.3/dvc-0.81.3.tar.gz

---
+++ packages/dvc/dvc-0.81.3.tar.gz/dvc-0.81.3/dvc/command/get.py
@@ -0,0 +1,58 @@
+import argparse
+import logging
+
+from .base import append_doc_link
+from .base import CmdBaseNoRepo
+from dvc.exceptions import DvcException
+
+
+logger = logging.getLogger(__name__)
+
+
+class CmdGet(CmdBaseNoRepo):
+    def run(self):
+        from dvc.repo import Repo
+
+        try:
+            Repo.get(
+                self.args.url,
+                path=self.args.path,
+                out=self.args.out,
+                rev=self.args.rev,
+            )
+            return 0
+        except DvcException:
+            logger.exception(
+                "failed to get '{}' from '{}'".format(
+                    self.args.path, self.args.url
+                )
+            )
+            return 1
+
+
+def add_parser(subparsers, parent_parser):
+    GET_HELP = (
+        "Download a file or directory from any DVC project or Git repository."
+    )
+    get_parser = subparsers.add_parser(
+        "get",
+        parents=[parent_parser],
+        description=append_doc_link(GET_HELP, "get"),
+        help=GET_HELP,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    get_parser.add_argument(
+        "url",
+        help="Location of DVC project or Git repository to download from",
+    )
+    get_parser.add_argument(
+        "path",
+        help="Path to a file or directory within the project or repository",
+    )
+    get_parser.add_argument(
+        "-o", "--out", nargs="?", help="Destination path to download files to"
+    )
+    get_parser.add_argument(
+        "--rev", nargs="?", help="Git revision (e.g. branch, tag, SHA)"
+    )
+    get_parser.set_defaults(func=CmdGet)
---
+++ packages/dvc/dvc-0.81.3.tar.gz/dvc-0.81.3/dvc/repo/fetch.py
@@ -0,0 +1,102 @@
+import logging
+
+from dvc.cache import NamedCache
+from dvc.config import NoRemoteError
+from dvc.exceptions import DownloadError
+from dvc.exceptions import OutputNotFoundError
+from dvc.external_repo import external_repo
+from dvc.scm.base import CloneError
+
+
+logger = logging.getLogger(__name__)
+
+
+def _fetch(
+    self,
+    targets=None,
+    jobs=None,
+    remote=None,
+    all_branches=False,
+    show_checksums=False,
+    with_deps=False,
+    all_tags=False,
+    recursive=False,
+):
+    """Download data items from a cloud and imported repositories
+
+    Returns:
+        int: number of successfully downloaded files
+
+    Raises:
+        DownloadError: thrown when there are failed downloads, either
+            during `cloud.pull` or trying to fetch imported files
+
+        config.NoRemoteError: thrown when downloading only local files and no
+            remote is configured
+    """
+    used = self.used_cache(
+        targets,
+        all_branches=all_branches,
+        all_tags=all_tags,
+        with_deps=with_deps,
+        force=True,
+        remote=remote,
+        jobs=jobs,
+        recursive=recursive,
+    )
+
+    downloaded = 0
+    failed = 0
+
+    try:
+        downloaded += self.cloud.pull(
+            used, jobs, remote=remote, show_checksums=show_checksums
+        )
+    except NoRemoteError:
+        if not used.external and used["local"]:
+            raise
+    except DownloadError as exc:
+        failed += exc.amount
+
+    for (repo_url, repo_rev), files in used.external.items():
+        d, f = _fetch_external(self, repo_url, repo_rev, files)
+        downloaded += d
+        failed += f
+
+    if failed:
+        raise DownloadError(failed)
+
+    return downloaded
+
+
+def _fetch_external(self, repo_url, repo_rev, files):
+    failed = 0
+
+    cache_dir = self.cache.local.cache_dir
+    try:
+        with external_repo(repo_url, repo_rev, cache_dir=cache_dir) as repo:
+            with repo.state:
+                cache = NamedCache()
+                for name in files:
+                    try:
+                        out = repo.find_out_by_relpath(name)
+                    except OutputNotFoundError:
+                        failed += 1
+                        logger.exception(
+                            "failed to fetch data for '{}'".format(name)
+                        )
+                        continue
+                    else:
+                        cache.update(out.get_used_cache())
+
+                try:
+                    return repo.cloud.pull(cache), failed
+                except DownloadError as exc:
+                    failed += exc.amount
+    except CloneError:
+        failed += 1
+        logger.exception(
+            "failed to fetch data for '{}'".format(", ".join(files))
+        )
+
+    return 0, failed
