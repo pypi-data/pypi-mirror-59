5944e392a2359b8ceb936c489d004bcf4a8187fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:05 +0000

    Add package latexcornouaille/0.1.0/latexcornouaille-0.1.0.tar.gz

---
+++ packages/latexcornouaille/latexcornouaille-0.1.0.tar.gz/latexcornouaille-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['latexcornouaille']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['latexcornouaille = latexcornouaille.cli:main']}
+
+setup_kwargs = {
+    'name': 'latexcornouaille',
+    'version': '0.1.0',
+    'description': 'custom python package for latex',
+    'long_description': None,
+    'author': 'David Couronné',
+    'author_email': 'couronne.david@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
