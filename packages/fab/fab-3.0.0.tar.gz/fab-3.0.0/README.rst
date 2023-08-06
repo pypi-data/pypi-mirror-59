f010f8a7029225d8617f7a87e6c67b2bb84ca825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:08 +0000

    Add package fab/3.0.0/fab-3.0.0.tar.gz

---
+++ packages/fab/fab-3.0.0.tar.gz/fab-3.0.0/README.rst
@@ -0,0 +1,20 @@
+fab
+===
+
+A fabulous, lightweight wrapper around Phabricator's API, Conduit, to handle
+authentication and other annoying things.
+
+Usage
+-----
+
+.. code:: python
+
+    import phabricator
+
+    phab = phabricator.Phabricator('https://...', 'Username', token='api-token')
+    phab.request('user.whoami')
+
+License
+-------
+Copyright 2014, 2018, 2020 Kunal Mehta, under the LGPL v3 or later. See COPYING for more
+details.
---
+++ packages/fab/fab-3.0.0.tar.gz/fab-3.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fab',
+      version='3.0.0',
+      description='Wrapper around Phabricator API',
+      long_description=open('README.rst').read(),
+      long_description_content_type='text/x-rst',
+      author='Kunal Mehta',
+      author_email='legoktm@member.fsf.org',
+      license='LGPL-3.0-or-later',
+      url='https://github.com/legoktm/fab',
+      packages=['phabricator'],
+      package_data={'phabricator': ['py.typed']},
+      python_requires='>=3.4',
+      install_requires=['requests']
+      )
