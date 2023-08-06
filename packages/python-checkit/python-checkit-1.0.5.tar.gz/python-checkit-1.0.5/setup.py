40c868cdad0afd7071f025840071b7ba01ce4d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:22 +0000

    Add package python-checkit/1.0.5/python-checkit-1.0.5.tar.gz

---
+++ packages/python-checkit/python-checkit-1.0.5.tar.gz/python-checkit-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='python-checkit',
+      version='1.0.5',
+      description='Checkit API Client',
+      url='https://bitbucket.org/metadonors/python-checkit',
+      author='Metadonors',
+      author_email='fabrizio.arzeni@metadonors.it',
+      license='MIT',
+      packages=['pycheckit'],
+      install_requires=[
+          'requests',
+      ],
+      extras_require={
+        'dev': [
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov'
+        ]
+      },
+      zip_safe=False)
