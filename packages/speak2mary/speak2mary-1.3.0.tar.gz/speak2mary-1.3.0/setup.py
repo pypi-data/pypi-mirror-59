576dfdb8f4d5f5c800dbfe9a18af826881df6421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:50 +0000

    Add package speak2mary/1.3.0/speak2mary-1.3.0.tar.gz

---
+++ packages/speak2mary/speak2mary-1.3.0.tar.gz/speak2mary-1.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='speak2mary',
+      version='1.3.0',
+      description='A Python wrapper for Mary TTS',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      author='Poeschl',
+      url='https://github.com/Poeschl/speak2mary',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      license='Apache License - Version 2.0',
+      package_dir={'speak2mary': 'speak2mary'},
+      packages=['speak2mary'],
+      python_requires='>=3',
+      )
