6d45cb9945b901afcffce6508da4d8a860f0acce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:12 +0000

    Add package alloylib/0.0.4/alloylib-0.0.4-py3-none-any.whl

---
+++ packages/alloylib/alloylib-0.0.4-py3-none-any.whl/alloy/math/__init__.py
@@ -0,0 +1,11 @@
+# Copyright - Transporation, Bots, and Disability Lab - Carnegie Mellon University
+# Released under MIT License
+
+from .basic import *
+from .vector_2D_op import *
+from .vector_3D_op import *
+
+__all__ = []
+__all__ += basic.__all__
+__all__ += vector_2D_op.__all__
+__all__ += vector_3D_op.__all__
