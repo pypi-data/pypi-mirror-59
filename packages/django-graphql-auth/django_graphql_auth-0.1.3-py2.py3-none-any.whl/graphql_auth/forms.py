05fa3731b613057981e1d711e2b1cf2fbe9daa96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:53 +0000

    Add package django-graphql-auth/0.1.3/django_graphql_auth-0.1.3-py2.py3-none-any.whl

---
+++ packages/django-graphql-auth/django_graphql_auth-0.1.3-py2.py3-none-any.whl/graphql_auth/forms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    PasswordChangeForm as DjangoPasswordChangeForm,
+    SetPasswordForm as DjangoSetPasswordForm,
+)
+from django.contrib.auth import get_user_model
+from django import forms
+
+from .utils import set_fields
+from .settings import graphql_auth_settings as settings
+
+UserModel = get_user_model()
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = set_fields(settings.REGISTER_MUTATION_FIELDS) + set_fields(
+            settings.REGISTER_MUTATION_FIELDS_OPTIONAL
+        )
+
+
+class PasswordChangeForm(DjangoPasswordChangeForm):
+    """Password change form"""
+
+
+class SetPasswordForm(DjangoSetPasswordForm):
+    """Password change form without old password"""
+
+
+class UpdateAccountForm(UserChangeForm):
+    class Meta:
+        model = UserModel
+        fields = set_fields(settings.UPDATE_MUTATION_FIELDS)
+
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(max_length=254,)
