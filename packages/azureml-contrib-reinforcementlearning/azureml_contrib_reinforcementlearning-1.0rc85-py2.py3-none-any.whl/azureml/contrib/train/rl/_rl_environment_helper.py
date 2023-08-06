ec58994911b2b4f4ca666728cd96c9ac81757f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package azureml-contrib-reinforcementlearning/1.0rc85/azureml_contrib_reinforcementlearning-1.0rc85-py2.py3-none-any.whl

---
+++ packages/azureml-contrib-reinforcementlearning/azureml_contrib_reinforcementlearning-1.0rc85-py2.py3-none-any.whl/azureml/contrib/train/rl/_rl_environment_helper.py
@@ -0,0 +1,85 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from os import path
+from azureml.exceptions import TrainingException
+import ruamel.yaml
+import copy
+from azureml.core.runconfig import EnvironmentDefinition
+
+
+def _setup_environment(framework, use_gpu, conda_dependencies, base_env=None,
+                       environment_variables=None, shm_size=None):
+    if not base_env:
+        env = EnvironmentDefinition()
+        is_custom_env = False
+    else:
+        env = copy.deepcopy(base_env)
+        is_custom_env = True
+
+    if is_custom_env:
+        if conda_dependencies:
+            env.python.conda_dependencies._merge_dependencies(conda_dependencies)
+        else:
+            env.python.user_managed_dependencies = True
+    elif conda_dependencies:
+        _load_env_defaults(env, use_gpu, framework)
+        env.python.conda_dependencies._merge_dependencies(conda_dependencies)
+    else:
+        _load_framework_image(env, use_gpu, framework)
+
+    if shm_size:
+        env.docker.shm_size = shm_size
+    if environment_variables:
+        env.environment_variables = environment_variables
+
+    return env
+
+
+def _load_env_defaults(target_env, use_gpu, framework):
+    from azureml.core.conda_dependencies import CondaDependencies
+    _SCENARIO_FILE_NOT_FOUND_ERROR = 'Scenario file for {name}:{version} not found.'
+
+    if use_gpu:
+        image_type = "gpu"
+    else:
+        image_type = "cpu"
+    scenario_filename = '{}-{}.yml'.format(repr(framework),
+                                           image_type)
+    scenario_path = path.join(path.dirname(__file__), "scenarios", scenario_filename)
+
+    if not path.isfile(scenario_path):
+        raise TrainingException((_SCENARIO_FILE_NOT_FOUND_ERROR).
+                                format(name=framework._name, version=framework._version))
+
+    with open(scenario_path, "r") as input:
+        scenario = ruamel.yaml.round_trip_load(input)
+        base_image = scenario.get('baseImage', None)
+        dockerfile = scenario.get('dockerFile', None)
+        dependencies = scenario.get('inlineCondaDependencies', None)
+
+        target_env.python.conda_dependencies = CondaDependencies(_underlying_structure=dependencies)
+
+        # Base image and dockerfile are mutually exclusive.
+        # Make sure scenario file isn't specifying both.
+        if base_image is not None and dockerfile is not None:
+            raise TrainingException("Incorrect scenario file specification for {}. Scenario file can specify either "
+                                    "base image or docker image".format(scenario_path))
+
+        target_env.docker.base_image = base_image
+        target_env.docker.base_dockerfile = dockerfile
+        target_env.docker.enabled = True
+
+
+def _load_framework_image(target_env, use_gpu, framework):
+
+    if use_gpu:
+        image_type = "gpu"
+    else:
+        image_type = "cpu"
+    base_image = "viennaprivate.azurecr.io/{0}:{1}-{2}".format(framework.name.lower(),
+                                                               framework.version, image_type)
+    target_env.docker.base_image = base_image
+    target_env.docker.enabled = True
+    target_env.python.user_managed_dependencies = True
