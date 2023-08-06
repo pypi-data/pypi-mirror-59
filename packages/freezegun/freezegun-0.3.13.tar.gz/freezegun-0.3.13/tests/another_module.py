1bc311f86001b2bd6fff21083d7298dcc46471f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:09 +0000

    Add package freezegun/0.3.13/freezegun-0.3.13.tar.gz

---
+++ packages/freezegun/freezegun-0.3.13.tar.gz/freezegun-0.3.13/tests/another_module.py
@@ -0,0 +1,73 @@
+from calendar import timegm
+from datetime import date, datetime
+from time import time, localtime, gmtime, strftime
+
+from freezegun.api import (
+    FakeDatetime,
+    FakeDate,
+    fake_time,
+    fake_localtime,
+    fake_gmtime,
+    fake_strftime,
+    fake_timegm,
+)
+
+
+# Reals
+
+def get_datetime():
+    return datetime
+
+
+def get_date():
+    return date
+
+
+def get_time():
+    return time
+
+
+def get_localtime():
+    return localtime
+
+
+def get_gmtime():
+    return gmtime
+
+
+def get_strftime():
+    return strftime
+
+
+def get_timegm():
+    return timegm
+
+
+# Fakes
+
+def get_fake_datetime():
+    return FakeDatetime
+
+
+def get_fake_date():
+    return FakeDate
+
+
+def get_fake_time():
+    return fake_time
+
+
+def get_fake_localtime():
+    return fake_localtime
+
+
+def get_fake_gmtime():
+    return fake_gmtime
+
+
+def get_fake_strftime():
+    return fake_strftime
+
+
+def get_fake_timegm():
+    return fake_timegm
