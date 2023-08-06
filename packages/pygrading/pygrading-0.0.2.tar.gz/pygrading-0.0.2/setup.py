150c4ee384aa29726a7f099d864915a11b185e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:25 +0000

    Add package pygrading/0.0.2/pygrading-0.0.2.tar.gz

---
+++ packages/pygrading/pygrading-0.0.2.tar.gz/pygrading-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygrading",
+    version="0.0.2",
+    author="Charles Zhang",
+    author_email="694556046@qq.com",
+    description="A Python ToolBox for CourseGrading platform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PhenomingZ/PyGrading",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
