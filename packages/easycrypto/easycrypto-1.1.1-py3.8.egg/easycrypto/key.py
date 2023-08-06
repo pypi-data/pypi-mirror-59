b21be95b175663d6e513180102a8657cabc034cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:10 +0000

    Add package easycrypto/1.1.1/easycrypto-1.1.1-py3.8.egg

---
+++ packages/easycrypto/easycrypto-1.1.1-py3.8.egg/easycrypto/key.py
@@ -0,0 +1,29 @@
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+
+class Key:
+
+    ITERATION_COUNT = 10000
+    KEY_SIZE = 32
+
+    @classmethod
+    def generate(cls, password, password_salt_size):
+        salt = os.urandom(password_salt_size)
+        return cls.generate_with_salt(password, salt), salt
+
+    @classmethod
+    def generate_with_salt(cls, password, salt):
+        kdf = PBKDF2HMAC(
+            algorithm = hashes.SHA256(),
+            length = cls.KEY_SIZE,
+            salt = salt,
+            iterations = cls.ITERATION_COUNT,
+            backend = default_backend()
+        )
+        try:
+            pwd_in_bytes = bytes(password, 'utf-8')
+        except TypeError:
+            pwd_in_bytes = bytes(password)
+        return kdf.derive(pwd_in_bytes)
