6078b394baed4607d0f08a9fb59b70fd59915a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:25 +0000

    Add package jk-temporary/0.2020.1.20/jk_temporary-0.2020.1.20.tar.gz

---
+++ packages/jk-temporary/jk_temporary-0.2020.1.20.tar.gz/jk_temporary-0.2020.1.20/jk_temporary/__init__.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/python3
+
+
+
+
+from .temputils import createRandomFilePath, createTempDir, createTempFilePath, createTempFile, createTempFileBinary, createTempFileUTF8, TempDir
+
+
+
+
+__version__ = "0.2020.1.20"
