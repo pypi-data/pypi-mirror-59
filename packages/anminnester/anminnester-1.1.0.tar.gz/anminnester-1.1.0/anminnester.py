eb2b4d776a0f15680493eeea818de1ec944bfd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:58 +0000

    Add package anminnester/1.1.0/anminnester-1.1.0.tar.gz

---
+++ packages/anminnester/anminnester-1.1.0.tar.gz/anminnester-1.1.0/anminnester.py
@@ -0,0 +1,19 @@
+# '''-------------------------'''
+# def print_lol(the_list):
+#     '''________________________'''
+#     for i in the_list:
+#         if isinstance(i,list):
+#             print_lol(i)
+#         else:
+#             print(i)
+
+'''-------------------------'''
+def print_lol(the_list,level):
+    '''________________________'''
+    for i in the_list:
+        if isinstance(i,list):
+            print_lol(i,level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(i)
