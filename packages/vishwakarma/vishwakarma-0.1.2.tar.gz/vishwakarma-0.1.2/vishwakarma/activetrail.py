a92d03f304ad97b2be8355038db824454fd7730e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:27 +0000

    Add package vishwakarma/0.1.2/vishwakarma-0.1.2.tar.gz

---
+++ packages/vishwakarma/vishwakarma-0.1.2.tar.gz/vishwakarma-0.1.2/vishwakarma/activetrail.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+'''Build visualization for a pgmpy model
+
+Example:
+
+Attributes:
+
+Todo:
+
+'''
+import pgmpy
+import jsonpickle
+import pickle
+import requests
+import tempfile
+import os
+import random
+import json
+import pandas as pd
+import shutil
+import string
+import datetime
+from IPython.display import Image
+
+
+def activetrail(obj,node,observed_node, width=600):
+    '''Build visualization for a pgmpy model
+
+    Args:
+        obj (pgmpy.models): The pgmpy model that needs to be visualized
+        width (int): Width of the image in px (default 600)
+
+    Returns:
+        Image: The image that can be displayed inline in a Jupyter notebook
+    '''
+    # list of supported classes
+    supp_classes = (pgmpy.models.BayesianModel,
+                    pgmpy.models.NaiveBayes)
+
+    if isinstance(obj, supp_classes):
+        tmp_dir_name = ''
+        try:
+            # create a temp directory & temp file
+            tmp_dir_name = tempfile.mkdtemp()
+            # generate a tmp file name (excluding file extension)
+            epoch = datetime.datetime.now().strftime('%s')
+            tmp_file_name = ''.join(
+                [random.choice(string.ascii_letters + string.digits) for n in range(8)])
+            tmp_file_name = os.path.join(
+                tmp_dir_name, tmp_file_name + epoch + '.png')
+            tmp_pkl_file_name = os.path.join(
+                tmp_dir_name, tmp_file_name + epoch + '.pkl')
+
+            # serialize the object
+            data = pickle.dumps(obj)
+            
+            json_data = {"data":data,"node":node,"observed_node":observed_node}
+            
+            json_data = pickle.dumps(json_data)
+
+
+            url = 'http://api.diagram.ai/vishwakarma/activetrail/'
+            endpoint = 'activetrail/'
+
+            resp = requests.post(url,data=json_data,
+                    headers={'Content-Type': 'application/octet-stream'})
+
+            if(resp.ok):
+                # get the image file and write it to temp dir
+                if resp.headers.get('Content-Type') == 'image/png':
+                    open(tmp_file_name, 'wb').write(resp.content)
+                    # now return this image as an Image object displayable in
+                    # the jupyter notebook
+                    return Image(filename=tmp_file_name, width=width)
+            else:
+                raise Exception(resp.raise_for_status())
+
+        finally:
+            # cleanup the temp directory
+            shutil.rmtree(tmp_dir_name, ignore_errors=True)
+
+    else:
+        raise TypeError(
+            'Expected pgmpy modely; unsupported object type: ' +
+            type(obj))
