3db636e2321127938d197dd4ffbc1b69a842512f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:25 +0000

    Add package cutadapt/2.8/cutadapt-2.8.tar.gz

---
+++ packages/cutadapt/cutadapt-2.8.tar.gz/cutadapt-2.8/tests/test_qualtrim.py
@@ -0,0 +1,13 @@
+from dnaio import Sequence
+from cutadapt.qualtrim import nextseq_trim_index
+
+
+def test_nextseq_trim():
+    s = Sequence('n', '', '')
+    assert nextseq_trim_index(s, cutoff=22) == 0
+    s = Sequence(
+        'n',
+        'TCTCGTATGCCGTCTTATGCTTGAAAAAAAAAAGGGGGGGGGGGGGGGGGNNNNNNNNNNNGGNGG',
+        'AA//EAEE//A6///E//A//EA/EEEEEEAEA//EEEEEEEEEEEEEEE###########EE#EA'
+    )
+    assert nextseq_trim_index(s, cutoff=22) == 33
