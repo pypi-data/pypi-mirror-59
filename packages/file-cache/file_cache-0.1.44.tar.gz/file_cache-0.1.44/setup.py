7c4ff40363515b50e734708f2221c9e04524d6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:04 +0000

    Add package file-cache/0.1.44/file_cache-0.1.44.tar.gz

---
+++ packages/file-cache/file_cache-0.1.44.tar.gz/file_cache-0.1.44/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+import setuptools
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='file_cache',
+    version='0.1.44',
+    description='Cache dataframe with local hd5 file, and reduce the memory by convert type for number',
+    long_description='File_cache can help you cache the pandas result with hd5 file, You can get demo from: https://github.com/Flyfoxs/file_cache/blob/master/demo.ipynb',
+    url='https://github.com/Flyfoxs/file_cache',
+    author='Felix Li',
+    author_email='lilao@163.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='pandas file cache python file_cache',
+    packages=setuptools.find_packages(),
+)
