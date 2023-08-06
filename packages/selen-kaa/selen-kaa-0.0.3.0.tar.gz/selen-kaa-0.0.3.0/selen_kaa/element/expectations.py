91bf157429f45ac8360e9569b6662a66e08b169b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:58 +0000

    Add package selen-kaa/0.0.3.0/selen-kaa-0.0.3.0.tar.gz

---
+++ packages/selen-kaa/selen-kaa-0.0.3.0.tar.gz/selen-kaa-0.0.3.0/selen_kaa/element/expectations.py
@@ -0,0 +1,130 @@
+from selenium.common.exceptions import TimeoutException
+
+from selen_kaa.utils import custom_types
+from selen_kaa.element.element_waits import ElementWaits
+
+TimeoutType = custom_types.TimeoutType
+
+
+class Expectations(ElementWaits):
+    """True if expectation is fulfilled else False.
+    Errors are handled by returning False.
+    """
+
+    def be_visible(self, timeout: TimeoutType):
+        """True when an element is visible on the html page.
+        :param timeout: time to wait element visibility.
+
+        """
+        timeout_ = timeout if timeout else self._timeout
+        try:
+            return super().be_visible(timeout=timeout_)
+        except TimeoutException:
+            return False
+
+    def be_invisible(self, timeout: TimeoutType):
+        """True if an element is not visible on the page.
+        :param timeout: time to wait element visibility.
+
+        """
+        timeout_ = timeout if timeout else self._timeout
+        try:
+            return super().be_invisible(timeout=timeout_)
+        except TimeoutException:
+            return False
+
+    def have_class(self, expected_class: str, timeout: TimeoutType):
+        """True when an element has a specific class.
+        :param expected_class: class_name for expected class (not css_selector).
+        :param timeout: time to wait for an element ho have a class name.
+
+        """
+        timeout_ = timeout if timeout else self._timeout
+        try:
+            return super().have_class(expected_class, timeout=timeout_)
+        except TimeoutException:
+            return False
+
+    def include_element(self, child_css_selector: str, timeout: TimeoutType):
+        """True when an element gets a desired child element.
+        :param child_css_selector: a css selector for a child element.
+        :param timeout: time to wait for the condition.
+
+        """
+        timeout_ = timeout if timeout else self._timeout
+        try:
+            return super().include_element(child_css_selector, timeout_)
+        except TimeoutException:
+            return False
+
+    def contain_text(self, text: str, timeout: TimeoutType):
+        """True if an element contains a provided text in its text attribute.
+        :param text: expected text.
+        :param timeout: time to wait for the condition.
+
+        """
+        timeout_ = self._timeout
+        if timeout:
+            timeout_ = timeout
+        try:
+            return super().contain_text(text, timeout_)
+        except TimeoutException:
+            return False
+
+    def have_similar_text(self, text: str, timeout: TimeoutType):
+        """True if an element has a similar text in texts attribute.
+        Not precise comparision, e.g. returns True for:
+        "some" in "this is some text", " test\n" and "test", "TEST" and "test"
+        :param text: a text to compare for similarity.
+        :param timeout: time to wait for the condition.
+
+        """
+        timeout_ = self._timeout
+        if timeout:
+            timeout_ = timeout
+        try:
+            return super().have_similar_text(text, timeout_)
+        except TimeoutException:
+            return False
+
+    def have_exact_text(self, text: str, timeout: TimeoutType):
+        """True if an element has exactly provided text, and no other text.
+        Precise comparision, e.g. returns False if "some" == "this is some text"
+        :param text: exact text to search inside an element
+        :param timeout: time to wait for the condition.
+
+        """
+        timeout_ = self._timeout
+        if timeout:
+            timeout_ = timeout
+        try:
+            return super().have_exact_text(text, timeout_)
+        except TimeoutException:
+            return False
+
+    def not_present_in_dom(self, timeout: TimeoutType):
+        """True for an element to be stale or absent in DOM.
+        :param timeout: equal to the self.timeout if other not passed. :type: int
+
+        """
+        timeout_ = self._timeout
+        if timeout:
+            timeout_ = timeout
+        try:
+            return super().not_present_in_dom(timeout_)
+        except TimeoutException:
+            return False
+
+    def be_on_the_screen(self, timeout: TimeoutType):
+        """True for an element is present on the screen (inside the viewport).
+        False if element's coordinates don't match viewport height and width.
+        :param timeout: equal to the self.timeout if other not passed.
+
+        """
+        timeout_ = self._timeout
+        if timeout:
+            timeout_ = timeout
+        try:
+            return super().be_on_the_screen(timeout_)
+        except TimeoutException:
+            return False
---
+++ packages/selen-kaa/selen-kaa-0.0.3.0.tar.gz/selen-kaa-0.0.3.0/selen_kaa/element/se_elements_array.py
@@ -0,0 +1,60 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selen_kaa.utils.se_utils import get_selector_type
+from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
+from selen_kaa.utils import custom_types
+from selen_kaa.element.se_web_element import SeWebElement
+
+
+TimeoutType = custom_types.TimeoutType
+
+
+class SeElementsArray:
+    """Lazy initialization of a list of web_elements.
+    We need this for calling a list of wrapped web_elements,
+    instead of standard find_elements().
+    """
+
+    DEFAULT_TIMEOUT = 4
+
+    def __init__(self, webdriver: WebDriver, css_selector: str, timeout: TimeoutType = DEFAULT_TIMEOUT):
+        self._webdriver = webdriver
+        self._css_selector = css_selector
+        self._timeout = timeout
+        self._elements_array = []
+
+    @property
+    def _lazy_array(self):
+        if not self._elements_array:
+            elements_ = WebDriverWait(self._webdriver, self._timeout).until(
+                presence_of_all_elements_located((get_selector_type(self._css_selector), self._css_selector))
+            )
+            for elem in elements_:
+                wrapped_elem = SeWebElement(self._webdriver, self._css_selector, self._timeout)
+                wrapped_elem.web_element = elem
+                self._elements_array.append(wrapped_elem)
+
+        return self._elements_array
+
+    def __getattr__(self, attr):
+        try:
+            orig_attr = self._lazy_array.__getattribute__(attr)
+            if callable(orig_attr):
+                def hooked(*args, **kwargs):
+                    # prevent recursion
+                    result = orig_attr(*args, **kwargs)
+                    # prevent recursion
+                    if result == self._lazy_array:
+                        return self
+                    return result
+                return hooked
+            return orig_attr
+        except AttributeError as exc:
+            raise AttributeError(f"No attribute {attr}.\n{exc}")
+
+    def __getitem__(self, index):
+        return self._lazy_array[index]
+
+    def __len__(self):
+        return len(self._lazy_array)
