b6f1e85dac5554e7ae2c087aa0bf516ea139d893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:24 +0000

    Add package panels/0.2.2/panels-0.2.2-cp37-cp37m-win_amd64.whl

---
+++ packages/panels/panels-0.2.2-cp37-cp37m-win_amd64.whl/panels/version.py
@@ -0,0 +1,13 @@
+# This file is generated automatically by the setup.py
+short_version = '0.2.2'
+version = '0.2.2'
+full_version = '0.2.2'
+svn_revision = ''
+isreleased = True
+if isreleased:
+    __version__ = version
+else:
+    __version__ = full_version
+
+if not isreleased:
+    version = full_version
