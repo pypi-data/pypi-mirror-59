e04c6572ec37bb46e9071bf08e2933954d1bd1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package e3-core/21.0.0/e3_core-21.0.0-py2-none-any.whl

---
+++ packages/e3-core/e3_core-21.0.0-py2-none-any.whl/e3/anod/status.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+from enum import Enum
+
+
+class ReturnValue(Enum):
+    """Return values for anod commands."""
+
+    success = 0
+    failure = 1
+    missing = 2
+    notready = 75
+    force_skip = 122
+    force_fail = 123
+    unknown = 124
+    skip = 125
+    unchanged = 126
