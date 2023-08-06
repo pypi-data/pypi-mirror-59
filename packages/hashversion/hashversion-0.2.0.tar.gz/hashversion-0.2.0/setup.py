4deff705e8ae6cb37fb5ad1d1d0a622d3dc76b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:19 +0000

    Add package hashversion/0.2.0/hashversion-0.2.0.tar.gz

---
+++ packages/hashversion/hashversion-0.2.0.tar.gz/hashversion-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hashversion']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'gitpython>=3.0.5,<4.0.0', 'toml>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['hashver = hashversion.cli:cli']}
+
+setup_kwargs = {
+    'name': 'hashversion',
+    'version': '0.2.0',
+    'description': 'Automate versioning and related requirements when using hashver',
+    'long_description': '# Hash Version\n\n![License](https://img.shields.io/github/license/miniscruff/hashversion-python.svg)\n![Issues](https://img.shields.io/github/issues/miniscruff/hashversion-python.svg)\n\nPython CLI for automating hash versioning and related requirements.\nTo help automate versioning, changelog management and other pieces of continuous\ndeployments.\n\n## Installation\n```bash\n> git clone https://github.com/miniscruff/hashversion-python.git\n> cd  hashver-python\n> poetry install\n> pytest\n> black .\n```\n\n## Bugs\n\nThis project is getting upgrades in my free time if there is a problem please create a bug report in the issues section.\n\n## License\n\n- Licensed under [MIT License](https://github.com/miniscruff/hashversion-python/blob/master/LICENSE)\n',
+    'author': 'Ronnie Smith',
+    'author_email': 'halfpint1170@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/miniscruff/hashversion-python',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
