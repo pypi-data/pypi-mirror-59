a52932f9370bcabeb4fc1bbce8f61ee87a26b9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:50 +0000

    Add package nsl.stac/0.3.2/nsl.stac-0.3.2.tar.gz

---
+++ packages/nsl.stac/nsl.stac-0.3.2.tar.gz/nsl.stac-0.3.2/setup.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+from setuptools import setup
+
+src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+old_path = os.getcwd()
+os.chdir(src_path)
+sys.path.insert(0, src_path)
+
+kwargs = {
+    'name': 'nsl.stac',
+    'description': 'gRPC Spatio Temporal Asset Catalog library',
+    'url': 'https://github.com/nearspacelabs/stac-client-python',
+    'long_description': "gRPC Spatio Temporal Asset Catalog library provided by Near Space Labs",
+    'author': 'David Raleigh',
+    'author_email': 'david@nearspacelabs.com',
+    'license': 'Apache 2.0',
+    'version': '0.3.2',
+    'python_requires': '>3.6.0',
+    'packages': ['nsl.stac'],
+    'install_requires': [
+        'grpcio-tools',
+        'protobuf',
+        'epl.protobuf',
+        'boto3',
+        'google-cloud-storage'
+    ],
+    'zip_safe': False
+}
+
+clssfrs = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+]
+kwargs['classifiers'] = clssfrs
+
+setup(**kwargs)
