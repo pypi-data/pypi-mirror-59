3185ad1d386d227fdd5de25a89b5b2221383518c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:58 +0000

    Add package usuarios-cpf-django/0.0.2/usuarios-cpf-django-0.0.2.tar.gz

---
+++ packages/usuarios-cpf-django/usuarios-cpf-django-0.0.2.tar.gz/usuarios-cpf-django-0.0.2/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+
+
+version = '0.0.2'
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme_file, 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='usuarios-cpf-django',
+    version=version,
+    python_requires='>=3.5',
+    url='https://gitlab.com/semus/django-utils/usuarios_semus',
+    author='Paulo Roberto Cruz',
+    author_email='paulo.cruz9@gmail.com',
+    download_url='https://gitlab.com/semus/django-utils/usuarios_semus',
+    description="Gerenciamento de usuarios para apps Djando na semus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    packages=[
+        'usuarios_semus', 'usuarios_semus.migrations',
+    ],
+    include_package_data=True,
+    license='BSD',
+    install_requires=["Django>=3.0", "pycpfcnpj==1.5.1"],
+    tests_require=['mock', 'django-environ', 'pytest', 'pytest-django'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',                 
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Security',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3 :: Only',
+                 ],
+    test_suite='tests.main',
+    find_links=[]
+)
---
+++ packages/usuarios-cpf-django/usuarios-cpf-django-0.0.2.tar.gz/usuarios-cpf-django-0.0.2/usuarios_semus/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+from uuid import uuid4
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import UserManager
+from .validators import validate_cpf
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    cpf = models.CharField(_('CPF'), max_length=11, unique=True, validators=[validate_cpf])
+    email = models.EmailField(_('email address'), unique=True)
+    full_name = models.CharField(_('full name'), max_length=60)  
+    nick_name = models.CharField(_('nick name'), max_length=30)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('is_staff'), default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'cpf'
+    REQUIRED_FIELDS = ['email', 'full_name', 'nick_name']
+
+    class Meta:
+        verbose_name = _('Usuario')
+        verbose_name_plural = _('Usuarios')
+    
+    def get_full_name(self):          
+        return self.full_name
+
+    def get_short_name(self):        
+        return self.nick_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
