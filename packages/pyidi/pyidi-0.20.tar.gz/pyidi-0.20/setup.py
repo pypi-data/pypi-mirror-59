b5faa86fe6e381e2f4d09e7a0ad00ef7348e775b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:02 +0000

    Add package pyidi/0.20/pyidi-0.20.tar.gz

---
+++ packages/pyidi/pyidi-0.20.tar.gz/pyidi-0.20/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+desc = """\
+Python Image Displacement Identification
+========================================
+This module supports the identification of disploacement from image date
+
+For the showcase see: `github.com/ladisk/pyIDI <https://github.com/ladisk/pyIDI/blob/master/Showcase.ipynb>`_
+"""
+
+from setuptools import setup
+
+setup(name='pyidi',
+      version='0.20',
+      author='Klemen Zaletelj, Domen Gorjup, Janko Slavič',
+      author_email='janko.slavic@fs.uni-lj.si, ladisk@gmail.com',
+      description='Python Image Displacement Identification.',
+      url='https://github.com/ladisk/pyidi',
+      packages=['pyidi', 'pyidi.methods'],
+      long_description=desc,
+      install_requires=['numpy>=1.15.4', 'scipy>=1.1.0', 'tqdm', 'matplotlib>=3.0.0', 'pyMRAW>=0.22'],
+      keywords='computer vision dic gradient-based image identification',
+      )
