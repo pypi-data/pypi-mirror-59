6854e5492d2e38b5d92a9ca65933b1f601fd9d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:13 +0000

    Add package skift/0.0.17/skift-0.0.17.tar.gz

---
+++ packages/skift/skift-0.0.17.tar.gz/skift-0.0.17/tests/test_pickle.py
@@ -0,0 +1,84 @@
+"""Test skift."""
+
+import os
+import pickle
+
+import pytest
+import pandas as pd
+import tempfile
+
+from sklearn.exceptions import NotFittedError
+
+from skift import FirstColFtClassifier
+
+
+def test_pickle():
+    ftdf = pd.DataFrame(
+        data=[['woof woof', 0], ['meow meow', 1]],
+        columns=['txt', 'lbl']
+    )
+    ft_clf = FirstColFtClassifier()
+    ft_clf.fit(ftdf[['txt']], ftdf['lbl'])
+
+    assert ft_clf.predict([['woof woof']])[0] == 0
+    assert ft_clf.predict([['meow meow']])[0] == 1
+    assert ft_clf.predict([['meow']])[0] == 1
+    assert ft_clf.predict([['woof lol']])[0] == 0
+    assert ft_clf.predict([['meow lolz']])[0] == 1
+
+    fd, pic_fpath = tempfile.mkstemp()
+    with open(pic_fpath, 'wb+') as bfile:
+        pickle.dump(ft_clf, bfile)
+    with open(pic_fpath, 'rb') as bfile:
+        ft_clf2 = pickle.load(bfile)
+
+    assert ft_clf2 != ft_clf
+    assert ft_clf2.predict([['woof woof']])[0] == 0
+    assert ft_clf2.predict([['meow meow']])[0] == 1
+    assert ft_clf2.predict([['meow']])[0] == 1
+    assert ft_clf2.predict([['woof lol']])[0] == 0
+    assert ft_clf2.predict([['meow lolz']])[0] == 1
+
+    # Clean up
+    os.close(fd)    # Prevent a file-handle leak
+    os.unlink(pic_fpath)
+
+
+def test_pickle_unfitted():
+    ftdf = pd.DataFrame(
+        data=[['woof woof', 0], ['meow meow', 1]],
+        columns=['txt', 'lbl']
+    )
+    ft_clf = FirstColFtClassifier()
+
+    fd, pic_fpath = tempfile.mkstemp()
+
+    with open(pic_fpath, 'wb+') as bfile:
+        pickle.dump(ft_clf, bfile)
+    with open(pic_fpath, 'rb') as bfile:
+        ft_clf2 = pickle.load(bfile)
+
+    with pytest.raises(NotFittedError):
+        assert ft_clf.predict([['woof woof']])[0] == 0
+
+    ft_clf.fit(ftdf[['txt']], ftdf['lbl'])
+    assert ft_clf.predict([['woof woof']])[0] == 0
+    assert ft_clf.predict([['meow meow']])[0] == 1
+    assert ft_clf.predict([['meow']])[0] == 1
+    assert ft_clf.predict([['woof lol']])[0] == 0
+    assert ft_clf.predict([['meow lolz']])[0] == 1
+
+    assert ft_clf2 != ft_clf
+    with pytest.raises(NotFittedError):
+        assert ft_clf2.predict([['woof woof']])[0] == 0
+
+    ft_clf2.fit(ftdf[['txt']], ftdf['lbl'])
+    assert ft_clf2.predict([['woof woof']])[0] == 0
+    assert ft_clf2.predict([['meow meow']])[0] == 1
+    assert ft_clf2.predict([['meow']])[0] == 1
+    assert ft_clf2.predict([['woof lol']])[0] == 0
+    assert ft_clf2.predict([['meow lolz']])[0] == 1
+
+    # Clean up
+    os.close(fd)    # Prevent a file-handle leak
+    os.unlink(pic_fpath)
