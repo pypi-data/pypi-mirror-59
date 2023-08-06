76262483ea4e15f6b9a49cd64f6ce23cc0dfb4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:55 +0000

    Add package d-snek-cogs-sona/0.0.2/d-snek-cogs-sona-0.0.2.tar.gz

---
+++ packages/d-snek-cogs-sona/d-snek-cogs-sona-0.0.2.tar.gz/d-snek-cogs-sona-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+      name="d-snek-cogs-sona",
+      version="0.0.2",
+      author="Jonathan de Jong",
+      author_email="jonathan@automatia.nl",
+      description="Discord SNEK Cog; Sona ABC",
+      url="https://git.jboi.dev/ShadowJonathan/snek-sona",
+      packages=['cogs.sona'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.6",
+      install_requires=["d-snek>=1.2.4", "discord.py>=1.2.5"],
+      setup_requires=["wheel"],
+      extras_require={
+            "dev": ["ipython", "twine"],
+      }
+)
