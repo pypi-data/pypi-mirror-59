f15d496807252fc95d35f9d5e5e38b76d024a1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:06 +0000

    Add package Autogovernance/0.5.2/Autogovernance-0.5.2-py3-none-any.whl

---
+++ packages/Autogovernance/Autogovernance-0.5.2-py3-none-any.whl/autogovernance/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 12:32:13 2020
+
+@author: LP885RH
+"""
+from element import QR_object
+from element import BusinessTerm
+from element import Attributes
+from element import Element
+
