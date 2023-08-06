5a94c6c043ba9a1d0e7c8a7ea46f174d4b5950ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:29 +0000

    Add package lorem-text/1.5/lorem_text-1.5.tar.gz

---
+++ packages/lorem-text/lorem_text-1.5.tar.gz/lorem_text-1.5/docs/usage.rst
@@ -0,0 +1,48 @@
+=====
+Usage
+=====
+
+To install Lorem Text, run this command in your terminal::
+
+        pip install lorem_text
+
+
+To use Lorem Text in a project::
+
+    from lorem_text import lorem
+
+
+For randomly generated sentence of lorem ipsum text where the first word is capitalized, and the sentence ends in either a period or question mark::
+
+    lorem.sentence()
+
+Generate a single paragraph:: 
+
+    lorem.paragraph()
+
+Generate multiple paragraphs of lorem ipsum text each paragraph's consists of 1 to 3 sentences::
+
+    paragraph_length = 5
+    lorem.paragraphs(paragraph_length)
+
+
+Generate random lorem ipsum words seperated with single space::
+
+    words = 10
+    lorem.words(words)
+
+
+Command Line Interface 
+----------------------
+
+For randomly generated paragraph of lorem ipsum text::
+
+    lorem_text
+
+Lorem ipsum sentence::
+
+    lorem_text --s 
+
+Lorem ipsum words::
+
+    lorem_text --words=50
---
+++ packages/lorem-text/lorem_text-1.5.tar.gz/lorem_text-1.5/tests/test_lorem_text.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""Tests for `lorem_text` package."""
+
+import re
+import unittest
+from click.testing import CliRunner
+
+from lorem_text import lorem
+from lorem_text import cli
+
+
+class TestLorem_text(unittest.TestCase):
+    """Tests for `lorem_text` package."""
+
+    # def setUp(self):
+    #     """Set up test fixtures, if any."""
+    #     pass
+
+    # def tearDown(self):
+    #     """Tear down test fixtures, if any."""
+    #     pass
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_short_words(self):
+        self.assertEqual(len(lorem.words(4).split()), 4, "Should be 4")
+
+    def test_long_words(self):
+        self.assertEqual(len(lorem.words(600).split()), 600, "Should be 600")
+
+    def test_sentence(self):
+        sen = lorem.sentence()
+        size = len(re.split(",", sen))
+        self.assertTrue(2 <= size <= 5)
+
+    def test_paragraph(self):
+        para = lorem.paragraph()
+        size = len(re.split(",", para))
+        self.assertTrue(2 <= size <= 15)
+
+    def test_paragraphs(self):
+        paras = lorem.paragraphs(2)
+        size = len(paras)
+        self.assertTrue(size >= 10)
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        help_result = runner.invoke(cli.main, ['--help'])
+        sentence_result = runner.invoke(cli.main, ['--s'])
+        words_result = runner.invoke(cli.main, ['--words=4'])
+        assert help_result.exit_code == 0
+        assert sentence_result.exit_code == 0
+        assert words_result.exit_code == 0
+        # Test for paragraph
+        para_result = (runner.invoke(cli.main)).output
+        size = len(re.split(",", para_result))
+        assert (3 <= size <= 15)
+        # Test for words
+        words_result = (runner.invoke(cli.main, "--words=120")).output
+        word_len = len(words_result.split())
+        assert word_len == 120
+        # Test for sentence
+        sentence_result = (runner.invoke(cli.main, "--s")).output
+        size = len(re.split(",", sentence_result))
+        assert (2 <= size <= 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
