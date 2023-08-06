384f016d27872ee025d084048a46b3756c8e6fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:04 +0000

    Add package appteka/0.8.4/appteka-0.8.4.tar.gz

---
+++ packages/appteka/appteka-0.8.4.tar.gz/appteka-0.8.4/test/test_editor.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+from PyQt5 import QtWidgets
+
+sys.path.insert(0, os.path.abspath("."))
+from appteka.pyqt import testing
+from appteka.pyqt.codetextedit import CodeTextEdit
+
+
+class TestCodeTextEdit(testing.TestDialog):
+    """Tests for CodeTextEdit"""
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 600)
+
+    def test_text(self):
+        e = CodeTextEdit()
+        self.set_widget(e)
+
+        code = ""
+        code += '{\n'
+        code += '  "a": 1,\n'
+        code += '  "b": 2\n'
+        code += '}'
+        e.set_text(code)
+
+        self.add_assertion("Some text is printed")
+        self.add_assertion("Lines numbered")
+        self.add_assertion("Current line is highlighted")
+        self.add_assertion("Font is monospace")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    d = TestCodeTextEdit()
+    d.run()
+    sys.exit(app.exec())
