9b1726f1bafb4d13f9f339d780d4706d34fb297f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:14 +0000

    Add package R4/0.0.2/R4-0.0.2.tar.gz

---
+++ packages/R4/R4-0.0.2.tar.gz/R4-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+setup(
+    name="R4",
+    version="0.0.2",
+    author_email="rizkimaulana348@gmail.com",
+    description="R4 is a python package for encrypt your important data",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rizki4106/R4.git",
+    download_url="https://github.com/rizki4106/R4/archive/master.zip",
+    keywords=["encrypt", "decrypt", "base64", "base64_encode", "base64_decode","md5"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=['R4'],
+    include_package_data=True,
+)
