9ab9eadffb41f4cce475b6f824c7f2107b297f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:16 +0000

    Add package pymlconf/2.1.0/pymlconf-2.1.0.tar.gz

---
+++ packages/pymlconf/pymlconf-2.1.0.tar.gz/pymlconf-2.1.0/pymlconf/__init__.py
@@ -0,0 +1,10 @@
+
+from .models import Mergable, MergableList, MergableDict, Root, \
+    ConfigurationNamespace, DeferredRoot
+
+from .errors import ConfigurationAlreadyInitializedError, \
+    ConfigurationNotInitializedError
+
+
+__version__ = '2.1.0'
+
