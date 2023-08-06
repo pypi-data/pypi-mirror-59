ce5dc998d0e1c94e24c520081b6739808744edd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package gorillaz/0.1/gorillaz-0.1.tar.gz

---
+++ packages/gorillaz/gorillaz-0.1.tar.gz/gorillaz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gorillaz',
+      version='0.1',
+      description='Test Gaussian distributions',
+      packages=['gorillaz', 'distributions'],
+      zip_safe=False)
