8f047a2a0261e306119913df7ab55a9b43a3b942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:08 +0000

    Add package randomapi/0.3.1/randomapi-0.3.1.tar.gz

---
+++ packages/randomapi/randomapi-0.3.1.tar.gz/randomapi-0.3.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'randomapi',
+    version = '0.3.1',
+    description = 'RANDOM.org JSON-RPC API implementation',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'Mitchell Cohen <mitch.cohen@me.com>',
+    author_email = 'mitch.cohen@me.com',
+    maintainer = 'Thomas Chick (twitter.com/Tantusar)',
+    py_modules = ['randomapi'],
+    url = 'https://github.com/Tantusar/randomapi',
+    license = 'MIT License',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = '>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, >=3.6',
+    download_url = 'https://github.com/Tantusar/randomapi/archive/v0.3.1.tar.gz'
+)
