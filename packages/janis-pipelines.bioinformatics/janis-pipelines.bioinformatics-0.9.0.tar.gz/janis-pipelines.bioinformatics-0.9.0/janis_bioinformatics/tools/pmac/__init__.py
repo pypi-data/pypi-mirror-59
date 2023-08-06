faa12c5ebb3fc25b5660bc9ff910cb4d12ab6bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:53 +0000

    Add package janis-pipelines.bioinformatics/0.9.0/janis-pipelines.bioinformatics-0.9.0.tar.gz

---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.9.0.tar.gz/janis-pipelines.bioinformatics-0.9.0/janis_bioinformatics/tools/pmac/__init__.py
@@ -0,0 +1,3 @@
+from .combinevariants.versions import CombineVariants_0_0_4, CombineVariantsLatest
+from .trimiupac.versions import TrimIUPAC_0_0_4, TrimIUPACLatest
+from .parsefastqc.v0_1_0 import ParseFastqcAdaptors
