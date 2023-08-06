1b003544ba4576057554d2f2bdafdc68ec0c7020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:31 +0000

    Add package pape/0.0.3/pape-0.0.3-py3-none-any.whl

---
+++ packages/pape/pape-0.0.3-py3-none-any.whl/pape/utilities.py
@@ -0,0 +1,31 @@
+# Pape (a Python package)
+# Copyright 2020 Carter Pape
+# 
+# See file LICENSE for licensing terms.
+
+from math import floor
+
+_typical_prefix_map = {
+    0   :   "th",
+    1   :   "st",
+    2   :   "nd",
+    3   :   "rd",
+    4   :   "th",
+    5   :   "th",
+    6   :   "th",
+    7   :   "th",
+    8   :   "th",
+    9   :   "th",
+}
+
+def ordinal(number: int) -> str:
+    if not isinstance(number, int):
+        raise TypeError(f"Expected {int} but got {type(number)}")
+    
+    elif _is_teenthish(abs(number)):
+        return f"{number}th"
+    else:
+        return f"{number}{_typical_prefix_map[abs(number) % 10]}"
+
+def _is_teenthish(number: int) -> bool:
+    return (floor(number / 10) % 10) == 1
