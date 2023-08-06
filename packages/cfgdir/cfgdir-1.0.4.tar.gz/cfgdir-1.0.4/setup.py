ab6246228ea34a39b2304cf22b3345cc7421f692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:03 +0000

    Add package cfgdir/1.0.4/cfgdir-1.0.4.tar.gz

---
+++ packages/cfgdir/cfgdir-1.0.4.tar.gz/cfgdir-1.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from glob import glob
+from os.path import basename, splitext
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('VERSION', 'r') as fh:
+    version = fh.readline().strip()
+
+with open('requirements.txt', 'r') as fh:
+    requires = [l.strip() for l in fh.readlines()]
+
+setup(
+    name="cfgdir",
+    version=version,
+    author="Matt Krueger",
+    author_email="mkrueger@rstms.net",
+    description="output envdir as JSON or YAML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/rstms/cfgdir",
+    keywords='envdir configuration config',
+    packages=find_packages(exclude=('tests', 'docs')),
+    data_files=[('.', ['VERSION', 'LICENSE', 'requirements.txt'])],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'cfgdir=cfgdir:cli',
+        ],
+    },
+)
