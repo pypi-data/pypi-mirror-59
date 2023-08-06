2ff31ecb9b089a96592ff764476c43c8845e72a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:43 +0000

    Add package nesteri/1.4.0/nesteri-1.4.0.tar.gz

---
+++ packages/nesteri/nesteri-1.4.0.tar.gz/nesteri-1.4.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+        name        = 'nesteri',
+
+        version     = '1.4.0',
+
+        py_modules  = ['nesteri'],
+
+        author      = 'anakorellana',
+
+        author_email= 'anakorellana@gmail.com',
+
+        url         = 'http://www.headfirstlabs.com',
+
+        description = 'A simple printer of nested lists',)
