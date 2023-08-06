651b9b8c4aaf3fe4ee7675e6ac640b4886b21c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:36 +0000

    Add package kalayil/0.1.2/kalayil-0.1.2-py3-none-any.whl

---
+++ packages/kalayil/kalayil-0.1.2-py3-none-any.whl/kalayil.py
@@ -0,0 +1,17 @@
+import modelling
+from modelling import *
+
+
+if __name__ == '__main__':
+	print('''
+		=================================
+		Module Written by Santo K. Thomas
+		=================================
+		email: santokalayil@gmail.com
+		phone: +91 8891960880
+		Address:
+			Kalayil House
+			Cheenkalthadom P.O,
+			Mannarakulanji, 
+			Pathanamthitta
+		''')
