bdef39ba590f48a31d283ff181042ec00e8193dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:48 +0000

    Add package fs-django-sberbank/0.2.35/fs-django-sberbank-0.2.35.tar.gz

---
+++ packages/fs-django-sberbank/fs-django-sberbank-0.2.35.tar.gz/fs-django-sberbank-0.2.35/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='fs-django-sberbank',
+    version='0.2.35',
+    description='Django app for Sberbank payments',
+    url='http://github.com/madprogrammer/django-sberbank',
+    author='Sergey Anufrienko',
+    author_email='sergey.anoufrienko@gmail.com',
+    license='BSD',
+    packages=setuptools.find_packages(),
+    platforms='any',
+    zip_safe=False,
+    install_requires=[
+        'django',
+        'jsonfield2',
+        'djangorestframework',
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ]
+)
