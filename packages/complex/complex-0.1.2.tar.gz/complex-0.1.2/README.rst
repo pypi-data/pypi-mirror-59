c1125dcd01634f5b7b9cb8690294bf91e185b1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package complex/0.1.2/complex-0.1.2.tar.gz

---
+++ packages/complex/complex-0.1.2.tar.gz/complex-0.1.2/README.rst
@@ -0,0 +1,8 @@
+Complex
+==========
+Compute complex numbers operations such as addition, substraction, multiplication , division and modulus.
+
+:Author: Deepak Yadav
+:Version: 2.0
+:Copyright: This document has been placed in the public domain.
+
---
+++ packages/complex/complex-0.1.2.tar.gz/complex-0.1.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+  
+# reading long description from file 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+  
+# specify requirements of your package here 
+##'math'
+
+#] 
+  
+# some more details 
+CLASSIFIERS = [ 
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Scientific/Engineering :: Mathematics', 
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3.0', 
+    ] 
+  
+# calling the setup function  
+setup(name='complex', 
+      version='0.1.2', 
+      description='Compute complex numbers operations such as addition, substraction, multiplication , division and modulus.', 
+      long_description=long_description, 
+      long_description_content_type='text/markdown',
+      url='https://github.com/deepak7376/complex', 
+      author='Deepak Yadav', 
+      author_email='dky.united@gmail.com', 
+      license='MIT', 
+      packages=find_packages(), 
+      classifiers=CLASSIFIERS, 
+      install_requires=None, 
+      keywords='Complex Operations',
+      include_package_data=True,
+      zip_safe=False,
+      python_requires='>=3'
+
+      ) 
