ce69c35b9db63a0a785e717f63a0fa356acf9d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:33 +0000

    Add package tyssue/0.6.7/tyssue-0.6.7.tar.gz

---
+++ packages/tyssue/tyssue-0.6.7.tar.gz/tyssue-0.6.7/tyssue/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM tyssue SETUP.PY
+#
+short_version = '0.6.7'
+full_version = '0.6.7'
+git_revision = 'fec8c8cc847f478c83656dffa850224e769b51db'
+release = True
+if release:
+    version = full_version
+else:
+    version = short_version
