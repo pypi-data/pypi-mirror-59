cec06d279e679d9e39b00717462fc9ec8e281e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:38 +0000

    Add package odoo8-addon-hr-timesheet-holiday/8.0.1.1.0.99.dev10/odoo8_addon_hr_timesheet_holiday-8.0.1.1.0.99.dev10-py2-none-any.whl

---
+++ packages/odoo8-addon-hr-timesheet-holiday/odoo8_addon_hr_timesheet_holiday-8.0.1.1.0.99.dev10-py2-none-any.whl/odoo_addons/hr_timesheet_holiday/models/hr_analytic_timesheet.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Sunflower IT <http://sunflowerweb.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import _, api, models, fields
+from openerp.exceptions import Warning as UserError
+
+
+class HrAnalyticTimesheet(models.Model):
+    """Link leave requests to analytic timesheet entries"""
+    _inherit = 'hr.analytic.timesheet'
+
+    leave_id = fields.Many2one('hr.holidays', 'Leave id', ondelete='restrict')
+
+    @api.multi
+    def unlink(self):
+        leaves = self.mapped('leave_id')
+        if leaves and set(leaves.mapped('state')) - set([
+                'draft', 'cancel', 'refuse',
+        ]):
+            raise UserError(_(
+                "You can't delete timesheet lines with active leaves",
+            ))
+        return super(HrAnalyticTimesheet, self).unlink()
