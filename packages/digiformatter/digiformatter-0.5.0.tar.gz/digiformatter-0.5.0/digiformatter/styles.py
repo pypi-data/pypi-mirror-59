1e90ea2bd07de5b8b14e49e74a5af97521c16be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package digiformatter/0.5.0/digiformatter-0.5.0.tar.gz

---
+++ packages/digiformatter/digiformatter-0.5.0.tar.gz/digiformatter-0.5.0/digiformatter/styles.py
@@ -0,0 +1,55 @@
+from time import strftime, localtime
+
+import colored
+
+__all__ = ["styles"]
+
+
+class Styles:
+    __slots__ = ["_styles", "default", "timestring", "timestampCodes"]
+
+    def __init__(self):
+        self._styles = {}
+        self.timestring = "%d %b %H:%M:%S"
+        self.timestampCodes = colored.fg("magenta")
+
+    def create(self, name, *, fg = None, bg = None, attr = None):
+        """Create a custom style"""
+        codes = ""
+        if fg is not None:
+            codes += colored.fg(fg)
+        if bg is not None:
+            codes += colored.bg(bg)
+        if attr is not None:
+            codes += colored.attr(attr)
+        self._styles[name] = codes
+
+    def format(self, message, *, style="default", showtime=False):
+        """Format a message in the requested style"""
+        if style not in self._styles:
+            raise ValueError(f"Unknown style: {style}")
+        formatted = ""
+        if showtime:
+            formatted += self._timestamp()
+        formatted = self._styles.get(style, "") + message + colored.attr("reset")
+        return formatted
+
+    def print(self, *args, **kwargs):
+        """Print a message in the requested style"""
+        print(self.format(*args, **kwargs))
+
+    def _timestamp(self):
+        """Color styling for terminal messages"""
+        t = localtime()
+        return self.timestampCodes + strftime(f"{self._timestring} | ", t) + colored.attr("reset")
+
+    def __getattr__(self, name):
+        if name not in self._styles:
+            raise AttributeError
+        return lambda message: self.print(message, style=name)
+
+    def __str__(self):
+        return "styles: " + (" ".join(self.format(level, style=level) for level in self._styles.keys()))
+
+
+styles = Styles()
