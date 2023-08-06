d4de4a1956f7b4c90206876eaaad93bd89a41ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:14 +0000

    Add package slims-lisp/0.0.6/slims_lisp-0.0.6-py2-none-any.whl

---
+++ packages/slims-lisp/slims_lisp-0.0.6-py2-none-any.whl/slims_lisp/__main__.py
@@ -0,0 +1,50 @@
+import click
+from slims_lisp.slims import Slims
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(help="Download a file from a slims experiment attachment step.")
+@click.option('--url',
+    help='Slims REST URL. ex: https://<your_slims_address>/rest/rest',
+    required=True)
+@click.option('--proj',
+    help='Project name (if any).',
+    required=False)
+@click.option('-e', '--exp',
+    help='Experiment name.',
+    required=True)
+@click.option('-s', '--step',
+    default='data_collection',
+    help='Experiment step name.',
+    required=True,
+    show_default=True)
+@click.option('-a', '--attm',
+    help='Attachment name.',
+    required=True)
+@click.option('-o', '--output',
+    help='Output file name. [default: same as --attm]',
+    required=False)
+@click.option('-u', '--username',
+    prompt="User",
+    help='User name (prompted).',
+    required=True)
+@click.option('-p', '--pwd',
+    prompt="Password",
+    hide_input=True,
+    help='Password (prompted).',
+    required=True)
+def get(url, username, pwd, proj, exp, step, attm, output):
+    slims = Slims(url = url,
+        username = username,
+        pwd = pwd)
+    slims.get_attachment(proj = proj,
+        exp = exp,
+        step = step,
+        attm = attm,
+        output = output
+    )
+
+if __name__ == '__main__':
+    cli()
