2b7d04a123fa83c0caa8c5738bbdc7aa0f725260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:20 +0000

    Add package algoz/0.0.1/algoz-0.0.1.tar.gz

---
+++ packages/algoz/algoz-0.0.1.tar.gz/algoz-0.0.1/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="algoz",
+    version="0.0.1",
+    description="Algorithmia and Threeza interoperability",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/algoz",
+    author="microprediction",
+    author_email="info@3za.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["algoz"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=['Algorithmia','threeza'],
+    entry_points={
+        "console_scripts": [
+            "algoz=algoz.__main__:main",
+        ]
+     },
+     )
