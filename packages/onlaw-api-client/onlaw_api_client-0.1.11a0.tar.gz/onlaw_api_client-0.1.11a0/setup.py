114c8d367b1f0941ed84f6e7a59144a855952c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:15 +0000

    Add package onlaw-api-client/0.1.11a0/onlaw_api_client-0.1.11a0.tar.gz

---
+++ packages/onlaw-api-client/onlaw_api_client-0.1.11a0.tar.gz/onlaw_api_client-0.1.11a0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="onlaw_api_client",
+    version="0.1.11a",
+    author="Onlaw",
+    author_email="jens@onlaw.dk",
+    description="client for api.onlaw.dk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Onlaw/onlaw_api_client",
+    packages=setuptools.find_packages(),
+    install_requires=["aiohttp",
+                      "async",
+                      "asyncio",
+                      "attrs",
+                      "auth0-python",
+                      "certifi",
+                      "chardet",
+                      "idna",
+                      "multidict",
+                      "requests",
+                      "urllib3",
+                      "yarl"],
+    tests_require=["autopep8",
+                   "flake8",
+                   "flake8-mypy",
+                   "mypy",
+                   "pip-tools",
+                   "pre-commit",
+                   "setuptools ",
+                   "twine",
+                   "wheel"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
