400c9e447ed1d4da2c6640490579f47507b22866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:49 +0000

    Add package wemake/0.1.0/wemake-0.1.0.tar.gz

---
+++ packages/wemake/wemake-0.1.0.tar.gz/wemake-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wemake']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'wemake',
+    'version': '0.1.0',
+    'description': 'You should probably use wemake-python-styleguide instead',
+    'long_description': '# wps\n\n[![wemake.services](https://img.shields.io/badge/%20-wemake.services-green.svg?label=%20&logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC%2FxhBQAAAAFzUkdCAK7OHOkAAAAbUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP%2F%2F%2F5TvxDIAAAAIdFJOUwAjRA8xXANAL%2Bv0SAAAADNJREFUGNNjYCAIOJjRBdBFWMkVQeGzcHAwksJnAPPZGOGAASzPzAEHEGVsLExQwE7YswCb7AFZSF3bbAAAAABJRU5ErkJggg%3D%3D)](https://wemake.services)\n[![wemake-python-styleguide](https://img.shields.io/badge/style-wemake-000000.svg)](https://github.com/wemake-services/wemake-python-styleguide)\n\nYou should probably use wemake-python-styleguide instead!\n\nRun `pip install wemake-python-styleguide` to install our linter.\n\n\n## Credits\n\nThis project was generated with [`wemake-python-package`](https://github.com/wemake-services/wemake-python-package).\n',
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/wemake-services/wemake-python-styleguide',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/wemake/wemake-0.1.0.tar.gz/wemake-0.1.0/wemake/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+print('You should probably use wemake-python-styleguide instead!')
