d6b8b4bf25557fb96d66735e6684fcd49d96a2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:57 +0000

    Add package keepfresh/0.0.3/keepfresh-0.0.3.tar.gz

---
+++ packages/keepfresh/keepfresh-0.0.3.tar.gz/keepfresh-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+SRC_DIR = os.path.join(os.path.dirname(__name__), 'src')
+
+def get_readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setup(
+    name="keepfresh",
+    version="0.0.3",
+    description="Filesystem event polling",
+    author="Tomas Sheers",
+    author_email="t.sheers@outlook.com",
+    download_url="https://github.com/reritom/KeepFresh/archive/v0.0.3.tar.gz",
+    url="https://github.com/reritom/KeepFresh",
+    keywords=["FILESYSTEM", "EVENT", "POLLING"],
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
+    package_dir={'': SRC_DIR},
+    packages=find_packages(SRC_DIR),
+    entry_points={
+        'console_scripts': [
+            'keepfresh = keepfresh.command_line:main [keepfresh]',
+        ]
+    },
+)
