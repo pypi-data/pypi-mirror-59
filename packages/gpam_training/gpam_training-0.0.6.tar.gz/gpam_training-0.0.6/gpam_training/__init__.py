8fd19a041bb32bd56d5e966f81542f849a182d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package gpam_training/0.0.6/gpam_training-0.0.6.tar.gz

---
+++ packages/gpam_training/gpam_training-0.0.6.tar.gz/gpam_training-0.0.6/gpam_training/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module to facilitate the integration of a sklearn training pipeline into a deploy and retraining system
+"""
+
+from .multilabel_training import MultilabelTraining
+
+__version__ = "0.0.6"
