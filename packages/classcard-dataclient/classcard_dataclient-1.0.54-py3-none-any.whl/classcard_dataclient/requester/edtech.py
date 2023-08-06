6c8251f6fa0f0a91ab466b10834b45f3a7145137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:18 +0000

    Add package classcard-dataclient/1.0.54/classcard_dataclient-1.0.54-py3-none-any.whl

---
+++ packages/classcard-dataclient/classcard_dataclient-1.0.54-py3-none-any.whl/classcard_dataclient/requester/edtech.py
@@ -0,0 +1,26 @@
+from ..requester import Requester
+from ..settings import EDTECH_SERVER_TOKEN, CLASS_CARD_SCHOOL
+
+
+class EDTechRequester(Requester):
+    def __init__(self, *args, **kwargs):
+        super(EDTechRequester, self).__init__(*args, **kwargs)
+        self.school_id = kwargs.get('school_id', CLASS_CARD_SCHOOL)
+        self.headers = {"X-Custom-Header-3School": self.school_id,
+                        "X-Custom-Header-3App": "classcard",
+                        "Authorization": EDTECH_SERVER_TOKEN}
+
+    def get_class_list(self, params={}):
+        route = "/api/v1/schools/{}/sections/simple/".format(self.school_id)
+        res = self._get_method(route, params=params)
+        return res
+
+    def get_teacher_list(self, params={}):
+        route = "/api/v1/schools/{}/teachers/".format(self.school_id)
+        res = self._get_method(route, params=params)
+        return res
+
+    def get_student_list(self, params={}):
+        route = "/api/v1/schools/{}/students/".format(self.school_id)
+        res = self._get_method(route, params=params)
+        return res
