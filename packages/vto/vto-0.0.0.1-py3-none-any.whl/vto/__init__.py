d7f9a67dcfa50ac01ec3b6cbd8c44ee119c70f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:58 +0000

    Add package vto/0.0.0.1/vto-0.0.0.1-py3-none-any.whl

---
+++ packages/vto/vto-0.0.0.1-py3-none-any.whl/vto/__init__.py
@@ -0,0 +1,3 @@
+VERSION = '0.0.0.1'
+
+from ._samples import *
---
+++ packages/vto/vto-0.0.0.1-py3-none-any.whl/vto/_samples.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__description__ = '''
+'''
+
+__all__ = [
+    'R', 'G', 'Y', 'B', 'F', 'C', 'W',
+]
+
+"""color"""
+
+R = '\x1b[0;91m{}\x1b[0m'  # red
+G = '\x1b[0;92m{}\x1b[0m'  # green
+Y = '\x1b[0;93m{}\x1b[0m'  # yellow
+B = '\x1b[0;94m{}\x1b[0m'  # blue
+F = '\x1b[0;95m{}\x1b[0m'  # fuchsia
+C = '\x1b[0;96m{}\x1b[0m'  # cyan
+W = '\x1b[0;97m{}\x1b[0m'  # white
