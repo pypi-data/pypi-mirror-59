d6df77c18cafae9d1bedff614cadbf4993870d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:02 +0000

    Add package Autogovernance/0.4.1/Autogovernance-0.4.1-py3-none-any.whl

---
+++ packages/Autogovernance/Autogovernance-0.4.1-py3-none-any.whl/autogovernance/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 12:32:13 2020
+
+@author: LP885RH
+"""
+from autogovernance import element
+from autogovernance import tables
+from autogovernance import browser
