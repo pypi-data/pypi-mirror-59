b0bcb253a22367f3217ebc5acc89fe160695752c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:08 +0000

    Add package gensqlalorm/0.0.2/gensqlalorm-0.0.2.tar.gz

---
+++ packages/gensqlalorm/gensqlalorm-0.0.2.tar.gz/gensqlalorm-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='gensqlalorm',
+      version='0.0.2',
+      description='a generate sqlarchemy models utils',
+      url='https://github.com/SUT-GC/gen-sqla-orm',
+      author='gouchao',
+      author_email='sutgouc@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      packages=["gensqlalorm"],
+      install_requires=[
+          'PyMySQL>=0.9.3',
+          'PyYAML>=5.2',
+      ],
+      zip_safe=False)
