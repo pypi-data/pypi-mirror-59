3189faf4e04e725e43437e529d7f8e787787422c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package atomicgraphs/0.1.2/atomicgraphs-0.1.2.tar.gz

---
+++ packages/atomicgraphs/atomicgraphs-0.1.2.tar.gz/atomicgraphs-0.1.2/setup.py
@@ -0,0 +1,21 @@
+"""
+AtomicGraphs
+------------
+This is a python library to split an RDF Graph into atomic graphs.
+
+With this library using atomic graphs we want to make RDF Graphs containing
+blanknodes comparable.
+This package implements a colouring algorithm.
+"""
+from setuptools import setup
+
+setup(
+    name='atomicgraphs',
+    version='0.1.2',
+    description=("This is a library to split an RDF Graph into atomic graphs."),
+    long_description=__doc__,
+    author='Simaris',
+    # author_email='foomail@foo.com',
+    packages=['atomicgraphs'],
+    install_requires=['rdflib', 'sortedcontainers']
+)
